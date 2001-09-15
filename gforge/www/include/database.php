71581fdfa53aaeb265d2772a38e994f7eedd8913
lo-lan-do <>
2001-09-15 14:34:10 +0000

    Added db_drop_sequence_if_exists().

--- gforge/www/include/database.php
+++ gforge/www/include/database.php
@@ -244,4 +244,16 @@ function db_drop_table_if_exists ($tn) {
     echo db_error();
   }
 }
+
+function db_drop_sequence_if_exists ($tn) {
+  $sql = "SELECT COUNT(*) FROM pg_class WHERE relname='$tn';";
+  $rel = db_query($sql);
+  echo db_error();
+  $count = db_result($rel,0,0);
+  if ($count != 0) {
+    $sql = "DROP SEQUENCE $tn;";
+    $rel = db_query ($sql);
+    echo db_error();
+  }
+}
 ?>
