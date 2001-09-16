77cf854ad4cc5fe24fe4793477a397f062bb254a
Christian Bayle <cbayle@gmail.com>
2001-09-16 20:05:05 +0000

    This program is suid to root, please check Roland

--- gforge/utils/fileforge.c
+++ gforge/utils/fileforge.c
@@ -45,8 +45,10 @@ int legal_string (char* test_string) {
 int main (int argc, char** argv) {
 
   /* edit me */
-  char* src_dir   = "/usr/lib/sourceforge/ftp/incoming/";
-  char* dest_dir  = "/usr/lib/sourceforge/download/";
+  char* src_dir_root   = "/var/lib/sourceforge/chroot/home/users/";
+  char* src_dir_incoming = "/incoming/";
+  char* dest_dir_root  = "/var/lib/sourceforge/download/";
+  char* dest_dir_download = "/";
 
   /* don't edit me (unless mv isn't in /bin) */
   char* move_path = "/bin/mv";
@@ -54,46 +56,53 @@ int main (int argc, char** argv) {
   char* dest_file;
   char* src_file;
 
-  if (argc != 3) {
-    fprintf(stderr, "FAILURE: usage: fileforge file group");
+  if (argc != 4) {
+    fprintf(stderr, "FAILURE: usage: fileforge file user group\n");
     exit(1);
   } /* if */
   else {
     /* set source */
-    src_file = (char *) malloc(strlen(src_dir) + strlen(argv[1]) + 1);
-    strcpy(src_file, src_dir);
+    src_file = (char *) malloc(strlen(src_dir_root) + strlen(src_dir_incoming) + strlen(argv[1]) + 1 );
+    strcpy(src_file, src_dir_root);
+    strcat(src_file, argv[2]);
+    strcat(src_file, src_dir_incoming);
     strcat(src_file, argv[1]);
 
     /* set destination */
-    dest_file = (char *) malloc(strlen(dest_dir) + strlen(argv[2]) + 1);
-    strcpy(dest_file, dest_dir);
-    strcat(dest_file, argv[2]);
+    dest_file = (char *) malloc(strlen(dest_dir_root) + strlen(dest_dir_download) + strlen(argv[2]) + 1);
+    strcpy(dest_file, dest_dir_root);
+    strcat(dest_file, argv[3]);
+    strcat(dest_file, dest_dir_download);
 
     /* test for legal characters: [a-zA-Z0-9_-.]  */
     /* test for illegal combinations of legal characters: ".." */
     if (!legal_string(src_file)) {
+      fprintf(stderr,"SRC=%s\nDEST=%s\n",src_file,dest_file);
       fprintf(stderr, "FAILURE: illegal characters in source file\n");
       exit(1);
     } /* if */
 
     if (!legal_string(dest_file)) {
+      fprintf(stderr,"SRC=%s\nDEST=%s\n",src_file,dest_file);
       fprintf(stderr, "FAILURE: illegal characters in destination file\n");
       exit(1);
     } /* if */
 
     if ((mkdir(dest_file, 0775) != 0) && errno != EEXIST) {
+      fprintf(stderr,"SRC=%s\nDEST=%s\n",src_file,dest_file);
       fprintf(stderr, "FAILURE: destination directory could not be created\n");
       exit(1);
     } /* if */
 
     /* exec it */
     if (execl(move_path, move_file, src_file, dest_file, (char *)0) == -1) {
+      fprintf(stderr,"SRC=%s\nDEST=%s\n",src_file,dest_file);
       perror("FAILURE");
       exit(1);
     } /* if */
   } /* else */
 
-  printf("OK\n");
+  /* printf("OK\n"); */
   free(dest_file);
   free(src_file);
   exit(0);
