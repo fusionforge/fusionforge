284747e97339a7b6d69c4092a2596aae8145611c
Christian Bayle <cbayle@gmail.com>
2001-10-23 17:32:49 +0000

    Now using update-inetd

--- gforge/deb-specific/install-cvs.sh
+++ gforge/deb-specific/install-cvs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-inetdname=inetd
 if [ $# != 1 ]
 then
 	echo "Usage: $0 {configure|purge}"
@@ -11,22 +10,13 @@ else
 		# Setup our own CVS
 		echo "Modifying inetd for cvs server"
 		echo "CVS usual config is changed for sourceforge one"
-		# To easily support xinetd but don't now if it's like inetd
-		if ! grep -q "#Sourceforge comment#" /etc/${inetdname}.conf ; then
-		    	perl -pi -e "s/^cvspserver/#Sourceforge comment#cvspserver/" /etc/${inetdname}.conf
-	    		echo "cvspserver	stream	tcp	nowait.400	root	/usr/sbin/tcpd	/usr/lib/sourceforge/bin/cvs-pserver" >> /etc/${inetdname}.conf
-			#update-inetd --add  "cvspserver stream tcp nowait.400 root /usr/sbin/tcpd /usr/lib/sourceforge/bin/cvs-pserver"
-	    		/etc/init.d/${inetdname} restart
-		fi
+		update-inetd --add  "cvspserver	stream	tcp	nowait.400	root	/usr/sbin/tcpd	/usr/lib/sourceforge/bin/cvs-pserver"
 	else
 		if [ "$1" = "purge" ]
 		then
 			echo "Purging inetd for cvs server"
-		    	perl -pi -e "s/^#Sourceforge comment#cvspserver/cvspserver/" /etc/${inetdname}.conf
-		    	perl -pi -e "s:.*sourceforge/bin/cvs-pserver\n::" /etc/${inetdname}.conf
-			# A little bit too violent
-			# This remove all line containing cvspserver
-			#update-inetd --remove cvspserver 
+			echo "You should dpkg-reconfigure cvs to use std install"
+			update-inetd --remove cvspserver 
 		else
 			echo "Usage: $0 {configure|purge}"
 			exit 1
