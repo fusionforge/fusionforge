1895bcbb1c96abfb43657331a52ec18d881e2c6b
lo-lan-do <>
2001-10-25 08:47:26 +0000

    Experimental support for backing up the inetd conf and restoring it.

--- gforge/deb-specific/install-cvs.sh
+++ gforge/deb-specific/install-cvs.sh
@@ -10,13 +10,15 @@ else
 		# Setup our own CVS
 		echo "Modifying inetd for cvs server"
 		echo "CVS usual config is changed for sourceforge one"
+		update-inetd --comment-chars "#SF_WAS_HERE#" --disable cvspserver
 		update-inetd --add  "cvspserver	stream	tcp	nowait.400	root	/usr/sbin/tcpd	/usr/lib/sourceforge/bin/cvs-pserver"
 	else
 		if [ "$1" = "purge" ]
 		then
 			echo "Purging inetd for cvs server"
-			echo "You should dpkg-reconfigure cvs to use std install"
-			update-inetd --remove cvspserver 
+			# echo "You should dpkg-reconfigure cvs to use std install"
+			update-inetd --remove  "cvspserver	stream	tcp	nowait.400	root	/usr/sbin/tcpd	/usr/lib/sourceforge/bin/cvs-pserver"
+			update-inetd --comment-chars "#SF_WAS_HERE#" --enable cvspserver
 		else
 			echo "Usage: $0 {configure|purge}"
 			exit 1
