b33e86d179f7eed38d4c0e4840fb8b3d9a937557
Christian Bayle <cbayle@gmail.com>
2001-11-02 20:28:19 +0000

    Take for the moment 2.5 files for 2.6, more check needed

--- gforge/utils/include.pl
+++ gforge/utils/include.pl
@@ -1,5 +1,3 @@
-#!/usr/bin/perl
-#
 # $Id$
 #
 # include.pl - Include file for all the perl scripts that contains reusable functions
@@ -8,15 +6,15 @@
 ##############################
 # Global Variables
 ##############################
-$db_include	=	"/etc/local.inc";	# Local Include file for database username and password
+$db_include	=	"/etc/sourceforge/local.pl";	# Local Include file for database username and password
 $tar_dir	=	"/tmp";			# Place to put deleted user's accounts
 $uid_add	=	"20000";		# How much to add to the database uid to get the unix uid
-$gid_add	=	"1000";			# How much to add to the database gid to get the unix uid
-$anoncvs_add	=	"2000";			# How much to add to the gid to get the unix uid of anoncvs user
-$homedir_prefix =	"/home/users/";		# What prefix to add to the user's homedir
-$grpdir_prefix  =	"/home/groups/";	# What prefix to add to the user's homedir
-$file_dir	=	"/home/dummy/dumps/";	# Where should we stick files we're working with
-$dummy_uid      =       "103";                  # UserID of the dummy user that will own group's files
+$gid_add	=	"10000";			# How much to add to the database gid to get the unix uid
+$homedir_prefix =	"/var/lib/sourceforge/chroot/home/users/";		# What prefix to add to the user's homedir
+$grpdir_prefix  =	"/var/lib/sourceforge/chroot/home/groups/";	# What prefix to add to the user's homedir
+$file_dir	=	"/var/lib/sourceforge/";	# Where should we stick files we're working with
+$cvs_root       =       "/var/lib/sourceforge/chroot/cvsroot/";    # Where should we stick files we're working with
+$dummy_uid      =       "9999";                  # UserID of the dummy user that will own group's files
 $date           =       int(time()/3600/24);    # Get the number of days since 1/1/1970 for /etc/shadow
 $ldap_prefix	=	"/usr/local/ldap/bin/";	# Where OpenLDAP tools installed
 
@@ -24,16 +22,16 @@ $ldap_prefix	=	"/usr/local/ldap/bin/";	# Where OpenLDAP tools installed
 # Configuration parsing Functions
 ##################################
 sub parse_local_inc {
-	my ($foo, $bar);
-	
-	# open up database include file and get the database variables
-	open(FILE, $db_include) || die "Can't open $db_include: $!\n";
-	while (<FILE>) {
-		next if ( /^\s*\/\// );
-		($foo, $bar) = split /=/;
-		if ($foo) { eval $_ };
-	}
-	close(FILE);
+    require $db_include;
+#  	my ($foo, $bar);
+#  	# open up database include file and get the database variables
+#  	open(FILE, $db_include) || die "Can't open $db_include: $!\n";
+#  	while (<FILE>) {
+#  		next if ( /^\s*\/\// );
+#  		($foo, $bar) = split /=/;
+#  		if ($foo) { eval $_ };
+#  	}
+#  	close(FILE);
 }
 
 ##############################
@@ -43,10 +41,24 @@ sub db_connect {
 	&parse_local_inc;
 	
 	# connect to the database
-	$dbh ||= DBI->connect("DBI:Pg:dbname=$sys_dbname;host=$sys_dbhost;user=$sys_dbuser;password=$sys_dbpasswd");
-	#$dbh ||= DBI->connect("DBI:mysql:$sys_dbname:$sys_dbhost", "$sys_dbuser", "$sys_dbpasswd");
+	$dbh ||= DBI->connect("DBI:Pg:dbname=$sys_dbname;host=$sys_dbhost", "$sys_dbuser", "$sys_dbpasswd");
 
 	die "Cannot connect to database: $!" if ( ! $dbh );
+}
+
+sub db_drop_table_if_exists {
+    my ($sql, $res, $n, $tn) ;
+    $tn = shift ;
+    $sql = "SELECT COUNT(*) FROM pg_class WHERE relname='$tn'";
+    $res = $dbh->prepare($sql);
+    $res->execute();
+    ($n) = $res->fetchrow() ;
+    $res->finish () ;
+    if ($n != 0) {
+	$sql = "DROP TABLE $tn";
+	$res = $dbh->prepare($sql);
+	$res->finish () ;
+    }
 }
 
 ##############################
