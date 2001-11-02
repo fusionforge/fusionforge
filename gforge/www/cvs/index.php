c5b3c907ee9ebec697f5e4d2960407514ff6dc0e
Christian Bayle <cbayle@gmail.com>
2001-11-02 13:33:42 +0000

    Solved conflicts arising from 2.6 import/merge. [CB]

--- gforge/www/cvs/index.php
+++ gforge/www/cvs/index.php
@@ -1,15 +1,22 @@
 <?php
-//
-// SourceForge: Breaking Down the Barriers to Open Source Development
-// Copyright 1999-2000 (c) The SourceForge Crew
-// http://sourceforge.net
-//
-// $Id: index.php,v 1.32 2000/10/09 03:18:27 tperdue Exp $
+/**
+  *
+  * SourceForge CVS Frontend
+  *
+  * SourceForge: Breaking Down the Barriers to Open Source Development
+  * Copyright 1999-2001 (c) VA Linux Systems
+  * http://sourceforge.net
+  *
+  * @version   $Id: index.php,v 1.40 2001/07/12 21:48:12 dbellizzi Exp $
+  *
+  */
 
-require ('pre.php');    
+
+require_once('pre.php');    
+require_once('common/include/account.php');
 
 //only projects can use the bug tracker, and only if they have it turned on
-$project=project_get_object($group_id);
+$project =& group_get_object($group_id);
 
 if (!$project->isProject()) {
  	exit_error('Error','Only Projects Can Use CVS');
@@ -18,9 +25,9 @@ if (!$project->usesCVS()) {
 	exit_error('Error','This Project Has Turned Off CVS');
 }
 
-site_project_header(array('title'=>'CVS Repository','group'=>$group_id,'toptab'=>'cvs'));
+site_project_header(array('title'=>'CVS Repository','group'=>$group_id,'toptab'=>'cvs','pagename'=>'cvs','sectionvals'=>array($project->getPublicName())));
 
-$res_grp = db_query("SELECT * FROM groups WHERE group_id=$group_id");
+$res_grp = db_query("SELECT * FROM groups WHERE group_id='$group_id'");
 
 $row_grp = db_fetch_array($res_grp);
 
@@ -32,14 +39,15 @@ print '<TABLE width="100%"><TR valign="top"><TD width="65%">'."\n";
 
 if ($row_grp['is_public']) {
 	print '<P><B>Anonymous CVS Access</B>
-<P>This project\'s SourceForge CVS repository can be checked out through anonymous
+<P>This project\'s ' . $Language->getText('general', 'sitename') . ' CVS repository can be checked out through anonymous
 (pserver) CVS with the following instruction set. The module you wish
 to check out must be specified as the <I>modulename</I>. When prompted
 for a password for <I>anonymous</I>, simply press the Enter key.
 
-<P><FONT size="-1" face="courier">cvs -d:pserver:anonymous@cvs.'.$row_grp['http_domain'].':/cvsroot/'.$row_grp['unix_group_name'].' login
-<BR>&nbsp;<BR>cvs -z3 -d:pserver:anonymous@cvs.'.$row_grp['http_domain'].':/cvsroot/'.$row_grp['unix_group_name'].' co <I>modulename</I>
-</FONT>
+<P><tt>cvs -d:pserver:anonymous@cvs.'.$row_grp['http_domain'].':/cvsroot/'.$row_grp['unix_group_name'].' login
+<BR>&nbsp;<BR>
+cvs -z3 -d:pserver:anonymous@cvs.'.$row_grp['http_domain'].':/cvsroot/'.$row_grp['unix_group_name'].' co <I>modulename</I>
+</tt>
 
 <P>Updates from within the module\'s directory do not need the -d parameter.';
 }
@@ -52,9 +60,9 @@ be installed on your client machine. Substitute <I>modulename</I> and
 <I>developername</I> with the proper values. Enter your site password when
 prompted.
 
-<P><FONT size="-1" face="courier">export CVS_RSH=ssh
+<P><tt>export CVS_RSH=ssh
 <BR>&nbsp;<BR>cvs -z3 -d:ext:<I>developername</I>@cvs.'.$row_grp['http_domain'].':/cvsroot/'.$row_grp['unix_group_name'].' co <I>modulename</I>
-</FONT>';
+</tt>';
 
 // ################## summary info
 
@@ -65,7 +73,7 @@ print $HTML->box1_top("Repository History");
 
 $res_cvshist = db_query("SELECT * FROM group_cvs_history WHERE group_id='$group_id'");
 if (db_numrows($res_cvshist) < 1) {
-	print '<P>This project has no CVS history.';
+	//print '<P>This project has no CVS history.';
 } else {
 
 print '<P><B>Developer (30 day/Commits) (30 day/Adds)</B><BR>&nbsp;';
@@ -86,8 +94,8 @@ if ($row_grp['is_public']) {
 of this project\'s code. You may also view the complete histories of any
 file in the repository.
 <UL>
-<LI><A href="http://'.$sys_cvs_host.'/cgi-bin/cvsweb.cgi?cvsroot='
-.$row_grp['unix_group_name'].'"><B>Browse CVS Repository</B>';
+<LI><A href="'.account_group_cvsweb_url($row_grp['unix_group_name']).'">
+<B>Browse CVS Repository</B></A>';
 }
 
 print $HTML->box1_bottom();
