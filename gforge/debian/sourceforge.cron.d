177a77a21342104ed4439a0676fde85fd57ca195
Christian Bayle <cbayle@gmail.com>
2002-02-17 21:18:25 +0000

    Added missing path due to new common dir

--- gforge/debian/sourceforge.cron.d
+++ gforge/debian/sourceforge.cron.d
@@ -6,35 +6,35 @@
 25 * * * * root [ -d /var/cache/sourceforge ] && find /var/cache/sourceforge/ -type f -and -cmin +60 -exec /bin/rm -f "{}" \; &> /dev/null
 
 # Grab projects from trove map and put into foundry_projects table
-15 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/populate_foundries.php ] && /usr/lib/sourceforge/bin/populate_foundries.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+15 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/populate_foundries.php ] && /usr/lib/sourceforge/bin/populate_foundries.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Recalculate user popularity metric
-25 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/calculate_user_metric.php ] && /usr/lib/sourceforge/bin/calculate_user_metric.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+25 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/calculate_user_metric.php ] && /usr/lib/sourceforge/bin/calculate_user_metric.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Daily recalculate of the sums under the trove map
-30 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/db_trove_maint.php ] && /usr/lib/sourceforge/bin/db_trove_maint.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+30 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/db_trove_maint.php ] && /usr/lib/sourceforge/bin/db_trove_maint.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Daily deletion of sessions, closing jobs, etc
-35 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/project_cleanup.php ] && /usr/lib/sourceforge/bin/project_cleanup.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+35 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/project_cleanup.php ] && /usr/lib/sourceforge/bin/project_cleanup.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Daily crunching of survey data and other associated ratings
-40 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/rating_stats.php ] && /usr/lib/sourceforge/bin/rating_stats.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+40 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/rating_stats.php ] && /usr/lib/sourceforge/bin/rating_stats.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Daily project metrics
-45 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/project_metric.php ] && /usr/lib/sourceforge/bin/project_metric.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+45 1 * * * sourceforge [ -x /usr/lib/sourceforge/bin/project_metric.php ] && /usr/lib/sourceforge/bin/project_metric.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Weekly project metrics
-50 1 * * Mon sourceforge [ -x /usr/lib/sourceforge/bin/project_weekly_metric.php ] && /usr/lib/sourceforge/bin/project_weekly_metric.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+50 1 * * Mon sourceforge [ -x /usr/lib/sourceforge/bin/project_weekly_metric.php ] && /usr/lib/sourceforge/bin/project_weekly_metric.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Database vacuuming
 # Theoretically unneeded: postgres does it at 4:00 by default
-# 0 2 * * * sourceforge [ -x /usr/lib/sourceforge/bin/vacuum.php ] && /usr/lib/sourceforge/bin/vacuum.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+# 0 2 * * * sourceforge [ -x /usr/lib/sourceforge/bin/vacuum.php ] && /usr/lib/sourceforge/bin/vacuum.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Daily rotation of the activity_log
-0 0 * * * sourceforge [ -x /usr/lib/sourceforge/bin/rotate_activity.php ] && /usr/lib/sourceforge/bin/rotate_activity.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+0 0 * * * sourceforge [ -x /usr/lib/sourceforge/bin/rotate_activity.php ] && /usr/lib/sourceforge/bin/rotate_activity.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # Daily aggregating of the numbers
-15 0 * * * sourceforge [ -x /usr/lib/sourceforge/bin/site_stats.php ] && /usr/lib/sourceforge/bin/site_stats.php -d include_path=/usr/lib/sourceforge/www/include &> /dev/null
+15 0 * * * sourceforge [ -x /usr/lib/sourceforge/bin/site_stats.php ] && /usr/lib/sourceforge/bin/site_stats.php -d include_path=/usr/lib/sourceforge/:/usr/lib/sourceforge/www/include &> /dev/null
 
 # DNS Update
 0 * * * * sourceforge [ -x /usr/lib/sourceforge/bin/dns_conf.pl ] && /usr/lib/sourceforge/bin/dns_conf.pl && /usr/sbin/invoke-rc.d bind9 reload &>/dev/null
