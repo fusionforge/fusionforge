a9fbf9e0160d24176b56a1de07d17867fe3103c6
Christian Bayle <cbayle@gmail.com>
2002-02-22 14:22:17 +0000

    Enhanced version

--- tools/desc.sh
+++ tools/desc.sh
@@ -1,33 +1,33 @@
 # !/bin/sh
-# -a = --data-only
-# -u = use passwd auth
-# -f = -file=
-# -d = --inserts = not COPY
-[ ! -d desc ] && mkdir desc
-passwd=`grep sys_dbpasswd /etc/sourceforge/database.inc | cut -d\" -f2`
-export passwd
 
-echo "Creating tables List"
-psql -U sourceforge -h `hostname -s` sourceforge 2>&1 > desc/tables <<-FIN
-$passwd
-\dt
+createlist()
+{
+echo "Creating $1 List"
+psql -U sourceforge -h `hostname -s` sourceforge 2>&1 > $DESCDIR/$1 <<-FIN
+$DBPASS
+\\$2
 FIN
+cat $DESCDIR/$1 | grep "^ [a-z]" | sed "s/^ \([a-z].[^|]*\)|.*/\1/" | sort -u > $DESCDIR/$1.tmp
+mv $DESCDIR/$1.tmp $DESCDIR/$1
+[ ! -d $DESCDIR/$1.dump ] && mkdir $DESCDIR/$1.dump
+cat  $DESCDIR/$1 | while read tablename
+do
+	pg_dump --no-owner --no-reconnect -h `hostname -s` sourceforge -u -c -d -t $tablename -f $DESCDIR/$1.dump/$tablename <<-FIN
+sourceforge
+$DBPASS
+FIN
+done
+}
 
-echo "Creating indices List"
-psql -U sourceforge -h `hostname -s` sourceforge 2>&1 > desc/indices <<-FIN
-$passwd
-\di
-FIN
+[ "x$DESCDIR" == "x" ] && DESCDIR=desc
+echo "Desc in $DESCDIR"
+[ "x$DBPASS" == "x" ] && DBPASS=`grep sys_dbpasswd /etc/sourceforge/database.inc| cut -d\" -f2 ` && [ "x$DBPASS" == "x" ] && echo "Can't get DB Passwd" && exit
+export DBPASS
 
-echo "Creating sequences List"
-psql -U sourceforge -h `hostname -s` sourceforge 2>&1 > desc/sequences <<-FIN
-$passwd
-\ds
-FIN
+[ ! -d $DESCDIR ] && mkdir $DESCDIR
 
-echo "Creating views List"
-psql -U sourceforge -h `hostname -s` sourceforge 2>&1 > desc/views <<-FIN
-$passwd
-\dv
-FIN
+createlist tables dt
+createlist indices di
+createlist sequences ds
+createlist views dv
 
