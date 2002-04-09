e027bf9122b9c41b2c1da2dcb0a76a45ca86da04
(no author) <>
2002-04-09 17:41:12 +0000

    This commit was manufactured by cvs2svn to create tag 'debian_version_2_5-29'.

---
+++ gforge/cronjobs/stats/db_stats_agg_drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE frs_dlstats_filetotal_agg;
+DROP TABLE frs_dlstats_grouptotal_agg;
+DROP TABLE frs_dlstats_group_agg;
+DROP TABLE stats_project_months;
+DROP TABLE stats_project_all;
+DROP TABLE stats_project_developers_last30;
+DROP TABLE stats_project_last_30;
+DROP TABLE stats_site_pages_by_month;
+DROP TABLE stats_site_last_30;
+DROP TABLE stats_site_months;
+DROP TABLE stats_site_all;
