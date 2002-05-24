bb432b50d8e2fbe6eb22a4c573e5fc703f32dc12
lo-lan-do <>
2002-05-24 19:17:48 +0000

    Splitting the package, take 2:
    Another big commit.  This one involves lots of new files, a few
    deleted files, and introduces the DSF-Helper magic.  For more about
    that, please read debian/changelog and debian/README.Maintainer.
    
    The current state of the package is: "seems to be working".  Don't try
    multi-host installations yet, that's not done.  Don't try partial
    installations either (like, only installing parts of the package).  I
    think the packages should be installable through apt, but I only
    tested them through "dpkg -i sourceforge*deb" here.  Also, when you
    upgrade, please upgrade all the packages at the same time, as there
    will probably be glitches if you don't.

---
+++ gforge/deb-specific/dsf-helper.pl
@@ -0,0 +1,111 @@
+#! /usr/bin/perl -w
+#
+# DSF-Helper, a Debhelper-inspired helper for Debian Sourceforge
+#
+# Roland Mas <lolando@debian.org>
+# This code is copyright Roland Mas 2002
+# You are welcome to use, modify and redistribute this file under the terms of
+# the GNU General Public License version 2.
+
+use strict ;
+use vars qw! @known_files @file_list @package_list %chunks ! ;
+use subs qw! &pkgfile &get_chunk ! ;
+use diagnostics ;
+
+###
+# STATIC DATA
+
+@known_files = qw/ config templates preinst postinst prerm postrm / ;
+
+###
+# HELPER FUNCTIONS
+
+sub pkgfile {
+    my $package=shift;
+    my $filename=shift;
+
+    if (-f "debian/$package.$filename.dsfh-in") {
+	return "debian/$package.$filename.dsfh-in";
+    }
+    else {
+	return "";
+    }
+} ;
+
+sub get_chunk {
+    my $varname = shift ;
+    my $function = shift ;
+
+    unless (defined $chunks{$varname}) {
+	# First time this variable is encountered, creating slot
+	# print "Creating data structure for var $varname.\n" ;
+	$chunks{$varname} = {} ;
+    } ;
+    
+    unless (defined $chunks{$varname}{$function}) {
+	# First time this instance is accessed, loading data
+	# print "Loading data for var $varname and function $function.\n" ;
+	$chunks{$varname}{$function} = '' ;
+	my $fname = "debian/dsf-helper/$varname.$function" ; 
+	unless (-f $fname) {
+	    print "File $fname file not found, aborting.\n" ;
+	    exit 1 ;
+	}
+	
+	open F, $fname ;
+	while (<F>) {
+	    $chunks{$varname}{$function} .= $_ ;
+	}
+	close F ;
+    }
+
+    return $chunks{$varname}{$function} ;
+}
+
+###
+# DO THE JOB
+@package_list = split /\n/, qx! dh_listpackages ! ;
+
+if ($ARGV[0] && $ARGV[0] eq "--clean") {
+ EXTLOOP: for my $ext (@known_files) {
+   PKGLOOP: for my $pkg (@package_list) {
+       my $srcfile = &pkgfile ($pkg, $ext) ;
+       next PKGLOOP unless $srcfile ;
+       my $destfile = $srcfile ;
+       $destfile =~ s/\.dsfh-in$// ;
+       do {
+	   # print "Removing $destfile\n" ;
+	   unlink $destfile ;
+       } if -f $destfile ;
+   }
+ }
+   exit 0 ;
+}
+
+ EXTLOOP: for my $ext (@known_files) {
+     # print "Extension: $ext\n" ;
+   PKGLOOP: for my $pkg (@package_list) {
+       # print "  Package: $pkg\n" ;
+       my $srcfile = &pkgfile ($pkg, $ext) ;
+       next PKGLOOP unless $srcfile ;
+       my $destfile = $srcfile ;
+       $destfile =~ s/\.dsfh-in$// ;
+       print "$srcfile -> $destfile\n" ;
+
+       open S, "< $srcfile" ;
+       open D, "> $destfile" ;
+       
+       while (my $l = <S>) {
+	   chomp $l ;
+	   while ($l =~ m!\#DSFHELPER:([-_a-zA-Z0-9/]+)\#!) {
+	       my $chunkname = $1 ;
+	       my $chunk = get_chunk ($chunkname, $ext) ;
+	       $l =~ s!\#DSFHELPER:$chunkname\#!$chunk! ;
+	   }
+	   print D "$l\n" ;
+       }
+
+       close D ;
+       close S ;
+   }
+ }
