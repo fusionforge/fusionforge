0950cf8684ca0d2d64ca95ed1744b6accf6d0833
Christian Bayle <cbayle@gmail.com>
2002-10-20 16:04:31 +0000

    more and improved tools

--- tools/delete_deleted_group.sql
+++ tools/delete_deleted_group.sql
@@ -1,4 +1,4 @@
-detete from forum where group_forum_id in 
+delete from forum where group_forum_id in 
 (select group_forum_id  from forum_group_list where group_id in
 	(select group_id from groups where status='D')
 );
@@ -6,5 +6,13 @@ detete from forum where group_forum_id in
 delete from forum_group_list where group_id in ( select group_id from groups where status='D');
 
 delete from user_group where group_id in (select group_id from groups where status = 'D');
+
+delete from artifact_message where artifact_id in (select artifact_id from artifact where group_artifact_id in (select group_artifact_id from artifact_group_list where group_id in (select group_id from groups where status = 'D')));
+delete from artifact where group_artifact_id in (select group_artifact_id from artifact_group_list where group_id in (select group_id from groups where status = 'D'));
+delete from artifact_perm where group_artifact_id in (select group_artifact_id from artifact_group_list where group_id in (select group_id from groups where status = 'D'));
+delete from artifact_group where group_artifact_id in (select group_artifact_id from artifact_group_list where group_id in (select group_id from groups where status = 'D'));
+delete from artifact_category where group_artifact_id in (select group_artifact_id from artifact_group_list where group_id in (select group_id from groups where status = 'D'));
+delete from artifact_group_list where group_id in (select group_id from groups where status = 'D');
+delete from frs_package where group_id in (select group_id from groups where status = 'D');
 
 delete from groups where status = 'D' ;
---
+++ tools/find_user_group_double.sql
@@ -0,0 +1,15 @@
+-- select user_group.user_group_id,user_group.group_id,user_group.user_id
+-- ,ug.user_group_id,ug.group_id, ug.user_id 
+-- from user_group, user_group ug 
+-- where user_group.group_id=ug.group_id
+-- and user_group.user_id=ug.user_id
+-- and user_group.user_group_id != ug.user_group_id
+
+select user_group.* 
+from user_group, user_group ug
+where user_group.group_id=ug.group_id
+and user_group.user_id=ug.user_id
+and user_group.user_group_id != ug.user_group_id
+
+-- select user_group_id,group_id,user_id from user_group
+-- order by user_group_id,group_id,user_id
