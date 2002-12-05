318d2f224def9d544e79a980a4e24a1de95801ea
lo-lan-do <>
2002-12-05 17:45:45 +0000

    Fixed buildability.

--- gforge-plugin-helloworld/include/helloworld-init.php
+++ gforge-plugin-helloworld/include/helloworld-init.php
@@ -1,5 +1,14 @@
+<?php
+
 require_once ('/usr/lib/sourceforge/plugins/helloworld/include/HelloWorldPlugin.class') ;
 
 $HelloWorldPluginObject = new HelloWorldPlugin () ;
 
 plugin_manager_get_object ()->RegisterPlugin ("helloworld", $HelloWorldPluginObject) ;
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+
+?>
