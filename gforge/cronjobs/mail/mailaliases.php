abba5562faaa4075abedfae717c7722461ea0be2
bigdisk <>
2002-12-21 14:40:58 +0000

    placing stubbed-out mailing list/alias scripts in CVS

---
+++ gforge/cronjobs/mail/mailaliases.php
@@ -0,0 +1,20 @@
+#! /usr/bin/php4 -f
+<?php
+
+require ('squal_pre.php');
+
+/*
+$res=db_query("SELECT user_name,email FROM users WHERE status = 'A' AND email != ''");
+echo db_error();
+
+$rows=db_numrows($res);
+
+for ($i=1; $i<$rows; $i++) {
+
+
+
+}
+
+*/
+
+?>
