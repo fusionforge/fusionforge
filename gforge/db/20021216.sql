e986bbbd77c7877eb0153b07864f6f85cf2986e4
Christian Bayle <cbayle@gmail.com>
2003-01-04 01:01:15 +0000

    split of 20021125.sql in 20021124-1_drop_foundry.sql 20021124-2_theming.sql 20021124-3_gforge-debian-sf-sync.sql 20021125.sql delete 20021125-debian.sql 20021216-debian.sql added README to sync with debian packaging

--- gforge/db/20021216.sql
+++ gforge/db/20021216.sql
@@ -1,3 +1,8 @@
+--
+-- If the theme with theme_id=1 exits it is copied at the end of the list
+--
+INSERT INTO themes (dirname, fullname) SELECT dirname,fullname FROM themes where theme_id=1;
+DELETE FROM themes WHERE theme_id=1;
 INSERT INTO themes (theme_id,dirname, fullname) VALUES (1,'gforge', 'Default Theme');
 
 ALTER TABLE theme_prefs
