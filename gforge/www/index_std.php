1c0342bf1dc70249570241799620e0d8e4922308
tcopeland <>
2003-01-06 21:44:05 +0000

    updated download version

--- gforge/www/index_std.php
+++ gforge/www/index_std.php
@@ -33,7 +33,7 @@ echo $HTML->boxBottom();
 echo $HTML->boxTop('Getting GForge');
 ?>
 <b>Download:</b><br>
-<a href="/project/showfiles.php?group_id=1">GForge3.0pre7</a><br>
+<a href="/project/showfiles.php?group_id=1">GForge3.0pre8</a><br>
 <a href="http://postgresql.org/">PostgreSQL</a><br>
 <a href="http://www.php.net/">PHP 4.x</a><br>
 <a href="http://www.apache.org/">Apache</a><br>
