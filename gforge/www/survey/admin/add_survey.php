60ef88c55d4cb8166481f35c44a84d72049d6a33
rts <>
2003-01-12 03:31:58 +0000

    - add missing < - don't display empty table if there are no existing surveys

--- gforge/www/survey/admin/add_survey.php
+++ gforge/www/survey/admin/add_survey.php
@@ -18,7 +18,7 @@ $is_admin_page='y';
 survey_header(array('title'=>'Add A Survey','pagename'=>'survey_admin_add_survey'));
 
 if (!session_loggedin() || !user_ismember($group_id,'A')) {
-	echo "h1>Permission Denied</h1>";
+	echo "<h1>Permission Denied</h1>";
 	survey_footer(array());
 	exit;
 }
@@ -75,31 +75,33 @@ Function  ShowResultsEditSurvey($result) {
 	$cols  =  db_NumFields($result);
 	echo "<h3>$rows Found</h3>";
 
-	echo /*"<table bgcolor=\"NAVY\"><tr><td bgcolor=\"NAVY\">*/ "<table border=\"0\">\n";
+	if ($rows > 0) {
+		echo /*"<table bgcolor=\"NAVY\"><tr><td bgcolor=\"NAVY\">*/ "<table border=\"0\">\n";
 
-	/*  Create  the  headers  */
-	echo "<tr style=\"background-color:$GLOBALS[COLOR_MENUBARBACK]\">\n";
-	for ($i  =  0;  $i  <  $cols;  $i++)  {
-		printf( "<th><span style=\"color:white\"><strong>%s</strong></span></th>\n",  db_fieldname($result,$i));
-	}
-	echo "</tr>";
-	for($j  =  0;  $j  <  $rows;  $j++)  {
-
-		if ($j%2==0) {
-			$row_bg="white";
-		} else {
-			$row_bg="$GLOBALS[COLOR_LTBACK1]";
+		/*  Create  the  headers  */
+		echo "<tr style=\"background-color:$GLOBALS[COLOR_MENUBARBACK]\">\n";
+		for ($i  =  0;  $i  <  $cols;  $i++)  {
+			printf( "<th><span><strong>%s</strong></span></th>\n",  db_fieldname($result,$i));
 		}
-
-		echo "<tr style=\"background-color:$row_bg\">\n";
-		echo "<td><a href=\"edit_survey.php?group_id=$group_id&amp;survey_id=".db_result($result,$j,0)."\">".db_result($result,$j,0)."</a></td>";
-		for ($i = 1; $i < $cols; $i++)  {
-			printf("<td>%s</td>\n",db_result($result,$j,$i));
-		}
-
 		echo "</tr>";
+		for($j  =  0;  $j  <  $rows;  $j++)  {
+
+			if ($j%2==0) {
+				$row_bg="white";
+			} else {
+				$row_bg="$GLOBALS[COLOR_LTBACK1]";
+			}
+
+			echo "<tr style=\"background-color:$row_bg\">\n";
+			echo "<td><a href=\"edit_survey.php?group_id=$group_id&amp;survey_id=".db_result($result,$j,0)."\">".db_result($result,$j,0)."</a></td>";
+			for ($i = 1; $i < $cols; $i++)  {
+				printf("<td>%s</td>\n",db_result($result,$j,$i));
+			}
+
+			echo "</tr>";
+		}
+		echo "</table>"; //</td></tr></TABLE>";
 	}
-	echo "</table>"; //</td></tr></TABLE>";
 }
 
 /*
