35e9a49374bd429ab115958964170dcdad3e6e25
bigdisk <>
2003-01-15 20:22:57 +0000

    moving tracker/browse.php logic into separate class

--- gforge/www/tracker/browse.php
+++ gforge/www/tracker/browse.php
@@ -10,7 +10,7 @@
   * @version   $Id$
   *
   */
-
+require_once('common/tracker/ArtifactFactory.class');
 //
 //  make sure this person has permission to view artifacts
 //
@@ -18,105 +18,24 @@ if (!$ath->userCanView()) {
 	exit_permission_denied();
 }
 
-if (session_loggedin()) {
-	$u =& session_get_user();
+$af = new ArtifactFactory($ath);
+if (!$af || !is_object($af)) {
+	exit_error('Error','Could Not Get Factory');
+} elseif ($af->isError()) {
+	exit_error('Error',$af->getErrorMessage());
 }
 
-if (!$offset || $offset < 0) {
-	$offset=0;
-}
+$af->setup($offset,$_sort_col,$_sort_ord,$max_rows,$set,$_assigned_to,$_status,$_category,$_group);
+$_sort_col=$af->order_col;
+$_sort_ord=$af->sort;
+$_status=$af->status;
+$_assigned_to=$af->assigned_to;
+$_category=$af->category;
+$_group=$af->group;
 
-if (!$set) {
-	/*
-		if no set is passed in, see if a preference was set
-		if no preference or not logged in, use open set
-	*/
-	if (session_loggedin()) {
-		$custom_pref=$u->getPreference('art_cust'.$ath->getID());
-		if ($custom_pref) {
-			$pref_arr=explode('|',$custom_pref);
-			$_assigned_to=$pref_arr[0];
-			$_status=$pref_arr[1];
-			$_category=$pref_arr[2];
-			$_group=$pref_arr[3];
-			$order=$pref_arr[4];
-			$sort=$pref_arr[5];
-			$set='custom';
-		} else {
-			//default to open
-			$_assigned_to=0;
-			$_status=1;
-		}
-	} else {
-		//default to open
-		$_assigned_to=0;
-		$_status=1;
-	}
-}
-
-//
-//	validate the column names and sort order passed in from user
-//	before saving it to prefs
-//
-if ($order=='artifact_id' || $order=='summary' || $order=='open_date' || $order=='close_date' || $order=='assigned_to' || $order=='submitted_by' || $order=='priority') {
-	$_sort_col=$order;
-	if (($sort == 'ASC') || ($sort == 'DESC')) {
-		$_sort_ord=$sort;
-	} else {
-		$_sort_ord='ASC';
-	}
-} else {
-	$_sort_col='artifact_id';
-	$_sort_ord='ASC';
-}
-
-if ($set=='custom') {
-	if (session_loggedin()) {
-		/*
-			if this custom set is different than the stored one, reset preference
-		*/
-		$pref_=$_assigned_to.'|'.$_status.'|'.$_category.'|'.$_group.'|'.$_sort_col.'|'.$_sort_ord;
-		if ($pref_ != $u->getPreference('art_cust'.$ath->getID())) {
-			$u->setPreference('art_cust'.$ath->getID(),$pref_);
-		}
-	}
-}
-
-/*
-	Display items based on the form post - by user or status or both
-*/
-
-//if status selected, add more to where clause
-if ($_status && ($_status != 100)) {
-	//for open tasks, add status=100 to make sure we show all
-	$status_str="AND artifact.status_id='$_status'";
-} else {
-	//no status was chosen, so don't add it to where clause
-	$status_str='';
-}
-
-//if assigned to selected, add to where clause
-if ($_assigned_to) {
-	$assigned_str="AND artifact.assigned_to='$_assigned_to'";
-} else {
-	//no assigned to was chosen, so don't add it to where clause
-	$assigned_str='';
-}
-
-//if category selected, add to where clause
-if ($_category && ($_category != 100)) {
-	$category_str="AND artifact.category_id='$_category'";
-} else {
-	//no assigned to was chosen, so don't add it to where clause
-	$category_str='';
-}
-
-//if artgroup selected, add to where clause
-if ($_group && ($_group != 100)) {
-	$group_str="AND artifact.artifact_group_id='$_group'";
-} else {
-	//no artgroup to was chosen, so don't add it to where clause
-	$group_str='';
+$art_arr =& $af->getArtifacts();
+if (!$art_arr && $af->isError()) {
+	exit_error('Error',$af->getErrorMessage());
 }
 
 //build page title to make bookmarking easier
@@ -195,8 +114,8 @@ echo '
 	<tr>
 		<td align="right"><span style="font-size:smaller">Sort By:&nbsp;<a href="javascript:help_window(\'/help/tracker.php?helpname=sort_by\')"><strong>(?)</strong></a></span></td>'.
 		'<td><span style="font-size:smaller">'. 
-		html_build_select_box_from_arrays($order_arr,$order_name_arr,'order',$_sort_col,false) .'</td>'.
-		'<td><span style="font-size:smaller">'.html_build_select_box_from_arrays($sort_arr,$sort_name_arr,'sort',$_sort_ord,false) .'</td>'.
+		html_build_select_box_from_arrays($order_arr,$order_name_arr,'_sort_col',$_sort_col,false) .'</td>'.
+		'<td><span style="font-size:smaller">'.html_build_select_box_from_arrays($sort_arr,$sort_name_arr,'_sort_ord',$_sort_ord,false) .'</td>'.
 		'<td><span style="font-size:smaller"><input type="submit" name="submit" value="Browse" /></td>
 	</tr>
 	</form></table>';
@@ -206,27 +125,146 @@ echo '
 */
 echo $ath->getBrowseInstructions();
 
-//
-//	now run the query using the criteria chosen above
-//
-$sql="SELECT artifact.priority,artifact.group_artifact_id,artifact.artifact_id,artifact.summary,
-	artifact.open_date AS date,users.user_name AS submitted_by,user2.user_name AS assigned_to 
-	FROM artifact,users,users user2 
-	WHERE users.user_id=artifact.submitted_by 
-	 $status_str $assigned_str $category_str $group_str 
-	AND user2.user_id=artifact.assigned_to 
-	AND group_artifact_id='". $ath->getID() ."'
-	ORDER BY group_artifact_id $_sort_ord, $_sort_col $_sort_ord";
-
-$result=db_query($sql,51,$offset);
-
-if ($result && db_numrows($result) > 0) {
+if ($art_arr && count($art_arr) > 0) {
 
 	if ($set=='custom') {
-		$set .= '&_assigned_to='.$_assigned_to.'&_status='.$_status.'&_category='.$_category.'&_group='.$_group.'&order='.$_sort_col.'&sort='.$_sort_ord;
+		$set .= '&_assigned_to='.$_assigned_to.'&_status='.$_status.'&_category='.$_category.'&_group='.$_group.'&_sort_col='.$_sort_col.'&_sort_ord='.$_sort_ord;
 	}
 
-	$ath->showBrowseList($result,$offset,$set);
+	$title_arr=array();
+	$title_arr[]='Request ID';
+	$title_arr[]='Summary';
+	$title_arr[]='Date';
+	$title_arr[]='Assigned To';
+	$title_arr[]='Submitted By';
+
+	$IS_ADMIN=$ath->userIsAdmin();
+
+	if ($IS_ADMIN) {
+		echo '
+		<form name="artifactList" action="'. $PHP_SELF .'?group_id='.$group_id.'&atid='.$ath->getID().'" METHOD="POST">
+		<input type="hidden" name="func" value="massupdate">';
+	}
+
+	echo $GLOBALS['HTML']->listTableTop ($title_arr);
+
+	$then=(time()-$ath->getDuePeriod());
+	$rows=count($art_arr);
+	for ($i=0; $i < $rows; $i++) {
+		echo '
+		<tr bgcolor="'. html_get_priority_color( $art_arr[$i]->getPriority() ) .'">'.
+		'<td NOWRAP>'.
+		($IS_ADMIN?'<input type="CHECKBOX" name="artifact_id_list[]" value="'.
+			$art_arr[$i]->getID() .'"> ':'').
+			$art_arr[$i]->getID() .
+			'</td>'.
+		'<td><a href="'.$PHP_SELF.'?func=detail&aid='.
+			$art_arr[$i]->getID() .
+			'&group_id='. $group_id .'&atid='.
+			$ath->getID().'">'.
+			$art_arr[$i]->getSummary() .
+			'</a></td>'.
+		'<td>'. (($set != 'closed' && $art_arr[$i]->getOpenDate() < $then)?'* ':'&nbsp; ') .
+				date($sys_datefmt,$art_arr[$i]->getOpenDate()) .'</td>'.
+		'<td>'. $art_arr[$i]->getAssignedRealName() .'</td>'.
+		'<td>'. $art_arr[$i]->getSubmittedRealName() .'</td></tr>';
+	}
+
+	/*
+		Show extra rows for <-- Prev / Next -->
+	*/
+	if (($offset > 0) || ($rows >= 50)) {
+		echo '
+			<tr><td colspan="2">';
+		if ($offset > 0) {
+			echo '<a href="'.$PHP_SELF.'?func=browse&group_id='.$group_id.'&atid='.$ath->getID().'&set='.
+			$set.'&offset='.($offset-50).'"><strong><-- Previous 50</strong></a>';
+		} else {
+			echo '&nbsp;';
+		}
+		echo '</td><td>&nbsp;</td><td colspan="2">';
+		if ($rows >= 50) {
+			echo '<a href="'.$PHP_SELF.'?func=browse&group_id='.$group_id.'&atid='.$ath->getID().'&set='.
+			$set.'&offset='.($offset+50).'"><strong>Next 50 --></strong></a>';
+		} else {
+			echo '&nbsp;';
+		}
+		echo '</td></tr>';
+	}
+
+	echo $GLOBALS['HTML']->listTableBottom();
+	/*
+		Mass Update Code
+	*/
+	if ($IS_ADMIN) {
+		echo '<script language="JavaScript">
+	<!--
+	function checkAll(val) {
+		al=document.artifactList;
+		len = al.elements.length;
+		var i=0;
+		for( i=0 ; i<len ; i++) {
+			if (al.elements[i].name==\'artifact_id_list[]\') {
+				al.elements[i].checked=val;
+			}
+		}
+	}
+	//-->
+	</script>
+
+			<table width="100%" border="0">
+			<tr><td colspan="2">
+<font size=1>
+<a href="javascript:checkAll(1)">Check&nbsp;All</a>
+-
+  <a href="javascript:checkAll(0)">Clear&nbsp;All</a>
+</font>
+<p>
+<FONT COLOR="#FF0000"><strong>Admin:</strong></FONT>  If you wish to apply changes to all items selected above,
+	use these controls to change their properties and click once on "Mass Update".
+			</td></tr>
+
+			<tr>
+			<td><strong>Category: <a href="javascript:help_window(\'/help/tracker.php?helpname=category\')"><strong>(?)</strong></a>
+				</strong><br />'. $ath->categoryBox ('category_id','xzxz','No Change') .'</td>
+			<td><strong>Group: <a href="javascript:help_window(\'/help/tracker.php?helpname=group\')"><strong>(?)</strong></a></strong>
+				<br />'. $ath->artifactGroupBox ('artifact_group_id','xzxz','No Change') .'</td>
+			</tr>
+
+			<tr>
+			<td><strong>Priority: <a href="javascript:help_window(\'/help/tracker.php?helpname=priority\')"><strong>(?)</strong></a>
+				</strong><br />';
+			echo build_priority_select_box ('priority', '100', true);
+			echo '</td><td>';
+			if ($ath->useResolution()) {
+				echo '
+				<strong>Resolution: <a href="javascript:help_window(\'/help/tracker.php?helpname=resolution\')"><strong>(?)</strong></a>
+					</strong><br />';
+				echo $ath->resolutionBox('resolution_id','xzxz',true,'No Change');
+			} else {
+				echo '&nbsp;
+				<input type="hidden" name="resolution_id" value="100">';
+			}
+
+			echo '</td>
+			</tr>
+
+			<tr>
+			<td><strong>Assigned To: <a href="javascript:help_window(\'/help/tracker.php?helpname=assignee\')"><strong>(?)</strong></a>
+				</strong><br />'. $ath->technicianBox ('assigned_to','xzxz',true,'No Change') .'</td>
+			<td><strong>Status: <a href="javascript:help_window(\'/help/tracker.php?helpname=status\')"><strong>(?)</strong></a></strong>
+				<br />'. $ath->statusBox ('status_id','xzxz',true,'No Change') .'</td>
+			</tr>
+
+			<tr><td colspan="2"><strong>Canned Response:
+				<a href="javascript:help_window(\'/help/tracker.php?helpname=canned_response\')"><strong>(?)</strong></a>
+				</strong><br />'. $ath->cannedResponseBox ('canned_response') .'</td></tr>
+
+			<tr><td colspan="3" align="MIDDLE"><input type="SUBMIT" name="submit" value="Mass Update"></td></tr>
+
+			</TABLE>
+		</form>';
+	}
 
 	echo '* Denotes Requests > '. ($ath->getDuePeriod()/86400) .' Days Old';
 	show_priority_colors_key();
