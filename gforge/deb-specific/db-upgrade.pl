bb90b737d6d3b872e7c739791f69bcc3209ceb92
lo-lan-do <>
2003-01-15 17:13:49 +0000

    Removed duplicate block for 2.6-0+checkpoint+11 (plugins system).  No need to do it twice.

--- gforge/deb-specific/db-upgrade.pl
+++ gforge/deb-specific/db-upgrade.pl
@@ -786,34 +786,6 @@ eval {
  	$dbh->commit () ;
     }
 
-
-    $version = &get_db_version ;
-    $target = "2.6-0+checkpoint+11" ;
-    if (is_lesser $version, $target) {
- 	debug "Adding tables for the plugin subsystem." ;
-
- 	@reqlist = (
-		    "CREATE SEQUENCE plugins_pk_seq",
-		    "CREATE TABLE plugins (plugin_id integer DEFAULT nextval('plugins_pk_seq'::text) NOT NULL, plugin_name varchar(32) UNIQUE NOT NULL, plugin_desc text, CONSTRAINT plugins_pkey PRIMARY KEY (plugin_id))",
-		    "CREATE SEQUENCE group_plugin_pk_seq",
-		    "CREATE TABLE group_plugin (group_plugin_id integer DEFAULT nextval('group_plugin_pk_seq'::text) NOT NULL, group_id integer, plugin_id integer, CONSTRAINT group_plugin_pkey PRIMARY KEY (group_plugin_id), CONSTRAINT group_plugin_group_id_fk FOREIGN KEY (group_id) REFERENCES groups(group_id) MATCH FULL, CONSTRAINT group_plugin_plugin_id_fk FOREIGN KEY (plugin_id) REFERENCES plugins(plugin_id) MATCH FULL)",
-		    "CREATE SEQUENCE user_plugin_pk_seq",
-		    "CREATE TABLE user_plugin (user_plugin_id integer DEFAULT nextval('user_plugin_pk_seq'::text) NOT NULL, user_id integer, plugin_id integer, CONSTRAINT user_plugin_pkey PRIMARY KEY (user_plugin_id), CONSTRAINT user_plugin_user_id_fk FOREIGN KEY (user_id) REFERENCES users(user_id) MATCH FULL, CONSTRAINT user_plugin_plugin_id_fk FOREIGN KEY (plugin_id) REFERENCES plugins(plugin_id) MATCH FULL)",
- 		    ) ;
- 	foreach my $s (@reqlist) {
- 	    $query = $s ;
- 	    # debug $query ;
- 	    $sth = $dbh->prepare ($query) ;
- 	    $sth->execute () ;
- 	    $sth->finish () ;
- 	}
- 	@reqlist = () ;
- 	&update_db_version ($target) ;
- 	debug "Committing." ;
- 	$dbh->commit () ;
-    }
-
-
     $version = &get_db_version ;
     $target = "2.6-0+checkpoint+11" ;
     if (is_lesser $version, $target) {
