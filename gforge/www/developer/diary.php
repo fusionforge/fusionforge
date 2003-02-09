637c47f05e8bb2e08286557b019be05067710a1f
rts <>
2003-02-09 23:37:20 +0000

    Valid XHTML 1.0 Transitional

--- gforge/www/developer/diary.php
+++ gforge/www/developer/diary.php
@@ -24,28 +24,27 @@ if ($diary_user) {
 	echo $HTML->header(array('title'=>$Language->getText('my_diary','title')));
 
 	echo '
-	<h2>'.$Language->getText('developer','diary_and_notes').': '. $user_obj->getRealName() .'</h2>
-	<p>&nbsp;</p>
-
-	<p>&nbsp;</p>';
-
-	echo $HTML->boxTop($Language->getText('my_diary','existing_entries'));
+	<h2>'.$Language->getText('developer','diary_and_notes').': '. $user_obj->getRealName() .'</h2>';
 
 	if ($diary_id) {
 		$sql="SELECT * FROM user_diary WHERE user_id='$diary_user' AND id='$diary_id' AND is_public=1";
 		$res=db_query($sql);
+
+		echo $HTML->boxTop($Language->getText('developer','date').": ".date($sys_datefmt, db_result($res,$i,'date_posted')));
 		if (!$res || db_numrows($res) < 1) {
-			echo '<tr><td colspan="2">'.$Language->getText('developer','entry_not_found').'.</td></tr>';
+			echo $Language->getText('developer','entry_not_found');
 		} else {
-			echo '<tr><td colspan="2"><strong>'.$Language->getText('developer','date').':</strong> '. date($sys_datefmt, db_result($res,$i,'date_posted')) .'<br />
-			<strong>'.$Language->getText('developer','subject').':</strong> '. db_result($res,$i,'summary') .'<p>
+			echo'<strong>'.$Language->getText('developer','subject').':</strong> '. db_result($res,$i,'summary') .'<p>
 			<strong>'.$Language->getText('developer','body').':</strong><br />
 			'. nl2br(db_result($res,$i,'details')) .'
-			</p></td></tr>';
+			</p>';
 		}
+		echo $HTML->boxBottom();
 	}
 
-
+	echo $HTML->boxTop($Language->getText('my_diary','existing_entries'));
+	echo '<table cellspacing="2" cellpadding="0" width="100%" border="0">
+';
 	/*
 
 		List all diary entries
@@ -57,9 +56,14 @@ if ($diary_user) {
 	$rows=db_numrows($result);
 	if (!$result || $rows < 1) {
 		echo '
-			<tr><td colspan="2"><strong>'.$Language->getText('developer','no_entries').'</strong></td></tr>';
+			<tr><td><strong>'.$Language->getText('developer','no_entries').'</strong></td></tr>';
 		echo db_error();
 	} else {
+		echo '
+			<tr>
+				<th>'.$Language->getText('developer','subject').'</th>
+				<th>'.$Language->getText('developer','date').'</th>
+			</tr>';
 		for ($i=0; $i<$rows; $i++) {
 			echo '
 			<tr '. $GLOBALS['HTML']->boxGetAltRowStyle($i) .'><td><a href="'. $PHP_SELF .'?diary_id='.
@@ -69,6 +73,7 @@ if ($diary_user) {
 		echo '
 		<tr><td colspan="2" bgcolor="'.$HTML->COLOR_CONTENT_BACK.'">&nbsp;</td></tr>';
 	}
+	echo "</table>\n";
 
 	echo $HTML->boxBottom();
 
