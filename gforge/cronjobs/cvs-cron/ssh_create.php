6a9be32603ca5fa0d2793a9a73e11108f3488bf6
bigdisk <>
2003-03-04 03:01:11 +0000

    *** empty log message ***

--- gforge/cronjobs/cvs-cron/ssh_create.php
+++ gforge/cronjobs/cvs-cron/ssh_create.php
@@ -3,36 +3,34 @@
 
 require_once('squal_pre.php');
 
-$res=db_query("SELECT user_name,user_id,authorized_keys FROM users WHERE authorized_keys != ''");
+$res=db_query("SELECT user_name,user_id,authorized_keys 
+	FROM users 
+	WHERE authorized_keys != ''
+	AND status='A'");
 
 for ($i=0; $i<db_numrows($res); $i++) {
 	$ssh_key=db_result($res,$i,'authorized_keys');
 	$username=db_result($res,$i,'user_name');
 	$uid=db_result($res,$i,'user_id');
-/*
-	$ssh_key =~ s/\#\#\#/\n/g;
-	$uid += 1000;
 
-	push @user_authorized_keys, $ssh_key . "\n";
+	$ssh_key str_replace('###',"\n",$ssh_key);
+	$uid += 1000;
 
 	$ssh_dir = "$homedir_prefix$username/.ssh";
 
-	if (! -d $ssh_dir) {
-		mkdir $ssh_dir, 0755;
+	if (!is_dir($ssh_dir)) {
+		mkdir ($ssh_dir, 0755);
 	}
 
-	#print("Writing authorized_keys for $username: ");
+	$h8 = fopen("$ssh_dir/authorized_keys","w");
+	fwrite($h8,$ssh_key);
+	fclose($h8);
 
-	write_array_file("$ssh_dir/authorized_keys", @user_authorized_keys);
 	system("chown $uid:$uid $homedir_prefix$username");
 	system("chown $uid:$uid $ssh_dir");
 	system("chmod 0644 $ssh_dir/authorized_keys");
 	system("chown $uid:$uid $ssh_dir/authorized_keys");
 
-	#print ("Done\n");
-
-	undef @user_authorized_keys;
-*/
 }
 
 ?>
