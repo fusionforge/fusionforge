37c3bc4b526e1d5400c52ecbc5b0b8d8ae1cc8d2
Christian Bayle <cbayle@gmail.com>
2003-04-01 20:50:52 +0000

    Added register project in the user menu

--- gforge/www/include/html.php
+++ gforge/www/include/html.php
@@ -522,8 +522,9 @@ function site_user_header($params) {
 	echo ($HTML->PrintSubMenu(
 		      array($Language->getText('menu','my_personal_page'),
 			    $Language->getText('menu','diary_notes'),
-			    $Language->getText('menu','account_maintenance')),
-		      array('"/my/"','"/my/diary.php"','"/account/"')));
+			    $Language->getText('menu','account_maintenance'),
+			    $Language->getText('menu','register_project')),
+		      array('"/my/"','"/my/diary.php"','"/account/"','"/register/"')));
 	plugin_hook ("usermenu", false) ;
 	echo ($HTML->EndSubMenu());
 }
