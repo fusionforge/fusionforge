d069b353970e843b97c9818119277b53007347d4
bigdisk <>
2003-04-06 19:09:19 +0000

    updating homepage to b1

--- gforge/www/index_std.php
+++ gforge/www/index_std.php
@@ -38,7 +38,7 @@ echo $HTML->boxBottom();
 echo $HTML->boxTop('Getting GForge');
 ?>
 <strong>Download:</strong><br />
-<a href="/project/showfiles.php?group_id=1">GForge3.0pre9</a><br />
+<a href="/project/showfiles.php?group_id=1">GForge3.0b1</a><br />
 <a href="http://postgresql.org/">PostgreSQL</a><br />
 <a href="http://www.php.net/">PHP 4.x</a><br />
 <a href="http://www.apache.org/">Apache</a><br />
