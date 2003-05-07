4baf00419b149a23037dc3502e74b67dd2b47e0f
tcopeland <>
2003-05-07 17:31:41 +0000

    Added a popup window if the server name isn't filled in and the user hits the refresh button

--- gforge/contrib/soap/java/src/org/gforge/soapclient/GeneralPanel.java
+++ gforge/contrib/soap/java/src/org/gforge/soapclient/GeneralPanel.java
@@ -2,17 +2,15 @@ package org.gforge.soapclient;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTree;
-import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.tree.TreeNode;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeModel;
 import javax.swing.tree.DefaultTreeModel;
-import java.awt.GridLayout;
-import java.awt.Color;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -21,6 +19,10 @@ public class GeneralPanel extends JPanel {
     private class RefreshListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             try {
+                if (Settings.getInstance().get(ConfigurationPanel.SERVER_PROPERTY) == null || Settings.getInstance().get(ConfigurationPanel.SERVER_PROPERTY).equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please fill in a server name on the Configuration tab (i.e., cougaarforge.cougaar.org)");
+                    return;
+                }
                 Client client = new Client(Settings.getInstance().get(ConfigurationPanel.SERVER_PROPERTY),
                         Settings.getInstance().get(ConfigurationPanel.GROUP_PROPERTY)
                         );
