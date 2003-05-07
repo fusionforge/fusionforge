96fb74946bdc3ae840363fab55b56cb2c9941c75
(no author) <>
2003-05-07 19:47:40 +0000

    This commit was manufactured by cvs2svn to create tag 'soapclient_rc2'.

--- gforge/debian/gforge-dns-bind9.cron.d
+++
@@ -1,6 +0,0 @@
-#
-# Regular cron jobs for the gforge-dns-bind9 package
-#
-
-# DNS Update
-0 * * * * root [ -f /var/lib/gforge/bind/dns.head ] && [ -x /usr/lib/gforge/bin/install-dns.sh ] && /usr/lib/gforge/bin/install-dns.sh configure > /dev/null 2>&1
--- gforge/docs/xdocs/generate_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-maven -b pdf:pdf xdoc
-cp docs/project.pdf gforge.pdf
-zip -q -r gforge_html.zip docs/
-rm maven.log velocity.log
