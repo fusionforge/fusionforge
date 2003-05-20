6bd24beb3337545ca01685eff5bf29053ecf768c
lo-lan-do <>
2003-05-20 21:22:47 +0000

    Fixed file name.

--- tools/make-theme-orig.sh
+++ tools/make-theme-orig.sh
@@ -5,11 +5,11 @@ set -e
 theme=$1
 version=$2
 
-rm -f gforge-theme-$theme-$version.orig.tar
-rm -f gforge-theme-$theme-$version.orig.tar.gz
+rm -f gforge-theme-$theme_$version.orig.tar
+rm -f gforge-theme-$theme_$version.orig.tar.gz
 
-tar cf gforge-theme-$theme-$version.orig.tar --no-recursion gforge-theme-$theme
+tar cf gforge-theme-$theme_$version.orig.tar --no-recursion gforge-theme-$theme
 
-find gforge-theme-$theme/www/ -name CVS -type d -prune -or -print | xargs tar rf gforge-theme-$theme-$version.orig.tar --no-recursion
+find gforge-theme-$theme/www/ -name CVS -type d -prune -or -print | xargs tar rf gforge-theme-$theme_$version.orig.tar --no-recursion
 
-gzip gforge-theme-$theme-$version.orig.tar
+gzip gforge-theme-$theme_$version.orig.tar
