957165225dfa9d919ce3a6180551b0e692ab1c5f
tcopeland <>
2003-06-13 18:50:13 +0000

    Added a link from the main admin page to the 'recent logins' page

--- gforge/www/admin/index.php
+++ gforge/www/admin/index.php
@@ -106,6 +106,7 @@ $abc_array = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','
 	<li><a href="edit_frs_processor.php"><?php echo $Language->getText('admin_index','add_edit_delete_processors'); ?></a></li>
 	<li><a href="edit_frs_theme.php"><?php echo $Language->getText('admin_index','add_edit_delete_themes'); ?></a></li>
 	<li><a href="loadtabfiles.php"><?php echo $Language->getText('admin_index','translation_file_tool'); ?></a></li>
+	<li><a href="/stats/lastlogins.php"><?php echo $Language->getText('admin_index','recent_logins'); ?></a></li>
 </ul>
 
 <p>
