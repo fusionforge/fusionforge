3904941e8e06b0ac31565fe5ec8669086abd2717
tcopeland <>
2003-06-23 20:27:09 +0000

    Converted spaces to tabs, fixed brace placement

--- gforge/www/scm/controlleroo.php
+++ gforge/www/scm/controlleroo.php
@@ -14,7 +14,6 @@
 
 require_once('pre.php');    
 require_once('common/include/account.php');
-
 require_once('common/include/cvsweb/ErrorHandler.class');
 require_once('common/include/cvsweb/DirectoryHandler.class');
 require_once('common/include/cvsweb/FileHandler.class');
@@ -26,7 +25,7 @@ $cvsroot = $project->getUnixName();
 $sys_cvsroot_dir = '/cvsroot/';
 
 if (!$project->isProject()) {
- 	exit_error($Language->getText('scm_index','error_only_projects_can_use_cvs'));
+	exit_error($Language->getText('scm_index','error_only_projects_can_use_cvs'));
 }
 if (!$project->usesCVS()) {
 	exit_error($language->getText('scm_index','error_this_project_has_turned_off'));
@@ -50,106 +49,94 @@ if($project->enableAnonCVS()) {
 	}
 }
 
-if ($allow)
-{
-    $DHD = new DirectoryHandler();
-    $FHD = new FileHandler();
-    $RCH = new RCSHandler();
-
-    $CVSROOT = $sys_cvsroot_dir.$cvsroot;
-    $DIRNAME = ($dir_name != "")?"/$dir_name":"";
-    $DIRNAME = $CVSROOT.$DIRNAME;
-
-    $DIRPATH = explode("/",$dir_name);
-    echo("Current directory: ");
-    for($i=0;$i<count($DIRPATH);$i++)
-    {
-	$LINKPATH = array();
-	for($j=0;$j<=$i;$j++)
+if ($allow) {
+	$DHD = new DirectoryHandler();
+	$FHD = new FileHandler();
+	$RCH = new RCSHandler();
+	$CVSROOT = $sys_cvsroot_dir.$cvsroot;
+	$DIRNAME = ($dir_name != "")?"/$dir_name":"";
+	$DIRNAME = $CVSROOT.$DIRNAME;
+	$DIRPATH = explode("/",$dir_name);
+	echo("Current directory: ");
+	for($i=0;$i<count($DIRPATH);$i++) {
+		$LINKPATH = array();
+		for($j=0; $j<=$i; $j++) {
 	    $LINKPATH[] = $DIRPATH[$j];
-	$LINK = implode("/",$LINKPATH);
-	$value = ($DIRPATH[$i] == "")?$project->getPublicName():$DIRPATH[$i];
-	echo("<b><a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$LINK&hide_attic=$hide_attic\">[$value]</a>/</b>");
-    }
+		}
+		$LINK = implode("/",$LINKPATH);
+		$value = ($DIRPATH[$i] == "")?$project->getPublicName():$DIRPATH[$i];
+		echo("<b><a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$LINK&hide_attic=$hide_attic\">[$value]</a>/</b>");
+	}
 
-    if($hide_attic)
-	echo("<br><br>[<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$dir_name&hide_attic=0\">Unhide Attic</a>]");
-    else
-	echo("<br><br>[<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$dir_name&hide_attic=1\">Hide Attic</a>]");
+	if($hide_attic) {
+		echo("<br><br>[<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$dir_name&hide_attic=0\">Unhide Attic</a>]");
+	} else {
+		echo("<br><br>[<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$dir_name&hide_attic=1\">Hide Attic</a>]");
+	}
+	echo("<br>");
 
-    echo("<br>");
+	// hide some files and directories
+	$hideFile = ".|CVS";
+	if($hide_attic) {
+		$hideFile .= "|Attic";
+	}
 
-    // hide some files and directories
-    $hideFile = ".|CVS";
-    if($hide_attic)
-	$hideFile .= "|Attic";
+	$DHD->hideFiles($hideFile);
 
-    $DHD->hideFiles($hideFile);
+	if (false === ($dirContent = $DHD->readDirectory($DIRNAME))) {
+		echo("Error: ".$DHD->getError());
+	}
 
-    if(false === ($dirContent = $DHD->readDirectory($DIRNAME)))
-	echo("Error: ".$DHD->getError());
-    
-    echo("<table cellpadding=\"1\" cellspacing=\"1\" border=\"0\" width=\"100%\">");
-    echo("<tr>");
-    echo("<td width=\"20%\" bgcolor=\"#AAAAAA\"><b>File</b></td>");
-    echo("<td width=\"10%\" bgcolor=\"#AAAAAA\"><b>REV</b></td>");
-    echo("<td width=\"10%\" bgcolor=\"#AAAAAA\"><b>Age</b></td>");
-    echo("<td width=\"10%\" bgcolor=\"#AAAAAA\"><b>Author</b></td>");
-    echo("<td width=\"50%\" bgcolor=\"#AAAAAA\"><b>Last log entry</b></td>");
-    echo("</tr>");
-    $i = 0;
-    foreach($dirContent AS $k=>$v)
-    {   
-	$bgc = "#F0F0F0";
-	if($i % 2 == 0)
+	echo("<table cellpadding=\"1\" cellspacing=\"1\" border=\"0\" width=\"100%\">");
+	echo("<tr>");
+	echo("<td width=\"20%\" bgcolor=\"#AAAAAA\"><b>File</b></td>");
+	echo("<td width=\"10%\" bgcolor=\"#AAAAAA\"><b>REV</b></td>");
+	echo("<td width=\"10%\" bgcolor=\"#AAAAAA\"><b>Age</b></td>");
+	echo("<td width=\"10%\" bgcolor=\"#AAAAAA\"><b>Author</b></td>");
+	echo("<td width=\"50%\" bgcolor=\"#AAAAAA\"><b>Last log entry</b></td>");
+	echo("</tr>");
+	$i = 0;
+	foreach($dirContent AS $k=>$v) {   
+		$bgc = "#F0F0F0";
+		if ($i % 2 == 0) {
 	    $bgc = "#FFFFFF";
-	$i++;
-	if($FHD->getFileType($DIRNAME."/".$v) == 1)
-        {
-	    echo("<tr><td colspan=\"5\" width=\"100%\" bgcolor=\"".$bgc."\"><b>&nbsp;&nbsp;&nbsp;");
-	    echo("<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$dir_name/$v&hide_attic=$hide_attic\">$v</a>");
-	    echo("</b></td></tr>");
+		}
+		$i++;
+		if($FHD->getFileType($DIRNAME."/".$v) == 1) {
+			echo("<tr><td colspan=\"5\" width=\"100%\" bgcolor=\"".$bgc."\"><b>&nbsp;&nbsp;&nbsp;");
+			echo("<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/controlleroo.php?group_id=$group_id&dir_name=$dir_name/$v&hide_attic=$hide_attic\">$v</a>");
+			echo("</b></td></tr>");
+		} else {	
+			$rcsFile = $DIRNAME."/".$v;
+			$fileName = substr($v,0,-2);
+			if(false === ($rcsInfo = $RCH->getRevisionInfo($rcsFile))) {
+				echo($RCH->getError());
+			}
+			$fileLink = "<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/viewFile.php?group_id=$group_id&file_name=$dir_name/$fileName&hide_attic=$hide_attic\">$fileName</a>";
+			// create 'Age' string
+			$age = time() - strtotime($rcsInfo['DATE']);
+			if($age < 24*3600) {
+				$age /= 3600;
+				$age = floor($age)." hour(s)";
+			} elseif($age < 30*24*3600) {
+				$age /= 24*3600;
+				$age = floor($age)." day(s)";
+			} else {
+				$age /= 30*24*3600;
+				$age = floor($age)." month(s)";
+			}
+			echo("<tr>");
+			echo("<td width=\"20%\" bgcolor=\"".$bgc."\">&#187;&nbsp;&nbsp;".$fileLink."</td>");
+			echo("<td width=\"10%\" bgcolor=\"".$bgc."\">".$rcsInfo['REV']."</td>");
+			echo("<td width=\"10%\" bgcolor=\"".$bgc."\">".$age."</td>");
+			echo("<td width=\"10%\" bgcolor=\"".$bgc."\">".$rcsInfo['AUTHOR']."</td>");
+			echo("<td width=\"50%\" bgcolor=\"".$bgc."\">".$rcsInfo['LOG']."</td>");
+			echo("</tr>");
+		}
+		echo("</table>");
 	}
-	//elseif(substr($v,-2) == ",v")
-	else
-        {	
-    	    $rcsFile = $DIRNAME."/".$v;
-	    $fileName = substr($v,0,-2);
-	    if(false === ($rcsInfo = $RCH->getRevisionInfo($rcsFile)))
-		echo($RCH->getError());
-	    $fileLink = "<a href=\"http://".$GLOBALS['sys_default_domain']."/scm/viewFile.php?group_id=$group_id&file_name=$dir_name/$fileName&hide_attic=$hide_attic\">$fileName</a>";
-	    // create 'Age' string
-	    $age = time() - strtotime($rcsInfo['DATE']);
-	    if($age < 24*3600)
-	    {
-		$age /= 3600;
-		$age = floor($age)." hour(s)";
-	    }
-	    elseif($age < 30*24*3600)
-	    {
-		$age /= 24*3600;
-		$age = floor($age)." day(s)";
-	    }
-	    else
-	    {
-		$age /= 30*24*3600;
-		$age = floor($age)." month(s)";
-	    }
-	    
-	    echo("<tr>");
-	    echo("<td width=\"20%\" bgcolor=\"".$bgc."\">&#187;&nbsp;&nbsp;".$fileLink."</td>");
-	    echo("<td width=\"10%\" bgcolor=\"".$bgc."\">".$rcsInfo['REV']."</td>");
-	    echo("<td width=\"10%\" bgcolor=\"".$bgc."\">".$age."</td>");
-	    echo("<td width=\"10%\" bgcolor=\"".$bgc."\">".$rcsInfo['AUTHOR']."</td>");
-	    echo("<td width=\"50%\" bgcolor=\"".$bgc."\">".$rcsInfo['LOG']."</td>");
-	    echo("</tr>");
-	}
-    }
-    echo("</table>");
-}
-else
-{
-    echo("Error: u don't have acces rigths to <b>".$project->getPublicName()."</b> CVS root = $cvsroot");
+} else {
+	echo("Error: u don't have access rights to <b>".$project->getPublicName()."</b> CVS root = $cvsroot");
 }
 site_project_footer(array());
 ?>
