4ec1e7788c3cfaa3a326ed787c64900d62f2cb39
tcopeland <>
2003-08-05 19:37:45 +0000

    Rewrote history_parse.php in php; left some older code in (commented out) there just in case it's needed later.  As is, the script can now run as a cronjob and also be run to backfill stats.

--- gforge/cronjobs/cvs-cron/history_parse.php
+++ gforge/cronjobs/cvs-cron/history_parse.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php
+#!/usr/local/bin/php -q
 <?php
 
 require_once('squal_pre.php');
@@ -11,141 +11,162 @@ require_once('squal_pre.php');
  * @version   $Id$
  */
 
+$cvsroot="/cvsroot";
+if (!chdir($cvsroot)) {
+	print("Unable to make $cvsroot the working directory.\n");
+	exit;
+}
+
+function rundate($historyfile, $group_id, $mon, $day, $year, $day_begin, $day_end) {
+	global $cvsroot;
+	$cvs_co=$cvs_commit=$cvs_add=0;
+	
+	for ($i=0; $i<=count($historyfile)-1; $i++) {
+		# Split the cvs history entry into its 6 fields.
+		$fields = explode('|',trim($historyfile[$i]));
+		$cvstime=$fields[0];	
+		$user=$fields[1];	
+		$curdir=$fields[2];	
+		$module=$fields[3];	
+		$rev=$fields[4];	
+		$file=$fields[5];	
+
+		$type = substr($cvstime, 0, 1);
+		$time_parsed = hexdec( substr($cvstime, 1, 8) );
+	
+		## See if the entry was made for the specified day
+		if ($time_parsed > $day_begin && $time_parsed < $day_end) {
+			if ($type == "M") {
+				$cvs_commit++;
+			} elseif ( $type == "A" ) {
+				$cvs_add++;
+			} elseif ( $type == "O" ) {
+				$cvs_co++;
+			}
+		}
+	}
+
+	$sql = "INSERT INTO stats_cvs_group (month,day,group_id,checkouts,commits,adds) VALUES ('$year$mon','$day','$group_id',$cvs_co,$cvs_commit,$cvs_add)";
+	#print $sql."\n";
+	$res = db_query($sql);	
+	print db_error();
+}
+
+function generateTodayStats($historyfile, $group_id) {
+	$today=mktime();
+	$yesterday=mktime(0,0,0,strftime("%m", $today), strftime("%d", $today)-1, strftime("%Y", $today));
+	$day = strftime("%d", $yesterday);
+	$month = strftime("%m", $yesterday);
+	$year = strftime("%Y", $yesterday);
+	rundate($historyfile, $group_id, $month, $day, $year, $yesterday, $today);
+}
+
+function generateStats($historyfile, $group_id, $days) {
+	for ($i =0; $i<$days; $i++) {
+		$day_begin=mktime(0,0,0,5,1 + $i, 2003);
+		$day_end=mktime(0,0,0,5,1 + $i +1, 2003);
+		$day = strftime("%d", $day_begin);
+		$month = strftime("%m", $day_begin);
+		$year = strftime("%Y", $day_begin);
+		rundate($historyfile, $group_id, $month, $day, $year, $day_begin, $day_end);
+	}
+}
+function delete() {
+	$sql = "delete from stats_cvs_group";
+	$res = db_query($sql);	
+	print db_error();
+}
+
+if ($argv[1] != nil && $argv[1] == "delete") {
+	delete();
+	exit();
+}
+
+# get each group
+$sql = "select group_id, unix_group_name from groups where status='A'";
+$result = db_query($sql);
+$ids = util_result_column_to_array($result,0);
+$names = util_result_column_to_array($result,1);
+for ($i=0; $i<count($ids); $i++) {
+	$group_id=$ids[$i];
+	$group=$names[$i];
+	if (!file_exists($group)) {
+		continue;
+	}
+	
+	print "Processing group $group\n";
+	$historyfile = file("$cvsroot/$group/CVSROOT/history");
+	if (!$historyfile) {
+		print "Unable to open history for $group\n";
+		continue;
+	}
+	if ($argv[1] == nil || $argv[1] == "") {
+		generateTodayStats($historyfile, $group_id);
+	} else {
+		generateStats($historyfile, $group_id, $argv[1]);
+	}
+}
+
+exit;
+
+
 /*
-
-//
-//	TRANSLATE TO PHP
-//
-
-
-my ($year, $month, $day, $day_begin, $day_end);
-my ($group, $histline, $daily_log_file, $key, $verbose);
-my $verbose = 1;
-my $base_log_dir = "/var/log/gforge/cvs";
-my $cvsroot = "/var/lib/gforge/chroot/cvsroot";
-
-$|=0 if $verbose;
-$|++;
-
-   ## Set the time to collect stats for
+## Set the time to collect stats for
 if ( $ARGV[0] && $ARGV[1] && $ARGV[2] ) {
 
-        $day_begin = timegm( 0, 0, 0, $ARGV[2], $ARGV[1] - 1, $ARGV[0] - 1900 );
-        $day_end = timegm( 0, 0, 0, (gmtime( $day_begin + 86400 ))[3,4,5] );
+        $day_begin = mktime( 0, 0, 0, $ARGV[2], $ARGV[1] - 1, $ARGV[0] - 1900 );
+        $day_end = mktime( 0, 0, 0, (gmtime( $day_begin + 86400 ))[3,4,5] );
 	
-	$year = $ARGV[0];
-	$month = $ARGV[1];
+$year = $ARGV[0];
+$month = $ARGV[1];
 	$day = $ARGV[2];
 
 } else {
-
-           ## Start at midnight last night.
-        $day_end = timegm( 0, 0, 0, (gmtime( time() ))[3,4,5] );
-           ## go until midnight yesterday.
-        $day_begin = timegm( 0, 0, 0, (gmtime( time() - 86400 ))[3,4,5] );
-
-	$year	= strftime("%Y", gmtime( $day_begin ) );
-	$month	= strftime("%m", gmtime( $day_begin ) );
-	$day	= strftime("%d", gmtime( $day_begin ) );
-
+## Start at midnight last night.
+$day_end = mktime( 0, 0, 0, 8,4,2003);
+## go until midnight yesterday.
+$day_begin = mktime( 0, 0, 0, 8,3,2003);
+$year	= strftime("%Y", mktime( $day_begin ) );
+$month	= strftime("%m", mktime( $day_begin ) );
+$day	= strftime("%d", mktime( $day_begin ) );
 }
-
-my $daily_log_file;
-
-print "Parsing cvs logs looking for traffic on day $day, month $month, year $year.\n" if $verbose;
-
-if ( -d $base_log_dir ) {
-	$daily_log_file = $base_log_dir . "/" . sprintf("%04d", $year);
-	if ( ! -d $daily_log_file ) {
+*/
+/*
+if (file_exists("$base_log_dir")) {
+	$daily_log_file = $base_log_dir."/".sprintf("%04d", $year);
+	if (!file_exists("$daily_log_file")) {
 		print "Making dest dir \'$daily_log_file\'\n";
 		mkdir( $daily_log_file, 0755 ) || die("Could not mkdir $daily_log_file");
 	} 
-	$daily_log_file .= "/" . sprintf("%02d", $month);
-	if ( ! -d $daily_log_file ) {
+	$daily_log_file = $daily_log_file."/".sprintf("%02d", $month);
+	if (!file_exists("$daily_log_file")) {
 		print "Making dest dir \'$daily_log_file\'\n";
 		mkdir( $daily_log_file, 0755 ) || die("Could not mkdir $daily_log_file");
 	}
-	$daily_log_file .= "/cvs_traffic_" . sprintf("%04d%02d%02d",$year,$month,$day) . ".log";
+	$daily_log_file = $daily_log_file."/cvs_traffic_".sprintf("%04d%02d%02d",$year,$month,$day).".log";
 } else {
-	die("Base log directory \'$base_log_dir\' does not exist!");
+	die("Base log directory $base_log_dir does not exist!");
 }
 
-open(DAYS_LOG, "> $daily_log_file") || die "Unable to open the log file \'$daily_log_file\'";
+if (!fopen($daily_log_file, "w")) {
+	print "Unable to open the log file $daily_log_file\n\n";
+	exit;
+}
 print "Opened log file at \'$daily_log_file\' for writing...\n";
-print "Running tree at $cvsroot/\n";
-
-chdir( "$cvsroot" ) || die("Unable to make $cvsroot the working directory.\n");
-foreach $group ( glob("*") ) {
-	
-	next if ( ! -d "$group" );
-
-	my ($cvs_co, $cvs_commit, $cvs_add, %usr_commit, %usr_add );
-
-	open(HISTORY, "< $cvsroot/$group/CVSROOT/history") or print "E::Unable to open history for $group\n";
-	while ( <HISTORY> ) {
-		my ($time_parsed, $type, $cvstime, $user, $curdir, $module, $rev, $file );
- 
-		   ## Split the cvs history entry into it's 6 fields.
-		($cvstime,$user,$curdir,$module,$rev,$file) = split(/\|/, $_, 6 );
-
-		$type = substr($cvstime, 0, 1);
-		$time_parsed = hex( substr($cvstime, 1, 8) );
-
-		   ## If the entry was made in the past 24 hours 
-		   ## (i.e. - since the last run of this script...)
-		if ( ($time_parsed > $day_begin) && ($time_parsed < $day_end) ) {
-
-			   ## log commits
-			if ( $type eq "M" ) {
-				$cvs_commit++;
-				$usr_commit{$user}++;
-				next;
-			}
-
-			   ## log adds
-			if ( $type eq "A" ) {
-				$cvs_add++;
-				$usr_add{$user}++;
-				next;
-			}
-
-			   ## log checkouts
-			if ( $type eq "O" ) {
-				$cvs_co++;
-				## we don't care about checkouts on a per-user
-				## most of them will be anon anyhow.
-				next;
-			}
-		
-		} elsif ( $time_parsed > $day_end ) {
-			if ( $verbose >= 2 ) {
-				print "Short circuting execution, parsed date exceeded current threshold.\n";
-			}
-			last;
-		}
-
-	}
-	close( HISTORY );
-
-                $sql = "INSERT INTO stats_cvs_group
-                        (month,day,group_id,checkouts,commits,adds)
-                        VALUES ('$year$mon','$day','$group_id','$checkouts','$commits','$adds')";
-
-	   ## Now, we'll print all of the results for that project, in the following format:
-	   ## (G|U|E)::proj_name::user_name::checkouts::commits::adds
-	   ## If 'G', then record is group statistics, and field 2 is a space...
-	   ## If 'U', then record is per-user stats, and field 2 is the user name...
-	   ## If 'E', then record is an error, and field 1 is a description, there are no other fields.
+*/
+/*
+	 ## Now, we'll print all of the results for that project, in the following format:
+	 ## (G|U|E)::proj_name::user_name::checkouts::commits::adds
+	 ## If 'G', then record is group statistics, and field 2 is a space...
+	 ## If 'U', then record is per-user stats, and field 2 is the user name...
+	 ## If 'E', then record is an error, and field 1 is a description, there are no other fields.
 	if ( $cvs_co || $cvs_commit || $cvs_add ) {
-		print DAYS_LOG "G::" . $group . ":: ::" . ($cvs_co?$cvs_co:"0") . "::"
-			. ($cvs_commit?$cvs_commit:"0") . "::" . ($cvs_add?$cvs_add:"0") . "\n";
-	
-		foreach $key ( keys %usr_commit ) {
-	
-			print DAYS_LOG "U::" . $group . "::" . $key . "::0::" . ($usr_commit{$key}?$usr_commit{$key}:"0") 
-				. "::" . ($usr_add{$key}?$usr_add{$key}:"0") . "\n";
+		print "DATA\n";
+		print 'DAYS_LOG "G::" . $group . ":: ::" . ($cvs_co?$cvs_co:"0") . "::" . ($cvs_commit?$cvs_commit:"0") . "::" . ($cvs_add?$cvs_add:"0") . "\n"';
+		$keys = array_keys($usr_commit);
+		foreach ($keys as $key) {
+			print 'DAYS_LOG "U::" . $group . "::" . $key . "::0::" . ($usr_commit[$key]?$usr_commit[$key]:"0") . "::" . ($usr_add[$key]?$usr_add[$key]:"0") . "\n"';
 		}
 	}
-}
 */
 ?>
