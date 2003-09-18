2cb5bb550728fb0cde094ede0340eafc4880bdbf
lo-lan-do <>
2003-09-18 19:39:13 +0000

    Restart Apache-SSL if installed, too.

--- gforge/deb-specific/create-vhosts.sh
+++ gforge/deb-specific/create-vhosts.sh
@@ -3,4 +3,9 @@
 set -e
 
 /usr/lib/gforge/bin/prepare-vhosts-file.pl
-/usr/sbin/invoke-rc.d apache reload
+if [ -x /usr/sbin/apache ]; then
+    invoke-rc.d apache reload
+fi
+if [ -x /usr/sbin/apache-ssl ]; then
+    invoke-rc.d apache-ssl reload
+fi
