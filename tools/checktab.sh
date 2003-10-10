5231265c24baafc0480fb4b252ac6d78ece78e7b
(no author) <>
2003-10-10 10:22:44 +0000

    This commit was manufactured by cvs2svn to create tag 'debian_version_3_0-9'.

--- tools/checktab.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-for file in $@
-do 
-	echo "================= Bad Record Begin For $file  ================="
-	cat $file | grep -v '^.[^ 	]*	.[^ 	]*	' | grep -v '^#'
-	echo "================= Bad Record End ================="
-done
