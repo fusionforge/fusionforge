f53d67d842908c9ec5b2950d5f91117f7c583c3e
lo-lan-do <>
2003-11-17 14:05:18 +0000

    install-db.sh now uses grep and cut (instead of Perl) to extract the configuration variables from the configuration file, which should help gforge-sourceforge-transition work better.

--- gforge/deb-specific/install-db.sh
+++ gforge/deb-specific/install-db.sh
@@ -26,10 +26,10 @@ case "$target" in
 	;;
     configure-files)
 	# Tell PostgreSQL to let us use the database
-	db_passwd=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbpasswd\n";')
-	ip_address=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbhost\n";')
-	db_name=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbname\n";')
-	db_user=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbuser\n";')
+	db_passwd=$(grep ^db_password= /etc/gforge/gforge.conf | cut -d= -f2-)
+	ip_address=$(grep ^ip_address= /etc/gforge/gforge.conf | cut -d= -f2-)
+	db_name=$(grep ^db_name= /etc/gforge/gforge.conf | cut -d= -f2-)
+	db_user=$(grep ^db_user= /etc/gforge/gforge.conf | cut -d= -f2-)
 	pattern=$(basename $0).XXXXXX
 	pg_version=$(dpkg -s postgresql | awk '/^Version: / { print $2 }')
 	if dpkg --compare-versions $pg_version lt 7.3 ; then
@@ -82,9 +82,9 @@ EOF
     configure)
 	# Create the appropriate database user
 	pg_version=$(dpkg -s postgresql | awk '/^Version: / { print $2 }')
-	db_name=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbname\n";')
-	db_user=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbuser\n";')
-	db_passwd=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbpasswd\n";')
+	db_passwd=$(grep ^db_password= /etc/gforge/gforge.conf | cut -d= -f2-)
+	db_name=$(grep ^db_name= /etc/gforge/gforge.conf | cut -d= -f2-)
+	db_user=$(grep ^db_user= /etc/gforge/gforge.conf | cut -d= -f2-)
 	pattern=$(basename $0).XXXXXX
 	tmp1=$(mktemp /tmp/$pattern)
 	tmp2=$(mktemp /tmp/$pattern)
@@ -191,15 +191,21 @@ EOF
         fi
 	;;
     purge)
-	db_name=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbname\n";')
-	db_user=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbuser\n";')
+	db_name=$(grep ^db_name= /etc/gforge/gforge.conf | cut -d= -f2-)
+	db_user=$(grep ^db_user= /etc/gforge/gforge.conf | cut -d= -f2-)
 	su -s /bin/sh postgres -c "dropdb $db_name" > /dev/null 2>&1 || true
 	su -s /bin/sh postgres -c "dropuser $db_user" > /dev/null 2>&1 || true
 	rm -f /var/lib/postgres/data/gforge_passwd
 	kill -HUP $(head -1 /var/lib/postgres/data/postmaster.pid)
 	;;
     dump)
-    	db_name=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbname\n";')
+	if [ -e /etc/sourceforge/local.pl ] ; then
+	    db_name=$(perl -e'require "/etc/sourceforge/local.pl"; print "$sys_dbname\n";')
+	elif [ -e /etc/gforge/gforge.conf ] ; then
+	    db_name=$(grep ^db_name= /etc/gforge/gforge.conf | cut -d= -f2-)
+	else
+	    db_name=sourceforge
+	fi
 	if [ "x$2" != "x" ] ;then
 		DUMPFILE=$2
 	else
@@ -214,7 +220,7 @@ EOF
 	su -s /bin/sh $DB -c /usr/lib/postgresql/bin/pg_dump $DB > $DUMPFILE
 	;;
     restore)
-	db_name=$(perl -e'require "/etc/gforge/local.pl"; print "$sys_dbname\n";')
+	db_name=$(grep ^db_name= /etc/gforge/gforge.conf | cut -d= -f2-)
 	pattern=$(basename $0).XXXXXX
 	newpg=$(mktemp /tmp/$pattern)
 	pg_version=$(dpkg -s postgresql | awk '/^Version: / { print $2 }')
