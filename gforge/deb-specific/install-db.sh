2ac4f17a2d19505c0ca93831a28fa2745d6878f6
lo-lan-do <>
2004-01-10 21:41:44 +0000

    Better detection of errors.

--- gforge/deb-specific/install-db.sh
+++ gforge/deb-specific/install-db.sh
@@ -91,7 +91,7 @@ EOF
 	if dpkg --compare-versions $pg_version lt 7.3 ; then
 	    if su -s /bin/sh postgres -c "createuser --no-createdb --no-adduser $db_user" 1> $tmp1 2> $tmp2 \
 		&& [ "$(head -1 $tmp1)" = 'CREATE USER' ] \
-		|| grep -q "^ERROR:  CREATE USER: user name \"$db_user\" already exists$" $tmp2 ; then
+		|| grep -q "^ERROR: .* user name \"$db_user\" already exists$" $tmp2 ; then
 	        # Creation OK or user already existing -- no problem here
 		rm -f $tmp1 $tmp2
 	    else
@@ -131,7 +131,7 @@ EOF
 	tmp2=$(mktemp /tmp/$pattern)
 	if su -s /bin/sh postgres -c "createdb --encoding=UNICODE $db_name" 1> $tmp1 2> $tmp2 \
 	    && [ "$(head -1 $tmp1)" = 'CREATE DATABASE' ] \
-	    || grep -q "ERROR:  CREATE DATABASE: database \"$db_name\" already exists" $tmp2 ; then
+	    || grep -q "ERROR: .* database \"$db_name\" already exists" $tmp2 ; then
 	    # Creation OK or database already existing -- no problem here
 	    echo -n ""
 	    rm -f $tmp1 $tmp2
