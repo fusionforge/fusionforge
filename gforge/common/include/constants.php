4e22993455455064b5e9d9df8120fea109111f9d
Guillaume Smet <guillaume.smet@gmail.com>
2004-03-28 17:05:44 +0000

    move constants to this file instead of including a lot of files

--- gforge/common/include/constants.php
+++ gforge/common/include/constants.php
@@ -1,6 +1,26 @@
 <?php
 
-require('common/include/constants/search.php');
+/* Search */
 
+define('SEARCH__TYPE_IS_ARTIFACT', 'artifact');
+define('SEARCH__TYPE_IS_SOFTWARE', 'soft');
+define('SEARCH__TYPE_IS_FORUM', 'forums');
+define('SEARCH__TYPE_IS_PEOPLE', 'people');
+define('SEARCH__TYPE_IS_SKILL', 'skill');
+define('SEARCH__TYPE_IS_DOCUMENT', 'document');
+define('SEARCH__TYPE_IS_ARTIFACT', 'artifact');
+
+define('SEARCH__DEFAULT_ROWS_PER_PAGE', 25);
+
+/* Mailing lists */
+
+define('MAIL__MAILING_LIST_IS_PRIVATE', '0');
+define('MAIL__MAILING_LIST_IS_PUBLIC', '1');
+define('MAIL__MAILING_LIST_IS_DELETED', '9');
+
+define('MAIL__MAILING_LIST_IS_REQUESTED', '1');
+define('MAIL__MAILING_LIST_IS_CREATED', '2');
+
+define('MAIL__MAILING_LIST_NAME_MIN_LENGTH', 4);
 
 ?>
