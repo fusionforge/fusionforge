ba9d12255be5cbb2fe8573837b8ebc7b922ace9f
Christian Bayle <cbayle@gmail.com>
2004-04-14 12:54:00 +0000

    Few adjustment

--- tools/replicate_gforge.sh
+++ tools/replicate_gforge.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # This is the server you want to replicate
 # You must have ssh root@servertoreplicate working
-OLDSFSERVER=servertoreplicate
+OLDSFSERVER=oldforge.mydomain.org
 # This is the db name on the remote server sourceforge if this is an old sf 2.5/2.6 server
 # gforge for a more recent gforge server
 #OLDDB=sourceforge
@@ -11,11 +11,11 @@ NEWDB=gforge
 export NEWDB
 # By default I do nothing 
 # Sync the db
-DO_DB=0
+DO_DB=1
 # Get remote db data
-GET_REMOTE=0
+GET_REMOTE=1
 # Sync remote files
-SYNC_FILE=0
+SYNC_FILE=1
 export SYNC_FILE
 
 if [ $DO_DB == 1 ] 
@@ -23,10 +23,10 @@ then
 	if [ $GET_REMOTE == 1 ]
 	then
 		ssh root@$OLDSFSERVER "su -s /bin/sh $OLDDB -c \"/usr/lib/postgresql/bin/pg_dump $OLDDB\"" > /root/db_dump_$OLDDB
-		[ -f /root/db_dump_$OLDDB ] && echo "Adjusting /root/db_dump.tar" && perl -pi -e "s/connect - sourceforge/connect - gforge/" /root/db_dump_$OLDDB
+		[ -f /root/db_dump_$OLDDB ] && echo "Adjusting /root/db_dump_$OLDDB" && perl -pi -e "s/connect - sourceforge/connect - gforge/" /root/db_dump_$OLDDB
 	fi
 	/usr/lib/gforge/bin/install-db.sh restore /root/db_dump_$OLDDB
-	/usr/lib/gforge/bin/db-upgrade.pl
+	su -s /bin/sh gforge -c /usr/lib/gforge/bin/db-upgrade.pl
 fi
 
 if [ $SYNC_FILE == 1 ] 
