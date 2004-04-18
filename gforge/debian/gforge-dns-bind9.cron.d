c1a2ba6e180df8c32ceb7fe588de3d0c0b751cea
Christian Bayle <cbayle@gmail.com>
2004-04-18 18:45:05 +0000

    bis repetita placent

--- gforge/debian/gforge-dns-bind9.cron.d
+++ gforge/debian/gforge-dns-bind9.cron.d
@@ -3,4 +3,4 @@
 #
 
 # DNS Update
-0 * * * * root [ -f /var/lib/gforge/bind/dns.head ] && [ -x /usr/lib/gforge/bin/dns_conf.pl ] && /usr/sbin/invoke-rc.d bind9 reload &>/dev/null
+0 * * * * root [ -f /var/lib/gforge/bind/dns.head ] && [ -x /usr/lib/gforge/bin/dns_conf.pl ] && /usr/lib/gforge/bin/dns_conf.pl &>/dev/null
