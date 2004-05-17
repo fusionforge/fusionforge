bd2b3c364c2a8f2f4df09bcd00d40ae23ed038b6
Christian Bayle <cbayle@gmail.com>
2004-05-17 18:06:59 +0000

    As the name says it tries to delete user marqued deleted

---
+++ tools/delete_deleted_user.sql
@@ -0,0 +1,29 @@
+delete from forum where posted_by in 
+(select user_id  from users where status='D');
+
+delete from project_messages where posted_by in 
+(select user_id  from users where status='D');
+
+delete from user_group where user_id in 
+(select user_id  from users where status='D');
+
+delete from artifact_perm where user_id in 
+(select user_id  from users where status='D');
+
+delete from artifact_history where artifact_id in (select artifact_id from artifact where assigned_to in
+(select user_id  from users where status='D'));
+
+delete from artifact_history where mod_by in
+(select user_id  from users where status='D');
+
+
+delete from artifact_message where artifact_id in (select artifact_id from artifact where assigned_to in
+(select user_id  from users where status='D'));
+
+delete from artifact where assigned_to in
+(select user_id  from users where status='D');
+
+delete from artifact where submitted_by in
+(select user_id  from users where status='D');
+
+delete from users where status='D';
