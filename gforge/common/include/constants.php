0c42df9ab75e343ef83dfcb0a5c6852682149593
tom <>
2004-07-07 20:45:51 +0000

    Removed duplicate SEARCH__TYPE_IS_ARTIFACT declaration

--- gforge/common/include/constants.php
+++ gforge/common/include/constants.php
@@ -8,7 +8,6 @@ define('SEARCH__TYPE_IS_FORUM', 'forums');
 define('SEARCH__TYPE_IS_PEOPLE', 'people');
 define('SEARCH__TYPE_IS_SKILL', 'skill');
 define('SEARCH__TYPE_IS_DOCUMENT', 'document');
-define('SEARCH__TYPE_IS_ARTIFACT', 'artifact');
 
 define('SEARCH__DEFAULT_ROWS_PER_PAGE', 25);
 
