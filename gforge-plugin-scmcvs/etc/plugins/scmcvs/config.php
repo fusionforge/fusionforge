21f93858b9eedcac5ecec4dc16eba44e1efe77a0
Christian Bayle <cbayle@gmail.com>
2004-07-22 09:15:45 +0000

    Moved from etc

---
+++ gforge-plugin-scmcvs/etc/plugins/scmcvs/config.php
@@ -0,0 +1,12 @@
+<?php
+ 
+$default_cvs_server = $GLOBALS['sys_default_domain'] ;
+// $default_cvs_server = "cvs." . $GLOBALS['sys_default_domain'] ;
+$enabled_by_default = 1 ;
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+ 
+?>
---
+++ gforge-plugin-scmcvs/etc/plugins/scmcvs/config.pl
@@ -0,0 +1,4 @@
+$this_server = $sys_default_domain ;
+# $this_server = "cvs." . $sys_default_domain ;
+
+1 ;
