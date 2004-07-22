9ed54d7bc40e63b3e1852ee81964576834e5e679
tperdue <>
2004-07-22 20:23:11 +0000

    moving licenses to table in db

---
+++ gforge/www/admin/edit_licenses.php
@@ -0,0 +1,21 @@
+<?php
+/**
+  *
+  * Site Admin page to edit File Release System processor types
+  *
+  * SourceForge: Breaking Down the Barriers to Open Source Development
+  * Copyright 1999-2001 (c) VA Linux Systems
+  * http://sourceforge.net
+  *
+  * @version   $Id$
+  *
+  */
+
+
+$unit        = 'license';
+$table       = 'licenses';
+$primary_key = 'license_id';
+
+include_once('admin_table.php');
+
+?>
