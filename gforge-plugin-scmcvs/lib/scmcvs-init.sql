64994e8779f71ac1efe3186815fc079161d07f1c
Christian Bayle <cbayle@gmail.com>
2004-07-29 07:49:28 +0000

    All is now commented

--- gforge-plugin-scmcvs/lib/scmcvs-init.sql
+++ gforge-plugin-scmcvs/lib/scmcvs-init.sql
@@ -1,14 +1,14 @@
-CREATE SEQUENCE plugin_scmcvs_grp_usage_pk_seq ;
+--CREATE SEQUENCE plugin_scmcvs_grp_usage_pk_seq ;
 
-CREATE TABLE plugin_scmcvs_group_usage (
-	group_usage_id integer DEFAULT nextval('plugin_scmcvs_grp_usage_pk_seq'::text) NOT NULL,
-	group_id integer DEFAULT 0 NOT NULL,
-	CONSTRAINT "plugin_scmcvs_group_usage_pkey" PRIMARY KEY ("group_usage_id"),
-	CONSTRAINT "plugin_scmcvs_groupusage_groupid_fkey" FOREIGN KEY (group_id) REFERENCES groups(group_id) MATCH FULL ON DELETE CASCADE
-) ;
-
+--CREATE TABLE plugin_scmcvs_group_usage (
+--	group_usage_id integer DEFAULT nextval('plugin_scmcvs_grp_usage_pk_seq'::text) NOT NULL,
+--	group_id integer DEFAULT 0 NOT NULL,
 --	cvs_host text DEFAULT '' NOT NULL,
 --	anon_cvs integer DEFAULT 0 NOT NULL,
+--	CONSTRAINT "plugin_scmcvs_group_usage_pkey" PRIMARY KEY ("group_usage_id"),
+--	CONSTRAINT "plugin_scmcvs_groupusage_groupid_fkey" FOREIGN KEY (group_id) REFERENCES groups(group_id) MATCH FULL ON DELETE CASCADE
+--) ;
+
 
 --CREATE SEQUENCE plugin_scmcvs_stats_pk_seq ;
 
@@ -23,4 +23,4 @@ CREATE TABLE plugin_scmcvs_group_usage (
 --	changes integer DEFAULT 0 NOT NULL,
 --	CONSTRAINT "plugin_scmcvs_stats_pkey" PRIMARY KEY ("stats_id"),
 --	CONSTRAINT "plugin_scmcvs_stats_groupid_fkey" FOREIGN KEY (group_id) REFERENCES groups(group_id) MATCH FULL ON DELETE CASCADE
-) ;
+--) ;
