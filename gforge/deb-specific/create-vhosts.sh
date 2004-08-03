e2f194c4c20829ab51daee79cf73c94751a3a368
(no author) <>
2004-08-03 14:17:43 +0000

    This commit was manufactured by cvs2svn to create tag 'debian_version_3_1-18'.

--- gforge/deb-specific/create-vhosts.sh
+++ gforge/deb-specific/create-vhosts.sh
@@ -4,8 +4,8 @@ set -e
 
 /usr/lib/gforge/bin/prepare-vhosts-file.pl
 if [ -x /usr/sbin/apache ]; then
-    invoke-rc.d apache reload > /dev/null 2>&1
+    /usr/sbin/invoke-rc.d apache reload > /dev/null 2>&1
 fi
 if [ -x /usr/sbin/apache-ssl ]; then
-    invoke-rc.d apache-ssl reload > /dev/null 2>&1
+    /usr/sbin/invoke-rc.d apache-ssl reload > /dev/null 2>&1
 fi
