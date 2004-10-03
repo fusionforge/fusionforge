69e160b01e8d75337747632c99ff8d7ca0871cb5
Christian Bayle <cbayle@gmail.com>
2004-10-03 19:19:45 +0000

    Enhanced and at least working for me nss views

--- gforge/db/20041001.sql
+++ gforge/db/20041001.sql
@@ -2,12 +2,29 @@ CREATE VIEW nss_passwd AS
 	SELECT unix_uid+20000 AS uid,
 		unix_uid+20000 AS gid,
 		user_name AS login,
-		CHAR(1) 'x' AS passwd,
+		unix_pw AS passwd,
 		realname AS gecos,
 		shell,
 		'/var/lib/gforge/chroot/home/users/' || user_name AS homedir
 	FROM users
-	WHERE unix_status='A';
+	WHERE status='A'
+	UNION
+	SELECT group_id+50000 AS uid,
+		group_id+20000 AS gid,
+		'anoncvs_' || unix_group_name AS login,
+		CHAR(1) 'x' AS passwd,
+		group_name AS gecos,
+		'/bin/false' AS shell,
+		'/var/lib/gforge/chroot/home/groups' || group_name AS homedir
+	FROM groups
+	UNION
+	SELECT 9999 AS uid,
+		9999 AS gid,
+		'gforge_scm' AS login,
+		CHAR(1) 'x' AS passwd,
+		'Gforge SCM user' AS gecos,
+		'/bin/false' AS shell,
+		'/var/lib/gforge/chroot/home' AS homedir;
 
 CREATE VIEW nss_shadow AS
 	SELECT user_name AS login,
@@ -19,16 +36,25 @@ CREATE VIEW nss_shadow AS
 
 CREATE VIEW nss_groups AS
 	SELECT group_id+10000 AS gid,
-	unix_group_name AS name,
-	CHAR(1) 'x' AS passwd
-	FROM groups;
-
+		unix_group_name AS name,
+		group_name AS descr,
+		CHAR(1) 'x' AS passwd
+	FROM groups
+	UNION
+	SELECT unix_uid+20000 AS gid,
+		user_name AS name,
+		lastname AS descr,
+		CHAR(1) 'x' AS passwd
+	FROM users;
 
 CREATE VIEW nss_usergroups AS
 	SELECT group_id+10000 AS gid,
-		user_id+20000 AS uid
-	FROM user_group
---	WHERE group_id!=800	-- drop unused and overfull Debian group
-;
-
-GRANT SELECT ON nss_passwd, nss_groups, nss_usergroups TO gforge_nss;
+		users.unix_uid+20000 AS uid
+	FROM user_group,users 
+	WHERE user_group.user_id=users.user_id
+	UNION 
+	SELECT unix_uid+20000 AS gid,
+		unix_uid+20000 AS uid
+	FROM users;
+--         WHERE group_id!=800    -- drop unused and overfull Debian group
+	;
