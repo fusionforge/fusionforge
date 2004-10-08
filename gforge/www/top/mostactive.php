efcf79364923ed0687dcafa2155452bc6536e07d
Guillaume Smet <guillaume.smet@gmail.com>
2004-10-08 20:37:46 +0000

    copyright update

--- gforge/www/top/mostactive.php
+++ gforge/www/top/mostactive.php
@@ -1,10 +1,30 @@
 <?php
-//
-// SourceForge: Breaking Down the Barriers to Open Source Development
-// Copyright 1999-2000 (c) The SourceForge Crew
-// http://sourceforge.net
-//
-// $Id$
+/**
+ *
+ * GForge Top-Statistics: Most active Users
+ *
+ * Copyright 1999-2000 (c) The SourceForge Crew
+ * The rest Copyright 2002-2004 (c) GForge Team
+ * http://gforge.org/
+ *
+ * @version $Id$
+ *
+ * This file is part of GForge.
+ *
+ * GForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 require ('pre.php');    
 require_once ('common/include/Stats.class');    
