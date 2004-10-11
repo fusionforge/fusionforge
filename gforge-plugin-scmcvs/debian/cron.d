9cce9005d862693e8dd0babc0c16651b545f8a97
Christian Bayle <cbayle@gmail.com>
2004-10-11 14:39:39 +0000

    splited cvs_dump_update.pl in cvs_dump.pl and cvs_update.pl

--- gforge-plugin-scmcvs/debian/cron.d
+++ gforge-plugin-scmcvs/debian/cron.d
@@ -4,4 +4,4 @@
 
 # CVS tarballs
 5 2 * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/tarballs.sh ] && /usr/lib/gforge/plugins/scmcvs/bin/tarballs.sh generate
-5 * * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump_update.pl ] && /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump_update.pl
+5 * * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump.pl ] && su -s /bin/sh gforge -c /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump.pl && [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_update.pl ] && /usr/lib/gforge/plugins/scmcvs/bin/cvs_update.pl
