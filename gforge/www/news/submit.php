7cda8164092c63e6443e582cf0b840f82d2549eb
tom <>
2004-10-22 16:14:47 +0000

    Fixed bug [#918] news description length is wrongly checked

--- gforge/www/news/submit.php
+++ gforge/www/news/submit.php
@@ -56,7 +56,7 @@ if (session_loggedin()) {
 
 				db_begin();
 				$f=new Forum(group_get_object($sys_news_group));
-				if (!$f->create(ereg_replace('[^_\.0-9a-z-]','-', strtolower($summary)),$summary,1,'',0,0)) {
+				if (!$f->create(ereg_replace('[^_\.0-9a-z-]','-', strtolower($summary)),$details,1,'',0,0)) {
 					db_rollback();
 					exit_error('Error',$f->getErrorMessage());
 				}
