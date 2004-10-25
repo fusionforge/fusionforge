76275dfd99a635a7d50a83439990ece05254647a
Christian Bayle <cbayle@gmail.com>
2004-10-25 14:34:33 +0000

    Don't insert if the theme already exists

--- gforge/db/20041014.sql
+++ gforge/db/20041014.sql
@@ -1,2 +1,4 @@
-insert into themes (dirname,fullname,enabled) 
-	values ('ultralite','Ultra-Lite Text-only Theme',true);
+-- Insert ultralite, only if it doesn't already exists
+INSERT INTO themes (dirname,fullname,enabled) (SELECT 'ultralite','Ultra-Lite Text-only Theme',true WHERE (SELECT COUNT(*) FROM themes WHERE dirname ='ultralite' ) = 0) ;	
+UPDATE themes SET fullname = 'Ultra-Lite Text-only Theme' where dirname = 'ultralite';
+
