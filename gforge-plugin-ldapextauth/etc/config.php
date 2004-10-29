79a33767015cd83df103060d45a34623ef21ce02
Christian Bayle <cbayle@gmail.com>
2004-10-29 22:57:17 +0000

    Added AD support

--- gforge-plugin-ldapextauth/etc/config.php
+++ gforge-plugin-ldapextauth/etc/config.php
@@ -2,6 +2,8 @@
 
 $base_dn = "dc=users,dc=example,dc=com" ;
 $ldap_server = "ldap.example.com" ;
+//$ldap_kind="AD"; // Active Directory server
+//$ldap_port=389;
 
 // Local Variables:
 // mode: php
--- gforge-plugin-ldapextauth/etc/mapping.php
+++ gforge-plugin-ldapextauth/etc/mapping.php
@@ -1,6 +1,7 @@
 <?php
 
 function plugin_ldapextauth_mapping ($entry) {
+	global $Language;
 	$result = array () ;
 	
 	$realname = $entry['gecos'][0] ;
@@ -9,21 +10,29 @@ function plugin_ldapextauth_mapping ($entry) {
 	$result['firstname'] = $rnarray[0] ;
 	$result['lastname'] = $rnarray[1] ;
 	$result['email'] = $entry['uid'][0] . '@' . $GLOBALS['sys_default_domain'] ;
+	//$result['email'] = $entry['mail'][0] ; // AD
 	// You may also want to customise $result['language_id']
+	//$result['language_id']=$Language->getLanguageId();
 	// You may also want to customise $result['timezone']
+	//$result['timezone']=$GLOBALS['sys_default_timezone'];
 	// You may also want to customise $result['jabber_address']
 	// You may also want to customise $result['address']
 	// You may also want to customise $result['address2']
 	// You may also want to customise $result['phone']
+	//$result['phone'] = $entry['telephonenumber'][0]; //AD
 	// You may also want to customise $result['fax']
 	// You may also want to customise $result['title']
 	// You may also want to customise $result['ccode']
+	//$result['ccode']=$GLOBALS['sys_default_country_code'];
+	// You may also want to customise $result['themeid']
+	$result['themeid']=$GLOBALS['sys_default_theme_id'];
 	
 	return $result ;
 }
 
 function plugin_ldapextauth_getdn ($plugin, $username) {
 	return "uid=$username," . $plugin->base_dn ;
+	//return 'DOMAIN\\' . "$username" ; // AD
 }
 
 // Local Variables:
