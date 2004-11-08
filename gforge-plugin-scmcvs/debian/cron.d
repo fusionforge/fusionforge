bfd4fc808b49ba0aa25dd4576ccc9c18f4c6acb2
(no author) <>
2004-11-08 02:03:55 +0000

    This commit was manufactured by cvs2svn to create tag 'upstream_version_4_0_1'.

--- gforge-plugin-scmcvs/debian/cron.d
+++
@@ -1,7 +0,0 @@
-#
-# Regular cron jobs for the gforge-plugin-scmcvs package
-#
-
-# CVS tarballs
-5 2 * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/tarballs.sh ] && /usr/lib/gforge/plugins/scmcvs/bin/tarballs.sh generate
-5 * * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump.pl ] && su -s /bin/sh gforge -c /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump.pl && [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_update.pl ] && /usr/lib/gforge/plugins/scmcvs/bin/cvs_update.pl
