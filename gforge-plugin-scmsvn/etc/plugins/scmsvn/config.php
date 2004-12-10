a7e0056d3b0c5b23a43f2c11b0b0a896d90151b0
tperdue <>
2004-12-10 04:36:26 +0000

    massive modding of scmsvn plugin

---
+++ gforge-plugin-scmsvn/etc/plugins/scmsvn/config.php
@@ -0,0 +1,16 @@
+<?php
+ 
+//$default_svn_server = $GLOBALS['sys_default_domain'] ;
+$default_svn_server = "svn." . $GLOBALS['sys_default_domain'] ;
+$use_ssh = false;
+$use_dav = true;
+$use_ssl = true;
+$svn_root = 'svn';
+$enabled_by_default = 0 ;
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+ 
+?>
