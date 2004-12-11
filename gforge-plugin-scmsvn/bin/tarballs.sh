c1b94151fcda04907f78854f1b20a497c67cb054
Christian Bayle <cbayle@gmail.com>
2004-12-11 23:33:11 +0000

    Applied mpeltier patch

--- gforge-plugin-scmsvn/bin/tarballs.sh
+++ gforge-plugin-scmsvn/bin/tarballs.sh
@@ -1,26 +1,34 @@
 #! /bin/sh
 # 
-# $Id$
-#
-# Generate CVS repositories tarballs
-# Christian Bayle, Roland Mas, debian-sf (Sourceforge for Debian)
+# Generate SVN repositories tarballs thanks to hot-backup.py
+# Subversion tools - a simple tar.gz file of the SVN repository may
+# lead to an unusable archive.
 
 set -e
+#set -x
 
 if [  $(id -u) != 0 ] ; then
     echo "You must be root to run this, please enter passwd"
     exec su -c "$0 $1"
 fi
 
-CVSROOT=/var/lib/gforge/chroot/cvsroot
-CVSTARDIR=/var/lib/gforge/cvstarballs
+SVNROOT=/var/lib/gforge/chroot/svnroot
+SCMTARDIR=/var/lib/gforge/scmtarballs
+TMPDIR=/tmp
 
 case "$1" in
     generate)
-	cd $CVSROOT
-	ls | while read dir ; do
-	    tar czf $CVSTARDIR/${dir}-cvsroot.tar.gz.new ${dir}
-	    mv $CVSTARDIR/${dir}-cvsroot.tar.gz.new $CVSTARDIR/${dir}-cvsroot.tar.gz
+	# create tmp dir
+	work_dir=$TMPDIR/gforge-plugin-scnsvn.$$
+	mkdir -p $work_dir
+	trap "rm -rf $work_dir" ERR EXIT
+
+	cd $work_dir
+	ls $SVNROOT | while read dir ; do
+	    /usr/lib/subversion/hot-backup.py $SVNROOT/$dir . > /dev/null
+	    tar czf $dir-scmroot.tar.gz.new $dir*
+	    mv -f ${dir}-scmroot.tar.gz.new $SCMTARDIR/${dir}-scmroot.tar.gz
+	    rm -rf $dir*
 	done
 	;;
     
@@ -33,6 +41,5 @@ case "$1" in
     *)
 	echo "Usage: $0 {generate}"
 	exit 1
-	;;
-	
+	;;	
 esac
--- gforge-plugin-scmsvn/debian/cron.d
+++ gforge-plugin-scmsvn/debian/cron.d
@@ -2,7 +2,14 @@
 # Regular cron jobs for the gforge-plugin-scmsvn package
 #
 
-# SVN tarballs
-# 5 2 * * * root [ -x /usr/lib/gforge/plugins/scmsvn/bin/tarballs.sh ] && /usr/lib/gforge/plugins/scmsvn/bin/tarballs.sh generate
-5 * * * * root [ -x /usr/lib/gforge/plugins/scmsvn/bin/svn_dump_update.pl ] && /usr/lib/gforge/plugins/scmsvn/bin/svn_dump_update.pl
-45 4 * * Sun root [ -x /usr/lib/gforge/plugins/scmsvn/bin/svn-stats.pl ] && /usr/lib/gforge/plugins/scmsvn/bin/svn-stats.pl
+# Tarballs
+5 3 * * * root [ -x /usr/lib/gforge/plugins/scmsvn/bin/tarballs.sh ] && /usr/lib/gforge/plugins/scmsvn/bin/tarballs.sh generate
+
+# Snapshots
+35 3 * * * root [ -x /usr/lib/gforge/plugins/scmsvn/bin/snapshots.sh ] && /usr/lib/gforge/plugins/scmsvn/bin/snapshots.sh generate
+
+# Repositories update
+45 * * * * root [ -x /usr/lib/gforge/plugins/scmsvn/bin/svn_dump_update.pl ] && /usr/lib/gforge/plugins/scmsvn/bin/svn_dump_update.pl
+
+# Statistics
+55 4 * * Sun root [ -x /usr/lib/gforge/plugins/scmsvn/bin/svn-stats.pl ] && /usr/lib/gforge/plugins/scmsvn/bin/svn-stats.pl
