aaa04ec87c983764af539fe98db2c0abb39b07a0
Christian Bayle <cbayle@gmail.com>
2004-12-12 16:24:43 +0000

    Let's use new interface

--- gforge/www/survey/admin/index.php
+++ gforge/www/survey/admin/index.php
@@ -40,17 +40,7 @@ if (!session_loggedin() || !user_ismember($group_id,'A')) {
 
 ?>
 
-<p>
-<UL>
-<LI><a href="/survey/admin/question.php?group_id=<?php echo $group_id; ?>"><?php echo $Language->getText('survey_admin_index','add_question'); ?></a><br />
-<LI><a href="/survey/admin/survey.php?group_id=<?php echo $group_id; ?>"><?php echo $Language->getText('survey_admin_index','add_survey'); ?></a><br />
-<LI><a href="/survey/admin/show_results.php?group_id=<?php echo $group_id; ?>"><?php echo $Language->getText('survey_admin_index','show_results'); ?></a><br />
-</UL>
-</p>
 <?php echo $Language->getText('survey_admin_index','its_simple_to_create', array('<p><strong>/survey/survey.php?group_id='.$group_id.'&survey_id=XX</strong>')); ?>
-</li>
-</ol>
-</p>
 
 <p><?php echo $Language->getText('survey_admin_index','you_can_now_activate',array('<a href="/survey/admin/survey.php?group_id='.$group_id.'">','</a>')); ?>
 
