aea52494bb373206e89f4cf06a1c496c16ac693c
Christian Bayle <cbayle@gmail.com>
2004-12-12 00:26:38 +0000

    Added mpeltier patch

--- gforge-plugin-scmcvs/bin/tarballs.sh
+++ gforge-plugin-scmcvs/bin/tarballs.sh
@@ -6,6 +6,7 @@
 # Christian Bayle, Roland Mas, debian-sf (Sourceforge for Debian)
 
 set -e
+#set -x 
 
 if [  $(id -u) != 0 ] ; then
     echo "You must be root to run this, please enter passwd"
@@ -13,14 +14,14 @@ if [  $(id -u) != 0 ] ; then
 fi
 
 CVSROOT=/var/lib/gforge/chroot/cvsroot
-CVSTARDIR=/var/lib/gforge/scmtarballs
+SCMTARDIR=/var/lib/gforge/scmtarballs
 
 case "$1" in
     generate)
 	cd $CVSROOT
 	ls | while read dir ; do
-	    tar czf $CVSTARDIR/${dir}-scmroot.tar.gz.new ${dir}
-	    mv $CVSTARDIR/${dir}-scmroot.tar.gz.new $CVSTARDIR/${dir}-scmroot.tar.gz
+	    tar czf $SCMTARDIR/${dir}-scmroot.tar.gz.new ${dir}
+	    mv $SCMTARDIR/${dir}-scmroot.tar.gz.new $SCMTARDIR/${dir}-scmroot.tar.gz
 	done
 	;;
     
@@ -34,5 +35,4 @@ case "$1" in
 	echo "Usage: $0 {generate}"
 	exit 1
 	;;
-	
 esac
--- gforge-plugin-scmcvs/debian/cron.d
+++ gforge-plugin-scmcvs/debian/cron.d
@@ -2,6 +2,14 @@
 # Regular cron jobs for the gforge-plugin-scmcvs package
 #
 
-# CVS tarballs
+# Tarballs
 5 2 * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/tarballs.sh ] && /usr/lib/gforge/plugins/scmcvs/bin/tarballs.sh generate
+
+# Snapshots
+35 2 * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/snapshots.sh ] && /usr/lib/gforge/plugins/scmcvs/bin/snapshots.sh generate
+
+# Repositories update
 5 * * * * root [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump.pl ] && su -s /bin/sh gforge -c /usr/lib/gforge/plugins/scmcvs/bin/cvs_dump.pl && [ -x /usr/lib/gforge/plugins/scmcvs/bin/cvs_update.pl ] && /usr/lib/gforge/plugins/scmcvs/bin/cvs_update.pl
+
+# Statistics
+45 4 * * Sun root [ -x /usr/lib/gforge/plugins/scmsvn/bin/svn-stats.pl ] && /usr/lib/gforge/plugins/scmsvn/bin/svn-stats.pl
