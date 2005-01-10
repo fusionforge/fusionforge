816b0ea9a5e7e98a8501bae8365b02f4f7976277
(no author) <>
2005-01-10 14:35:50 +0000

    This commit was manufactured by cvs2svn to create tag 'debian_version_3_1-25'.

--- gforge/deb-specific/install-chroot.sh
+++ gforge/deb-specific/install-chroot.sh
@@ -47,12 +47,13 @@ case "$1" in
 	# TODO: remove unneeded stuff from that list
 	for binary in \
 	    /usr/sbin/sshd \
+	    /lib/libnss_ldap.so.* \
 	    /usr/bin/cvs \
 	    /bin/ls \
 	    /bin/sh \
 	    /bin/bash \
 	    /bin/chgrp ; do
-	  if [ -x "$binary" ] ; then
+	  if [ -x "$binary" -o -L "$binary" ] ; then
 	      echo "$binary"
 	      ldd $binary | cut -d" " -f3
 	  fi
@@ -67,7 +68,7 @@ case "$1" in
 	# For /dev/log
 	if ! grep -q "^SYSLOGD.*/var/lib/gforge/chroot/dev/log.*" /etc/init.d/sysklogd ; then 
 		echo '######################################################################################################'
-		echo 'WARNING: you must have SYSLOGD="-p /dev/log -p /var/lib/gforge/chroot/dev/log" in /etc/init.d/sysklogd'
+		echo 'WARNING: you must have SYSLOGD="-p /dev/log -a /var/lib/gforge/chroot/dev/log" in /etc/init.d/sysklogd'
 		echo 'To have cvs pserver running correctly'
 		echo '######################################################################################################'
 	fi
--- gforge/deb-specific/ssh_dump_update.pl
+++ gforge/deb-specific/ssh_dump_update.pl
@@ -13,6 +13,8 @@ my $ssh_array = ();
 
 &db_connect;
 
+$dbh->{AutoCommit} = 0;
+
 # Dump the Table information
 $query = "SELECT user_name,unix_uid,authorized_keys FROM users WHERE authorized_keys != '' AND status !='D'";
 $c = $dbh->prepare($query);
@@ -22,16 +24,10 @@ while(my ($username, $unix_uid, $ssh_key) = $c->fetchrow()) {
 	push @ssh_array, $new_list;
 }
 
-# Now write out the files
-write_array_file($file_dir."dumps/ssh_dump", @ssh_array);
-
-my @ssh_key_file = open_array_file($file_dir."dumps/ssh_dump");
 my ($username, $ssh_keys, $ssh_dir);
 
 if($verbose){print("\n\n	Processing Users\n\n")};
-while ($ln = pop(@ssh_key_file)) {
-	chop($ln);
-
+while ($ln = pop(@ssh_array)) {
 	($username, $uid, $ssh_key) = split(":", $ln);
 
 	$ssh_key =~ s/\#\#\#/\n/g;
@@ -44,6 +40,7 @@ while ($ln = pop(@ssh_key_file)) {
 
 	if (! -d $ssh_dir) {
 		mkdir $ssh_dir, 0755;
+		system("chown $uid:$uid $ssh_dir");
 	}
 
 	if($verbose){print("Writing authorized_keys for $username: ")};
@@ -57,4 +54,36 @@ while ($ln = pop(@ssh_key_file)) {
 	if($verbose){print ("Done\n")};
 
 	undef @user_authorized_keys;
+}
+undef @ssh_array;
+
+### Phase 2: remove the files when needed
+
+# Dump the Table information
+$query = "SELECT user_name,unix_uid FROM users WHERE authorized_keys = '' OR authorized_keys IS NULL OR status = 'D'";
+$c = $dbh->prepare($query);
+$c->execute();
+while(my ($username, $unix_uid, $ssh_key) = $c->fetchrow()) {
+	$new_list = "$username:$unix_uid\n";
+	push @ssh_array, $new_list;
+}
+
+if($verbose){print("\n\n	Processing Users\n\n")};
+while ($ln = pop(@ssh_array)) {
+	($username, $uid) = split(":", $ln);
+
+	$username =~ tr/[A-Z]/[a-z]/;
+	$uid += $uid_add;
+
+	$ssh_dir = "$homedir_prefix$username/.ssh";
+
+	if (-d $ssh_dir) {
+	    if($verbose){print("Resetting authorized_keys for $username: ")};
+	    
+	    unlink("$ssh_dir/authorized_keys");
+	    system("chown $uid:$uid $homedir_prefix$username");
+	    system("chown $uid:$uid $ssh_dir");
+
+	    if($verbose){print ("Done\n")};
+	}
 }
