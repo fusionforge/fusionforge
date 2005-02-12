96cf117cc9e9b70176a46ae07adc65f4625a48e9
Christian Bayle <cbayle@gmail.com>
2005-02-12 19:35:46 +0000

    Test if apache2 is running to choose wether restarting apache of apache2

--- gforge/deb-specific/create-vhosts.sh
+++ gforge/deb-specific/create-vhosts.sh
@@ -8,8 +8,16 @@ su -s /bin/sh gforge -c /usr/lib/gforge/bin/prepare-vhosts-file.pl
 		/var/lib/gforge/etc/templates/httpd.vhosts \
 		/var/lib/gforge/etc/httpd.vhosts \
 		/etc/gforge/gforge.conf
-if [ -x /usr/sbin/apache ]; then
-    /usr/sbin/invoke-rc.d apache reload > /dev/null 2>&1
+
+test -f /etc/default/apache2 && . /etc/default/apache2
+if [ "$NO_START" != "0" ]; then
+	if [ -x /usr/sbin/apache ]; then
+    		/usr/sbin/invoke-rc.d apache reload > /dev/null 2>&1
+	fi
+else
+	if [ -x /usr/sbin/apache2 ]; then
+    		/usr/sbin/invoke-rc.d apache2 reload > /dev/null 2>&1
+	fi
 fi
 if [ -x /usr/sbin/apache-ssl ]; then
     /usr/sbin/invoke-rc.d apache-ssl reload > /dev/null 2>&1
