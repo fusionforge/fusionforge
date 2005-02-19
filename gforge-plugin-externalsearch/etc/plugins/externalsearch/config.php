8ac0b0c7ef3afd2f717d78c51616e5bc2057e35f
Guillaume Smet <guillaume.smet@gmail.com>
2005-02-19 13:04:45 +0000

    added the gforge-plugin-externalsearch to the repository

---
+++ gforge-plugin-externalsearch/etc/plugins/externalsearch/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$externalSearchEngines = array(
+	'Google' => 'http://www.google.com/search?as_sitesearch='.$GLOBALS['sys_default_domain'].'&as_q=',
+	'AllTheWeb' => 'http://alltheweb.com/search?advanced=1&dincl='.$GLOBALS['sys_default_domain'].'&q='
+);
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+ 
+?>
---
+++ gforge-plugin-externalsearch/include/externalsearch-init.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * External search plugin
+ *
+ * Copyright 2004 (c) Guillaume Smet
+ *
+ * http://gforge.org
+ *
+ * @version $Id$
+ */
+
+require_once('ExternalSearchPlugin.class') ;
+
+define(SEARCH__TYPE_IS_EXTERNAL, 'external');
+
+$externalSearchPluginObject = new ExternalSearchPlugin();
+
+register_plugin($externalSearchPluginObject) ;
+
+// End:
+
+?>
