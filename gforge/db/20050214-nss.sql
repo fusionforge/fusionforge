86b55929efd00c7241ff933df5c512efdff5be4f
Christian Bayle <cbayle@gmail.com>
2005-02-26 14:42:43 +0000

    Clean obsolete comments used during conception

--- gforge/db/20050214-nss.sql
+++ gforge/db/20050214-nss.sql
@@ -1,59 +1,10 @@
 -- 
 ALTER TABLE users  ADD COLUMN unix_gid INTEGER;
- ALTER TABLE users  ALTER COLUMN unix_gid SET DEFAULT 0;
----ALTER TABLE users  ADD COLUMN register_purpose TEXT;
----ALTER TABLE users  ADD COLUMN register_time INTEGER;
----ALTER TABLE users  ALTER COLUMN register_time SET DEFAULT 0;
--- ALTER TABLE users  RENAME COLUMN user_name TO unix_user_name;
--- Should be done to have the same field name convention in users and groups
----ALTER TABLE groups ADD COLUMN unix_gid INTEGER;
----ALTER TABLE groups ALTER COLUMN unix_gid SET DEFAULT 0;
----ALTER TABLE groups ADD COLUMN unix_status CHARACTER(1);
----ALTER TABLE groups ALTER COLUMN unix_status SET DEFAULT 'A';
----ALTER TABLE groups ADD COLUMN add_date INTEGER;
----ALTER TABLE groups ALTER COLUMN add_date SET DEFAULT 0;
+ALTER TABLE users  ALTER COLUMN unix_gid SET DEFAULT 0;
 --
 UPDATE users SET unix_uid=unix_uid + 20000 WHERE unix_uid!=0;
 UPDATE users SET unix_uid=user_id  + 20000 WHERE unix_uid=0;
 UPDATE users SET unix_gid=unix_uid;
--- Prepare TABLE for user registration
----UPDATE users SET register_time=add_date;
---
----UPDATE groups SET unix_gid=group_id + 10000;
----UPDATE groups SET add_date=register_time;
----UPDATE groups SET unix_status='A';
---
--- List users an groups
---
---SELECT unix_group_name, unix_gid, status, unix_status FROM groups;
---SELECT unix_uid, unix_gid, user_name, unix_pw, realname, shell, status, unix_status FROM users;
---
--- Insert scm_<project_account> in the users table
--- These users are used by CVS pserver
--- We can get rid of these users changing lock dir place and making it world writeable.
---DELETE FROM users WHERE user_name LIKE 'scm_%';
---DELETE FROM users WHERE user_name LIKE 'anoncvs_%';
---INSERT INTO users ( unix_uid, unix_gid, user_name, unix_pw, realname, shell, status, unix_status) 
---        SELECT 
---		unix_gid+40000 ,
---		unix_gid ,
---		'scm_' || unix_group_name,
---		CHAR(1) 'x',
---		unix_group_name,
---		'/bin/false',
---		'N',
---		'A'
---	FROM groups
---	WHERE unix_status='A'
---	AND status='A'
---	AND enable_anonscm = 1;
---	
--- Dummy user
---
---DELETE FROM users WHERE user_name='dummy';
---INSERT INTO users ( unix_uid, unix_gid, user_name, unix_pw, realname, shell, status, unix_status)
---	VALUES (9999,9999,'dummy','x','Dummy User','/bin/false','N','A');
---
 --
 -- Passwd view
 --
@@ -90,56 +41,23 @@ CREATE TABLE nss_groups AS
 	SELECT 0 AS user_id, group_id,unix_group_name AS name, group_id + 10000 AS gid
 	FROM groups;
 --
--- Insert users group ids in group table
+-- Insert users group ids in nss_group table
 -- 
---DELETE FROM groups WHERE unix_group_name IN (SELECT user_name FROM users);
---INSERT INTO groups (unix_group_name, unix_gid, status, unix_status)
---	SELECT
---		user_name AS name,
---		unix_gid AS gid,
---		'N',
---		'A'
---	FROM users
---	WHERE unix_status='A'
---	AND status = 'A';
 INSERT INTO nss_groups (user_id,group_id,name, gid)
 	SELECT user_id,0,user_name, unix_gid
 	FROM users
 	WHERE unix_status='A'
 	AND status = 'A';
 --
--- Insert scm group ids in group table
+-- Insert scm group ids in nss_group table
 --
---DELETE FROM groups WHERE unix_group_name LIKE 'scm_%';
---INSERT INTO groups (unix_group_name, unix_gid, status, unix_status)
---	SELECT 
---		'scm_' || unix_group_name,
---		group_id + 50000,
---		'N',
---		'A'
---	FROM groups
---	WHERE unix_status='A'
---	AND status = 'A'
---	AND enable_anonscm = 1;
 INSERT INTO nss_groups (user_id,group_id,name, gid)
 	SELECT 0,group_id,'scm_' || unix_group_name, group_id + 50000
 	FROM groups
 	WHERE status = 'A'
 	AND enable_anonscm = 1;
 --
--- Groups view
---
---DROP VIEW nss_groups;
---CREATE VIEW nss_groups AS
---	SELECT
---		unix_group_name AS name,
---		CHAR(1) 'x' AS passwd,
---		unix_gid AS gid,
---		status
---	FROM groups
---	WHERE unix_status='A';
---
--- User_Group view
+-- User_Group Table
 --
 DROP VIEW nss_usergroups ;
 CREATE TABLE nss_usergroups AS (
