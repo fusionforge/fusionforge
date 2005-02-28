8c9065d86ef2931c3f8bb056c5c876e00ad0e855
Christian Bayle <cbayle@gmail.com>
2005-02-28 15:51:35 +0000

    Deal with 2 new files

--- gforge/deb-specific/db-upgrade.pl
+++ gforge/deb-specific/db-upgrade.pl
@@ -1980,6 +1980,46 @@ END;
         $dbh->commit () ;
     }
 
+    $version = &get_db_version ;
+    $target = "4.1-0" ;
+    if (&is_lesser ($version, $target)) {
+        &debug ("Upgrading with 20050224.sql") ;
+
+        @reqlist = @{ &parse_sql_file ("/usr/lib/gforge/db/20050224.sql") } ;
+        foreach my $s (@reqlist) {
+            $query = $s ;
+            # debug $query ;
+            $sth = $dbh->prepare ($query) ;
+            $sth->execute () ;
+            $sth->finish () ;
+        }
+        @reqlist = () ;
+
+        &update_db_version ($target) ;
+        &debug ("Committing.") ;
+        $dbh->commit () ;
+    }
+
+    $version = &get_db_version ;
+    $target = "4.1-1" ;
+    if (&is_lesser ($version, $target)) {
+        &debug ("Upgrading with 20050225-nsssetup.sql") ;
+
+        @reqlist = @{ &parse_sql_file ("/usr/lib/gforge/db/20050225-nsssetup.sql") } ;
+        foreach my $s (@reqlist) {
+            $query = $s ;
+            # debug $query ;
+            $sth = $dbh->prepare ($query) ;
+            $sth->execute () ;
+            $sth->finish () ;
+        }
+        @reqlist = () ;
+
+        &update_db_version ($target) ;
+        &debug ("Committing.") ;
+        $dbh->commit () ;
+    }
+
     &debug ("It seems your database $action went well and smoothly.  That's cool.") ;
     &debug ("Please enjoy using Gforge.") ;
 
