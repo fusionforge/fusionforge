18f6b1109be3380f73af1a8e45dad53947585b35
Guillaume Smet <guillaume.smet@gmail.com>
2005-03-03 20:10:22 +0000

    s/refresh/gforge-config

--- gforge/rpm-specific/scripts/refresh.sh
+++
@@ -1,3 +0,0 @@
-cd /usr/share/gforge && ./setup -confdir /etc/gforge/ -input /etc/gforge/gforge.conf -noapache && cd -
-
-echo 'If you have modified the database configuration, you have to restart apache (service httpd restart)'
