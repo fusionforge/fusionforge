1bdd53dcf8e6c8d1fd5032273fd57497089dc57d
Guillaume Smet <guillaume.smet@gmail.com>
2005-03-11 16:06:56 +0000

    really bad day...

--- gforge/db/20050311.sql
+++ gforge/db/20050311.sql
@@ -6,8 +6,8 @@ CREATE RULE artifact_delete_agg AS
 		WHERE (artifact_counts_agg.group_artifact_id = old.group_artifact_id);
 		
 CREATE RULE projecttask_delete_agg AS
-	ON INSERT TO project_task DO
+	ON DELETE TO project_task DO
 	UPDATE project_counts_agg SET
-		count = (project_counts_agg.count + 1),
-		open_count = (CASE WHEN old.status_id=1 THEN project_counts_agg.open_count + 1 ELSE project_counts_agg.open_count END)
+		count = (project_counts_agg.count - 1),
+		open_count = (CASE WHEN old.status_id=1 THEN project_counts_agg.open_count - 1 ELSE project_counts_agg.open_count END)
 		WHERE (project_counts_agg.group_project_id = old.group_project_id);
