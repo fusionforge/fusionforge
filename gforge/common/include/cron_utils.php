466763e501a5acf3d7f9fcdc3abb8d2e2e00922d
Guillaume Smet <guillaume.smet@gmail.com>
2005-03-28 16:51:02 +0000

    added chroot utils

--- gforge/common/include/cron_utils.php
+++ gforge/common/include/cron_utils.php
@@ -3,6 +3,7 @@
 //
 //	This key id# is important - do not change or renumber
 //
+$cron_arr = array();
 $cron_arr[0]='unused';
 $cron_arr[1]='calculate_user_metric.php';
 $cron_arr[2]='check_stale_tracker_items.php';
@@ -31,6 +32,37 @@ function cron_entry($job,$output) {
 	$sql="INSERT INTO cron_history (rundate,job,output) 
 		values ('".time()."','$job','$output')";
 	return db_query($sql);
+}
+
+function cron_debug($string) {
+	global $verbose;
+	if($verbose) {
+		echo $string."\n";
+	}
+}
+
+function checkChroot() {
+	global $sys_chroot;
+	if(isset($sys_chroot) && !empty($sys_chroot) && is_dir($sys_chroot)) {
+		return true;
+	}
+	return false;
+}
+
+function chrootPath($path) {
+	global $sys_chroot;
+	if(checkChroot()) {
+		$path = $sys_chroot.$path;
+	}
+	return $path;
+}
+
+function chrootCommand($command) {
+	global $sys_chroot;
+	if(checkChroot()) {
+		$command = 'chroot '.$sys_chroot.' '.$command;
+	}
+	return $command;
 }
 
 ?>
