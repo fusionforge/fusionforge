238037d026afe618373ff8d377d6cd3d54248f76
mpeltier <>
2005-04-19 10:31:24 +0000

    Modified configuration for ViewCVS PHP wrapper (this script needs to be run now only when the main viewcvs config changes)

--- gforge-plugin-scmsvn/bin/install-viewcvs.sh
+++ gforge-plugin-scmsvn/bin/install-viewcvs.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+# Create ViewCVS config for GForge from the own ViewCVS conf file.
+# Should be launched each time you change the ViewCVS conf is modified.
+
+server_admin=$(grep ^server_admin= /etc/gforge/gforge.conf | cut -d= -f2-)
+gforge_chroot=$(grep ^gforge_chroot= /etc/gforge/gforge.conf | cut -d= -f2-)
+svndir=$(grep ^svndir= /etc/gforge/gforge.conf | cut -d= -f2-)
+cvsdir=$(grep ^cvsdir= /etc/gforge/gforge.conf | cut -d= -f2-)
+
 python <<EOF
 import sys
 import os
@@ -8,17 +16,31 @@ import ConfigParser
 r = ConfigParser.RawConfigParser ()
 r.read ("/etc/viewcvs/viewcvs.conf")
 r.set ("general","svn_roots","")
-l = os.listdir ("/var/lib/gforge/chroot/svnroot")
-l2 = []
-for d in l:
-  l2.append (d + ": svn://localhost/" + d)
-r.set ("general","svn_roots",string.join (l2, ', '))
 r.set ("general","cvs_roots","")
-if len(l) == 0:
-  sys.exit("No svn repository found. I dunno what to set for default_root")
-r.set ("general","default_root", l [0])
+r.set ("general","root_parents","$gforge_chroot/$svndir : svn")
+# uncomment the next line if you want to use ViewCVS to browse CVS repositories
+#r.set ("general","root_parents","$gforge_chroot/$svndir : svn, $gforge_chroot/$cvsdir : cvs")
+r.set ("general","address","<a href=\"mailto:$server_admin\">CVS/SVN Admin</a>");
+r.set ("general","default_root", "")
+r.set ("templates","query","/etc/gforge/plugins/scmsvn/viewcvs/templates/query.ezt")
+r.set ("templates","diff","/etc/gforge/plugins/scmsvn/viewcvs/templates/diff.ezt")
+r.set ("templates","graph","/etc/gforge/plugins/scmsvn/viewcvs/templates/graph.ezt")
+r.set ("templates","annotate","/etc/gforge/plugins/scmsvn/viewcvs/templates/annotate.ezt")
+r.set ("templates","markup","/etc/gforge/plugins/scmsvn/viewcvs/templates/markup.ezt")
+r.set ("templates","revision","/etc/gforge/plugins/scmsvn/viewcvs/templates/revision.ezt")
+r.set ("templates","query_form","/etc/gforge/plugins/scmsvn/viewcvs/templates/query_form.ezt")
+r.set ("templates","query_results","/etc/gforge/plugins/scmsvn/viewcvs/templates/query_results.ezt")
+r.set ("templates","error","/etc/gforge/plugins/scmsvn/viewcvs/templates/error.ezt")
+r.set ("templates","directory","/etc/gforge/plugins/scmsvn/viewcvs/templates/directory.ezt")
+r.set ("templates","log","/etc/gforge/plugins/scmsvn/viewcvs/templates/log_table.ezt")
+r.set ("options", "generate_etags", 0)
+r.set ("options","docroot","/plugins/scmsvn/viewcvs");
+r.set ("options","icons","/plugins/scmsvn/viewcvs/icons");
 r.write (open ("/var/lib/gforge/etc/viewcvs.conf", "w"))
 EOF
 
 sed 's,pathname = CONF_PATHNAME .*,pathname = "/var/lib/gforge/etc/viewcvs.conf",' \
     /usr/lib/python2.3/site-packages/viewcvs/viewcvs.py > /var/lib/gforge/etc/viewcvs.py
+
+echo "Created ViewCVS conf for GForge - make sure to rerun $0 \
+each time you change the ViewCVS conf"
