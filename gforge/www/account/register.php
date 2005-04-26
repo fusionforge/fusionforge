7d39e3680034634c5186561134523473641fc0a8
mpeltier <>
2005-04-26 18:32:24 +0000

    Applied [#1196] www/account/register.php xhtml 1.0 compliant patch by Vicente J. Ruiz Jurado

--- gforge/www/account/register.php
+++ gforge/www/account/register.php
@@ -57,11 +57,10 @@ if ($submit) {
 	}
 }
 
-
 $HTML->header(array('title'=>'User Account Registration'));
 
 if ($feedback) {
-	print "<p><FONT color=#FF0000>$feedback $register_error</FONT>";
+	print "<p><font color=\"#FF0000\">$feedback $register_error</font>";
 } 
 if (!isset($timezone) || empty($timezone)) {
 	$timezone = (isset($sys_default_timezone) ? $sys_default_timezone : 'GMT');
@@ -74,70 +73,89 @@ if (!isset($ccode) || empty($ccode)) {
 <form action="<?php echo $PHP_SELF; ?>" method="post">
 <p>
 <?php echo $Language->getText('account_register','loginname'); echo utils_requiredField(); ?><br />
-<input type="text" name="unix_name" value="<?php print($unix_name); ?>">
+<input type="text" name="unix_name" value="<?php print($unix_name); ?>" />
+</p>
 <p>
 <?php echo $Language->getText('account_register','password'); echo utils_requiredField(); ?><br />
-<input type="password" name="password1">
+<input type="password" name="password1" />
+</p>
 <p>
 <?php echo $Language->getText('account_register','password2'); echo utils_requiredField(); ?><br />
-<input type="password" name="password2">
-<p>
+<input type="password" name="password2" />
+</p
+><p>
 <?php echo $Language->getText('account_options', 'title2'); ?><br />
-<input type="text" name="title" value="<?php echo $title; ?>" size="10">
+<input type="text" name="title" value="<?php echo $title; ?>" size="10" />
+</p>
 <p>
 <?php echo $Language->getText('account_register','firstname'); echo utils_requiredField(); ?><br />
-<input size=30 type="text" name="firstname" value="<?php print($firstname); ?>">
+<input size="30" type="text" name="firstname" value="<?php print($firstname); ?>" />
+</p>
 <p>
 <?php echo $Language->getText('account_register','lastname'); echo utils_requiredField(); ?><br />
-<input size=30 type="text" name="lastname" value="<?php print($lastname); ?>">
+<input size="30" type="text" name="lastname" value="<?php print($lastname); ?>" />
+</p>
 <p>
 <?php echo $Language->getText('account_register','language'); ?><br />
 <?php echo html_get_language_popup ($Language,'language_id',$Language->getLanguageId()); ?>
+</p>
 <p>
 <?php echo $Language->getText('account_register','timezone'); ?><br />
 <?php echo html_get_timezone_popup('timezone', $timezone); ?>
+</p>
 <p>
 <?php echo $Language->getText('account_register','theme'); ?><br />
 <?php echo html_get_theme_popup('theme_id', $theme_id); ?>
+</p>
 <p>
 <?php echo $Language->getText('account_register','ccode'); ?><br />
 <?php echo html_get_ccode_popup('ccode', $ccode); ?>
+</p>
 <p>
 @<?php echo $Language->getText('account_register','emailaddr', $GLOBALS['sys_users_host']); ?>
-<br /><input size=30 type="text" name="email" value="<?php print($email); ?>">
+<br /><input size="30" type="text" name="email" value="<?php print($email); ?>" />
+</p>
 <p>
 <?php
 if ($sys_use_jabber) {
 	echo $Language->getText('account_register','jabberaddr').'<br />
-	<input size=30 type="text" name="jabber_address" value="'. $jabber_address .'"><br />
-    <input type="checkbox" name="jabber_only" value="1">
-    '.$Language->getText('account_register','jabberonly').'.';
+	<input size="30" type="text" name="jabber_address" value="'. $jabber_address .'" /><br />
+	<input type="checkbox" name="jabber_only" value="1" />
+	'.$Language->getText('account_register','jabberonly').'.';
 }
 ?>
+</p>
 <p>
 <?php echo $Language->getText('account_options', 'address'); ?><br />
-<input type="text" name="address" value="<?php echo $address; ?>" size="80">
+<input type="text" name="address" value="<?php echo $address; ?>" size="80" />
+</p>
 <p>
 <?php echo $Language->getText('account_options', 'address'); ?><br />
-<input type="text" name="address2" value="<?php echo $address2; ?>" size="80">
+<input type="text" name="address2" value="<?php echo $address2; ?>" size="80" />
+</p>
 <p>
 <?php echo $Language->getText('account_options', 'phone'); ?><br />
-<input type="text" name="phone" value="<?php echo $phone; ?>" size="20">
+<input type="text" name="phone" value="<?php echo $phone; ?>" size="20" />
+</p>
 <p>
 <?php echo $Language->getText('account_options', 'fax'); ?><br />
-<input type="text" name="fax" value="<?php echo $fax; ?>" size="20">
+<input type="text" name="fax" value="<?php echo $fax; ?>" size="20" />
+</p>
 <p>
-<input type="checkbox" name="mail_site" value="1" checked>
+<input type="checkbox" name="mail_site" value="1" checked="checked" />
 <?php echo $Language->getText('account_register','siteupdate'); ?>
+</p>
 <p>
-<input type="checkbox" name="mail_va" value="1">
+<input type="checkbox" name="mail_va" value="1" />
 <?php echo $Language->getText('account_register','communitymail'); ?>
+</p>
 <p>
 <?php echo $Language->getText('account_register','mandatory', utils_requiredField()); ?>
 </p>
 <p>
-<input type="submit" name="submit" value="<?php echo $Language->getText('account_register','register'); ?>">
+<input type="submit" name="submit" value="<?php echo $Language->getText('account_register','register'); ?>" />
+</p>
 </form>
-<p><a href="pending-resend.php"><?php echo $Language->getText('account_register','resend_pending'); ?></a>
+<p><a href="pending-resend.php"><?php echo $Language->getText('account_register','resend_pending'); ?></a></p>
 
 <?php $HTML->footer(array()); ?>
