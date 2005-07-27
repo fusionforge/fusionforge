93773e66aec5abb888725f1797fcd9a1e9599d1a
tperdue <>
2005-07-27 14:51:03 +0000

    setting default vhost to cvs.* for cvs

--- gforge-plugin-scmcvs/etc/plugins/scmcvs/config.php
+++ gforge-plugin-scmcvs/etc/plugins/scmcvs/config.php
@@ -1,7 +1,7 @@
 <?php
  
-$default_cvs_server = $GLOBALS['sys_cvs_host'] ;
-// $default_cvs_server = "cvs." . $GLOBALS['sys_default_domain'] ;
+//$default_cvs_server = $GLOBALS['sys_cvs_host'] ;
+$default_cvs_server = "cvs." . $GLOBALS['sys_default_domain'] ;
 $enabled_by_default = 1 ;
 
 // Local Variables:
