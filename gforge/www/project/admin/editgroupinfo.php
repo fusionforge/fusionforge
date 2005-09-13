23e033d47cc6b6cb230fbb8982357388e8d83d89
danper <>
2005-09-13 19:34:32 +0000

    additional plugins for group (plugin_hook) wasn�t updating correctly

--- gforge/www/project/admin/editgroupinfo.php
+++ gforge/www/project/admin/editgroupinfo.php
@@ -61,6 +61,7 @@ if (getStringFromRequest('submit')) {
 	$use_stats = getStringFromRequest('use_stats');
 	$new_doc_address = getStringFromRequest('new_doc_address');
 	$send_all_docs = getStringFromRequest('send_all_docs');
+	$use_wikiplugin = getStringFromRequest('use_wikiplugin');
   
 	$res = $group->update(
 		session_get_user(),
