2e9acfc6d16aa69357586a2c07a15613cc7e6a34
danper <>
2005-10-25 16:03:25 +0000

    removed local.inc require

--- gforge-plugin-cvstracker/etc/plugins/cvstracker/config.php
+++ gforge-plugin-cvstracker/etc/plugins/cvstracker/config.php
@@ -1,5 +1,7 @@
 <?php
 
+$sys_default_domain = "trialgforge.org";
+$sys_plugins_path = "/opt/gforge/gforge/plugins/";
 $sys_cvsroot_path = $cvsdir_prefix;
 $cvs_binary_version = "1.12";
 $cvs_tracker_debug = false;
