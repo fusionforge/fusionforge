6ca26b5581f6315f09dd4c11b7db87423e7e14ad
danper <>
2005-10-26 12:56:53 +0000

    added configuration variable to support SSL requests

--- gforge-plugin-cvstracker/bin/post.php
+++ gforge-plugin-cvstracker/bin/post.php
@@ -167,7 +167,7 @@ if ( $cvs_binary_version == "1.12" ) {
 // Our POSTer in Gforge
 $snoopy = new Snoopy;
 
-if (strtoupper(getStringFromServer('HTTPS')) == 'ON') {
+if (strtoupper($use_ssl) == 'YES') {
 	$http = "https://";
 } else {
 	$http = "http://";
