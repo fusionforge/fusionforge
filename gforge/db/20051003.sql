daa8a6c05d447dcc138d68e96650a84afd6d0365
tperdue <>
2005-10-29 12:38:33 +0000

    commenting out this conversion script - users will have to manually determine if they need to run it

--- gforge/db/20051003.sql
+++ gforge/db/20051003.sql
@@ -1,5 +1,5 @@
-ALTER TABLE plugin_cvstracker_data_master DROP COLUMN cvs_date;
-ALTER TABLE plugin_cvstracker_data_master ADD COLUMN cvs_date int;
-UPDATE plugin_cvstracker_data_master SET cvs_date=extract(epoch from now());
-ALTER TABLE plugin_cvstracker_data_master ALTER COLUMN cvs_date SET NOT NULL;
+--ALTER TABLE plugin_cvstracker_data_master DROP COLUMN cvs_date;
+--ALTER TABLE plugin_cvstracker_data_master ADD COLUMN cvs_date int;
+--UPDATE plugin_cvstracker_data_master SET cvs_date=extract(epoch from now());
+--ALTER TABLE plugin_cvstracker_data_master ALTER COLUMN cvs_date SET NOT NULL;
 
