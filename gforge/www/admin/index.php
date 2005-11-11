4a50c57f752ca6cd5f5b5ba2c3b726a84a7a5129
danper <>
2005-11-11 19:55:33 +0000

    added plugin manager for admin site

--- gforge/www/admin/index.php
+++ gforge/www/admin/index.php
@@ -159,6 +159,7 @@ $abc_array = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','
 	<li><a href="/admin/languages/loadtabfiles.php"><?php echo $Language->getText('admin_index','translation_file_tool'); ?></a></li>
 	<li><a href="/stats/lastlogins.php"><?php echo $Language->getText('admin_index','recent_logins'); ?></a></li>
 	<li><a href="cronman.php"><?php echo $Language->getText('admin_index','cronman'); ?></a></li>
+	<li><a href="pluginman.php"><?php echo $Language->getText('admin_index','pluginman'); ?></a></li>
 	<?php plugin_hook("site_admin_option_hook", false); ?>
 </ul>
 
