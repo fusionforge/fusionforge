86e9413dc2ce89cc1ac76509390436811c6be5a6
(no author) <>
2005-12-09 17:22:45 +0000

    This commit was manufactured by cvs2svn to create branch 'gforgegroup'.

--- gforge/debian/gforge-ldap-openldap.cron.d
+++
@@ -1,6 +0,0 @@
-#
-# Regular cron jobs for the gforge-shell-ldap package
-#
-
-# LDAP update
-30 * * * * root [ -x /usr/lib/gforge/bin/install-ldap.sh ] && /usr/lib/gforge/bin/install-ldap.sh update > /dev/null 2>&1
--- gforge/utils/decode_images.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-for u in `find ../www -name '*.uu'` ; do f=${u%.uu} ; uudecode $u -o $f ; done
-
-
