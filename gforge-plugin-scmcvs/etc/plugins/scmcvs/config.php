1d4fc6cfaeb56c4b21182676a343c607665d45b3
tperdue <>
2005-12-19 00:38:48 +0000

    default domains should gforge.company.com

--- gforge-plugin-scmcvs/etc/plugins/scmcvs/config.php
+++ gforge-plugin-scmcvs/etc/plugins/scmcvs/config.php
@@ -3,10 +3,10 @@
 $default_cvs_server = "cvs." . $GLOBALS['sys_default_domain'] ;
 $enabled_by_default = 1 ;
 $cvs_binary_version='1.12';
-$use_ssl=true;
+$use_ssl=false;
 $GLOBALS['cvs_binary_version']=$cvs_binary_version;
 $sys_plugins_path='/opt/gforge/gforge/plugins';
-$sys_default_domain='gforge.org';
+$sys_default_domain='gforge.company.com';
 
 // Local Variables:
 // mode: php
