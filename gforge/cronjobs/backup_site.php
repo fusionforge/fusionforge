2809d1361899c9d942fc56125f78fa624485cb43
marcelo <>
2006-02-10 17:01:25 +0000

    Backup script now uses hot-backup.py script (if available) to backup SVN repositories

--- gforge/cronjobs/backup_site.php
+++ gforge/cronjobs/backup_site.php
@@ -76,7 +76,7 @@ if($retval!=0){
  **************************************/ 
 $output="";
 if (file_exists($sys_upload_dir)) {
-	@exec('tar -jcvf '.$sys_path_to_backup.'uploads-tmp-'.$datetime.'.tar.bz2 '.$sys_upload_dir.' 2>&1' ,$output,$retval);   //proceed upload dir tar file creation
+	@exec('tar -hjcvf '.$sys_path_to_backup.'uploads-tmp-'.$datetime.'.tar.bz2 '.$sys_upload_dir.' 2>&1' ,$output,$retval);   //proceed upload dir tar file creation
 	if($retval!=0){
 		$err.= implode("\n", $output);
 	}
@@ -104,7 +104,7 @@ if (file_exists($mailman_data_dir)) {
  **************************************/ 
 $output="";
 if (file_exists($cvsdir_prefix)) {
-	@exec('tar -jcvf '.$sys_path_to_backup.'cvsroot-tmp-'.$datetime.'.tar.bz2 '.$cvsdir_prefix.'/ 2>&1' ,$output,$retval);   //proceed cvsroot dir tar file creation
+	@exec('tar -hjcvf '.$sys_path_to_backup.'cvsroot-tmp-'.$datetime.'.tar.bz2 '.$cvsdir_prefix.'/ 2>&1' ,$output,$retval);   //proceed cvsroot dir tar file creation
 	if($retval!=0){
 		$err.= implode("\n", $output);
 	}
@@ -113,13 +113,42 @@ if (file_exists($cvsdir_prefix)) {
 }
 
 /**************************************
- * Backup SVN repositories
+ * Backup SVN repositories (using the hot-backup.py script)
  **************************************/ 
-$output="";
 if (file_exists($svndir_prefix)) {
-	@exec('tar -jcvf '.$sys_path_to_backup.'svnroot-tmp-'.$datetime.'.tar.bz2 '.$svndir_prefix.'/ 2>&1' ,$output,$retval);   //proceed svnroot dir tar file creation
-	if($retval!=0){
-		$err.= implode("\n", $output);
+	$hot_backup = dirname(__FILE__).'/hot-backup.py';
+	$svn_path = dirname(`which svn`);
+	$output="";	
+	
+	if (!file_exists($hot_backup) || !$svn_path) {
+		// hot-backup.py script not available, try regular backup (doesn't check for inconsistencies)
+		@exec('tar -hjcvf '.$sys_path_to_backup.'svnroot-tmp-'.$datetime.'.tar.bz2 '.$svndir_prefix.'/ 2>&1' ,$output,$retval);   //proceed svnroot dir tar file creation
+		if($retval!=0){
+			$err.= implode("\n", $output);
+		}
+	} else {
+		// backup the files using hot-backup script
+		$repos_backup_dir = $sys_path_to_backup.'/svn-repositories-'.$datetime.'/';
+		mkdir($repos_backup_dir);
+		$dh = opendir($svndir_prefix);
+		while ($file = readdir($dh)) {
+			if (preg_match('/^\\./', $file)) continue;		// skip files that start with a dot
+			$path = $svndir_prefix.'/'.$file;
+			if (!is_dir($path)) continue;		// not a repository
+			$cmd = 'SVN_PATH="'.$svn_path.'" '.$hot_backup.' '.$path.' '.$repos_backup_dir;
+			@exec($cmd, $output, $retval);
+			if($retval!=0){
+				$err.= implode("\n", $output);
+			}
+
+		}
+		
+		@exec('tar -hjcvf '.$sys_path_to_backup.'svnroot-tmp-'.$datetime.'.tar.bz2 '.$repos_backup_dir.' 2>&1' ,$output,$retval);
+		if($retval!=0){
+			$err.= implode("\n", $output);
+		}
+		
+		@exec("rm -rf ".$repos_backup_dir);
 	}
 } else {
 	$err.= 'Unable to find SVNROOT Dir. Value on local.inc is:'.$svndir_prefix;
