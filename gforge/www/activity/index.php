479e82cd9ac22d913612e9548f4bba0eaa583d67
tperdue <>
2006-02-16 23:10:28 +0000

    adding filter to activity

--- gforge/www/activity/index.php
+++ gforge/www/activity/index.php
@@ -18,6 +18,8 @@ require_once('pre.php');
 $group_id = getIntFromRequest("group_id");
 $begin = getStringFromRequest("start_date");
 $end = getStringFromRequest("end_date");
+$show=getArrayFromRequest("show");
+
 if (!$begin || $begin==0) {
 	$begin = (time()-(30*86400));
 } else {
@@ -43,8 +45,32 @@ if (!$group || !is_object($group)) {
 
 site_project_header(array('title'=>$Language->getText('projectactivity','headertype'),'group'=>$group_id,'toptab'=>'activity'));
 
+$ids=array();
+$ids[]='commit';
+$ids[]='trackeropen';
+$ids[]='trackerclose';
+$ids[]='frsrelease';
+$ids[]='forumpost';
+
+$texts=array();
+$texts[]='Commits';
+$texts[]='Tracker Opened';
+$texts[]='Tracker Closed';
+$texts[]='FRS Release';
+$texts[]='Forum Post';
+
+if (count($show) < 1) {
+	$show=$ids;
+}
+foreach ($show as $showthis) {
+	if (array_search($showthis,$ids) === false) {
+		exit_error('Error','Invalid Data Passed to query');
+	}
+}
+$multiselect=html_build_multiple_select_box_from_arrays($ids,$texts,'show[]',$show,5,false);
+
 $sql="SELECT * FROM activity_vw WHERE activity_date BETWEEN '".$begin."' AND '".$end."'
-	AND group_id='$group_id' ORDER BY activity_date DESC";
+	AND group_id='$group_id' AND section IN ('".implode("','",$show)."') ORDER BY activity_date DESC";
 //echo $sql;
 $res=db_query($sql);
 echo db_error();
@@ -56,18 +82,21 @@ if ($rows<1) {
 
 	?>
 <br />
-<table border="0" cellspacing="0">
+<table border="0" cellspacing="0" cellpadding="3" bgcolor="#E0E0E0">
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 <input type="hidden" name="group_id" value="<?php echo $group_id; ?>">
-<tr><td>
-<strong><?php echo $Language->getText('projectactivity','startdate') ?></strong><br />
-<input name="start_date" value="<?php echo date('Y-m-d',$begin); ?>" size="10" maxlength="10" />
-</td><td>
-<strong><?php echo $Language->getText('projectactivity','enddate') ?></strong><br />
-<input name="end_date" value="<?php echo date('Y-m-d',$end); ?>" size="10" maxlength="10" />
-</td><td>
-<input type="submit" name="submit" value="Submit">
-</tr></td>
+<tr>
+	<td><strong><?php echo $Language->getText('projectactivity','headeractivity') ?></strong></td>
+	<td><strong><?php echo $Language->getText('projectactivity','startdate') ?></strong></td>
+	<td><strong><?php echo $Language->getText('projectactivity','enddate') ?></strong></td>
+	<td></td>
+</tr>
+<tr>
+	<td><?php echo $multiselect; ?></td>
+	<td valign="top"><input name="start_date" value="<?php echo date('Y-m-d',$begin); ?>" size="10" maxlength="10" /></td>
+	<td valign="top"><input name="end_date" value="<?php echo date('Y-m-d',$end); ?>" size="10" maxlength="10" /></td>
+	<td valign="top"><input type="submit" name="submit" value="Submit"></td>
+</tr>
 </form>
 </table>
 <br />
@@ -107,6 +136,11 @@ if ($rows<1) {
 			case 'frsrelease': {
 				$icon=html_image("ic/cvs16b.png","20","20",array("border"=>"0","ALT"=>"SCM"));
 				$url='<a href="/frs/?release_id='.$arr['subref_id'].'&amp;group_id='.$arr['group_id'].'">FRS Release [#'.$arr['description'].']</a>';
+				break;
+			}
+			case 'forumpost': {
+				$icon=html_image("ic/forum20g.png","20","20",array("border"=>"0","ALT"=>"Forum"));
+				$url='<a href="/forum/?msg_id='.$arr['subref_id'].'&amp;group_id='.$arr['group_id'].'">Forum Post [#'.$arr['subref_id'].']'.$arr['description'].'</a>';
 				break;
 			}
 		}
