4fa0a76224a8ca110500e88a0d3fc21393b12730
tperdue <>
2006-02-26 22:20:17 +0000

    making my page use tabs

--- gforge/www/my/index.php
+++ gforge/www/my/index.php
@@ -61,25 +61,33 @@ if (!session_loggedin()) { // || $sf_user_hash) {
 	echo site_user_header(array('title'=>$Language->getText('my','title',user_getname())));
 	?>
 
-	<p>
-    <?php echo $Language->getText('my', 'about_blurb'); ?>
-	</p>
-	<table width="100%" border="0">
-	<tr><td valign="top" width="50%">
+<script type="text/javascript" src="/dojo/dojo.js"></script>
+<script type="text/javascript">
+    dojo.require("dojo.widget.TabPane");
+    dojo.require("dojo.widget.LinkPane");
+    dojo.require("dojo.widget.ContentPane");
+</script>
+<div id="mainTabPane" dojoType="TabPane" style="width: 100%; height: 40em;" selectedTab="assignedartifactstab">
+<div dojoType="ContentPane" label="<?php echo $Language->getText('my','assignedartifacts'); ?>" id="assignedartifactstab">
 	<?php
 	/*
 		Artifacts
 	*/
 	$last_group=0;
-	echo $HTML->boxTop($Language->getText('my', 'assigneditems'));
+	$order_name_arr=array();
+	$order_name_arr[]=$Language->getText('tracker','id');
+	$order_name_arr[]=$Language->getText('tracker','priority');
+	$order_name_arr[]=$Language->getText('tracker','summary');
+	echo $HTML->listTableTop($order_name_arr);
+
 	$artifactsForUser = new ArtifactsForUser(session_get_user());
 	$assignedArtifacts =& $artifactsForUser->getAssignedArtifactsByGroup();
 	if (count($assignedArtifacts) > 0) {
+		$i=0;
 		foreach($assignedArtifacts as $art) {
-			echo '</td></tr>';
 			if ($art->ArtifactType->getID() != $last_group) {
 				echo '
-				<tr><td colspan="2"><strong><a href="/tracker/?group_id='.
+				<tr><td colspan="3" bgcolor="#FFFFFF"><strong><a href="/tracker/?group_id='.
 				$art->ArtifactType->Group->getID().'&atid='.
 				$art->ArtifactType->getID().'">'.
 				$art->ArtifactType->Group->getPublicName().' - '.
@@ -87,54 +95,126 @@ if (!session_loggedin()) { // || $sf_user_hash) {
 
 			}
 			echo '
-			<tr style="background-color:'.html_get_priority_color($art->getPriority()).'">
+			<tr '. $HTML->boxGetAltRowStyle($i++) .'>
 			<td width="10%">'.$art->getID().'</td>
+			<td width="10%" style="background-color:'.html_get_priority_color($art->getPriority()).'">'.$art->getPriority().'</td>
 			<td><a href="/tracker/?func=detail&amp;aid='.
 			$art->getID().
 			'&amp;group_id='.$art->ArtifactType->Group->getID().
 			'&amp;atid='.$art->ArtifactType->getID().'">' . $art->getSummary() . '</a>';
+			echo '</td></tr>';
 
 			$last_group = $art->ArtifactType->getID();
 		}
 	} else {
 		echo '
-			<strong>'.$Language->getText('my', 'no_tracker_items_assigned').'</strong>';
+			<tr><td colspan="3" bgcolor="#FFFFFF">'.$Language->getText('my', 'no_tracker_items_assigned').'</td></tr>';
 	}
+	echo $HTML->listTableBottom();
+?>
+</div>
+<div dojoType="ContentPane" label="<?php echo $Language->getText('my','assignedtasks'); ?>" id="assignedtasksstab">
+<?php
+	/*
+		Tasks assigned to me
+	*/
+	$last_group=0;
+	$order_name_arr=array();
+	$order_name_arr[]=$Language->getText('tracker','id');
+	$order_name_arr[]=$Language->getText('tracker','priority');
+	$order_name_arr[]=$Language->getText('tracker','summary');
+	echo $HTML->listTableTop($order_name_arr);
+	$projectTasksForUser = new ProjectTasksForUser(session_get_user());
+	$userTasks =& $projectTasksForUser->getTasksByGroupProjectName();
 
+	if (count($userTasks) > 0) {
+		$i=0;
+		foreach ($userTasks as $task) {
+			/* Deduce summary style */
+			$style_begin='';
+			$style_end='';
+			if ($task->getPercentComplete()==100) {
+				$style_begin='<span style="text-decoration:underline">';
+				$style_end='</span>';
+			}
+			//if ($task->getProjectGroup()->getID() != $last_group) {
+			$projectGroup =& $task->getProjectGroup();
+			$group =& $projectGroup->getGroup();
+			if ($projectGroup->getID() != $last_group) {
+				echo '
+				<tr><td colspan="3" bgcolor="#FFFFFF"><strong><a href="/pm/task.php?group_id='.
+				$group->getID().
+				'&amp;group_project_id='.
+				$projectGroup->getID().'">'.
+				$group->getPublicName().' - '.
+				$projectGroup->getName().'</a></strong></td></tr>';
+			}
+			echo '
+			<tr '. $HTML->boxGetAltRowStyle($i++) .'>
+			<td width="10%">'.$task->getID().'</td>
+			<td width="10%" style="background-color:'.html_get_priority_color($task->getPriority()).'">'.$task->getPriority().'</td>
+			<td><a href="/pm/task.php?func=detailtask&amp;project_task_id='.
+			$task->getID().
+			'&amp;group_id='.$group->getID().
+			'&amp;group_project_id='.$projectGroup->getID().'">'.$style_begin.$task->getSummary().$style_end.'</a>';
+			echo '</td></tr>';
+
+			$last_group = $projectGroup->getID();
+		}
+	} else {
+		echo '
+		<tr><td colspan="3" bgcolor="#FFFFFF">'.$Language->getText('my', 'no_open_tasks').'</td></tr>';
+		echo db_error();
+	}
+	echo $HTML->listTableBottom();
+?>
+</div>
+<div dojoType="ContentPane" label="<?php echo $Language->getText('my','submittedartifacts'); ?>" id="submittedartifactstab">
+<?php
 	$last_group="0";
-	echo $HTML->boxMiddle($Language->getText('my', 'submitteditems'),false,false);
+	$order_name_arr=array();
+	$order_name_arr[]=$Language->getText('tracker','id');
+	$order_name_arr[]=$Language->getText('tracker','priority');
+	$order_name_arr[]=$Language->getText('tracker','summary');
+	echo $HTML->listTableTop($order_name_arr);
 	$submittedArtifacts =& $artifactsForUser->getSubmittedArtifactsByGroup();
 	if (count($submittedArtifacts) > 0) {
+		$i=0;
 		foreach ($submittedArtifacts as $art) {
-			echo '</td></tr>';
 			if ($art->ArtifactType->getID() != $last_group) {
 				echo '
-				<tr><td colspan="2"><strong><a href="/tracker/?group_id='.
+				<tr><td colspan="3" bgcolor="#FFFFFF"><strong><a href="/tracker/?group_id='.
 				$art->ArtifactType->Group->getID().'&atid='.
 				$art->ArtifactType->getID().'">'.
 				$art->ArtifactType->Group->getPublicName().' - '.
 				$art->ArtifactType->getName().'</a></strong></td></tr>';
 			}
 			echo '
-			<tr style="background-color:'.html_get_priority_color($art->getPriority()).'">
+			<tr '. $HTML->boxGetAltRowStyle($i++) .'>
 			<td width="10%">'.$art->getID().'</td>
+			<td width="10%" style="background-color:'.html_get_priority_color($art->getPriority()).'">'.$art->getPriority().'</td>
 			<td><a href="/tracker/?func=detail&amp;aid='.
       $art->getID().
       '&amp;group_id='.$art->ArtifactType->Group->getID().
       '&amp;atid='.$art->ArtifactType->getID().'">' . $art->getSummary() .'</a>';
+			echo '</td></tr>';
 
 			$last_group = $art->ArtifactType->getID();
 		}
 	} else {
 		echo '
-		<strong>'.$Language->getText('my', 'no_tracker_items_submitted').'</strong>';
+		<tr><td colspan="3" bgcolor="#FFFFFF">'.$Language->getText('my', 'no_tracker_items_submitted').'</td></tr>';
 	}
-
+	echo $HTML->listTableBottom();
+?>
+</div>
+<div dojoType="ContentPane" label="<?php echo $Language->getText('mytab','monitoring'); ?>" id="monitortab">
+<?php
 	/*
 		Forums that are actively monitored
 	*/
 	$last_group=0;
-	echo $HTML->boxMiddle($Language->getText('my', 'monitoredforum'),false,false);
+	echo $HTML->boxTop($Language->getText('my', 'monitoredforum'),false,false);
 	$forumsForUser = new ForumsForUser(session_get_user());
 	$forums =& $forumsForUser->getMonitoredForums();
 	if (count($forums) < 1) {
@@ -200,139 +280,13 @@ if (!session_loggedin()) { // || $sf_user_hash) {
 	}
 
 	echo $HTML->boxBottom();
-
-//second column of "my" page
-
-	?>
-	</td><td valign="top" width="50%">
-	<?php
-	/*
-		Tasks assigned to me
-	*/
-	$last_group=0;
-	echo $HTML->boxTop($Language->getText('my', 'tasks'));
-	//echo "<a href=. onclick=\"window.open('/pm/ganttofuser.php')\"><strong>".$Language->getText('pm_include_grouphtml','gantt_chart')."</strong></a>";
-	$projectTasksForUser = new ProjectTasksForUser(session_get_user());
-	$userTasks =& $projectTasksForUser->getTasksByGroupProjectName();
-
-	if (count($userTasks) > 0) {
-		foreach ($userTasks as $task) {
-			echo '</td></tr>';
-			/* Deduce summary style */
-			$style_begin='';
-			$style_end='';
-			if ($task->getPercentComplete()==100) {
-				$style_begin='<span style="text-decoration:underline">';
-				$style_end='</span>';
-			}
-			//if ($task->getProjectGroup()->getID() != $last_group) {
-			$projectGroup =& $task->getProjectGroup();
-			$group =& $projectGroup->getGroup();
-			if ($projectGroup->getID() != $last_group) {
-				echo '
-				<tr><td colspan="2"><strong><a href="/pm/task.php?group_id='.
-				$group->getID().
-				'&amp;group_project_id='.
-				$projectGroup->getID().'">'.
-				$group->getPublicName().' - '.
-				$projectGroup->getName().'</a></strong></td></tr>';
-			}
-			echo '
-			<tr style="background-color:'.html_get_priority_color($task->getPriority()).'">
-			<td width="10%">'.$task->getID().'</td>
-			<td><a href="/pm/task.php?func=detailtask&amp;project_task_id='.
-			$task->getID().
-			'&amp;group_id='.$group->getID().
-			'&amp;group_project_id='.$projectGroup->getID().'">'.$style_begin.$task->getSummary().$style_end.'</a>';
-
-			$last_group = $projectGroup->getID();
-		}
-	} else {
-		echo '
-		<strong>'.$Language->getText('my', 'no_open_tasks').'</strong>';
-		echo db_error();
-	}
-
-
-	/*
-		DEVELOPER SURVEYS
-
-		This needs to be updated manually to display any given survey
-	*/
-	if($sys_use_survey) {
-		$sql="SELECT * from survey_responses ".
-			"WHERE survey_id='1' AND user_id='".user_getid()."' AND group_id='1'";
-	
-		$result=db_query($sql);
-	
-		echo $HTML->boxMiddle($Language->getText('my', 'survey'),false,false);
-	
-		if (db_numrows($result) < 1) {
-			show_survey(1,1);
-		} else {
-			echo '
-			<strong>'.$Language->getText('my','survey_taken').'</strong>';
-		}
-	}
-	/*
-	 * Pending projects and news bytes
-	 */
-	$admingroup = group_get_object (1) ;
-	if (!$admingroup || !is_object($admingroup)) {
-		//don't have perm to see this
-	} elseif ($admingroup->isError()) {
-		//don't have perm to see this
-	} else {
-		$perm =& $admingroup->getPermission( session_get_user() );
-		if ($perm && is_object($perm) && $perm->isAdmin()) {
-			$sql="SELECT group_name FROM groups where status='P';";
-			$result=db_query($sql);
-			$rows=db_numrows($result);
-			if ($rows) {
-				echo $HTML->boxMiddle($Language->getText('my','pending_projects'), false, false);
-
-				if ($rows==1){
-					echo $Language->getText('my','pending_projects_1');
-				} else {
-					echo $Language->getText('my','pending_projects_2',$rows);
-				}
-			
-				echo " <a href=\"/admin/approve-pending.php\">";
-				echo $Language->getText('my','pending_projects_3');
-				echo "</a>.";
-			}
-		}
-	}
-	$newsgroup = group_get_object ($GLOBALS['sys_news_group']) ;
-	if (!$newsgroup || !is_object($newsgroup)) {
-		//don't have perm to see this
-	} elseif ($newsgroup->isError()) {
-		//don't have perm to see this
-	} else {
-		$perm =& $newsgroup->getPermission( session_get_user() );
-		if ($perm && is_object($perm) && $perm->isAdmin()) {
-			$sql="SELECT * FROM news_bytes nb, groups g WHERE nb.is_approved=0 and nb.group_id = g.group_id and g.status = 'A'";
-			$result=db_query($sql);
-			$rows=db_numrows($result);
-			if ($rows) {
-				echo $HTML->boxMiddle($Language->getText('my','pending_news_bytes'), false, false);
-
-				if ($rows==1){
-					echo $Language->getText('my','pending_news_bytes_1');
-				} else{
-					echo $Language->getText('my','pending_news_bytes_2',$rows);
-				}
-
-				echo " <a href=\"/news/admin/?group_id=".$GLOBALS['sys_news_group']."\">";
-
-				echo $Language->getText('my','pending_news_bytes_3');
-				echo "</a>.";
-			}
-		}
-	}
+?>
+</div>
+<div dojoType="ContentPane" label="<?php echo $Language->getText('mytab','project'); ?>" id="projectstab">
+<?php
 	/*
 		   Personal bookmarks
-	*/
+	* /
 	echo $HTML->boxMiddle($Language->getText('my', 'bookmarks'),false,false);
 
 	$result = db_query("SELECT bookmark_url, bookmark_title, bookmark_id from user_bookmarks where ".
@@ -358,7 +312,7 @@ if (!session_loggedin()) { // || $sf_user_hash) {
 		PROJECT LIST
 	*/
 
-	echo $HTML->boxMiddle($Language->getText('my', 'projects'),false,false);
+	echo $HTML->boxTop($Language->getText('my', 'projects'),false,false);
 	// Include both groups and foundries; developers should be similarly
 	// aware of membership in either.
 	$result = db_query("SELECT groups.group_name,"
@@ -400,22 +354,10 @@ if (!session_loggedin()) { // || $sf_user_hash) {
 		}
 	}
 	echo $HTML->boxBottom();
-
-	echo '</td></tr>
-
-
-	<!--  Bottom Row   -->
-
-
-	<tr><td colspan="2">';
-
-	echo show_priority_colors_key();
-
-	echo '
-	</td></tr>
-
-	</table>';
-
+?>
+</div>
+</div>
+<?php
 	echo site_user_footer(array());
 
 }
