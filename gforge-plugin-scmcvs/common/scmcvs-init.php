f8446a3baf83a5285da55895d7c4c316a801206a
lo-lan-do <>
2006-06-06 19:37:00 +0000

    Using explicit include path.

--- gforge-plugin-scmcvs/common/scmcvs-init.php
+++ gforge-plugin-scmcvs/common/scmcvs-init.php
@@ -23,7 +23,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-require_once ('CVSPlugin.class') ;
+require_once ($GLOBALS['sys_plugins_path'].'/scmsvn/common/CVSPlugin.class') ;
 
 $CVSPluginObject = new CVSPlugin ;
 
