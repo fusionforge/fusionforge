20dd5cad93061d9affded9bb83a8b477fdb25371
julien <>
2006-10-26 07:37:29 +0000

    This is the project hierarchy plugin. When activated, projet administrators are able to create hierarchical or navigation links to other projects. The hierarchy tree has been revamped in order to display a dynamic view of the hierarchy.

---
+++ gforge/plugins/projects_hierarchy/db/project_hierarchy-init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE plugin_projects_hierarchy (
+	project_id integer DEFAULT 0 NOT NULL,
+	sub_project_id integer DEFAULT 0 NOT NULL,
+	link_type char(4) DEFAULT 'navi' NOT NULL,
+	activated boolean DEFAULT false NOT NULL
+	);
+
+INSERT INTO plugins (plugin_name,plugin_desc) VALUES ('hierarchy','Hierarchy GForge Plugin');
