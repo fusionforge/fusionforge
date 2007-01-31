56373ee09b8150ae34aafdc5ed556fdac3267e89
Christian Bayle <cbayle@gmail.com>
2007-01-31 23:28:08 +0000

    Sync Branch_4_6dev with last changes on trunk rev 5907

--- gforge/cronjobs/mail/privatize_list.py
+++ gforge/cronjobs/mail/privatize_list.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python
 
-def privatize_list(mlist):
-        mlist.advertised=0
-        mlist.subscribe_policy=3
-        mlist.member_posting_only=1
-        mlist.private_roster=1
-        mlist.archive_private=1
-        mlist.Save()
+mlist.advertised=0
+mlist.subscribe_policy=3
+mlist.member_posting_only=1
+mlist.private_roster=1
+mlist.archive_private=1
+mlist.Save()
 
