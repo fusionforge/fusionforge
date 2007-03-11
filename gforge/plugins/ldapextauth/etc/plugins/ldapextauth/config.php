96f918d89d620a327d048ec5a3bb85a6312f2e02
Christian Bayle <cbayle@gmail.com>
2007-03-11 23:54:55 +0000

    Remove some warning about undefined var

--- gforge/plugins/ldapextauth/etc/plugins/ldapextauth/config.php
+++ gforge/plugins/ldapextauth/etc/plugins/ldapextauth/config.php
@@ -1,9 +1,14 @@
 <?php
 
+// The three following are to remove warning, they should not be necessary, only for compatibility reason
+$sys_ldap_dn = "";
+$sys_ldap_server = "";
+$sys_ldap_port = "";
 $base_dn = "dc=users,dc=example,dc=com" ;
 $ldap_server = "ldap.example.com" ;
+$ldap_kind=""; // Std Directory server
 //$ldap_kind="AD"; // Active Directory server
-//$ldap_port=389;
+$ldap_port=389;
 
 // Local Variables:
 // mode: php
