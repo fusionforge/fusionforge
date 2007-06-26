764d84b59c2f253bcc606975e56a25dc5a0b492d
Christian Bayle <cbayle@gmail.com>
2007-06-26 21:32:31 +0000

    html cleanup and reformating

--- gforge/plugins/webcalendar/www/includes/index.php
+++ gforge/plugins/webcalendar/www/includes/index.php
@@ -1,4 +1,6 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?php 
+echo '<?xml version="1.0" encoding="utf-8"?>';
+?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "DTD/xhtml1-transitional.dtd">
