458cbc5c2d2dde8d7c4fc4112d7374f1f24043e8
ruben <>
2007-09-07 13:38:29 +0000

    [#3088] Fixed the daily_task_email cron (trunk)

--- gforge/cronjobs/daily_task_email.php
+++ gforge/cronjobs/daily_task_email.php
@@ -25,10 +25,23 @@
  */
 
 require_once('squal_pre.php');
-require_once('common/pm/ProjectTasksForUser.class.php');
+require_once('common/pm/ProjectTasksForUser.class');
 require_once('common/include/cron_utils.php');
 
-$err='';
+//
+//  Set up this script to run as the site admin
+//
+$res = db_query("SELECT user_id FROM user_group WHERE admin_flags='A' AND group_id='1'");
+
+if (!$res) {
+    echo db_error();
+    exit(1);
+}
+if (db_numrows($res) == 0) {
+    exit(0);
+}
+$id=db_result($res,0,0);
+session_set_new($id);
 
 // Get user id's from users who have open tasks
 $res = db_query("SELECT DISTINCT u.user_id, u.realname, u.email FROM users u, project_assigned_to pat, project_task_vw ptv 
@@ -50,29 +63,44 @@ for ($i=0; $i<db_numrows($res);$i++) {
 	if (!$user_object || !is_object($user_object)) {
 		$err .= "Could not get User object with ID: $user_id\n";
 	} else {
-
 		$projectTasksForUser = new ProjectTasksForUser($user_object);
+
 		if (!$projectTasksForUser || !is_object($projectTasksForUser)) {
 			$err .= "Could not get ProjectTasksForUser object for user with ID: $user_id\n";
 			continue;
 		}
+
 		// get the tasks the user should work on, today
 		$userTasks =& $projectTasksForUser->getTasksForToday();
+		$last_group = 0;
+		$last_projectgroup = 0;
 
 		// start composing the email	
 		$subject = 'Tasks for '.$realname.' for '.$today;
+
 		if (count($userTasks) > 0) {
+			$valid_tasks = 0;
+			$debug_info = "************ DEBUG FOR USER: $user_id ************\n";
 			ob_start();
 			// get the data of every task and compose the email
 			foreach ($userTasks as $task) {
-	
+				if ($task->getPercentComplete() == 100) {
+					$debug_info .= "Task ID: ".$task->getID()." complete (100%)\n------------\n";
+					continue;
+				}
+				$debug_info .= 'Task ID: ' . $task->getID() . "\n";
+
 				$end_date = date("n/j/y", $task->getEndDate());
-	
+
 				$projectGroup =& $task->getProjectGroup();
 				if ($projectGroup && is_object($projectGroup)) {
 
+					$debug_info .= 'Project Group ID: ' . $projectGroup->getID() . "\n";
+
 					$group =& $projectGroup->getGroup();
 					if ($group && is_object($group)) {
+
+						$debug_info .= 'Group ID: ' . $group->getID() . "\n";
 
 						if ($group->getID() != $last_group) {
 							echo $group->getPublicName().":\n";
@@ -87,16 +115,22 @@ for ($i=0; $i<db_numrows($res);$i++) {
 						echo 'http://'.$sys_default_domain.'/pm/task.php?func=detailtask&project_task_id='.
 						$task->getID().'&group_id='.$group->getID().'&group_project_id='.$projectGroup->getID();
 						echo "\n\n";
-				
+
 						$last_group = $group->getID();
 						$last_projectgroup = $projectGroup->getID();
+						$valid_tasks++;
 					}
 				}
+				$debug_info .= "------------\n";
 			}
 			$messagebody = ob_get_contents();
 			ob_end_clean();
-			util_send_message($email, $subject, $messagebody);
-//			echo "\n\n\n***************************************************\nTo: $email\nSubject: $subject\n\n$messagebody";
+			if ($valid_tasks > 0) {
+				util_send_message($email, $subject, $messagebody);
+			}
+			/* else {
+				echo $debug_info."\n";
+			}*/
 		}
 	}
 }
