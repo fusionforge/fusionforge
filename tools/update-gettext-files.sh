c4e839796379e9734416ed31c4cd0d7865d2a822
lo-lan-do <>
2007-11-07 14:45:56 +0000

    Updated gettext-generating script

--- tools/update-gettext-files.sh
+++ tools/update-gettext-files.sh
@@ -5,7 +5,12 @@ locales="eu bg ca zh_TW nl en eo fr de el he id it ja ko la nb pl pt_BR pt ru zh
 # xgettext -j -d gforge -o translations/gforge.pot -L PHP --from-code=iso-8859-1 $(find -name \*.php -or -name \*.class | grep -v -e {arch} -e svn-base)
 
 rm translations/gforge.pot
-xgettext -d gforge -o translations/gforge.pot -L PHP --from-code=iso-8859-1 $(find -name \*.php -or -name \*.class | grep -v -e {arch} -e svn-base)
+
+find -name \*.php \
+    | grep -v -e {arch} -e svn-base \
+    | grep -v ^./plugins/wiki \
+    | LANG=C sort \
+    | xargs xgettext -d gforge -o translations/gforge.pot -L PHP --from-code=iso-8859-1    
 
 for l in $locales ; do
     echo "Processing $l..."
