3ffc6fa6b9b1af26610f2ce12fb5fd9db95c576e
marcelo <>
2008-01-30 16:13:57 +0000

    Fixed usergroup script [#3652] (branch 4.6)

--- gforge/plugins/scmcvs/cronjobs/usergroup.php
+++ gforge/plugins/scmcvs/cronjobs/usergroup.php
@@ -65,12 +65,20 @@ $user_unix_gids =& util_result_column_to_array($res,'unix_gid');
 $user_pws =& util_result_column_to_array($res,'unix_pw');
 
 // Create the entries for the GForge users
-$gforge_lines = array();
+$gforge_lines_passwd = array();
+$gforge_lines_shadow = array();
+$gforge_lines_groups = array();
+
+// These will be the entries that already exist
+$unmanaged_lines_passwd = array();
+$unmanaged_lines_shadow = array();
+$unmanaged_lines_group = array();
 
 // user description is something like "MyGForge user"
 $user_description = preg_replace('/[^[:alnum:] _-]/', '', $sys_name);
 $user_description .= " user";
 
+/*
 for ($i=0; $i < count($gforge_users); $i++) {
 	$username = $gforge_users[$i];
 	$user_unix_uid = $user_unix_uids[$i];
@@ -82,10 +90,8 @@ for ($i=0; $i < count($gforge_users); $i++) {
 					$user_description.":/home/".$username.":".$shell;
 	$line_shadow = $username.":".$unix_passwd.":12090:0:99999:7:::";
 	
-	$gforge_lines_passwd[] = $line_passwd;
-	$gforge_lines_shadow[] = $line_shadow;
-	
 }
+*/
 
 /*************************************************************************
  * Step 1: Process /etc/passwd
@@ -93,12 +99,9 @@ for ($i=0; $i < count($gforge_users); $i++) {
 
 // Read the passwd file line by line
 $passwd_orig = file("/etc/passwd", "r");
-
-// Now write the file with the gforge users at the end
-$passwd = fopen("/etc/passwd".FILE_EXTENSION, "w");
-$unmanaged_etc = array();
 for ($i=0; $i < count($passwd_orig); $i++) {
 	$line = trim($passwd_orig[$i]);
+
 	// Skip the GForge users (will be written later)
 	if (preg_match("/^[[:blank:]]*#GFORGEBEGIN/", $line)) {
 		do {
@@ -106,7 +109,8 @@ for ($i=0; $i < count($passwd_orig); $i++) {
 			$line = trim($passwd_orig[$i]);
 		} while ($i < count($passwd_orig) && !preg_match("/^[[:blank:]]*#GFORGEEND/", $line));
 		
-		// Got to end of file (shouldn't happen, means #GFORGEEND wasn't found on file
+		// Got to end of file (shouldn't happen, means #GFORGEEND wasn't found on file, but
+		// it's not a fatal error
 		if ($i >= (count($passwd_orig)-1)) break;
 		
 		// read next line
@@ -114,95 +118,99 @@ for ($i=0; $i < count($passwd_orig); $i++) {
 		$line = trim($passwd_orig[$i]);
 	}
 	
+	// Here, we're outside the #GFORGE markers	
 	$entries = explode(":", $line);
 	if (!empty($entries[0])) {
 		$username = $entries[0];
-		$unmanaged_etc[] = $username;
-		fwrite($passwd, $line."\n");
-	} else {
-		// blank line or comment
-		fwrite($passwd, $line."\n");
+		$unmanaged_usernames[] = $username;
+	}
+	
+	$unmanaged_lines_passwd[] = $line;
+}
+
+// Now, check which of the GForge users were found outside the #GFORGE markers. In that
+// case, the user must not be written inside the markers (means the user is managed by
+// the sysadmin)
+for ($i=0; $i < count($gforge_users); $i++) {
+	$username = $gforge_users[$i];
+	$managed_by_gforge = !in_array($username, $unmanaged_usernames);
+
+	if ($managed_by_gforge) {
+		$user_unix_uid = $user_unix_uids[$i];
+		$user_unix_gid = $user_unix_gids[$i];
+		$shell = DEFAULT_SHELL;
+		$unix_passwd = $user_pws[$i];
+		
+		$line_passwd =	$username.":x:".$user_unix_uid.":".$user_unix_gid.":".
+						$user_description.":/home/".$username.":".$shell;
+		$gforge_lines_passwd[] = $line_passwd;
 	}
 }
+
+// Generate the contents of /etc/passwd
+$passwd_contents = implode("\n", $unmanaged_lines_passwd);
+$passwd_contents .= "\n";
+$passwd_contents .= "#GFORGEBEGIN\n";
+$passwd_contents .= implode("\n", $gforge_lines_passwd);
+$passwd_contents .= "\n#GFORGEEND\n";
 
 /*************************************************************************
  * Step 2: Process /etc/shadow
  *************************************************************************/
 
 // Read the shadow file line by line
-$passwd_orig = file("/etc/shadow", "r");
+$shadow_orig = file("/etc/shadow", "r");
+for ($i=0; $i < count($shadow_orig); $i++) {
+	$line = trim($shadow_orig[$i]);
 
-// Now write the file with the gforge users at the end
-$shadow = fopen("/etc/shadow".FILE_EXTENSION, "w");
-$unmanaged_shadow = array();
-for ($i=0; $i < count($passwd_orig); $i++) {
-	$line = trim($passwd_orig[$i]);
 	// Skip the GForge users (will be written later)
 	if (preg_match("/^[[:blank:]]*#GFORGEBEGIN/", $line)) {
 		do {
 			$i++;
-			$line = trim($passwd_orig[$i]);
-		} while ($i < count($passwd_orig) && !preg_match("/^[[:blank:]]*#GFORGEEND/", $line));
+			$line = trim($shadow_orig[$i]);
+		} while ($i < count($shadow_orig) && !preg_match("/^[[:blank:]]*#GFORGEEND/", $line));
 		
-		// Got to end of file (shouldn't happen, means #GFORGEEND wasn't found on file
-		if ($i >= (count($passwd_orig)-1)) break;
+		// Got to end of file (shouldn't happen, means #GFORGEEND wasn't found on file, but
+		// it's not a fatal error
+		if ($i >= (count($shadow_orig)-1)) break;
 		
 		// read next line
 		$i++;
-		$line = trim($passwd_orig[$i]);
+		$line = trim($shadow_orig[$i]);
 	}
 	
+	// Here, we're outside the #GFORGE markers	
 	$entries = explode(":", $line);
 	if (!empty($entries[0])) {
 		$username = $entries[0];
-		$unmanaged_shadow[] = $username;
-		fwrite($shadow, $line."\n");
-	} else {
-		// blank line or comment
-		fwrite($shadow, $line."\n");
+		$unmanaged_usernames[] = $username;
+	}
+	
+	$unmanaged_lines_shadow[] = $line;
+}
+for ($i=0; $i < count($gforge_users); $i++) {
+	$username = $gforge_users[$i];
+	$managed_by_gforge = !in_array($username, $unmanaged_usernames);
+
+	if ($managed_by_gforge) {
+		$unix_passwd = $user_pws[$i];
+		
+		$line_shadow = $username.":".$unix_passwd.":12090:0:99999:7:::";
+		$gforge_lines_shadow[] = $line_shadow;
 	}
 }
 
+// Generate the contents of /etc/shadow
+$shadow_contents = implode("\n", $unmanaged_lines_shadow);
+$shadow_contents .= "\n";
+$shadow_contents .= "#GFORGEBEGIN\n";
+$shadow_contents .= implode("\n", $gforge_lines_shadow);
+$shadow_contents .= "\n#GFORGEEND\n";
 
 /*************************************************************************
- * Step 3: Write the GForge users to /etc/passwd and /etc/shadow
- *************************************************************************/
-
-// now write the GForge users
-fwrite($passwd, "#GFORGEBEGIN\n");
-fwrite($shadow, "#GFORGEBEGIN\n");
-assert(count($gforge_lines_passwd) == count($gforge_lines_shadow));
-for ($i=0; $i < count($gforge_lines_passwd); $i++) {
-	$line_passwd = $gforge_lines_passwd[$i];
-	$line_shadow = $gforge_lines_shadow[$i];
-	
-	
-	$entries = explode(":", $line_passwd);
-	$username = $entries[0];
-	if (!in_array($username, $unmanaged_etc)) {
-		// Only write the user if it must be managed by gforge (that is, if it's a new
-		// user or it was found inside the #GFORGE markers).
-		fwrite($passwd, $line_passwd."\n");
-	}
-	
-	$entries = explode(":", $line_shadow);
-	$username = $entries[0];
-	if (!in_array($username, $unmanaged_shadow)) {
-		fwrite($shadow, $line_shadow."\n");
-	}
-}
-fwrite($passwd, "#GFORGEEND\n");
-fwrite($shadow, "#GFORGEEND\n");
-
-
-fclose($passwd);
-fclose($shadow);
-
-/*************************************************************************
- * Step 4: Parse /etc/group
+ * Step 3: Parse /etc/group
  *************************************************************************/
 $group_orig = file("/etc/group");
-$group = fopen("/etc/group".FILE_EXTENSION, "w");
 
 //	Add the groups from the gforge database
 $group_res = db_query("SELECT group_id, unix_group_name, (is_public=1 AND enable_anonscm=1 AND type_id=1) AS enable_pserver FROM groups WHERE status='A' AND type_id='1'");
@@ -210,8 +218,9 @@ $err .= db_error();
 
 $gforge_groups = array();
 for($i = 0; $i < db_numrows($group_res); $i++) {
-    $gforge_groups[] = db_result($group_res,$i,'unix_group_name');
-    $gids[db_result($group_res,$i,'unix_group_name')] = db_result($group_res,$i,'group_id') + 50000;	// 50000: hardcoded value (for now).
+	$group_name = db_result($group_res,$i,'unix_group_name');
+    $gforge_groups[] = $group_name;
+    $gids[$group_name] = db_result($group_res,$i,'group_id') + 50000;	// 50000: hardcoded value (for now).
 }
 
 for ($i=0; $i < count($group_orig); $i++) {
@@ -220,15 +229,15 @@ for ($i=0; $i < count($group_orig); $i++) {
 	if (preg_match("/^[[:blank:]]*#GFORGEBEGIN/", $line)) {
 		do {
 			$i++;
-			$line = trim($passwd_orig[$i]);
-		} while ($i < count($passwd_orig) && !preg_match("/^[[:blank:]]*#GFORGEEND/", $line));
+			$line = trim($group_orig[$i]);
+		} while ($i < count($group_orig) && !preg_match("/^[[:blank:]]*#GFORGEEND/", $line));
 		
 		// Got to end of file (shouldn't happen, means #GFORGEEND wasn't found on file
-		if ($i >= (count($passwd_orig)-1)) break;
+		if ($i >= (count($group_orig)-1)) break;
 		
 		// read next line
 		$i++;
-		$line = trim($passwd_orig[$i]);
+		$line = trim($group_orig[$i]);
 	}
 	
 	$entries = explode(":", $line);
@@ -236,20 +245,19 @@ for ($i=0; $i < count($group_orig); $i++) {
 		$groupname = $entries[0];
 		if (!in_array($groupname, $gforge_groups)) {
 			// write the user only if it's not a gforge user
-			fwrite($group, $line."\n");
+			$unmanaged_lines_group[] = $line;
 		}
 	} else {
 		// blank line or comment
-		fwrite($group, $line."\n");
+		$unmanaged_lines_group[] = $line;
 	}
 }
 
-// Now write the GForge groups
+// Now process the GForge groups
 // Note that we FORCE the GForge groups to be managed by GForge. This is different than the
 // users, where the administrator could move a user outside the #GFORGE markers and manually
 // manage the user. This is done because we must add the users to the group, and for this we
 // must manage them.
-fwrite($group, "#GFORGEBEGIN\n");
 
 for ($i = 0; $i < count($gforge_groups); $i++) {
 	$group_name = $gforge_groups[$i];
@@ -280,13 +288,40 @@ for ($i = 0; $i < count($gforge_groups); $i++) {
 	}
 	
 	$line .= implode(',', $gmembers);
-	
-	fwrite($group, $line."\n");
-		
+	$gforge_lines_group[] = $line;
 }
 
-fwrite($group, "#GFORGEEND\n");
-fclose($group);
+// Generate the contents of /etc/group
+$group_contents = implode("\n", $unmanaged_lines_group);
+$group_contents .= "\n";
+$group_contents .= "#GFORGEBEGIN\n";
+$group_contents .= implode("\n", $gforge_lines_group);
+$group_contents .= "\n#GFORGEEND\n";
+
+/*************************************************************************
+ * Step 4: Write all the data
+ *************************************************************************/
+
+// Write /etc/passwd
+$passwd_file = fopen("/etc/passwd".FILE_EXTENSION, "w");
+if ($passwd_file) {
+	fwrite($passwd_file, $passwd_contents);
+	fclose($passwd_file);
+}
+
+// Write /etc/shadow
+$shadow_file = fopen("/etc/shadow".FILE_EXTENSION, "w");
+if ($shadow_file) {
+	fwrite($shadow_file, $shadow_contents);
+	fclose($shadow_file);
+}
+
+// Write /etc/group
+$group_file = fopen("/etc/group".FILE_EXTENSION, "w");
+if ($group_file) {
+	fwrite($group_file, $group_contents);
+	fclose($group_file);
+}
 
 cron_entry(16,$err);
 
