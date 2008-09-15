a880dc9ac06fbbad9e87b1a2dc91b7bef5e31b9e
Alain Peyrat <aljeux@free.fr>
2008-09-15 20:24:38 +0000

    Apply [#5535] Patch for incorrect $sys_plugins_path on 4.6

--- gforge/plugins/scmcvs/etc/plugins/scmcvs/config.php
+++ gforge/plugins/scmcvs/etc/plugins/scmcvs/config.php
@@ -5,7 +5,7 @@ $enabled_by_default = true;
 $cvs_binary_version='1.12';
 $use_ssl=false;
 $GLOBALS['cvs_binary_version']=$cvs_binary_version;
-$sys_plugins_path='/opt/gforge/gforge/plugins';
+$sys_plugins_path='/opt/gforge/plugins';
 $sys_default_domain='gforge.company.com';
 
 // Local Variables:
