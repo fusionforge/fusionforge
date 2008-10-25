caa66821c9ee0df95642d28d18d9e624d8a224e5
bigdisk <>
2008-10-25 13:11:35 +0000

    changing header to GPL

--- gforge/gforge-install-2.php
+++ gforge/gforge-install-2.php
@@ -1,6 +1,31 @@
 #!/usr/bin/php
-
 <?php
+/**
+ * GForge Installation Dependency Setup
+ *
+ * Copyright 2006 GForge, LLC
+ * http://gforge.org/
+ *
+ * @version
+ *
+ * This file is part of GInstaller
+ *
+ * GForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+ * Francisco Gimeno
+ */
+
 	define ('GREEN', "\033[01;32m" );
 	define ('NORMAL', "\033[00m" );
 	define ('RED', "\033[01;31m" );
--- gforge/gforge-install-3-db.php
+++ gforge/gforge-install-3-db.php
@@ -1,13 +1,30 @@
 #!/usr/bin/php
 <?php
-	/**
-	 * Copyright (c) 2005-2006 GForge, LLC
-	 *
-	 * @version $Id: $
-	 *
-	 * May not be modified or redistributed without permission of GForge Group
-	 *
-	 */
+/**
+ * GForge Installation Dependency Setup
+ *
+ * Copyright 2006 GForge, LLC
+ * http://gforge.org/
+ *
+ * @version
+ *
+ * This file is part of GInstaller
+ *
+ * GForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+ * Francisco Gimeno
+ */
 
 	define ('GREEN', "\033[01;32m" );
 	define ('NORMAL', "\033[00m" );
