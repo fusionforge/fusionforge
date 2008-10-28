a7b9a2a4c28f83fcf956b1c3d109a07c839d7d5e
Alain Peyrat <aljeux@free.fr>
2008-10-28 20:46:57 +0000

    Fix missing GLOBALS for variable in fckeditor plugin

--- gforge/plugins/fckeditor/common/fckeditorPlugin.class.php
+++ gforge/plugins/fckeditor/common/fckeditorPlugin.class.php
@@ -53,7 +53,7 @@ class fckeditorPlugin extends Plugin {
 			require_once("/usr/share/fckeditor/fckeditor.php");
 		} else {
 			$use_system_fckeditor = false ;
-			require_once $gfplugins.'fckeditor/www/fckeditor.php';
+			require_once $GLOBALS['gfplugins'].'fckeditor/www/fckeditor.php';
 		}
 
 		if ($hookname == "groupisactivecheckbox") {
