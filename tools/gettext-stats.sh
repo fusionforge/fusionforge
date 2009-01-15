839da5df65a8b1ee45f643bc15742a2247c688c1
lo-lan-do <>
2009-01-15 14:11:35 +0000

    Merged the two gettext-related scripts and made them more flexible as to where they're invoked from

--- tools/gettext-stats.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-locales="eu bg ca zh_TW nl en eo fr de el he id it ja ko la nb pl pt_BR pt ru zh_CN es sv th"
-
-for l in $(echo $locales | xargs -n 1 | sort) ; do
-    printf "* %5s: " $l
-    msgfmt --statistics -o /dev/null translations/$l.po
-done
--- tools/update-gettext-files.sh
+++ tools/update-gettext-files.sh
@@ -2,17 +2,45 @@
 
 locales="eu bg ca zh_TW nl en eo fr de el he id it ja ko la nb pl pt_BR pt ru zh_CN es sv th"
 
-# xgettext -j -d gforge -o translations/gforge.pot -L PHP --from-code=iso-8859-1 $(find -name \*.php -or -name \*.class | grep -v -e {arch} -e svn-base)
+if [ -e gforge/translations/gforge.pot ] ; then        # We're in the parent dir
+    cd gforge
+elif [ -e ../gforge/translations/gforge.pot ] ; then   # We're in tools/ or gforge/
+    cd ../gforge
+elif [ -e ../translations/gforge.pot ] ; then          # In a subdir of gforge
+    cd ..
+else
+    echo "Couldn't find translations directory..."
+    exit 1
+fi
 
-rm translations/gforge.pot
+if [ "$1" = --stats ] ; then
+    mode=stats
+else
+    mode=refresh
+fi
 
-find -type f -\( -name \*.php -or -name users -or -name projects -\) \
-    | grep -v -e {arch} -e svn-base \
-    | grep -v ^./plugins/wiki \
-    | LANG=C sort \
-    | xargs xgettext -d gforge -o translations/gforge.pot -L PHP --from-code=iso-8859-1    
+locales=$(echo $locales | xargs -n 1 | sort)
 
-for l in $(echo $locales | xargs -n 1 | sort) ; do
-    echo "Processing $l..."
-    msgmerge -U translations/$l.po translations/gforge.pot
-done
+case $mode in
+    refresh)
+	rm translations/gforge.pot
+	
+	find -type f -\( -name \*.php -or -name users -or -name projects -\) \
+	    | grep -v -e {arch} -e svn-base \
+	    | grep -v ^./plugins/wiki \
+	    | LANG=C sort \
+	    | xargs xgettext -d gforge -o translations/gforge.pot -L PHP --from-code=iso-8859-1    
+	    
+	    for l in $locales ; do
+		echo "Processing $l..."
+		msgmerge -U translations/$l.po translations/gforge.pot
+	    done
+	    ;;
+
+    stats)
+	for l in $(echo $locales | xargs -n 1 | sort) ; do
+	    printf "* %5s: " $l
+	    msgfmt --statistics -o /dev/null translations/$l.po
+	done
+	;;
+esac
