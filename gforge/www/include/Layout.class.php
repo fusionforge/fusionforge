e2c48d4e1b8c793b3d748bb99a977918a2362791
lo-lan-do <>
2009-01-15 13:19:29 +0000

    Used the proper value for xml:lang attributes in web pages

--- gforge/www/include/Layout.class.php
+++ gforge/www/include/Layout.class.php
@@ -71,7 +71,7 @@ class Layout extends Error {
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="<?php echo _('en') ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo _('en') ?>" lang="<?php echo _('en') ?>">
 
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
