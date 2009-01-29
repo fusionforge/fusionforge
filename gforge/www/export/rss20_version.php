3341aacdbfe764b5440b9878f3057e6bb717bbf2
Christian Bayle <cbayle@gmail.com>
2009-01-29 22:15:40 +0000

    Quite stupid RSS giving FusionForge version

---
+++ gforge/www/export/rss20_version.php
@@ -0,0 +1,38 @@
+<?php
+// export projects list in RSS 2.0
+// Author: Scott Grayban <sgrayban@borgnet.us>
+//
+include "../env.inc.php";
+include "pre.php";
+require_once $gfcommon.'include/GForge.class.php';
+
+$forge=new GForge();
+$vers=$forge->software_version;
+$name=$forge->software_name;
+$date=gmdate('D, d M Y g:i:s',time())." GMT";
+$link="http://".$GLOBALS[sys_default_domain];
+
+header("Content-Type: text/xml");
+print '<?xml version="1.0"?>';
+?>
+<rss version="2.0">
+<channel>
+	<copyright><?php echo $name; ?></copyright>
+        <pubDate><?php echo $date; ?></pubDate>
+        <description><?php echo "$name $vers"; ?></description>
+        <link><?php echo $link; ?></link>
+        <title><?php echo $title; ?></title>
+        <webMaster><?php echo $webmaster; ?></webMaster>
+        <language>en-us</language>
+	<item>
+              	<title>Name</title>
+              	<link><?php echo $link; ?></link>
+              	<description><?php echo $name; ?></description>
+	</item>
+	<item>
+              	<title>Version</title>
+              	<link><?php echo $link; ?></link>
+              	<description><?php echo $vers; ?></description>
+	</item>
+</channel>
+</rss>
