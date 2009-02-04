4bcc06d5d0ad960ef0c684d5800041a660487232
Roland Mas <lolando@debian.org>
2009-02-04 19:51:02 +0000

    Reindenting according to current style

--- gforge/plugins/scmsvn/common/scmsvn-init.php
+++ gforge/plugins/scmsvn/common/scmsvn-init.php
@@ -1,26 +1,25 @@
 <?php
-/** Subversion plugin for Gforge
- * Copyright 2003 Roland Mas <lolando@debian.org>
- * Copyright 2004 Roland Mas <roland@gnurandal.com> 
- *                The Gforge Group, LLC <http://gforgegroup.com/>
- * Based on the CVS plugin, which was derived from Gforge, which was
- * derived from Sourceforge
+/** FusionForge Subversion plugin
  *
- * This file is not part of Gforge
+ * Copyright 2003-2009, Roland Mas <lolando@debian.org>
+ * Copyright 2004, GForge, LLC
  *
- * This plugin, like Gforge, is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ * This file is part of FusionForge.
  *
- * GForge is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * FusionForge is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GForge; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+ * along with FusionForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
  */
 
 global $gfplugins;
