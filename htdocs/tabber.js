d9124aaedf7ce70535905f3f433e04846863c154
Roland Mas <lolando@debian.org>
2009-02-20 14:24:01 +0000

    Removed CVS/SVN keywords

--- htdocs/tabber.js
+++ htdocs/tabber.js
@@ -1,5 +1,4 @@
 /*==================================================
-  $Id: tabber.js,v 1.9 2006/04/27 20:51:51 pat Exp $
   tabber.js by Patrick Fitzgerald pat@barelyfitz.com
 
   Documentation can be found at the following URL:
