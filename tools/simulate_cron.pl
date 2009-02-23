a02f02a9ac3c55f06ce2f7bf17389df7ae5d97f1
Alain Peyrat <aljeux@free.fr>
2009-02-23 23:30:55 +0000

    Add simulate_cron.pl in tools (simple script to run cron jobs)

---
+++ tools/simulate_cron.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+# 
+# Simple script to run all crons (for tests) 
+#
+# Author: aljeux <aljeux@free.fr>
+#
+
+use strict;
+
+my $file = shift;
+
+open(F, $file) || die "Unable to open file '$file': $!";
+while (<F>) {
+ next if /^\s*#/;
+ next if /^\s*$/;
+ if (/^([A-Z]+)="(.*)"/) {
+  $ENV{$1}=$2;
+  print "Loading $1=$2\n";
+ }
+ if (/\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+root\s+(.*)/) {
+  print "Running $1 (as root)\n";
+  system($1);
+ }
+ if (/\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+gforge\s+(.*)/) {
+  print "Running $1 (as gforge)\n";
+  system('su', 'gforge', '-c', $1);
+ }
+}
+close(F);
