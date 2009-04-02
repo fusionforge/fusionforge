a1bf54dd52f15828c362d9658f0e3350a89c7129
Roland Mas <lolando@debian.org>
2009-04-02 20:41:38 +0000

    Use application/octet-stream as default MIME type for forum attachments

--- gforge/db/20090402-forum-attachment-types.sql
+++ gforge/db/20090402-forum-attachment-types.sql
@@ -1,2 +1,2 @@
-ALTER TABLE forum_attachment         ADD COLUMN mimetype CHARACTER VARYING(32) DEFAULT 'unknown/unknown';
-ALTER TABLE forum_pending_attachment ADD COLUMN mimetype CHARACTER VARYING(32) DEFAULT 'unknown/unknown';
+ALTER TABLE forum_attachment         ADD COLUMN mimetype CHARACTER VARYING(32) DEFAULT 'application/octet-stream';
+ALTER TABLE forum_pending_attachment ADD COLUMN mimetype CHARACTER VARYING(32) DEFAULT 'application/octet-stream';
