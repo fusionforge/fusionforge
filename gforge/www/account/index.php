31fc839a086930c4d495f5d4df23b3a72999d0e9
Julien HEYMAN <julien.heyman@atosorigin.com>
2009-04-02 09:09:15 +0000

    All profile are not developer

--- gforge/www/account/index.php
+++ gforge/www/account/index.php
@@ -105,7 +105,7 @@ echo $HTML->boxTop(_('Account Maintenance'));
 <?php echo _('Account options:'); ?>
 </p>
 <ul>
-	<li><?php echo util_make_link_u ($u->getUnixName(),$u->getId(),'<strong>'._('View My Developer Profile').'</strong>'); ?></a></li>
+	<li><?php echo util_make_link_u ($u->getUnixName(),$u->getId(),'<strong>'._('View My Profile').'</strong>'); ?></a></li>
 <?php if($GLOBALS['sys_use_people']) { ?>
 	<li><?php echo util_make_link ('/people/editprofile.php','<strong>'._('Edit My Skills Profile').'</strong>'); ?></a></li>
 <?php } ?>
