83f5b7ec05c3dff2b1eb1da068490d99ca5768ee
Alain Peyrat <aljeux@free.fr>
2009-04-07 18:55:43 +0000

    Fixed typo in build test

--- tests/build/packages/BuildTests.php
+++ tests/build/packages/BuildTests.php
@@ -3,9 +3,9 @@
 require_once 'PHPUnit/Framework/TestCase.php';
 
 /**
- * Syntax test class.
+ * Packaging tests (tarball, debian, rpm).
  *
- * @package   SyntaxTests
+ * @package   PackagesTests
  * @author    Alain Peyrat <aljeux@free.fr>
  * @copyright 2009 Alain Peyrat. All rights reserved.
  * @license   http://www.opensource.org/licenses/gpl-license.php  GPL License
@@ -27,7 +27,7 @@ class Packages_Tests extends PHPUnit_Framework_TestCase
     {
 	    $tests = dirname( dirname( dirname( dirname (__FILE__)))); 
 	    $base = dirname( $tests );
-	    system("cd ..; make -f Makefile.deban BUILDRESULT=$base/build/packages all", $retval);
+	    system("cd ..; make -f Makefile.debian BUILDRESULT=$base/build/packages all", $retval);
 	    $this->assertEquals(0, $retval);
     }
 
