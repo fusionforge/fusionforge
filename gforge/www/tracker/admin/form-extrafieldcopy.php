86a1bd42b7c3e824fbaf9e504852fc035e683b80
Alain Peyrat <aljeux@free.fr>
2009-04-08 19:55:24 +0000

    Tracker: Verify that there is at least one destination (copy extra fields)

--- gforge/www/tracker/admin/form-extrafieldcopy.php
+++ gforge/www/tracker/admin/form-extrafieldcopy.php
@@ -5,6 +5,29 @@
 //
 		$id = getIntFromRequest('id');
 		$fb= new ArtifactExtraField($ath,$id);
+
+		// Get a list of all extra fields in trackers and groups that you have perms to admin
+		$sql="SELECT g.unix_group_name, agl.name AS tracker_name, aefl.field_name, aefl.extra_field_id
+			FROM groups g, 
+			artifact_group_list agl, 
+			artifact_extra_field_list aefl,
+			user_group ug,
+			artifact_perm ap
+			WHERE 
+			(ug.admin_flags='A' OR ug.artifact_flags='2' OR ap.perm_level>='2')
+			AND ug.user_id='".user_getid()."'
+			AND ug.group_id=g.group_id
+			AND g.group_id=agl.group_id 
+			AND agl.group_artifact_id=ap.group_artifact_id
+			AND ap.user_id='".user_getid()."'
+			AND aefl.group_artifact_id=agl.group_artifact_id
+			AND aefl.extra_field_id != $id
+			AND aefl.field_type IN (1,2,3,5,7)";
+		$res=db_query($sql);
+		if (db_numrows($res) < 1) {
+			exit_error('Cannot find a destination tracker where you have administration rights.');
+		}
+		
 		$title = sprintf(_('Copy choices from custom field %1$s'), $fb->getName());
 		$ath->adminHeader(array ('title'=>$title));
 		echo "<h3>".$title."</h3>";
@@ -14,54 +37,37 @@
 			$field_id_arr[] = $efearr[$i]['element_id'];
 			$field_arr[] = $efearr[$i]['element_name'];
 		}
+		?>
+		<form action="<?php echo getStringFromServer('PHP_SELF') .'?group_id='.$group_id.'&amp;atid='.$ath->getID(); ?>" method="post" >
+                <?php
 		echo '<table>';
 		echo '<tr>';
 		echo '<td></td><td><center><strong>';
 		echo _('Copy From');
 		echo '<br />';
 		echo $fb->getName();
-		echo '</center></strong></td><td></td><td><strong><center>';
+		echo '</strong></center></td><td></td><td><center><strong>';
 		
 		echo _('Into trackers and custom fields');
-		echo '</center></strong></tr><tr><td><strong><center>';
-		echo '</center></strong></td>';
-		echo '<td valign=top>';
+		echo '</strong></center></td></tr><tr><td><center><strong>';
+		echo '</strong></center></td>';
+		echo '<td valign="top">';
 		?>
 		
-		<form action="<?php echo getStringFromServer('PHP_SELF') .'?group_id='.$group_id.'&atid='.$ath->getID(); ?>" method="post" >
-		<input type="hidden" name="copy_opt" value="copy" >
-		<input type="hidden" name="id" value="<?php echo $id; ?>">
+		<input type="hidden" name="copy_opt" value="copy" />
+		<input type="hidden" name="id" value="<?php echo $id; ?>" />
 		<?php
 		echo html_build_multiple_select_box_from_arrays($field_id_arr,$field_arr,'copyid[]',array(),10,false);
-		echo '</td><td><strong><center>';
-		//get a list of all extra fields in trackers and groups that you have perms to admin
-		$sql="SELECT DISTINCT g.unix_group_name, agl.name AS tracker_name, aefl.field_name, aefl.extra_field_id
-			FROM groups g, 
-			artifact_group_list agl, 
-			artifact_extra_field_list aefl,
-			user_group ug,
-                        role_setting rs
-			WHERE
-                        (
-                           (rs.section_name = 'projectadmin' AND rs.value = 'A')
-                           OR (rs.section_name = 'trackeradmin' AND rs.value = '2')
-                           OR (rs.section_name = 'tracker' AND rs.value::integer >= 2 AND rs.ref_id = agl.group_artifact_id)
-                        )
-			AND ug.user_id='".user_getid()."'
-			AND ug.group_id=g.group_id
-			AND g.group_id=agl.group_id 
-			AND rs.role_id=ug.role_id
-			AND aefl.group_artifact_id=agl.group_artifact_id
-			AND aefl.field_type IN (1,2,3,5)";
-		$res=db_query($sql);
-
+		echo '</td><td><center><strong>';
+		
 //		echo db_error().$sql;
 
 		while($arr =db_fetch_array($res)) {
 				$name_arr[]=$arr['unix_group_name']. '::'. $arr['tracker_name'] . '::'. $arr['field_name'];
 				$id_arr[]=$arr['extra_field_id'];
 		}
-		echo '<td valign=top>';
+		echo '</strong></center></td>';
+		echo '<td valign="top">';
 
 		echo html_build_select_box_from_arrays($id_arr,$name_arr,'selectid',$selectid,false);
 		echo '</td></tr>';
