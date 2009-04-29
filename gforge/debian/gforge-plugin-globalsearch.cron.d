b19a68b463fb07a14b8208bcde0effcab466dade
Roland Mas <lolando@debian.org>
2009-04-29 09:43:19 +0000

    Fixed packaging after rebasing the globalsearch branch from Adullact

--- gforge/debian/gforge-plugin-globalsearch.cron.d
+++
@@ -1,1 +0,0 @@
-30 3 * * * gforge [ -x /usr/lib/gforge/plugins/globalsearch/bin/get_assoc_sites_projects.pl ] && /usr/lib/gforge/plugins/globalsearch/bin/get_assoc_sites_projects.pl > /dev/null 2>&1
