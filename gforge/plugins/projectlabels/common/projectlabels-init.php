f4c9279ba69f6d89d14575ed1bce49639a257672
Roland Mas <lolando@debian.org>
2009-04-29 10:11:18 +0000

    New projectlabels plugin, ported to 4.7

---
+++ gforge/plugins/projectlabels/common/projectlabels-init.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once ($GLOBALS['sys_plugins_path'].'/projectlabels/common/ProjectLabelsPlugin.class') ;
+
+$ProjectLabelsPluginObject = new ProjectLabelsPlugin ;
+
+register_plugin ($ProjectLabelsPluginObject) ;
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+
+?>
