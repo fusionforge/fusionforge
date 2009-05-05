7ea3cedc775ded86523004bf0dfe6eca6e277b8c
Roland Mas <lolando@debian.org>
2009-05-05 11:29:37 +0000

    Allow to use email when asking for a resend of the registration confirmation email

--- gforge/www/account/pending-resend.php
+++ gforge/www/account/pending-resend.php
@@ -28,6 +28,9 @@ if (getStringFromRequest('submit')) {
 	$loginname = getStringFromRequest('loginname');
 
 	$u = user_get_object_by_name($loginname);
+	if ($GLOBALS['sys_require_unique_email']) {
+		$u ||= user_get_object_by_email ($loginname);
+	}
 	if (!$u || !is_object($u)) {
 		exit_error('Error','Could Not Get User');
 	} elseif ($u->isError()) {
@@ -56,11 +59,21 @@ if (getStringFromRequest('submit')) {
 }
 
 $HTML->header(array('title'=>'Pending-resend'));
-echo _('Fill in a user name and click \'Submit\' to resend the confirmation email');
+if ($GLOBALS['sys_require_unique_email']) {
+	echo _('Fill in a user name or email address and click \'Submit\' to resend the confirmation email.');
+} else {
+	echo _('Fill in a user name and click \'Submit\' to resend the confirmation email.');
+}
 ?>
 
 <form action="<?php echo util_make_url('/account/pending-resend.php'); ?>" method="post">
-<p><?php echo _('Login name:'); ?>
+<p><?php 
+if ($GLOBALS['sys_require_unique_email']) {
+	echo _('Login name or email address:');
+} else {
+	echo _('Login name:'); 
+}
+?>
 <br /><input type="text" name="loginname" /></p>
 <p><input type="submit" name="submit" value="<?php echo _('Submit'); ?>" /></p>
 </form>
