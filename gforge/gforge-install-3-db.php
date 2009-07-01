92e3b7d985fbe2ca3908481de3f4383de236e3cd
Alain Peyrat <aljeux@free.fr>
2009-07-01 20:34:29 +0000

    Removed version for installer to avoid useless miss

--- gforge/gforge-install-3-db.php
+++ gforge/gforge-install-3-db.php
@@ -33,7 +33,7 @@ define ('RED', "\033[01;31m" );
 $STDOUT = fopen('php://stdout','w');
 $STDIN = fopen('php://stdin','r');
 
-show("\n-=# Welcome to FusionForge DB-Installer v4.9 #=-");
+show("\n-=# Welcome to FusionForge DB-Installer #=-");
 
 //TO DO: add dependency check
 //if (!run("php check-deps.php", true)) {
