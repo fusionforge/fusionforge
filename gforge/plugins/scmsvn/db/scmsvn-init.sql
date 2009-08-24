3b2f98a09e74b867f9513fa900112b3a81f851eb
Julien HEYMAN <julien.heyman@atosorigin.com>
2009-08-24 12:53:40 +0000

    Fixed SQL errors

--- gforge/plugins/scmsvn/db/scmsvn-init.sql
+++ gforge/plugins/scmsvn/db/scmsvn-init.sql
@@ -1,4 +1,4 @@
-DROP TABLE plugin_scmsvn_group_usage;
-DROP TABLE plugin_scmsvn_stats;
-DROP SEQUENCE plugin_scmsvn_grp_usage_pk_seq;
-DROP SEQUENCE plugin_scmsvn_stats_pk_seq;
+DROP TABLE IF EXISTS plugin_scmsvn_group_usage;
+DROP TABLE IF EXISTS plugin_scmsvn_stats;
+DROP SEQUENCE IF EXISTS plugin_scmsvn_grp_usage_pk_seq;
+DROP SEQUENCE IF EXISTS plugin_scmsvn_stats_pk_seq;
