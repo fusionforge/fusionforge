6a3ee40a15a8ee6891227bc619cc630e10fad7c9
Roland Mas <lolando@debian.org>
2009-08-27 13:46:53 +0000

    Started skeleton plugin for GNU Arch

---
+++ gforge/plugins/scmarch/common/ArchPlugin.class.php
@@ -0,0 +1,124 @@
+<?php
+/** FusionForge Arch plugin
+ *
+ * Copyright 2009, Roland Mas
+ *
+ * This file is part of FusionForge.
+ *
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * FusionForge is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FusionForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+class ArchPlugin extends SCMPlugin {
+	function ArchPlugin () {
+		global $gfconfig;
+		$this->SCMPlugin () ;
+		$this->name = 'scmarch';
+		$this->text = 'Arch';
+		$this->hooks[] = 'scm_generate_snapshots' ;
+
+		require_once $gfconfig.'plugins/scmarch/config.php' ;
+		
+		$this->default_arch_server = $default_arch_server ;
+		$this->arch_root = $arch_root;
+
+		$this->register () ;
+	}
+	
+	function getDefaultServer() {
+		return $this->default_arch_server ;
+	}
+
+	function getBlurb () {
+		return _('<p>Documentation for GNU Arch (sometimes referred to as "tla") is available <a href="http://www.gnu.org/software/gnu-arch/">here</a>.</p>') ;
+	}
+
+	function createOrUpdateRepo ($params) {
+		$project = $this->checkParams ($params) ;
+		if (!$project) {
+			return false ;
+		}
+				
+		if (! $project->usesPlugin ($this->name)) {
+			return false;
+		}
+
+		$repo = $this->arch_root . '/' . $project->getUnixName() ;
+		$unix_group = 'scm_' . $project->getUnixName() ;
+
+		$repo_exists = false ;
+		if (!$repo_exists) {
+			system ("mkdir -p $repo") ;
+		}
+
+		system ("chgrp -R $unix_group $repo") ;
+		if ($project->enableAnonSCM()) {
+			system ("chmod -R g+wXs,o+rX-w $repo") ;
+		} else {
+			system ("chmod -R g+wXs,o-rwx $repo") ;
+		}
+	}
+
+	function generateSnapshots ($params) {
+		global $sys_scm_snapshots_path ;
+		global $sys_scm_tarballs_path ;
+
+		$project = $this->checkParams ($params) ;
+		if (!$project) {
+			return false ;
+		}
+		
+		$group_name = $project->getUnixName() ;
+
+		$snapshot = $sys_scm_snapshots_path.'/'.$group_name.'-scm-latest.tar.gz';
+		$tarball = $sys_scm_tarballs_path.'/'.$group_name.'-scmroot.tar.gz';
+
+		if (! $project->usesPlugin ($this->name)) {
+			return false;
+		}
+
+		if (! $project->enableAnonSCM()) {
+			unlink ($snapshot) ;
+			unlink ($tarball) ;
+			return false;
+		}
+
+		$toprepo = $this->arch_root ;
+		$repo = $toprepo . '/' . $project->getUnixName() ;
+
+		if (!is_dir ($repo) || !is_file ("$repo/format")) {
+			unlink ($snapshot) ;
+			unlink ($tarball) ;
+			return false ;
+		}
+
+		$tmp = trim (`mktemp -d`) ;
+		if ($tmp == '') {
+			return false ;
+		}
+		system ("tar czCf $toprepo $tmp/tarball.tar.gz " . $project->getUnixName()) ;
+		chmod ("$tmp/tarball.tar.gz", 0644) ;
+		copy ("$tmp/tarball.tar.gz", $tarball) ;
+		unlink ("$tmp/tarball.tar.gz") ;
+		system ("rm -rf $tmp") ;
+	}
+  }
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+
+?>
---
+++ gforge/plugins/scmarch/etc/plugins/scmarch/config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+$default_arch_server = $GLOBALS['sys_scm_host'];
+$arch_root = '/var/lib/gforge/chroot/scmrepos/arch';
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+ 
+?>
