32aa8f4eed51ae2de0da35623b283677624f2ed1
Roland Mas <lolando@debian.org>
2009-08-31 15:13:59 +0000

    Ongoing migration to db_query_params()

--- gforge/common/tracker/ArtifactsForUser.class.php
+++ gforge/common/tracker/ArtifactsForUser.class.php
@@ -98,17 +98,18 @@ class ArtifactsForUser extends Error {
 	*/
 	function & getMonitoredArtifacts() {
 		$artifacts = array();
-		$sql="SELECT groups.group_name,groups.group_id," .
-				"artifact_group_list.group_artifact_id," .
-				"artifact_group_list.name " .
-			"FROM groups,artifact_group_list,artifact_type_monitor " .
-			"WHERE groups.group_id=artifact_group_list.group_id " .
-			"AND groups.status ='A' " .
-			"AND artifact_group_list.group_artifact_id=artifact_type_monitor.group_artifact_id " .
-			"AND artifact_type_monitor.user_id='".$this->User->getID()."' " .
-			"ORDER BY group_name DESC";
-			
-		$result=db_query($sql);
+
+		$result=db_query_params ('SELECT groups.group_name,groups.group_id,
+artifact_group_list.group_artifact_id,
+artifact_group_list.name 
+FROM groups,artifact_group_list,artifact_type_monitor 
+WHERE groups.group_id=artifact_group_list.group_id 
+AND groups.status =$1 
+AND artifact_group_list.group_artifact_id=artifact_type_monitor.group_artifact_id 
+AND artifact_type_monitor.user_id=$2 
+ORDER BY group_name DESC',
+					 array('A',
+					       $this->User->getID()));
 		$rows=db_numrows($result);
 		if ($rows < 1) {
 		        return $artifacts;
