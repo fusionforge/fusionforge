a47295f212b4182ed501537bb8f69a900ddabd8f
Alain Peyrat <aljeux@free.fr>
2009-09-24 20:55:38 +0000

    Add tempo when starting vz host to avoid failures

--- tests/scripts/start_vz.sh
+++ tests/scripts/start_vz.sh
@@ -5,5 +5,11 @@ sudo /root/start_vz.sh centos-5-i386-default-5.2-20081107 "$1"
 rm -f /home/albot/.ssh/known_hosts
 
 ssh -o 'StrictHostKeyChecking=no' root@centos52 uname -a
+ret=$?
+
+if [ $ret -ne 0 ];then
+	sleep 10;
+	ssh -o 'StrictHostKeyChecking=no' root@centos52 uname -a
+fi
 
 sleep 1
