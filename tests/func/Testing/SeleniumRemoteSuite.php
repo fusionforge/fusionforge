0762fdd7399b73688e1bdb205dc8a2b5b367d0c8
Alain Peyrat <aljeux@free.fr>
2009-10-04 17:30:27 +0000

    Fix remote tests on centos52

--- tests/func/Testing/SeleniumRemoteSuite.php
+++ tests/func/Testing/SeleniumRemoteSuite.php
@@ -9,9 +9,10 @@ class SeleniumRemoteSuite extends PHPUnit_Framework_TestSuite
 		system("ssh root@centos52 'tar jxf fusionforge-*allinone.tar.bz2'");
 		system("ssh root@centos52 'cd fusionforge-*; FFORGE_RPM_REPO=http://buildbot.fusionforge.org/job/fusionforge-trunk-full/ws/build/packages/ FFORGE_DB=fforge FFORGE_USER=gforge FFORGE_ADMIN_USER=ffadmin FFORGE_ADMIN_PASSWORD=ffadmin ./install.sh centos52.local'");
 
-		system("ssh root@centos52 'mkdir -p /opt/tests/func'");
-		system("scp func/db_reload.php root@centos52:/opt/tests/func/");
-		system("scp func/config.php.tests root@centos52:/opt/tests/func/config.php");
+		system("scp -r ../tests root@centos52:/opt");
+//		system("ssh root@centos52 'mkdir -p /opt/tests/func'");
+//		system("scp func/db_reload.php root@centos52:/opt/tests/func/");
+//		system("scp func/config.php.tests root@centos52:/opt/tests/func/config.php");
 
 		system("scp /usr/share/php/PHPUnit/Extensions/SeleniumTestCase/*pend.php root@centos52:/opt/tests");
 		system("scp /usr/share/php/PHPUnit/Extensions/SeleniumTestCase/phpunit_coverage.php root@centos52:/opt/gforge/www");
@@ -22,7 +23,7 @@ class SeleniumRemoteSuite extends PHPUnit_Framework_TestSuite
 
 	protected function tearDown()
 	{
-		system("cd scripts; ./stop_vm.sh centos52");
+		//system("cd scripts; ./stop_vm.sh centos52");
 	}
 }
 ?>
