11c1a32e6ecec522d6e1afb33b48ae5ac5eafeba
Alain Peyrat <aljeux@free.fr>
2009-11-26 19:27:21 +0000

    Patch by Olaf Lenz: Added myself to AUTHORS, updated README, added comments to setup and gforge-install-*.

--- gforge/gforge-install-1-deps.php
+++ gforge/gforge-install-1-deps.php
@@ -8,7 +8,7 @@
  *
  * @version
  *
- * This file is part of GInstaller
+ * This file is part of GInstaller. It is be called by install.sh.
  *
  * GForge is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
