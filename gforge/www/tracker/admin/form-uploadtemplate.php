1273acdef3b9f292b12210093a11156112c40c6a
Alain Peyrat <aljeux@free.fr>
2009-12-16 19:57:13 +0000

    Tracker: UI change in upload template (use a fieldset now)

--- gforge/www/tracker/admin/form-uploadtemplate.php
+++ gforge/www/tracker/admin/form-uploadtemplate.php
@@ -6,13 +6,22 @@ $ath->adminHeader(array('title'=>$title));
 
 echo '<h1>'.$title.'</h1>';
 ?>
+<table>
+<tr>
+<td>
+<fieldset>
+<legend><?php echo _('Select Template'); ?></legend>
 <form action="<?php echo getStringFromServer('PHP_SELF').'?group_id='.$group_id.'&amp;atid='.$ath->getID(); ?>" method="post" enctype="multipart/form-data">
-<p><input type="hidden" name="uploadtemplate" value="1" />
-
-<input type="file" name="input_file" size="30" /></p>
-
-<p><input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" /></p>
+<p>
+    <input type="hidden" name="uploadtemplate" value="1" />
+    <input type="file" name="input_file" size="30" />
+    <input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" />
+</p>
 </form>
+</fieldset>
+</td>
+</tr>
+</table>
 <?php
 
 $ath->footer(array());
