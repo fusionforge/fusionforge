128aaddb627f087b9bf84879066d9038f3a794d2
Alexandre Neymann <alexandre.neymann@dgfip.finances.gouv.fr>
2009-12-22 14:55:22 +0000

    fix sprintf, and add conditionnal report

--- gforge/www/include/project_home.php
+++ gforge/www/include/project_home.php
@@ -91,8 +91,11 @@ if ($project->usesStats()) {
 	if (!$actv_res) {
 		$actv_res=0;
 	}
-	print '<br />'.sprintf (_('Activity Ranking: %d'). $actv_res) ;
-	print '<br />'.sprintf(_('View project <a href="%1$s" >Statistics</a> or <a href="%2$s">Activity</a>'), util_make_url ('/project/stats/?group_id='.$group_id),util_make_url ('/project/report/?group_id='.$group_id));
+	print '<br />'.sprintf (_('Activity Ranking: %d'), $actv_res) ;
+	print '<br />'.sprintf(_('View project <a href="%1$s" >Statistics</a>'),util_make_url ('/project/stats/?group_id='.$group_id));
+	if ( ($project->usesTracker() && $GLOBALS['sys_use_tracker']) || ($project->usesPm() && $GLOBALS['sys_use_pm']) ) {
+		print sprintf(_(' or <a href="%1$s">Activity</a>'),util_make_url ('/project/report/?group_id='.$group_id));
+	}
 	print '<br />'.sprintf(_('View list of <a href="%1$s">RSS feeds</a> available for this project.'), util_make_url ('/export/rss_project.php?group_id='.$group_id)). '&nbsp;' . html_image('ic/rss.png',16,16,array('border'=>'0'));
 }
 
