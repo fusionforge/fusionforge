f87339088999b49824cce240b9dce3321549f4e3
Roland Mas <lolando@debian.org>
2010-02-10 16:15:15 +0000

    PHP notice

--- gforge/www/project/showfiles.php
+++ gforge/www/project/showfiles.php
@@ -2,9 +2,15 @@
 /*
 	Temporary redirect to prevent breakage of existing installs/links
 */
-$group_id = $_REQUEST["group_id"];
-$release_id = $_REQUEST["release_id"];
+$group_id = getIntFromRequest ('group_id');
+$release_id = getIntFromRequest ('release_id');
 
-header("Location: /frs/?group_id=$group_id&release_id=$release_id");
-
+if ($group_id) {
+	if ($release_id) {
+		header ("Location: ".util_make_uri("/frs/?group_id=$group_id&release_id=$release_id")); 
+	} else {
+		header ("Location: ".util_make_uri("/frs/?group_id=$group_id")); }
+} else {
+	header ("Location: ".util_make_uri("/")); 
+}
 ?>
