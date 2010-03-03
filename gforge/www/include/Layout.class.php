592b61bfa98ace2b4c5787bb8f7a30b6298567bd
Alain Peyrat <aljeux@free.fr>
2010-03-03 22:20:37 +0000

    Default Layout now use /js/common.js also

--- gforge/www/include/Layout.class.php
+++ gforge/www/include/Layout.class.php
@@ -306,28 +306,18 @@ class Layout extends Error {
 		plugin_hook ('cssfile',$this);
 	}
 
-        /** 
+	/**
 	 * headerJS() - creates the JS headers and calls the plugin javascript hook
 	 * @todo generalize this
 	 */
-	function headerJS() { 
-		?>
-                
-		<script language="JavaScript" type="text/javascript">
-			<!--
-
-			function admin_window(adminurl) {
-			AdminWin = window.open( adminurl, 'AdminWindow','scrollbars=yes,resizable=yes, toolbar=yes, height=400, width=400, top=2, left=2');
-			AdminWin.focus();
-		}
-		function help_window(helpurl) {
-			HelpWin = window.open( helpurl,'HelpWindow','scrollbars=yes,resizable=yes,toolbar=no,height=400,width=400');
-		}
-		// -->
-		<?php plugin_hook ("javascript",false) ; ?>
-			  </script> 
-				    <?php
-				    }
+	function headerJS() {
+		echo '
+		<script type="text/javascript" src="'. util_make_uri('/js/common.js') .'"></script>
+		<script type="text/javascript">';
+		plugin_hook ("javascript",false);
+		echo '
+		</script>';
+	}
                
         function bodyHeader($params){
 		?>
