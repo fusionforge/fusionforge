048dfffd2bd9393faa04ee003399236c2f4e544f
Alain Peyrat <aljeux@free.fr>
2010-03-15 20:40:48 +0000

    Fix broken SQL query in session_set_new()

--- gforge/common/include/session.php
+++ gforge/common/include/session.php
@@ -407,7 +407,7 @@ function session_set_new($user_id) {
 	session_cookie("session_ser", $cookie, "", $GLOBALS['sys_session_expire']);
 	$session_ser=$cookie;
 
-	$res = db_query_params ('SELECT count(*) FROM user_session WHERE session_hash = "$1"',
+	$res = db_query_params ('SELECT count(*) FROM user_session WHERE session_hash =$1',
 				array (session_get_session_cookie_hash($cookie))) ;
 	if (!$res || db_numrows($res) < 1) {
 		db_query_params ('INSERT INTO user_session (session_hash,ip_addr,time,user_id) VALUES ($1,$2,$3,$4)',
