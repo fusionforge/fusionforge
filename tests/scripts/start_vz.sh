68ef8bba08e7d9f6ed2cd53a9792a382cb84ec03
Alain Peyrat <aljeux@free.fr>
2010-03-18 21:38:04 +0000

    Use hostname from argument in start_vz (test script)

--- tests/scripts/start_vz.sh
+++ tests/scripts/start_vz.sh
@@ -2,12 +2,12 @@
 
 sudo /root/start_vz.sh centos-5-i386-default-5.2-20081107 "$1"
 
-ssh -o 'StrictHostKeyChecking=no' root@centos52 uname -a
+ssh -o 'StrictHostKeyChecking=no' "root@$1" uname -a
 ret=$?
 
 if [ $ret -ne 0 ];then
 	sleep 10;
-	ssh -o 'StrictHostKeyChecking=no' root@centos52 uname -a
+	ssh -o 'StrictHostKeyChecking=no' "root@$1" uname -a
 fi
 
 sleep 1
