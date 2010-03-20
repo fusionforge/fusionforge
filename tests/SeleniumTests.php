cadb87fb84e214862f13ca86387b3334efc0af53
Alain Peyrat <aljeux@free.fr>
2010-03-20 14:29:23 +0000

    Use new filesystem based layout to simplify test suite (done for func)

--- tests/SeleniumTests.php
+++ tests/SeleniumTests.php
@@ -6,17 +6,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-// Selenium based tests
-require_once 'func/Site/AllTests.php';
-require_once 'func/Trackers/AllTests.php';
-require_once 'func/Tasks/AllTests.php';
-require_once 'func/Forums/AllTests.php';
-//require_once 'PluginsWiki/AllTests.php';
-//require_once 'PluginsWebSvn/AllTests.php';
-require_once 'func/News/AllTests.php';
-//require_once 'scm/AllTests.php';
-//require_once 'docs/AllTests.php';
-
 class SeleniumTests
 {
 	public static function main()
@@ -28,16 +17,12 @@ class SeleniumTests
 	{
 		$suite = new PHPUnit_Framework_TestSuite('PHPUnit');
 
-		// Integration tests (Selenium).
-		$suite->addTest(Site_AllTests::suite());
-		$suite->addTest(Trackers_AllTests::suite());
-		$suite->addTest(Tasks_AllTests::suite());
-		$suite->addTest(Forums_AllTests::suite());
-		$suite->addTest(News_AllTests::suite());
-//		$suite->addTest(PluginsWiki_AllTests::suite());
-//		$suite->addTest(PluginsWebSvn_AllTests::suite());
-//		$suite->addTest(Scm_AllTests::suite());
-//		$suite->addTest(Docs_AllTests::suite());
+		// Selenium tests
+		$suite->addTestFiles(glob("func/Site/*Test.php"));
+		$suite->addTestFiles(glob("func/Trackers/*Test.php"));
+		$suite->addTestFiles(glob("func/Tasks/*Test.php"));
+		$suite->addTestFiles(glob("func/Forums/*Test.php"));
+		$suite->addTestFiles(glob("func/News/*Test.php"));
 		
 		return $suite;
 	}
