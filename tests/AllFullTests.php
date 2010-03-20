fc04b9254bd7af8d0f38ab917ead0ee1057422f5
Alain Peyrat <aljeux@free.fr>
2010-03-20 14:29:16 +0000

    Use new filesystem based layout to simplify test suite (done for build, code and unit)

--- tests/AllFullTests.php
+++ tests/AllFullTests.php
@@ -6,35 +6,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-// Unit tests
-require_once 'unit/utils/AllTests.php';
-require_once 'unit/config/AllTests.php';
-//require_once 'ACL/AllTests.php';
-
-
-// Code tests
-require_once 'code/syntax/AllTests.php';
-require_once 'code/deprecations/AllTests.php';
-
-// Build tests
-require_once 'build/packages/AllTests.php';
-require_once 'build/documentation/AllTests.php';
-
-// Remote tests
-//require_once 'remote/tarball/AllTests.php';
-
-// Selenium based tests
-//require_once 'func/Site/AllTests.php';
-//require_once 'func/Trackers/AllTests.php';
-//require_once 'func/Tasks/AllTests.php';
-//require_once 'func/Forums/AllTests.php';
-//require_once 'func/PluginsWiki/AllTests.php';
-//require_once 'func/PluginsWebSvn/AllTests.php';
-//require_once 'func/News/AllTests.php';
-//require_once 'func/scm/AllTests.php';
-//require_once 'func/docs/AllTests.php';
-
-
 class AllTests
 {
 	public static function main()
@@ -47,31 +18,13 @@ class AllTests
 		$suite = new PHPUnit_Framework_TestSuite('PHPUnit');
 
 		// Unit tests
-		$suite->addTest(Utils_AllTests::suite());
-		$suite->addTest(Config_AllTests::suite());
-//		$suite->addTest(ACL_AllTests::suite());
+		$suite->addTestFiles(glob("unit/*/*Test.php"));
 
 		// Code tests
-		$suite->addTest(Syntax_AllTests::suite());
-		$suite->addTest(Deprecations_AllTests::suite());
+		$suite->addTestFiles(glob("code/*/*Test.php"));
 
 		// Building packages and documentation tests
-		$suite->addTest(Packages_AllTests::suite());
-		$suite->addTest(Documentation_AllTests::suite());
-		
-		// Remote tests
-//		$suite->addTest(Remote_AllTests::suite());
-		
-		// Integration tests (Selenium).
-//		$suite->addTest(Site_AllTests::suite());
-//		$suite->addTest(Trackers_AllTests::suite());
-//		$suite->addTest(Tasks_AllTests::suite());
-//		$suite->addTest(Forums_AllTests::suite());
-//		$suite->addTest(News_AllTests::suite());
-//		$suite->addTest(PluginsWiki_AllTests::suite());
-//		$suite->addTest(PluginsWebSvn_AllTests::suite());
-//		$suite->addTest(Scm_AllTests::suite());
-//		$suite->addTest(Docs_AllTests::suite());
+		$suite->addTestFiles(glob("build/*/*Test.php"));
 
 		return $suite;
 	}
