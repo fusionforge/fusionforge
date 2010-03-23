a1dab29cc9eb78462be712bc0f040ce31b356d21
Alain Peyrat <aljeux@free.fr>
2010-03-23 18:05:59 +0000

    Fix warning with PHP5.3

--- gforge/www/soap/nusoap.php
+++ gforge/www/soap/nusoap.php
@@ -4018,7 +4018,7 @@ class nusoap_server extends nusoap_base {
 	function serialize_return() {
 		$this->debug('Entering serialize_return methodname: ' . $this->methodname . ' methodURI: ' . $this->methodURI);
 		// if fault
-		if (isset($this->methodreturn) && ((get_class($this->methodreturn) == 'soap_fault') || (get_class($this->methodreturn) == 'nusoap_fault'))) {
+		if (isset($this->methodreturn) && ((@get_class($this->methodreturn) == 'soap_fault') || (@get_class($this->methodreturn) == 'nusoap_fault'))) {
 			$this->debug('got a fault object from method');
 			$this->fault = $this->methodreturn;
 			return;
