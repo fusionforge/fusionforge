df23338afa33c5415f596add053753d5b8234ce0
Olivier Berger <olivier.berger@it-sudparis.eu>
2010-04-06 15:32:35 +0000

    Add a test to check that htmlpurifier PHP lib is indeed installed on the system running the tests

---
+++ tests/unit/utils/HtmlPurifierTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ * Simple test to check that htmlpurifier is installed
+ *
+ * @package   Tests
+ * @author    Olivier Berger <olivier.berger@it-sudparis.eu>
+ * @copyright 2009 Olivier Berger & Institut TELECOM
+ * @license   GPL License
+ */
+class HtmlPurifier_Tests extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Test that include of lib doesn't fail, otherwise give some hint on missing package
+	 */
+	public function testHtmlPurifier()
+	{
+	  try {
+	    // cannot test this as fails hard
+	    //require_once('HTMLPurifier.auto.php');
+	    // so include instead :
+	    include 'HTMLPurifier.auto.php';
+	  }
+	  
+	  catch (PHPUnit_Framework_Error $expected) {
+	    $this->fail('You probably need to install htmlpurifier : '.$expected->getMessage());
+            return;
+	  }
+ 	  
+	}
+
+}
