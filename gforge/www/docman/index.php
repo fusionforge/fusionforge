70e4ee2a42073997b9c22b6d4166404626a1a143
Franck VILLAUME <franck.villaume@capgemini.com>
2010-06-03 07:45:57 +0000

    fix copyrights

--- gforge/www/docman/index.php
+++ gforge/www/docman/index.php
@@ -1,20 +1,27 @@
 <?php
 /**
- * GForge Doc Mgr Facility
+ * FusionForge Documentation Manager
  *
- * Copyright 2002 GForge, LLC
- * http://gforge.org/
+ * Copyright 2000, Quentin Cregan/Sourceforge
+ * Copyright 2002-2003, Tim Perdue/GForge, LLC
+ * Copyright 2010, Franck Villaume
  *
+ * This file is part of FusionForge.
+ *
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FusionForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
-
-/*
-	Document Manager
-
-	by Quentin Cregan, SourceForge 06/2000
-
-	Complete OO rewrite by Tim Perdue 1/2003
-*/
 
 require_once('../env.inc.php');
 require_once $gfwww.'include/pre.php';
--- gforge/www/docman/search.php
+++ gforge/www/docman/search.php
@@ -1,17 +1,28 @@
 <?php
 /**
- * GForge Doc Search engine
+ * FusionForge Documentation Manager
  *
- * Fabio Bertagnin November 2005
+ * Copyright 2000, Quentin Cregan/Sourceforge
+ * Copyright 2002-2003, Tim Perdue/GForge, LLC
+ * Copyright 2005, Fabio Bertagnin
+ * Copyright 2010, Franck Villaume
+ *
+ * This file is part of FusionForge.
+ *
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FusionForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
-
-/*
-	Document Search Motor
-
-	by Fabio Bertagnin
-
-*/
 
 require_once('../env.inc.php');
 require_once $gfwww.'include/pre.php';
