d21074e9caadcbcb31135d70709c3b9956841b28
Roland Mas <lolando@debian.org>
2010-06-17 13:00:36 +0000

    Added missing dependency on HTMLPurifier

--- src/fusionforge-install-1-deps.php
+++ src/fusionforge-install-1-deps.php
@@ -60,7 +60,7 @@ function installRHEL4() {
 function installDebian() {
 
 	INFO("Installing Packages with apt-get");
-	passthru("apt-get -y install apache2 php5 php5-cli php5-pgsql cvs postgresql postgresql-contrib libipc-run-perl liburi-perl libapache2-svn libapache2-mod-php5 subversion subversion-tools php5-curl curl ssh lsb-release");
+	passthru("apt-get -y install apache2 php5 php5-cli php5-pgsql cvs postgresql postgresql-contrib libipc-run-perl liburi-perl libapache2-svn libapache2-mod-php5 subversion subversion-tools php5-curl curl ssh lsb-release php-htmlpurifier");
 	passthru("a2enmod headers");
 	passthru("a2enmod proxy");
 	passthru("a2enmod ssl");
