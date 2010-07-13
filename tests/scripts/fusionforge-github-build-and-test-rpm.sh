7b068440f758f840ba95a13ca9b938ba13145c40
Alain Peyrat <aljeux@free.fr>
2010-07-13 19:42:16 +0000

    Add experimental hudson script for test suite

---
+++ tests/scripts/fusionforge-github-build-and-test-rpm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+export CONFIG_PHP=func/config.php.buildbot
+export SELENIUM_RC_HOST=192.168.0.204
+export SELENIUM_RC_DIR=$WORKSPACE/reports
+export SELENIUM_RC_URL=${HUDSON_URL}job/$JOB_NAME/ws/reports
+export FFORGE_RPM_REPO=${HUDSON_URL}job/$JOB_NAME/ws/build/packages
+export HOST=centos52.local
+export CONFIGURED=true
+
+rm -fr build/ reports/
+mkdir -p build/packages reports/coverage
+
+make -f Makefile.rh BUILDRESULT=$WORKSPACE/build/packages all
+
+cp gforge/rpm-specific/fusionforge.repo build/packages/fusionforge.repo
+sed -i "s#http://fusionforge.org/#${HUDSON_URL}#" build/packages/fusionforge.repo
+sed -i "s#baseurl = .*#baseurl = $FFORGE_RPM_REPO/#" build/packages/fusionforge.repo
+
+cd tests
+phpunit --log-junit $WORKSPACE/reports/phpunit-selenium.xml RPMCentos52Tests.php
+
+cd ..
+cp $WORKSPACE/reports/phpunit-selenium.xml $WORKSPACE/reports/phpunit-selenium.xml.org
+xalan -in $WORKSPACE/reports/phpunit-selenium.xml.org -xsl fix_phpunit.xslt -out $WORKSPACE/reports/phpunit-selenium.xml
