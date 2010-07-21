4fdd928356af70e02ec4f0239e543352cf107ba5
Alain Peyrat <aljeux@free.fr>
2010-07-21 18:39:25 +0000

    Add script for fusionforge-trunk-build-rpm hudson job

---
+++ tests/scripts/fusionforge-trunk-build-rpm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+export FFORGE_RPM_REPO=${HUDSON_URL}job/fusionforge-trunk-build-rpm/ws/build/packages
+
+rm -fr $WORKSPACE/build
+mkdir -p $WORKSPACE/build/packages
+
+cp source/gforge/rpm-specific/fusionforge.repo $WORKSPACE/build/packages/fusionforge.repo
+sed -i "s#http://fusionforge.org/#${HUDSON_URL}#" $WORKSPACE/build/packages/fusionforge.repo
+sed -i "s#baseurl = .*#baseurl = $FFORGE_RPM_REPO/#" $WORKSPACE/build/packages/fusionforge.repo
+
+cd source
+make -f Makefile.rh BUILDRESULT=$WORKSPACE/build/packages all
