abe74b5b8398c3be09e63b20f2313b7bb10c7aa6
Franck VILLAUME <franck.villaume@capgemini.com>
2010-09-01 11:50:21 +0000

    fix triggered redirect to login page : use div

--- src/www/account/login.php
+++ src/www/account/login.php
@@ -124,9 +124,9 @@ if ($login && !$success) {
 echo '<p>';
 
 if ($triggered) {
-	echo '<span class="error">' ;
+	echo '<div class="warning">' ;
 	echo _('You\'ve been redirected to this login page because you have tried accessing a page that was not available to you as an anonymous user.');
-	echo '</span> ' ;
+	echo '</div> ' ;
 }
 echo _('Cookies must be enabled past this point.');
 
