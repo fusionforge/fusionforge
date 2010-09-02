4d6160fb3d5ed354ded190a7eae26c6cd5a47cb7
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2010-09-02 20:05:27 +0000

    Restore session_require_perm and session_require_global_perm

--- src/plugins/wiki/www/themes/fusionforge/themeinfo.php
+++ src/plugins/wiki/www/themes/fusionforge/themeinfo.php
@@ -23,13 +23,13 @@ class WikiTheme_fusionforge extends WikiTheme_Wikilens {
         //group is private
         if (!$project->isPublic()) {
             //if it's a private group, you must be a member of that group
-            session_require(array('group'=>$group_id));
+            session_require_perm ('project_read', $group_id);
         }
 
         //for dead projects must be member of admin project
         if (!$project->isActive()) {
             //only SF group can view non-active, non-holding groups
-            session_require(array('group'=>'1'));
+            session_require_global_perm ('forge_admin');
         }
 
         $HTML->header(array('title'=> $group_public_name.': '.htmlspecialchars($pagename),
