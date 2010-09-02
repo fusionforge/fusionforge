3801915b0e887d7e5d8e13f2382bb25e1ff79ba3
Franck VILLAUME <franck.villaume@capgemini.com>
2010-09-02 09:25:47 +0000

    fix header

--- src/www/forum/index.php
+++ src/www/forum/index.php
@@ -1,21 +1,25 @@
 <?php
 /**
- * GForge Forums Facility
+ * Forums Facility
  *
- * Copyright 2002 GForge, LLC
- * http://gforge.org/
+ * Copyright 1999-2001, Tim Perdue - Sourceforge
+ * Copyright 2002, Tim Perdue - GForge, LLC
  *
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
-
-/*
-    Message Forums
-    By Tim Perdue, Sourceforge, 11/99
-
-    Massive rewrite by Tim Perdue 7/2000 (nested/views/save)
-
-    Complete OO rewrite by Tim Perdue 12/2002
-*/
 
 require_once('../env.inc.php');
 require_once $gfcommon.'include/pre.php';
