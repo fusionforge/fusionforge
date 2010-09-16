bc0db50c7753b01b30b7a432deb41d7e71a95c18
Alain Peyrat <aljeux@free.fr>
2010-09-16 19:04:39 +0000

    Add flipAll() js func to allow dynamic javascript switching.

--- src/www/js/common.js
+++ src/www/js/common.js
@@ -57,3 +57,15 @@ function switch2edit (a,show,edit) {
     elemdisp2.style.display='block'
   }
 }
+
+function flipAll(formObj) {
+  var isFirstSet = -1;
+  for (var i=0; i < formObj.length; i++) {
+      fldObj = formObj.elements[i];
+      if ((fldObj.type == 'checkbox') && (fldObj.name.substring(0,2) == 'p[')) {
+         if (isFirstSet == -1)
+           isFirstSet = (fldObj.checked) ? true : false;
+         fldObj.checked = (isFirstSet) ? false : true;
+       }
+   }
+}
