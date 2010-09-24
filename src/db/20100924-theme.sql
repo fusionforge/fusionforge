7b6cc440eedbb85a1741805bd56e16df9550043f
Franck VILLAUME <franck.villaume@capgemini.com>
2010-09-24 14:49:26 +0000

    add update theme_id for users

--- src/db/20100924-theme.sql
+++ src/db/20100924-theme.sql
@@ -1,3 +1,4 @@
+update users set theme_id = 1 where theme_id = 2 or theme_id = 4;
 delete from themes where theme_id = 2;
 delete from themes where theme_id = 4;
 insert into themes (dirname,fullname,enabled) values ('funky','Funky','t');
