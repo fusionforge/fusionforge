43c5acd1b970d3f54c13a45d228e076c50dec2f2
Franck VILLAUME <franck.villaume@capgemini.com>
2010-09-28 12:52:57 +0000

    mostly licence

--- src/www/trove/admin/trove_cat_add.php
+++ src/www/trove/admin/trove_cat_add.php
@@ -1,13 +1,26 @@
 <?php
 /**
-  *
-  * Site Admin: Trove Admin: add new leaf category
-  *
-  * SourceForge: Breaking Down the Barriers to Open Source Development
-  * Copyright 1999-2001 (c) VA Linux Systems
-  * http://sourceforge.net
-  *
-  */
+ * Trove Admin: add new leaf category
+ *
+ * Copyright 1999-2001 (c) VA Linux Systems
+ * http://fusionforge.org/
+ *
+ * This file is part of FusionForge.
+ *
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FusionForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 require_once('pre.php');
 require_once('www/include/trove.php');
@@ -34,9 +47,7 @@ if ($GLOBALS['submit']) {
 			);
 
 		if (!$res || db_affected_rows($res)<1) {
-			exit_error(
-				_('Error in Trove operation'),
-				db_error()
+			exit_error( _('Error in Trove operation: ').db_error(),'');
 			);
 		}
 	} 
