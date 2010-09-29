724feb237902e3aa2e4fa2a42264757da7902a3b
Thorsten Glaser <mirabilos@evolvis.org>
2010-09-29 07:56:49 +0000

    postgresql init script, the remaining cases merci roland for spotting this problem

--- src/deb-specific/install-db.sh
+++ src/deb-specific/install-db.sh
@@ -306,6 +306,7 @@ EOF
 	chmod 644 ${pg_hba_dir}/pg_hba.conf
 
 	# Trying "postgresql" init script...
+	v=0
 	invoke-rc.d postgresql restart || v=$?
 	if test x"$v" = x"100"; then
 		# No "postgresql" init script (for packages << 8.4.4-2)
@@ -329,6 +330,7 @@ EOF
         perl -pi -e "s/$localtrust\n//" ${pg_hba_dir}/pg_hba.conf
 
 	# Trying "postgresql" init script...
+	v=0
 	invoke-rc.d postgresql reload || v=$?
 	if test x"$v" = x"100"; then
 		# No "postgresql" init script (for packages << 8.4.4-2)
