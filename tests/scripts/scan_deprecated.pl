06d37983c95be993d9859c7844c8670254de4f81
Alain Peyrat <aljeux@free.fr>
2010-10-03 17:05:58 +0000

    Initial code to track deprecated use in code

---
+++ tests/scripts/scan_deprecated.pl
@@ -0,0 +1,47 @@
+#!/bin/perl
+
+use File::Find;
+
+# Scan for old exit_error syntax (second argument is not a tab name)
+sub scan_exit_error ($) {
+	local $content = shift;
+
+	@matches = ();
+	while ($content =~ /exit_error\s*\((.*?),([^,]*?)\s*\)\s*;(.*)/s) {
+		$content = $3;
+		($match = $2) =~ s/\s+/ /;
+		$match =~ s/^'(.*)'$/\1/;
+		push(@matches, $match) if ($match !~ /^(admin|home|my|frs|tracker|docman|forums|news|mail|pm|scm|trove|surveys|summary|)$/);
+	}
+
+	$k = "exit_error>".$File::Find::name.": ";
+	print $k.join("\n$k", @matches)."\n" if (@matches);
+}
+
+# Scan for getStringFromRequest used for *_id vars, should be getIntFromRequest intead.
+sub scan_getStringFromRequest ($) {
+	local $content = shift;
+
+	@matches = ();
+	while ($content =~ /(getStringFromRequest\s*\('[^']*?_id'\))\s*;(.*)/s) {
+		$content = $3;
+		push(@matches, $1);
+	}
+
+	$k = "getString>".$File::Find::name.": ";
+	print $k.join("\n$k", @matches)."\n" if (@matches);	
+}
+
+sub wanted {
+	next unless /\.php$/;
+	open(F, $_);
+	$content = join('', <F>);
+	close(F);
+
+	scan_exit_error($content);
+	scan_getStringFromRequest($content);
+}
+
+find(\&wanted, @ARGV);
+
+
