0a36e35c6e83393cc39d81d0c7e067adb281c136
Alain Peyrat <aljeux@free.fr>
2010-10-03 17:05:53 +0000

    Tests: clean commented code, caching now performed by a squid cache on buildbot

--- tests/func/Testing/RPMSeleniumRemoteSuite.php
+++ tests/func/Testing/RPMSeleniumRemoteSuite.php
@@ -19,18 +19,9 @@ class RPMSeleniumRemoteSuite extends SeleniumRemoteSuite
 			system("ssh root@".HOST." 'cd /etc/yum.repos.d/; wget ".getenv('FFORGE_RPM_REPO')."/fusionforge.repo'");
 		}
 
-		# Prefill yum cache to reduce real downloads.
-		//system("scp -rp ~/yum-cache-prefill/* root@".HOST.":/var/cache/yum/");
-		
-		# Enable yum cache to get back the cache for next run.
-		//sed -i 's/keepcache=0/keepcache=1/' /var/lib/vz/private/$i/etc/yum.conf 
-
 		sleep(5);
 		
 		system("ssh root@".HOST." 'yum install -y fusionforge fusionforge-scmsvn fusionforge-online_help fusionforge-extratabs fusionforge-ldapextauth fusionforge-scmgit fusionforge-blocks'");
-
-		//cp -rp /var/lib/vz/private/$i/var/cache/yum/* ~/yum-cache-prefill
-		//rm -fr ~/yum-cache-prefill/fusionforge
 	}
 }
 ?>
