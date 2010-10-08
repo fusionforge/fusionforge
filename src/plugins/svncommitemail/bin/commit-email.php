1a282d8a0344364d11423aa6105dc121b3b941c9
Alain Peyrat <aljeux@free.fr>
2010-10-08 16:31:11 +0000

    Handle overloading the sendmail command (for the test suite).

--- src/plugins/svncommitemail/bin/commit-email.php
+++ src/plugins/svncommitemail/bin/commit-email.php
@@ -10,6 +10,9 @@ $svnlook = '/usr/bin/svnlook';
 $commit_email_pl = dirname(__FILE__).'/commit-email.pl';
 # End configuration
 
+# Set sendmail path to next script from our configuration.
+putenv("SENDMAIL=$sys_sendmail_path");
+
 if(!file_exists($svnlook) || !file_exists($commit_email_pl)) { die("Missing required executables."); }
 
 # Find who made the changes
