1163c172c2d8ff69f0a23c455cd8763bf1c37c72
Christian Bayle <cbayle@gmail.com>
2010-10-10 19:26:44 +0000

    Stop alternate vm

--- tests/scripts/stop_vz.sh
+++ tests/scripts/stop_vz.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-sudo /root/stop_vz.sh "$1"
+if sudo /root/stop_vz.sh "$1"
+then
+	echo "VM removed"
+else
+	pwd
+	. ../openvz/config.default
+	if [ -f ../openvz/config.`hostname` ]
+	then
+		. ../openvz/config.`hostname`
+	fi
+	echo "Run the folowing to destroy test server :"
+	echo "sudo /usr/sbin/vzctl stop $VEIDCEN"
+	echo "sudo /usr/sbin/vzctl destroy $VEIDCEN"
+fi
