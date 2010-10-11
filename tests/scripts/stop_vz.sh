c2b9540284c9e654fa56eb878a8f2b6504355ac1
Christian Bayle <cbayle@gmail.com>
2010-10-11 22:38:11 +0000

    Stop a vm based on env variables

--- tests/scripts/stop_vz.sh
+++ tests/scripts/stop_vz.sh
@@ -4,13 +4,6 @@ if sudo /root/stop_vz.sh "$1"
 then
 	echo "VM removed"
 else
-	pwd
-	. ../openvz/config.default
-	if [ -f ../openvz/config.`hostname` ]
-	then
-		. ../openvz/config.`hostname`
-	fi
-	echo "Run the folowing to destroy test server :"
-	echo "sudo /usr/sbin/vzctl stop $VEIDCEN"
-	echo "sudo /usr/sbin/vzctl destroy $VEIDCEN"
+	sudo /usr/sbin/vzctl stop $VEID
+	sudo /usr/sbin/vzctl destroy $VEID
 fi
