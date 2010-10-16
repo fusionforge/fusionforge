eab98155bbe730e35675eaec44d84e8c9a10df68
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2010-10-16 08:11:18 +0000

    Update file header

--- src/www/notepad.php
+++ src/www/notepad.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
- * GForge Notepad Facility
+ * FusionForge Notepad Facility
  *
  * Copyright 2003 FUJITSU PRIME SOFTWARE TECHNOLOGIES LIMITED
  * http://www.pst.fujitsu.com/
  *
- * This file is part of GForge.
+ * This file is part of FusionForge.
  *
- * GForge is free software; you can redistribute it and/or modify
+ * FusionForge is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * GForge is distributed in the hope that it will be useful,
+ * FusionForge is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GForge; if not, write to the Free Software
+ * along with FusionForge; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 /**
- * GForge Notepad Window
+ * FusionForge Notepad Window
  * By Hidenari Miwa, FUJITSU PRIME SOFTWARE TECHNOLOGIES LIMITED, 09/2003
  */
 require_once('env.inc.php');
@@ -58,7 +58,7 @@ if ($form) {
 
 <html>
   <head>
-    <title>GForge Notepad</title>
+    <title>FusionForge Notepad</title>
    <script type="text/javascript">
    <!-- 
     function load_initial_value() {
