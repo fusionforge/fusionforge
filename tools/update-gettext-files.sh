ea4e685a758ebe958d70ff22a685c17a9998cf23
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2010-10-18 17:02:27 +0000

    gforge --> src

--- tools/update-gettext-files.sh
+++ tools/update-gettext-files.sh
@@ -1,16 +1,14 @@
-#! /bin/sh
-
-if [ -e gforge/translations/gforge.pot ] ; then        # We're in the parent dir
-    cd gforge
-elif [ -e ../gforge/translations/gforge.pot ] ; then   # We're in tools/ or gforge/
-    cd ../gforge
-elif [ -e ../translations/gforge.pot ] ; then          # In a subdir of gforge
+#!/bin/sh
+if [ -e src/translations/gforge.pot ] ; then        # We're in the parent dir
+    cd src
+elif [ -e ../src/translations/gforge.pot ] ; then   # We're in tools/ or src/
+    cd ../src
+elif [ -e ../translations/gforge.pot ] ; then       # In a subdir of src/
     cd ..
 else
     echo "Couldn't find translations directory..."
     exit 1
 fi
-
 if [ "$1" = --stats ] ; then
     utils/manage-translations.sh stats
 else
