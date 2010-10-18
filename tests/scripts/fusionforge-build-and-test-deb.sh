ccc12c070da8b15163972cac520c234183f59300
Christian Bayle <cbayle@gmail.com>
2010-10-18 18:00:31 +0000

    Add Missing ~

--- tests/scripts/fusionforge-build-and-test-deb.sh
+++ tests/scripts/fusionforge-build-and-test-deb.sh
@@ -17,7 +17,7 @@ then
 	export VZTEMPLATE=debian-$DEBVERS-$ARCH-minimal
 	export VZPRIVATEDIR
 	export DIST
-	export BASEDIR=${BASEDIR:-/`id -un`/ws}
+	export BASEDIR=${BASEDIR:-/~`id -un`/ws}
 	export SELENIUM_RC_URL=http://`hostname -f`$BASEDIR/reports
 	export FFORGE_DEB_REPO=http://`hostname -f`$BASEDIR/build/debian
 	export HOST=debian6.local
