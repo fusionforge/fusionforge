bdd81d9c822b14c579f9da4d6064804d7bad4b39
Alain Peyrat <aljeux@free.fr>
2010-11-02 21:20:30 +0000

    Massive renamed GForge => FusionForge in Headers

--- src/plugins/cvssyncmail/common/CVSSyncMailPlugin.class.php
+++ src/plugins/cvssyncmail/common/CVSSyncMailPlugin.class.php
@@ -17,7 +17,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GForge; if not, write to the Free Software
+ * along with FusionForge; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 
