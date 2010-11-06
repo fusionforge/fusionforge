029c839b63a1951defd8881ec5ffaef46173860e
Christian Bayle <cbayle@gmail.com>
2010-11-06 18:28:45 +0000

    Build 3rd-party/php-mail-mbox

--- tests/scripts/fusionforge-build-and-test-deb.sh
+++ tests/scripts/fusionforge-build-and-test-deb.sh
@@ -45,6 +45,7 @@ mkdir -p $WORKSPACE/build/packages
 [ ! -d $WORKSPACE/reports ] || rm -fr $WORKSPACE/reports
 mkdir -p $WORKSPACE/reports/coverage
 
+(cd 3rd-party/php-mail-mbox ; make -f Makefile.debian BUILDRESULT=$WORKSPACE/build/packages LOCALREPODEB=$WORKSPACE/build/debian rsqueeze)
 make -f Makefile.debian BUILDRESULT=$WORKSPACE/build/packages LOCALREPODEB=$WORKSPACE/build/debian rsqueeze
 
 cd tests
