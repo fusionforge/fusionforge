3d94d260ca8041db9403603e70b5095e95ba68c5
Christian Bayle <cbayle@gmail.com>
2010-11-06 23:50:43 +0000

    Quick fix for db user an name

--- src/etc/templates/database.py
+++ src/etc/templates/database.py
@@ -1,6 +1,8 @@
 # PLEASE DO NOT REMOVE THIS LINE
 sys_dbhost="{db_host}";
-sys_dbname="{db_name}";
-sys_dbuser="{db_user}";
+sys_dbname="gforge";
+sys_dbuser="gforge";
+#sys_dbname="{db_name}";
+#sys_dbuser="{db_user}";
 sys_dbpasswd="{db_password}";
 sys_ldap_passwd="{ldap_web_add_password}";
