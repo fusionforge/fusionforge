6a8ff51b9b97ac09ff4ce4d26f45634b94cee0af
Roland Mas <lolando@debian.org>
2010-11-09 10:23:25 +0000

    Restart DB server to drop existing connections before trying to drop the DB

--- tests/func/db_reload.sh
+++ tests/func/db_reload.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 invoke-rc.d apache2 stop
+invoke-rc.d postgresql restart
 su - postgres -c "dropdb gforge"
 su - postgres -c "pg_restore -C -d template1" < /root/dump
 invoke-rc.d apache2 start
