5d3ed02d01287935d2cf1b2c47f4f3f56507f563
Roland Mas <lolando@debian.org>
2010-11-09 21:29:57 +0000

    Filter out more messages from db-upgrade.pl

--- src/deb-specific/install-db.sh
+++ src/deb-specific/install-db.sh
@@ -252,7 +252,7 @@ EOF
 	fi
 
 	# Install/upgrade the database contents (tables and data)
-	/usr/share/gforge/bin/db-upgrade.pl 2>&1 | grep -v ^NOTICE: \
+	/usr/share/gforge/bin/db-upgrade.pl 2>&1 | grep -v ^NOTICE: | grep -v ^DETAIL: | grep -v ^WARNING: \
 	    && p=${PIPESTATUS[0]} \
 	    || p=${PIPESTATUS[0]}
 	if [ $p != 0 ] ; then
