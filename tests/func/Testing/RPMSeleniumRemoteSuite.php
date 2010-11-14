68d4bb31e3a8e5d57ca499f3ec1e750f1ea93322
Alain Peyrat <aljeux@free.fr>
2010-11-14 20:09:34 +0000

    RPM tests: Try to save/restore yum timedhosts file to use the same download servers (proxy caching)

--- tests/func/Testing/RPMSeleniumRemoteSuite.php
+++ tests/func/Testing/RPMSeleniumRemoteSuite.php
@@ -21,7 +21,13 @@ class RPMSeleniumRemoteSuite extends SeleniumRemoteSuite
 
 		sleep(5);
 		
+		if (is_file("/tmp/timedhosts.txt")) {
+			system("scp -p /tmp/timedhosts.txt root@".HOST.":/var/cache/yum/timedhosts.txt");
+		}
+
 		system("ssh root@".HOST." 'yum install -y fusionforge fusionforge-plugin-scmsvn fusionforge-plugin-online_help fusionforge-plugin-extratabs fusionforge-plugin-ldapextauth fusionforge-plugin-scmgit fusionforge-plugin-blocks'");
+
+		system("scp -p root@".HOST.":/var/cache/yum/timedhosts.txt /tmp/timedhosts.txt");
 
 		// Install a fake sendmail to catch all outgoing emails.
 		system("ssh root@".HOST." 'perl -spi -e s#/usr/sbin/sendmail#/usr/share/tests/scripts/catch_mail.php# /etc/gforge/local.inc'");
