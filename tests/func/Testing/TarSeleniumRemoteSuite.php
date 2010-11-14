f48baba1c1e7321dc62fe008de23c6361e987cd5
Alain Peyrat <aljeux@free.fr>
2010-11-14 21:42:40 +0000

    Tar tests: Try to save/restore yum timedhosts file to use the same download servers (proxy caching)

--- tests/func/Testing/TarSeleniumRemoteSuite.php
+++ tests/func/Testing/TarSeleniumRemoteSuite.php
@@ -12,7 +12,14 @@ class TarSeleniumRemoteSuite extends SeleniumRemoteSuite
 
 		system("scp ../../build/packages/fusionforge-*.tar.bz2 root@centos52:");
 		system("ssh root@centos52 'tar jxf fusionforge-*.tar.bz2'");
+
+		if (is_file("/tmp/timedhosts.txt")) {
+			system("scp -p /tmp/timedhosts.txt root@".HOST.":/var/cache/yum/timedhosts.txt");
+		}
+
 		system("ssh root@centos52 'cd fusionforge-*; FFORGE_RPM_REPO=http://buildbot.fusionforge.org/job/fusionforge-trunk-build-and-test-rpm/ws/build/packages/ FFORGE_DB=fforge FFORGE_USER=gforge FFORGE_ADMIN_USER=ffadmin FFORGE_ADMIN_PASSWORD=ffadmin ./install.sh centos52.local'");
+
+		system("scp -p root@".HOST.":/var/cache/yum/timedhosts.txt /tmp/timedhosts.txt");
 
 		// Install a fake sendmail to catch all outgoing emails.
 		system("ssh root@".HOST." 'perl -spi -e s#/usr/sbin/sendmail#/opt/tests/scripts/catch_mail.php# /etc/gforge/local.inc'");
