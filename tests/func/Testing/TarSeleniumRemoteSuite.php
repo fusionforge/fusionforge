cf3c03ab61564b06becba99fa14f630ab35c3329
Alain Peyrat <aljeux@free.fr>
2010-11-23 18:06:14 +0000

    Use rpms from 5.1 branch for tar tests

--- tests/func/Testing/TarSeleniumRemoteSuite.php
+++ tests/func/Testing/TarSeleniumRemoteSuite.php
@@ -17,7 +17,7 @@ class TarSeleniumRemoteSuite extends SeleniumRemoteSuite
 			system("scp -p /tmp/timedhosts.txt root@".HOST.":/var/cache/yum/timedhosts.txt");
 		}
 
-		system("ssh root@centos52 'cd fusionforge-*; FFORGE_RPM_REPO=http://buildbot.fusionforge.org/job/fusionforge-trunk-build-and-test-rpm/ws/build/packages/ FFORGE_DB=fforge FFORGE_USER=gforge FFORGE_ADMIN_USER=ffadmin FFORGE_ADMIN_PASSWORD=ffadmin ./install.sh centos52.local'");
+		system("ssh root@centos52 'cd fusionforge-*; FFORGE_RPM_REPO=http://buildbot.fusionforge.org/job/fusionforge-Branch_5_1-build-and-test-rpm/ws/build/packages/ FFORGE_DB=fforge FFORGE_USER=gforge FFORGE_ADMIN_USER=ffadmin FFORGE_ADMIN_PASSWORD=ffadmin ./install.sh centos52.local'");
 
 		system("scp -p root@".HOST.":/var/cache/yum/timedhosts.txt /tmp/timedhosts.txt");
 
