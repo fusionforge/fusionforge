61922118b83bd5d6fcee2630d044d6ae1f87a1f1
Franck VILLAUME <franck.villaume@capgemini.com>
2010-11-26 10:35:23 +0000

    fix proper groupisactivecheckboxpost call: must return true

--- src/common/include/Plugin.class.php
+++ src/common/include/Plugin.class.php
@@ -109,26 +109,26 @@ class Plugin extends Error {
 	}
 
 	/**
-	 *  getGroups -  get a list of all groups using a plugin
+	 * getGroups -  get a list of all groups using a plugin
 	 *
-	 *  @return	array	array containing group ids
+	 * @return	array	array containing group ids
 	 */
 	function getGroups() {
-		$result = array () ;
+		$result = array() ;
 		$res = db_query_params ('SELECT group_plugin.group_id
-				           FROM group_plugin, plugins
-                                           WHERE group_plugin.plugin_id=plugins.plugin_id
-                                             AND plugins.plugin_name=$1
-                                         ORDER BY group_plugin.group_id ASC',
-					array ($this->name));
+					FROM group_plugin, plugins
+					WHERE group_plugin.plugin_id=plugins.plugin_id
+					AND plugins.plugin_name=$1
+					ORDER BY group_plugin.group_id ASC',
+					array($this->name));
 		$rows = db_numrows($res);
 		
 		for ($i=0; $i<$rows; $i++) {
 			$group_id = db_result($res,$i,'group_id');
-			$result[] = group_get_object ($group_id) ;
+			$result[] = group_get_object ($group_id);
 		}
 		
-		return $result ;
+		return $result;
 	}
 
 	function getThemePath(){
@@ -159,15 +159,19 @@ class Plugin extends Error {
 		echo "</tr>";
 	}
 
-	function groupisactivecheckboxpost (&$params) {
+	/*
+	 * @return	bool	actually only true ...
+	 */
+	function groupisactivecheckboxpost(&$params) {
 		// this code actually activates/deactivates the plugin after the form was submitted in the project edit public info page
 		$group = group_get_object($params['group']);
 		$flag = strtolower('use_'.$this->name);
 		if ( getStringFromRequest($flag) == 1 ) {
-			$group->setPluginUse ( $this->name );
+			$group->setPluginUse($this->name);
 		} else {
-			$group->setPluginUse ( $this->name, false );
+			$group->setPluginUse($this->name, false);
 		}
+		return true;
 	}
 
 	function userisactivecheckbox (&$params) {
