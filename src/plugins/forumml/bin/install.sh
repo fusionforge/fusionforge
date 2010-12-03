d70eeb2642896a653119be1c5762af1c4d3335cd
Christian Bayle <cbayle@gmail.com>
2010-12-03 14:59:17 +0000

    Restart exim4 and mailman daemons

--- src/plugins/forumml/bin/install.sh
+++ src/plugins/forumml/bin/install.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 /usr/share/gforge/plugins/forumml/bin/installFF.sh
 /usr/share/gforge/plugins/forumml/bin/ml_arch_2_DBFF.pl
+invoke-rc.d exim4 restart || true
+invoke-rc.d mailman restart || true
