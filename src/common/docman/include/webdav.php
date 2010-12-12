3036521efea6a78ad7e55349f0fc53123d3dfee2
Franck VILLAUME <franck.villaume@capgemini.com>
2010-12-12 20:51:39 +0000

    fix utf-8 encoding

--- src/common/docman/include/webdav.php
+++ src/common/docman/include/webdav.php
@@ -39,7 +39,7 @@ class HTTP_WebDAV_Server_Docman extends HTTP_WebDAV_Server {
 	 * @return	bool	success
 	 */
 	function checkAuth($group_id, $user, $pass) {
-		$g =& group_get_object($group_id);
+		$g = group_get_object($group_id);
 		if (!$g || !is_object($g))
 			return false;
 
@@ -214,6 +214,7 @@ class HTTP_WebDAV_Server_Docman extends HTTP_WebDAV_Server {
 		$analysed_path = $this->analyse($subpath, $group_id);
 
 		if ($analysed_path['isdir']) {
+			echo '<?xml version="1.0" encoding="utf-8"?>';
 			echo "<html><head><title>Index of ".htmlspecialchars($subpath)."</title></head>\n";
 			echo "<body>\n";
 			echo "<h1>Index of ".htmlspecialchars($subpath)."</h1>\n";
