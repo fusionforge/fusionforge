3de8079fbb1d3d3a215bd35ce94a2e6c66a6a49f
Roland Mas <lolando@debian.org>
2010-12-21 09:01:47 +0000

    Fixed 'else' problem

--- src/utils/apply-coding-style.sh
+++ src/utils/apply-coding-style.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
 # EXPERIMENTAL script to enforce coding style guidelines
-# There are still things to fix (including the removal of too many newlines)
+# There are still things to fix
 # DO NOT APPLY BLINDLY!
 
 # Things to fix:
 # * long lines (such as appear with long lists of parameters to functions) are unwrapped
-#   even when they were split across several lines previously
-# * "} else {" is turned into "}\nelse {"
+#   even when they were previously split across several lines
 
 set -e
 
@@ -61,6 +60,17 @@ if [ ! -x /tmp/PHP_Beautifier/scripts/php_beautifier ] ; then
          } else {
              $this->oBeaut->add($sTag);
          }
+--- a/Beautifier/Filter/IndentStyles.filter.php
++++ b/Beautifier/Filter/IndentStyles.filter.php
+@@ -265,7 +265,7 @@ class PHP_Beautifier_Filter_IndentStyles extends PHP_Beautifier_Filter
+     {
+         if ($this->oBeaut->getPreviousTokenContent() == '}') {
+             $this->oBeaut->removeWhitespace();
+-            $this->oBeaut->addNewLineIndent();
++            $this->oBeaut->add(' ');
+             $this->oBeaut->add(trim($sTag));
+             if (!$this->oBeaut->isNextTokenContent('{')) {
+                     $this->oBeaut->add(' ');
 EOF
 fi
 
