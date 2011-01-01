1d1a63564203ce384a2f49f34d9510602e1b634e
Alain Peyrat <aljeux@free.fr>
2011-01-01 20:20:59 +0000

    Apply fix [#247] Fix first.php to make it display $forge_name rather than $%1s by Sylvain Le Gall

--- src/www/account/first.php
+++ src/www/account/first.php
@@ -26,14 +26,15 @@
 require_once('../env.inc.php');
 require_once $gfcommon.'include/pre.php';
 
-site_user_header(array('title'=>sprintf(_('Welcome to %1$s'), forge_get_config ('forge_name'))));
+$forge_name = forge_get_config ('forge_name');
+site_user_header(array('title'=>sprintf(_('Welcome to %1$s'), $forge_name)));
 
-print '<p>' . _('You are now a registered user on %1$s, the online development environment for Open Source projects.') . '</p>';
+print '<p>' . sprintf(_('You are now a registered user on %1$s, the online development environment for Open Source projects.'), $forge_name) . '</p>';
 
-print '<p>' . _('As a registered user, you can participate fully in the activities on the site. You may now post messages to the project message forums, post bugs for software in %1$s, sign on as a project developer, or even start your own project.') . '</p>';
+print '<p>' . sprintf(_('As a registered user, you can participate fully in the activities on the site. You may now post messages to the project message forums, post bugs for software in %1$s, sign on as a project developer, or even start your own project.'), $forge_name) . '</p>';
 
 print '<p>';
-printf(_('--the %1$s staff.'), forge_get_config ('forge_name'));
+printf(_('--the %1$s staff.'), $forge_name);
 print '</p>';
 
 site_user_footer(array());
