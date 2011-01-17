698193235e118c89c07ea44418bcecdc5d416541
Roland Mas <lolando@debian.org>
2011-01-17 10:43:21 +0000

    Disable SSL on buildbot

--- tests/func/Testing/RPMSeleniumRemoteSuite.php
+++ tests/func/Testing/RPMSeleniumRemoteSuite.php
@@ -28,6 +28,7 @@ class RPMSeleniumRemoteSuite extends SeleniumRemoteSuite
 		system("ssh root@".HOST." 'yum install -y fusionforge fusionforge-plugin-scmsvn fusionforge-plugin-online_help fusionforge-plugin-extratabs fusionforge-plugin-ldapextauth fusionforge-plugin-scmgit fusionforge-plugin-blocks'");
 
 		system("scp -p root@".HOST.":/var/cache/yum/timedhosts.txt /tmp/timedhosts.txt");
+		system("ssh root@".HOST." '(echo [core];echo use_ssl=no) > /etc/gforge/config.ini.d/zzz-builbot.ini'");
 
 		// Install a fake sendmail to catch all outgoing emails.
 		system("ssh root@".HOST." 'perl -spi -e s#/usr/sbin/sendmail#/usr/share/tests/scripts/catch_mail.php# /etc/gforge/local.inc'");
