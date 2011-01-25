34988f71bc8b9eff86af1a41fa3d3a6d0fc66d19
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-01-25 13:13:56 +0000

    Add header to make things a little bit more explicit

--- src/plugins/mailman/var/lib/mailman/lists/extend.py
+++ src/plugins/mailman/var/lib/mailman/lists/extend.py
@@ -1,3 +1,12 @@
+
+#
+# This file is part of the FusionForge mailman plugin, extending
+# mailman to allow the use of its postgresql database for user
+# retrieval
+#
+# Copyright + License : FIXME
+#
+
 from Mailman.PsycopgConnector import PsycopgConnector
 from Mailman.ForgeSecurityManager import ForgeSecurityManager
 import sys
