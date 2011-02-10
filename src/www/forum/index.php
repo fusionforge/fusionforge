bab985732d809dd3c6cd7d5540806dfc10221857
Franck VILLAUME <franck.villaume@capgemini.com>
2011-02-10 14:38:51 +0000

    fix missing attr

--- src/www/forum/index.php
+++ src/www/forum/index.php
@@ -61,9 +61,9 @@ if ($group_id) {
 
 //	echo _('<p>Choose a forum and you can browse, search, and post messages.<p>');
 
-	echo $HTML->printsubMenu(array(_("My Monitored Forums")),array("/forum/myforums.php?group_id=$group_id"));
+	echo $HTML->printsubMenu(array(_("My Monitored Forums")), array("/forum/myforums.php?group_id=$group_id"), false);
 
-	plugin_hook ("blocks", "forum index");
+	plugin_hook("blocks", "forum index");
 
 	$tablearr=array(_('Forum'),_('Description'),_('Threads'),_('Posts'), _('Last Post'),_('Moderation Level'));
 	echo $HTML->listTableTop($tablearr);
