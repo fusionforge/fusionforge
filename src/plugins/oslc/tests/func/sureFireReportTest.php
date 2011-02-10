b36cdb2e032f478489b95cfd8f8d734896b47fcf
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-02-10 12:57:34 +0000

    It works ! ;-)

--- src/plugins/oslc/tests/func/sureFireReportTest.php
+++ src/plugins/oslc/tests/func/sureFireReportTest.php
@@ -6,17 +6,25 @@ require_once dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/te
 // provider JUnit test suite by the Maven Surefire plugin to detect if
 // tests are passed as before (non-regression)
 
-class Example extends FForge_SeleniumTestCase
+class SureFireReports extends FForge_SeleniumTestCase
 {
-  protected function setUp()
-  {
-    $this->setBrowser("*chrome");
-    $this->setBrowserUrl("https://forge.local/plugins/oslc/surefire/surefire-report.html");
-  }
 
   // Test the ServiceProviderCatalogTests results
-  public function testServiceProviderCatalogTests()
+  public function testServiceProviderCatalogTestsResults()
   {
+
+    // Make sure there's a project with a tracker
+    $this->populateStandardTemplate('trackers');
+    $this->init();
+
+    // start the OSLC test suite
+    echo "\nStarting OSLC test suite\n";
+    $directory = dirname(dirname(__FILE__));
+    echo "Executing " . $directory . "/run-provider-tests.sh\n";
+    system('cd '. $directory . '; ./run-provider-tests.sh');
+    echo "OSLC test suite executed\n\n";
+
+    // Check the results in the generated HTML report
     $this->open("/plugins/oslc/surefire/surefire-report.html");
     $this->click("link=net.openservices.provider.test.oslcv1tests");
 
@@ -25,7 +33,7 @@ class Example extends FForge_SeleniumTestCase
     $this->assertEquals("ServiceProviderCatalogTests", $this->getText("//div[@id='contentBox']/div[3]/div/h3[contains(.,'net.openservices.provider.test.oslcv1tests')]/../table/tbody/tr[2]/td[2]"));
 
     // Tests
-    $this->assertEquals("42", $this->getText("//div[@id='contentBox']/div[3]/div/h3[contains(.,'net.openservices.provider.test.oslcv1tests')]/../table/tbody/tr[2]/td[3]"));
+    $this->assertEquals("56", $this->getText("//div[@id='contentBox']/div[3]/div/h3[contains(.,'net.openservices.provider.test.oslcv1tests')]/../table/tbody/tr[2]/td[3]"));
     // Errors
     $this->assertEquals("0", $this->getText("//div[@id='contentBox']/div[3]/div/h3[contains(.,'net.openservices.provider.test.oslcv1tests')]/../table/tbody/tr[2]/td[4]"));
     // Failures
@@ -34,4 +42,5 @@ class Example extends FForge_SeleniumTestCase
     $this->assertEquals("0", $this->getText("//div[@id='contentBox']/div[3]/div/h3[contains(.,'net.openservices.provider.test.oslcv1tests')]/../table/tbody/tr[2]/td[6]"));
   }
 }
+
 ?>
