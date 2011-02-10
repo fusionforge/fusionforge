7aebdd64bc2c76d5e853cef8a16cc90677a7f79d
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-02-10 12:56:26 +0000

    Add a bit more verbose messages about what is done

--- tests/func/db_reload.sh
+++ tests/func/db_reload.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
+echo "Cleaning up the database"
 invoke-rc.d apache2 stop
+
 invoke-rc.d postgresql restart
-su - postgres -c "dropdb gforge"
+su - postgres -c "dropdb -e gforge"
+echo "Executing: pg_restore -C -d template1 < /root/dump"
 su - postgres -c "pg_restore -C -d template1" < /root/dump
+
 invoke-rc.d apache2 start
