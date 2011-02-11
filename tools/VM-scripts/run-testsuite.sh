258d6d939cb9b5b387ad6570cba3be8f11461bc3
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-02-11 10:35:18 +0000

    Remove the OSLC test suite from here since it should now be run from inside phpunit+Selenium's tests

--- tools/VM-scripts/run-testsuite.sh
+++ tools/VM-scripts/run-testsuite.sh
@@ -11,10 +11,3 @@ else
 	phpunit --verbose $i
     done
 fi
-
-# If available, run the JUnit OSLC provider test suite
-if [ -d /root/fusionforge/src/plugins/oslc/tests ]; then
-    cd /root/fusionforge/src/plugins/oslc/tests
-    ./setup-provider-test.sh
-    ./run-provider-tests.sh
-fi
