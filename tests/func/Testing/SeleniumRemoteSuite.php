1bd152313e16e532064b2a934a24cd48a6494df6
Christian Bayle <cbayle@gmail.com>
2011-02-27 17:45:25 +0000

    use HOST instead of hardcoded host

--- tests/func/Testing/SeleniumRemoteSuite.php
+++ tests/func/Testing/SeleniumRemoteSuite.php
@@ -18,8 +18,8 @@ class SeleniumRemoteSuite extends PHPUnit_Framework_TestSuite
 
 		//system("scp /usr/share/php/PHPUnit/Extensions/SeleniumTestCase/*pend.php root@centos52:/opt/tests");
 		//system("scp /usr/share/php/PHPUnit/Extensions/SeleniumTestCase/phpunit_coverage.php root@centos52:/opt/gforge/www");
-		//system("ssh root@centos52 'perl -spi -e \'s!^auto_prepend_file.*!auto_prepend_file=/opt/tests/prepend.php!\' /etc/php.ini');
-		//system("ssh root@centos52 'perl -spi -e \'s!^auto_append_file.*!auto_append_file=/opt/tests/append.php!\' /etc/php.ini');
+		//system("ssh root@".HOST." 'perl -spi -e \'s!^auto_prepend_file.*!auto_prepend_file=/opt/tests/prepend.php!\' /etc/php.ini');
+		//system("ssh root@".HOST." 'perl -spi -e \'s!^auto_append_file.*!auto_append_file=/opt/tests/append.php!\' /etc/php.ini');
 
 	}
 
