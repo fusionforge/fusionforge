7cc6fcc3e984b638f2857e9a2d4c060673acb6ee
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-03-01 18:11:32 +0000

    Somme comments/docs

--- src/common/include/PFO-RBAC.interface.php
+++ src/common/include/PFO-RBAC.interface.php
@@ -23,14 +23,28 @@
  * USA
  */
 
+// See http://wiki.planetforge.org/index.php/RBAC_API#Interfaces
+
 // Constants to identify role classes
 define ("PFO_ROLE_EXPLICIT",  1) ;
 define ("PFO_ROLE_ANONYMOUS", 2) ;
 define ("PFO_ROLE_LOGGEDIN",  3) ;
 define ("PFO_ROLE_UNION",     4) ;
 
-// Interface for the RBAC engine
+/**
+ * Interface for the RBAC engine
+ * @author Roland Mas
+ *
+ * This interface is meant to be implemented with a singleton pattern. 
+ * Its methods use the session management to decide what roles are available within the current session (if any), 
+ * and to provide the answer to the question “Does the current client have the permission for this action?”. 
+ * Other interesting questions that this interface is meant to answer include “does another account have the permission for that action?” 
+ * and, more generically, “who is allowed that action?”. 
+ */
 interface PFO_RBACEngine {
+	/**
+	 * singleton creator
+	 */
 	public static function getInstance() ;
 	public function getAvailableRoles() ; // From session
 	public function isActionAllowed($section, $reference, $action = NULL) ;
@@ -41,7 +55,12 @@ interface PFO_RBACEngine {
 	public function getUsersByAllowedAction($section, $reference, $action = NULL) ;
 }
 
-// Interfaces for the capabilities
+/**
+ * Interfaces for the capabilities
+ * @author Roland Mas
+ *
+ * Abstract interface, not meant to be implemented directly. 
+ */
 interface PFO_Role {
 	public function getName() ;
 	public function setName($name) ;
@@ -49,6 +68,10 @@ interface PFO_Role {
 
 	public function isPublic() ;
 	public function setPublic($flag) ;
+	/**
+	 * TODO: Enter description here ...
+	 * NULL if role is “floating” 
+	 */
 	public function getHomeProject() ;
 	public function getLinkedProjects() ;
 	public function linkProject($project) ;
@@ -64,22 +87,46 @@ interface PFO_Role {
 	public function setSettings($data) ;
 }
 
+/**
+ * Standard, explicit membership role (members are list of usernames). 
+ * @author Roland Mas
+ *
+ */
 interface PFO_RoleExplicit extends PFO_Role {
 	const roleclass = PFO_ROLE_EXPLICIT ;
 	public function addUsers($users) ;
 	public function removeUsers($users) ;
 }
 
+/**
+ * Union of roles.
+ * @author Roland Mas
+ *
+ */
 interface PFO_RoleUnion extends PFO_Role {
 	const roleclass = PFO_ROLE_UNION ;
 	public function addRole($role) ;
 	public function removeRole($role) ;
 }
 
+/**
+ * Implicit membership role : always applying
+ * 
+ * Global scope (public, no home project), always available (even when logged in). hasUser() always returns true. 
+ * @author Roland Mas
+ *
+ */
 interface PFO_RoleAnonymous extends PFO_Role {
 	const roleclass = PFO_ROLE_ANONYMOUS ;
 }
 
+/**
+ * Implicit membership role : the client has opened a session
+ * 
+ * Global scope (public, no home project), available whenever a valid session is opened. hasUser() always returns true.
+ * @author Roland Mas
+ *
+ */
 interface PFO_RoleLoggedin extends PFO_Role {
 	const roleclass = PFO_ROLE_LOGGEDIN ;
 }
