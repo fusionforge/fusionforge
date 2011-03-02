d6656ef9ce786c357929604ce55ed812eb2c1a83
Christian Bayle <cbayle@gmail.com>
2011-03-02 11:56:47 +0000

    hostname if fqdn

--- tests/scripts/start_lxc.sh
+++ tests/scripts/start_lxc.sh
@@ -25,7 +25,7 @@ sudo /usr/bin/lxc-create -n $HOST -f $lxcdir/config.$LXCDEBTEMPLATE -t $LXCDEBTE
 sudo /usr/lib/lxc/templates/lxc-$LXCDEBTEMPLATE.postinst -p /var/lib/lxc/$HOST -n $HOST --address=$IPDEBBASE.$VEIDDEB --netmask=$IPDEBMASK --gateway=$IPDEBGW --pubkey=$SSHPUBKEY --hostkeydir=$HOSTKEYDIR
 sudo /usr/bin/lxc-start -n $HOST -d
 
-ssh -o 'StrictHostKeyChecking=no' "root@$HOST.local" uname -a
+ssh -o 'StrictHostKeyChecking=no' "root@$HOST" uname -a
 ret=$?
 for loop in 1 2 3 4 5 6 7 8 9
 do
@@ -33,7 +33,7 @@ do
 	if [ $ret -ne 0 ];then
 		echo -n $loop
 		sleep 10;
-		ssh -o 'StrictHostKeyChecking=no' "root@$HOST.local" uname -a
+		ssh -o 'StrictHostKeyChecking=no' "root@$HOST" uname -a
 		ret=$?
 	fi
 done
