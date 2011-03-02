fd1331ff93bdd4c8519fb04e8a4a1f37a05315b3
Christian Bayle <cbayle@gmail.com>
2011-03-02 10:26:19 +0000

    Define lxcdir

--- tests/scripts/start_lxc.sh
+++ tests/scripts/start_lxc.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
 # You need to allow current user to sudo on /usr/bin/lxc-create /usr/bin/lxc-start /usr/lib/lxc/templates/lxc-debian6.postinst
+configdir=`dirname $0`/../config
+lxcdir=`dirname $0`/../lxc
 
 if [ -z "$LXCDEBTEMPLATE" ]
 then
-	configdir=`dirname $0`/../config
 	. $configdir/default
 	if [ -f $configdir/`hostname` ] ; then . $configdir/`hostname`; fi
 fi
@@ -20,6 +21,6 @@ then
 	fi
 fi
 
-sudo /usr/bin/lxc-create -n $HOST -f ../lxc/config.$LXCDEBTEMPLATE -t $LXCDEBTEMPLATE
+sudo /usr/bin/lxc-create -n $HOST -f $lxcdir/config.$LXCDEBTEMPLATE -t $LXCDEBTEMPLATE
 sudo /usr/lib/lxc/templates/lxc-$LXCDEBTEMPLATE.postinst -n $HOST --address=$IPDEBBASE.$VEIDDEB --netmask=$IPDEBMASK --gateway=$IPDEBGW --pubkey=$SSHPUBKEY --hostkeydir=$HOSTKEYDIR
 sudo /usr/bin/lxc-start -n $HOST -d
