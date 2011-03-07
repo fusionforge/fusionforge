a0f71821a059419a7e67b892c3c9c5108507fe05
Christian Bayle <cbayle@gmail.com>
2011-03-07 18:34:56 +0000

    guess the rc.d launcher

--- tests/func/db_reload.sh
+++ tests/func/db_reload.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
 
 echo "Cleaning up the database"
-invoke-rc.d apache2 stop
+if type invoke-rc.d 
+then
+	invoke-rc.d apache2 stop
+else
+	service httpd stop
+fi
 
 invoke-rc.d postgresql restart
 su - postgres -c "dropdb -e gforge"
 echo "Executing: pg_restore -C -d template1 < /root/dump"
 su - postgres -c "pg_restore -C -d template1" < /root/dump
 
-invoke-rc.d apache2 start
+if type invoke-rc.d 
+then
+	invoke-rc.d apache2 start
+else
+	service httpd start
+fi
