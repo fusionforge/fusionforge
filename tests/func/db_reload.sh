a9e41f2bf6a496d80fddad9dd3897826bfa9345b
Christian Bayle <cbayle@gmail.com>
2011-03-07 18:35:46 +0000

    don't display stderr

--- tests/func/db_reload.sh
+++ tests/func/db_reload.sh
@@ -2,7 +2,7 @@
 [ $# -eq 1 ] || exit 1
 
 echo "Cleaning up the database"
-if type invoke-rc.d 
+if type invoke-rc.d 2>/dev/null
 then
 	invoke-rc.d apache2 stop
 else
@@ -14,7 +14,7 @@ su - postgres -c "dropdb -e $1"
 echo "Executing: pg_restore -C -d template1 < /root/dump"
 su - postgres -c "pg_restore -C -d template1" < /root/dump
 
-if type invoke-rc.d 
+if type invoke-rc.d 2>/dev/null
 then
 	invoke-rc.d apache2 start
 else
