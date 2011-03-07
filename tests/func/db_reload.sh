f110374388bbf08e79357342a89e40d036bec7a6
Christian Bayle <cbayle@gmail.com>
2011-03-07 18:35:34 +0000

    now db_reload.sh, takes one arg

--- tests/func/db_reload.sh
+++ tests/func/db_reload.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+[ $# -eq 1 ] || exit 1
 
 echo "Cleaning up the database"
 if type invoke-rc.d 
@@ -9,7 +10,7 @@ else
 fi
 
 invoke-rc.d postgresql restart
-su - postgres -c "dropdb -e gforge"
+su - postgres -c "dropdb -e $1"
 echo "Executing: pg_restore -C -d template1 < /root/dump"
 su - postgres -c "pg_restore -C -d template1" < /root/dump
 
