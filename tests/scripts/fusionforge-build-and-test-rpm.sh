b1fcbfb55b12dd59afd626bf71ca6b0d2426a33e
Christian Bayle <cbayle@gmail.com>
2011-03-07 23:37:04 +0000

    Rename centos52.local as centos5.local

--- tests/scripts/fusionforge-build-and-test-rpm.sh
+++ tests/scripts/fusionforge-build-and-test-rpm.sh
@@ -41,7 +41,6 @@ then
 else
 	export SELENIUM_RC_URL=${HUDSON_URL}job/$JOB_NAME/ws/reports
 	export FFORGE_RPM_REPO=${HUDSON_URL}job/$JOB_NAME/ws/build/packages
-	export HOST=centos52.local
 	export VZTEMPLATE=centos-5-x86
 fi
 export DB_NAME=gforge
