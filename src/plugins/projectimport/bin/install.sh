1a715c18699644ab6edc60fc1936b7be352c710b
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-03-08 18:56:07 +0000

    Add chmod for www-data

---
+++ src/plugins/projectimport/bin/install.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+# 
+# Configure projectimport plugin
+
+# invoked by plugin.postinst
+
+set -e
+
+if [ $(id -u) != 0 ] ; then
+    echo "You must be root to run this, please enter passwd"
+    exec su -c "$0 $1"
+fi
+
+case "$1" in
+    configure)
+	chmod www-data:www-data /var/lib/gforge/plugins/projectimport
+	;;
+
+    *)
+	echo "Usage: $0 {configure}"
+	exit 1
+esac
