0b6a55e0e3307b54532b3bfc4f86d9744fd5795a
Christian Bayle <cbayle@gmail.com>
2011-03-08 13:49:45 +0000

    guess proper postgresql restart

--- tests/func/db_reload.sh
+++ tests/func/db_reload.sh
@@ -5,11 +5,12 @@ echo "Cleaning up the database"
 if type invoke-rc.d 2>/dev/null
 then
 	invoke-rc.d apache2 stop
+	invoke-rc.d postgresql restart
 else
 	service httpd stop
+	service postgresql restart
 fi
 
-invoke-rc.d postgresql restart
 su - postgres -c "dropdb -e $1"
 echo "Executing: pg_restore -C -d template1 < /root/dump"
 su - postgres -c "pg_restore -C -d template1" < /root/dump
