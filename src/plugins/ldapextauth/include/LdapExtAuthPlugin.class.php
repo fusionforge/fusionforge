cf471ad62423f0895ac512da9138bb44d97256f5
Roland Mas <lolando@debian.org>
2011-03-14 16:47:12 +0000

    Packaging+config for ldapextauth

--- src/plugins/ldapextauth/include/LdapExtAuthPlugin.class.php
+++ src/plugins/ldapextauth/include/LdapExtAuthPlugin.class.php
@@ -47,76 +47,25 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 		$this->_addHook("close_auth_session");
 
 		$this->cookie_name = 'forge_session_ldapextauth';
-		
-		$this->ldap_conn = false ;
-		$this->base_dn = '';
-		$this->user_dn = '';
-		$this->ldap_server = forge_get_config('ldap_host') ;
-		$this->ldap_port = forge_get_config('ldap_port') ;
-		$this->ldap_altserver = '';
-		$this->ldap_altport = '';
-		$this->ldap_start_tls = false;
-		$this->ldap_bind_dn = '';
-		$this->ldap_bind_pwd = '';
-		$this->ldap_skip_users = '';
-		// require_once $GLOBALS['gfconfig'].'plugins/ldapextauth/config.php' ;
-		if (isset($base_dn)) {
-			$this->base_dn = $base_dn ;
-		}
-		if (isset($user_dn)) {
-			$this->user_dn = $user_dn ;
-		}
-		if (isset($ldap_altserver)) {
-			$this->ldap_altserver = $ldap_altserver ;
-		}
-		if (isset($ldap_altport)) {
-			$this->ldap_altport = $ldap_altport ;
-		}
-		if (isset($ldap_start_tls)) {
-			$this->ldap_start_tls = $ldap_start_tls ;
-		}
-		if (isset($ldap_bind_pwd)) {
-			$this->ldap_bind_pwd = $ldap_bind_pwd;
-		}
-		if (isset($ldap_skip_users)) {
-			// Array of login not managed by LDAP (local account).
-			$this->ldap_skip_users = $ldap_skip_users ;
-		}
 
+		$this->ldap_conn = false ;
 		$this->saved_login = '';
 		$this->saved_password = '';
 		$this->saved_user = NULL;
 		$this->saved_data = array();
+
+		$this->declareConfigVars();
 	}
 	
-	function CallHook ($hookname, &$params) {
-		switch ($hookname) {
-		case "session_before_login":
-			// Authenticate against LDAP
-			debuglog("\nLDAP: CallHook ($hookname) - ". date("F j, Y, g:i a") );
-			$loginname = $params['loginname'] ;
-			$passwd = $params['passwd'] ;
-			// Skip users from LDAP check (local account).
-			if (in_array($loginname, $this->ldap_skip_users))
-				return true;
-			return $this->AuthUser ($loginname, $passwd) ;
-			break;
-		default:
-			return parent::CallHook($hookname, $params);
-		}
-	}
-
 	function syncAccountInfo($params) {
 		if (!$this->syncDataOn($params['event'])) {
 			return true;
 		}
 		$u = $params['user'];
-		$u->setEmail('toto@tata.com');
 		$data = $this->saved_data;
 
 		if ($u) {
 			if ($u->getStatus() == 'D') {
-				debuglog("Account deleted, reactivating it.");
 				$u->setStatus('A');
 			}
 			if (!session_login_valid_dbonly ($this->saved_login, $this->saved_password, false)) {
@@ -149,22 +98,40 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 </form>' ;
 	}
 
+	protected function declareConfigVars() {
+		parent::declareConfigVars();
+
+		forge_define_config_item ('start_tls', $this->name, 'no');
+		forge_set_config_item_bool ('start_tls', $this->name) ;
+
+		forge_define_config_item ('sync_data_as_user', $this->name, 'yes');
+		forge_set_config_item_bool ('sync_data_as_user', $this->name) ;
+
+		forge_define_config_item ('ldap_server', $this->name, 'ldap.example.com');
+		forge_define_config_item ('ldap_port', $this->name, 389);
+		forge_define_config_item ('base_dn', $this->name, 'ou=users,dc=example,dc=com');
+		forge_define_config_item ('user_dn', $this->name, 'uid=');
+		forge_define_config_item ('skipped_users', $this->name, '');
+		forge_define_config_item ('manager_dn', $this->name, 'uid=');
+		forge_define_config_item ('manager_password', $this->name, 'uid=');
+	}
+
 	/// HELPERS
 
 	function checkLDAPCredentials($loginname, $passwd) {
 		if (!$this->ldap_conn) {
-			$r = $this->ConnectLdap($this->ldap_server, $this->ldap_port);
-			if (!$r && $this->ldap_altserver) {
-				ldap_close($this->ldap_conn);
-				$r = $this->ConnectLdap($this->ldap_altserver, $this->ldap_altport);
-			}
+			$r = $this->ConnectLdap(forge_get_config('ldap_server', $this->name), forge_get_config('ldap_port', $this->name));
 			if (!$r) {
 				// No connection to LDAP directory
-				return FORGE_AUTH_NOT_AUTHORITATIVE;
+				if ($this->isRequired()) {
+					return FORGE_AUTH_AUTHORITATIVE_REJECT;
+				} else {
+					return FORGE_AUTH_NOT_AUTHORITATIVE;
+				}
 			}
 		}
 
-		$res = ldap_search($this->ldap_conn, $this->base_dn, $this->user_dn . $loginname) ;
+		$res = ldap_search($this->ldap_conn, forge_get_config('base_dn', $this->name), forge_get_config('user_dn', $this->name) . $loginname) ;
 		if (!$res || ldap_count_entries($this->ldap_conn, $res) == 0) {
 			// No user by that name in LDAP directory
 			return FORGE_AUTH_AUTHORITATIVE_REJECT;
@@ -184,28 +151,21 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 	}
 
 	function ConnectLDAP($server, $port) {
-
-		debuglog("LDAP: ldap_connect($server,$port)");
 		if ($port) {
 			$this->ldap_conn = ldap_connect ($server, $port);
 		} else {
 			$this->ldap_conn = ldap_connect ($server);
 		}
-		debuglog("LDAP: Ldap handle: ".$this->ldap_conn);
 
 		if (forge_get_config('ldap_version')) {
-			debuglog("LDAP: ldap_set_option ($this->ldap_conn, LDAP_OPT_PROTOCOL_VERSION, forge_get_config('ldap_version'));");
 			if (!ldap_set_option ($this->ldap_conn, LDAP_OPT_PROTOCOL_VERSION, forge_get_config('ldap_version'))) {
-				debuglog("LDAP: ldap_set_option() failed: ".ldap_error($this->ldap_conn));
 				return false;
 			}
 		}
 
-		if ($this->ldap_start_tls) {
-			debuglog("LDAP: ldap_start_tls($this->ldap_conn)");
+		if (forge_get_config('start_tls', $this->name)) {
 			if (!ldap_start_tls($this->ldap_conn)) {
-				syslog(LOG_ERR, "FusionForge: LDAP start_tls failed: ".ldap_error($this->ldap_conn));
-				debuglog("LDAP: ldap_start_tls() failed: ".ldap_error($this->ldap_conn));
+				error_log("LDAP start_tls failed: ".ldap_error($this->ldap_conn));
 				return false;
 			}
 		}
@@ -213,10 +173,8 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 		// If the ldap server does not allow anonymous bind,
 		// then authentificate with the server.
 		if ($this->ldap_bind_dn) {
-			debuglog("LDAP: ldap_bind() (application bind)");
 			if (!@ldap_bind($this->ldap_conn, $this->ldap_bind_dn, $this->ldap_bind_pwd)) {
-				debuglog("LDAP: ldap_bind() failed (application bind): ". ldap_error($this->ldap_conn));
-				syslog(LOG_ERR, "FusionForge:LDAP application bind failed, using DB login/passwd instead.");
+				error_log("LDAP application bind failed.");
 				return false;
 			}
 		}
@@ -229,19 +187,13 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 	function AuthUser ($loginname, $passwd) {
 		global $feedback;
 
-		debuglog("LDAP: AuthUser ($loginname)");
-
 		if  (!function_exists ( "ldap_connect" )) {
-			debuglog("LDAP: No ldap_connect function, ldap is missing in your php.");
+			error_log("No ldap_connect function, ldapextauth can't proceed.");
 			return false;
 		}
 
 		if (!$this->ldap_conn) {
-			$r = $this->ConnectLdap($this->ldap_server, $this->ldap_port);
-			if (!$r && $this->ldap_altserver) {
-				ldap_close($this->ldap_conn);
-				$r = $this->ConnectLdap($this->ldap_altserver, $this->ldap_altport);
-			}
+			$r = $this->ConnectLdap(forge_get_config('ldap_server', $this->name), forge_get_config('ldap_port', $this->name));
 			if (!$r) {
 				// Unable to connect to LDAP server(s), use internal login.
 				$GLOBALS['ldap_auth_failed']=true;
@@ -250,15 +202,11 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 		}
 
 		// Search LDAP for user account.
-		debuglog("LDAP: Searching for $loginname");
-		$res = ldap_search($this->ldap_conn, $this->base_dn, $this->user_dn . $loginname) ;
-		debuglog("LDAP: ldap_search ($this->ldap_conn, $this->base_dn, $this->user_dn . $loginname)");
-		debuglog("LDAP: Search handle is: $res");
+		$res = ldap_search($this->ldap_conn, forge_get_config('base_dn', $this->name), forge_get_config('user_dn', $this->name) . $loginname) ;
 
 		if (!$res) {
 			// User not found in LDAP => Account invalid
 			@ldap_unbind($this->ldap_conn);
-			debuglog("LDAP: Wrong password according to LDAP ($loginname)");
 			$feedback=_('Invalid Password Or User Name');
 			$GLOBALS['ldap_auth_failed']=true;
 			return false ;
@@ -267,7 +215,6 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 		$nb_result = ldap_count_entries($this->ldap_conn, $res);
 		if ($nb_result!==1) {
 			@ldap_unbind($this->ldap_conn);
-		        debuglog("LDAP: ldap_count_entries() returned $nb_result values");
 			$GLOBALS['ldap_auth_failed']=true;
 			return false ;
 		}
@@ -275,22 +222,16 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 		$info = ldap_get_entries ($this->ldap_conn,$res);
 		$dn = $info[0]['dn'];
 
-		debuglog("LDAP: dn=$dn");
-
 		$u = user_get_object_by_name ($loginname) ;
 
 		if ($u) {
-			debuglog("LDAP: User is present in database");
-
 			// User exists in DB
 			if (@ldap_bind($this->ldap_conn, $dn, $passwd)) {
-				debuglog("LDAP: ldap_bind() ok (user bind)");
 				// Password from form is valid in LDAP
 
 				// If account has been deleted but user is valid in LDAP,
 				// then reactivate the account.
 				if ($u->getStatus() == 'D') {
-					debuglog("Account deleted, reactivating it.");
 					$u->setStatus('A');
 				}
 
@@ -314,23 +255,17 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 					return true;
 				}
 				// Wrong password according to LDAP
-				debuglog("LDAP: Wrong password according to LDAP ($loginname)");
 				$feedback=_('Invalid Password Or User Name');
 				$GLOBALS['ldap_auth_failed']=true;
 				return false ;
 			}
 		} else {
-			debuglog("LDAP: User is not present in database\n");
-
 			// User doesn't exist in DB yet
 			if (@ldap_bind($this->ldap_conn, $dn, $passwd))
 			{
 
 				$ldapentry = $info[0] ;
 
-				debuglog("=> \$info => ".  var_export($info, true));
-				debuglog("=> \$info[cn] => ".  $ldapentry['dn']);
-				
 				$mappedinfo = plugin_ldapextauth_mapping ($ldapentry) ;
 				
 				// Insert into DB
@@ -404,7 +339,6 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 					$theme_id = $mappedinfo['themeid'] ;
 				}
 
-				debuglog("creating account for $unix_name");
 				if (!$u->create ($unix_name,$firstname,$lastname,$password1,$password2,$email,
 					    $mail_site,$mail_va,$language_id,$timezone,$jabber_address,$jabber_only,$theme_id,
 					    $unix_box, $address, $address2, $phone, $fax, $title, $ccode, $send_mail)) {
@@ -413,10 +347,8 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 					return false;
 				}
 
-				debuglog("activating account for $unix_name");
 				if (!$u->setStatus ('A')) {
 					$GLOBALS['ldap_auth_failed']=true;
-					debuglog("u->setStatus('A') failed: ".$u->getErrorMessage());
 					$feedback = "<br>Error Activating User: ".$u->getErrorMessage();
 					return false;
 				}
@@ -431,12 +363,6 @@ class LdapextauthPlugin extends ForgeAuthPlugin {
 		}
 	}
 
-}
-
-function debuglog($msg) {
-	$fp = fopen("/tmp/ldap.log", "a+");
-	fwrite ($fp, $msg."\n");
-	fclose($fp);
 }
 
 // Local Variables:
