dec412b36f8e85ea71b23b31057a24033a324dc3
Franck VILLAUME <franck.villaume@capgemini.com>
2011-03-20 19:04:24 +0000

    reactivate submenu for layout configuration

--- src/www/my/index.php
+++ src/www/my/index.php
@@ -5,6 +5,7 @@
  * Copyright 1999-2001, VA Linux Systems, Inc.
  * Copyright 2002-2004, GForge Team
  * Copyright 2009, Roland Mas
+ * Copyright 2011, Franck Villaume - TrivialDev
  *
  * This file is part of FusionForge.
  *
@@ -31,16 +32,31 @@ require_once $gfwww.'include/vote_function.php';
 require_once $gfcommon.'tracker/ArtifactsForUser.class.php';
 require_once $gfcommon.'forum/ForumsForUser.class.php';
 require_once $gfcommon.'pm/ProjectTasksForUser.class.php';
-require_once('common/widget/WidgetLayoutManager.class.php');
+require_once $gfcommon.'widget/WidgetLayoutManager.class.php';
+
 if (!session_loggedin()) { // || $sf_user_hash) {
-
 	exit_not_logged_in();
-
 }
 
 use_javascript('/tabber/tabber.js');
+site_user_header(array('title'=>sprintf(_('Personal Page For %s'), user_getname())));
 
-site_user_header(array('title'=>sprintf(_('Personal Page For %s'),user_getname())));
+$sql = "SELECT l.*
+		FROM layouts AS l INNER JOIN owner_layouts AS o ON(l.id = o.layout_id)
+		WHERE o.owner_type = $1
+		AND o.owner_id = $2
+		AND o.is_default = 1
+		";
+$res = db_query_params($sql,array('u', user_getid()));
+$layout_id = db_result($res, 0 , 'id');
+
+echo '<ul class="widget_toolbar">';
+$url = "/widgets/widgets.php?owner=u".user_getid().
+	"&amp;layout_id=".$layout_id;
+echo '	<li ><a href="'. $url .'">'. _("Add widgets") .'</a></li>';
+echo '	<li><a href="'. $url.'&amp;update=layout' .'">'. _("Customize layout") .'</a></li>';
+echo '</ul>';
+
 
 $lm = new WidgetLayoutManager();
 $lm->displayLayout(user_getid(), WidgetLayoutManager::OWNER_TYPE_USER);
