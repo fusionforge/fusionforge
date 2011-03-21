aea6029323b8dfd92d4239bbd1ecbbec99f08343
Olivier Berger <olivier.berger@it-sudparis.eu>
2011-03-21 13:48:22 +0000

    Adding early draft for Auth OpenID plugin

---
+++ src/plugins/authopenid/include/authopenid-init.php
@@ -0,0 +1,38 @@
+<?php
+
+/** External authentication via OpenID for FusionForge
+ *
+ * Copyright 2011, Olivier Berger & Institut Telecom
+ *
+ * This program was developped in the frame of the COCLICO project
+ * (http://www.coclico-project.org/) with financial support of the Paris
+ * Region council.
+ *
+ * This plugin, like FusionForge, is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FusionForge; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
+ */
+
+global $gfplugins;
+require_once $gfplugins.'authcas/include/AuthOpenIDPlugin.class.php' ;
+
+$AuthOpenIDPluginObject = new AuthOpenIDPlugin ;
+
+register_plugin ($AuthOpenIDPluginObject) ;
+
+// Local Variables:
+// mode: php
+// c-file-style: "bsd"
+// End:
+
+?>
