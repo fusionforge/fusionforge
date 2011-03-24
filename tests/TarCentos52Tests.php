b315ef4537dc3b1b7fd73ca9fd19889269c01e2a
Alain Peyrat <aljeux@free.fr>
2011-03-24 20:14:42 +0000

    Temporary disable PluginsSvnTracker tests, not compatible with remote launch (cronjob)

--- tests/TarCentos52Tests.php
+++ tests/TarCentos52Tests.php
@@ -27,7 +27,7 @@ class TarCentos52Tests
 		$suite->addTestFiles(glob("func/News/*Test.php"));
 		$suite->addTestFiles(glob("func/PluginsBlocks/*Test.php"));
 		$suite->addTestFiles(glob("func/PluginsOnlineHelp/*Test.php"));
-		$suite->addTestFiles(glob("func/PluginsSvnTracker/*Test.php"));
+//		$suite->addTestFiles(glob("func/PluginsSvnTracker/*Test.php"));
 		$suite->addTestFiles(glob("func/RBAC/*Test.php"));
 		$suite->addTestFiles(glob("func/Surveys/*Test.php"));
 
