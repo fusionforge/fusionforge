531d5a2be1b6664668e27fee3b3326bbaaef3cee
Franck VILLAUME <franck.villaume@capgemini.com>
2011-04-06 08:12:39 +0000

    remove forum_attachment_type

--- src/db/20110405-forum_attachment_fix-bug284.sql
+++ src/db/20110405-forum_attachment_fix-bug284.sql
@@ -1,7 +1,5 @@
 alter table forum_attachment alter column mimetype type text;
 alter table forum_attachment alter column mimetype set default 'application/octet-stream';
-alter table forum_attachment_type alter column mimetype type text;
-alter table forum_attachment_type alter column mimetype set default 'application/octet-stream';
 alter table forum_pending_attachment alter column mimetype type text;
 alter table forum_pending_attachment alter column mimetype set default 'application/octet-stream';
 
