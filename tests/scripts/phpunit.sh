1ea04d4d0be06f3b09e3523e01b6123485bff55d
Christian Bayle <cbayle@gmail.com>
2011-04-21 13:19:47 +0000

    allow several args to be passed to phpunit

--- tests/scripts/phpunit.sh
+++ tests/scripts/phpunit.sh
@@ -2,6 +2,7 @@
 if [ $# -ge 1 ]
 then
         testsuite=$1
+	shift
 else
         echo "You must give the testsuite to run :"
 	echo "	- DEBDebian60Tests.php"
@@ -75,7 +76,7 @@ killall -9 java
 LANG=C java -jar selenium-server.jar -browserSessionReuse -singleWindow >/dev/null &
 #LANG=C java -jar selenium-server.jar -singleWindow >/dev/null &
 cd tests
-phpunit --log-junit $SELENIUM_RC_DIR/phpunit-selenium.xml $2 $testsuite || retcode=$?
+phpunit --log-junit $SELENIUM_RC_DIR/phpunit-selenium.xml $@ $testsuite || retcode=$?
 cd ..
 # on debian
 killall -9 firefox-bin
