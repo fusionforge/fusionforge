41c26e8ee59b1116087eeda8cc18f9bac694ef58
Franck VILLAUME <franck.villaume@capgemini.com>
2011-05-18 13:07:23 +0000

    fix ordering in tree

--- src/plugins/projects_hierarchy/common/projects_hierarchyPlugin.class.php
+++ src/plugins/projects_hierarchy/common/projects_hierarchyPlugin.class.php
@@ -101,7 +101,7 @@ class projects_hierarchyPlugin extends Plugin {
 
 	function buildTree() {
 		global $project_name;
-		$res = db_query_params('select p1.group_id as father_id,p1.unix_group_name as father_unix_name,p1.group_name as father_name,p2.group_id as son_id,p2.unix_group_name as son_unix_name,p2.group_name as son_name from groups as p1,groups as p2,plugin_projects_hierarchy where p1.group_id=plugin_projects_hierarchy.project_id and p2.group_id=plugin_projects_hierarchy.sub_project_id and plugin_projects_hierarchy.activated=$1 AND plugin_projects_hierarchy.link_type=$2',
+		$res = db_query_params('select p1.group_id as father_id,p1.unix_group_name as father_unix_name,p1.group_name as father_name,p2.group_id as son_id,p2.unix_group_name as son_unix_name,p2.group_name as son_name from groups as p1,groups as p2,plugin_projects_hierarchy where p1.group_id=plugin_projects_hierarchy.project_id and p2.group_id=plugin_projects_hierarchy.sub_project_id and plugin_projects_hierarchy.activated=$1 AND plugin_projects_hierarchy.link_type=$2 order by father_name, son_name',
 			array ('t',
 				'shar'));
 		echo db_error();
