06f7084894633ce578ea9bebbf5ac86d9018e9ae
Thorsten Glaser <mirabilos@evolvis.org>
2011-05-19 15:34:16 +0000

    make util_{p,tt}wrap accept a second argument to override 'p' e.g. with 'div'

--- src/common/include/utils.php
+++ src/common/include/utils.php
@@ -1404,23 +1404,25 @@ if ( !function_exists('sys_get_temp_dir')) {
 }
 
 /* convert '\n' to <br /> or </p><p> */
-function util_pwrap($encoded_string) {
-	return str_replace("<p></p>", "",
-	    str_replace("<p><br />", "<p>",
-	    "<p>" . str_replace("<br /><br />", "</p><p>",
+function util_pwrap($encoded_string,$tag='p') {
+	return str_replace("<".$tag."></".$tag.">", "",
+	    str_replace("<".$tag."><br />", "<".$tag.">",
+	    "<".$tag.">" . str_replace("<br /><br />",
+	    "</".$tag."><".$tag.">",
 	    implode("<br />",
 	    explode("\n",
 	    $encoded_string
-	    ))) . "</p>"));
+	    ))) . "</".$tag.">"));
 }
-function util_ttwrap($encoded_string) {
-	return str_replace("<p><tt></tt></p>", "",
-	    str_replace("<p><tt><br />", "<p><tt>",
-	    "<p><tt>" . str_replace("<br /><br />", "</tt></p><p><tt>",
+function util_ttwrap($encoded_string,$tag='p') {
+	return str_replace("<".$tag."><tt></tt></".$tag.">", "",
+	    str_replace("<".$tag."><tt><br />", "<".$tag."><tt>",
+	    "<".$tag."><tt>" . str_replace("<br /><br />",
+	    "</tt></".$tag."><".$tag."><tt>",
 	    implode("<br />",
 	    explode("\n",
 	    $encoded_string
-	    ))) . "</tt></p>"));
+	    ))) . "</tt></".$tag.">"));
 }
 
 /* takes a string and returns it HTML encoded, URIs made to hrefs */
