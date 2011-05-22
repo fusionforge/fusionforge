feb049d49eedaf2efb137585fe9008bf826c10aa
Franck VILLAUME <franck.villaume@capgemini.com>
2011-05-22 11:38:42 +0000

    fix switch: missing break

--- src/plugins/projects_hierarchy/common/projects_hierarchyPlugin.class.php
+++ src/plugins/projects_hierarchy/common/projects_hierarchyPlugin.class.php
@@ -6,12 +6,11 @@
  * Copyright 2010-2011, Franck Villaume - Capgemini
  * http://fusionforge.org
  *
- * This file is part of FusionForge.
- *
- * FusionForge is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
  *
  * FusionForge is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,7 +18,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
@@ -170,10 +169,12 @@ class projects_hierarchyPlugin extends Plugin {
 			case "parent": {
 				$res = db_query_params('SELECT project_id as id FROM plugin_projects_hierarchy WHERE sub_project_id = $1',
 							array($group_id));
+				break;
 			}
 			case "child": {
 				$res = db_query_params('SELECT sub_project_id as id FROM plugin_projects_hierarchy WHERE project_id = $1',
 							array($group_id));
+				break;
 			}
 			default: {
 				return $localFamily;
