9c8b0af9516997b1ed150e685a6a0995aa53fee1
Christian Bayle <cbayle@gmail.com>
2011-05-24 08:23:48 +0000

    Revert db_reload changes, suspected to make build fail

--- tests/scripts/fusionforge-build-and-test-deb.sh
+++ tests/scripts/fusionforge-build-and-test-deb.sh
@@ -113,8 +113,8 @@ echo "Set forge admin password"
 ssh root@$HOST "/usr/share/gforge/bin/forge_set_password admin myadmin"
 ssh root@$HOST "LANG=C a2dissite default ; LANG=C invoke-rc.d apache2 reload ; LANG=C touch /tmp/fusionforge-use-pfo-rbac"
 ssh root@$HOST "(echo [core];echo use_ssl=no) > /etc/gforge/config.ini.d/zzz-builbot.ini"
-#ssh root@$HOST "su - postgres -c \"pg_dump -Fc $DB_NAME\" > /root/dump"
-ssh root@$HOST "su - postgres -c \"pg_dumpall\" > /root/dump"
+ssh root@$HOST "su - postgres -c \"pg_dump -Fc $DB_NAME\" > /root/dump"
+#ssh root@$HOST "su - postgres -c \"pg_dumpall\" > /root/dump"
 ssh root@$HOST "invoke-rc.d cron stop" || true
 
 retcode=0
