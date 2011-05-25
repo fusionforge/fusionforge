fusionforge/fusionforge

commit f6f2af360c3ef682dd234834a2b2f5c364cecb43

Make user list more readable in the project member admin page
git-svn-id: svn+ssh://scm.fusionforge.org/srv/svn/fusionforge/branches/Branch_5_1@13461 9d84d37e-dcb1-4aad-b103-6f3d92f53bf6

src/www/project/admin/users.php
@@ -315,7 +315,10 @@ echo '<th>'._('Role').'</th>';
 echo '<th style="text-align:right">'._('Action').'</th>';
 echo '</tr></thead><tbody>';
 
+$i = 0;
 foreach ($members as $user) {
+	$i++;
+	
 	$roles = array () ;
 	foreach (RBACEngine::getInstance()->getAvailableRolesForUser ($user) as $role) {
 		if ($role->getHomeProject() && $role->getHomeProject()->getID() == $group->getID()) {
@@ -325,9 +328,9 @@ foreach ($members as $user) {
 
 	sortRoleList ($roles) ;
 
+	echo '<tr '. $HTML->boxGetAltRowStyle($i) . '>' ;
 	$seen = false ;
 	foreach ($roles as $role) {
-		echo '<tr>' ;
 		if (!$seen) {
 			echo '<td style="white-space: nowrap;" rowspan="'.(count($roles)+1).'">
 			<a href="/users/'.$user->getUnixName().'">';
@@ -355,7 +358,7 @@ foreach ($members as $user) {
 		</div></td></tr>';
 	}
 
-	echo '<tr><td colspan="2">
+	echo '<tr '. $HTML->boxGetAltRowStyle($i) . '><td colspan="2">
 		<form action="'.getStringFromServer('PHP_SELF').'" method="post">
 		<input type="hidden" name="submit" value="y" />
 		<input type="hidden" name="form_unix_name" value="'.$user->getUnixName().'" />

commit 62b0574cc6435729954cb47de016a5c6f14a84a7

fix XHTML on a few more pages…
… including unbreak www/project/admin/users.php when someone
has multiple roles in a project, broken in r13461

Thanks to Rhonda for noticing the optical problem.

src/www/admin/userlist.php
@@ -63,6 +63,10 @@ function performAction($newStatus, $statusString, $user_id) {
 }
 
 function show_users_list ($users, $filter='') {
+	if (!count($users)) {
+		return;
+	}
+
 	echo '<p>' ._('Key') .':
 		<span class="active">'._('Active'). '</span>
 		<span class="deleted">' ._('Deleted') .'</span>

src/www/include/html.php
@@ -414,12 +414,12 @@ function html_build_select_box_from_arrays ($vals,$texts,$select_name,$checked_v
 	//we don't always want the default Any row shown
 	if ($show_any) {
 		$return .= '
-		<option value=""'.(($checked_val=='') ? ' selected="selected"' : '').'>'. $text_any .'</option>';
+		<option value=""'.(($checked_val=='') ? ' selected="selected"' : '').'>'. util_html_secure($text_any) .'</option>';
 	}
 	//we don't always want the default 100 row shown
 	if ($show_100) {
 		$return .= '
-		<option value="100"'.(($checked_val==100) ? ' selected="selected"' : '').'>'. $text_100 .'</option>';
+		<option value="100"'.(($checked_val==100) ? ' selected="selected"' : '').'>'. util_html_secure($text_100) .'</option>';
 	}
 
 	$checked_found=false;
@@ -429,7 +429,7 @@ function html_build_select_box_from_arrays ($vals,$texts,$select_name,$checked_v
 		//  if it was shown above, otherwise do show it
 		if (($vals[$i] != '100') || ($vals[$i] == '100' && !$show_100)) {
 			$return .= '
-				<option value="'.$vals[$i].'"';
+				<option value="'.util_html_secure($vals[$i]).'"';
 			if ((string)$vals[$i] == (string)$checked_val) {
 				$checked_found=true;
 				$return .= ' selected="selected"';
@@ -437,7 +437,7 @@ function html_build_select_box_from_arrays ($vals,$texts,$select_name,$checked_v
 			if (is_array($allowed) && !in_array($vals[$i], $allowed)) {
 				$return .= ' disabled="disabled" class="option_disabled"';
 			}
-			$return .= '>'./*htmlspecialchars(*/$texts[$i]/*)*/.'</option>';
+			$return .= '>'.util_html_secure($texts[$i]).'</option>';
 		}
 	}
 	//
@@ -446,7 +446,7 @@ function html_build_select_box_from_arrays ($vals,$texts,$select_name,$checked_v
 	//
 	if (!$checked_found && $checked_val != 'xzxz' && $checked_val && $checked_val != 100) {
 		$return .= '
-		<option value="'.$checked_val.'" selected="selected">'._('No Change').'</option>';
+		<option value="'.util_html_secure($checked_val).'" selected="selected">'._('No Change').'</option>';
 	}
 
 	$return .= '

src/www/project/admin/roleedit.php
@@ -5,6 +5,8 @@
  * Copyright 2004 (c) GForge LLC
  * Copyright 2010, Roland Mas
  * Copyright (C) 2011 Alain Peyrat - Alcatel-Lucent
+ * Copyright © 2011
+ *	Thorsten Glaser <t.glaser@tarent.de>
  *
  * This file is part of FusionForge. FusionForge is free software;
  * you can redistribute it and/or modify it under the terms of the
@@ -163,12 +165,12 @@ if (USE_PFO_RBAC) {
 		echo '<p><strong>'._('Role Name').'</strong></p>' ;
 		echo $role->getDisplayableName ($group) ;
 	} else {
-		echo '<p><strong>'._('Role Name').'</strong><br /><input type="text" name="role_name" value="'.$role->getName().'"><br />' ;
+		echo '<p><strong>'._('Role Name').'</strong><br /><input type="text" name="role_name" value="'.$role->getName().'" /><br />' ;
 		echo '<input type="checkbox" name="public" value="1"' ;
 		if ($role->isPublic()) {
-			echo ' checked' ;
+			echo ' checked="checked"' ;
 		}
-		echo '> '._('Shared role (can be referenced by other projects)').'</p>' ;
+		echo ' /> '._('Shared role (can be referenced by other projects)').'</p>' ;
 	}
 } else {
 	if ($role_id != 'observer') {

src/www/project/admin/users.php
@@ -328,9 +328,9 @@ foreach ($members as $user) {
 
 	sortRoleList ($roles) ;
 
-	echo '<tr '. $HTML->boxGetAltRowStyle($i) . '>' ;
 	$seen = false ;
 	foreach ($roles as $role) {
+		echo '<tr '. $HTML->boxGetAltRowStyle($i) . '>' ;
 		if (!$seen) {
 			echo '<td style="white-space: nowrap;" rowspan="'.(count($roles)+1).'">
 			<a href="/users/'.$user->getUnixName().'">';