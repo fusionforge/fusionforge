3386eef8adb2f49c99be40ee3153c0e75447767f
Christian Bayle <cbayle@gmail.com>
2011-06-30 11:48:15 +0000

    Cleanup, not yet operational

--- tests/scripts/fusionforge-build-and-test-src-cos.sh
+++ tests/scripts/fusionforge-build-and-test-src-cos.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 export CURDIR=`pwd`
 export WORKSPACE=${WORKSPACE:-$CURDIR}
@@ -26,10 +26,8 @@ then
 	export BASEDIR=${BASEDIR:-/~`id -un`/ws}
 	export SELENIUM_RC_HOST=localhost
 	export SELENIUM_RC_URL=http://`hostname -f`$BASEDIR/reports
-	export FFORGE_RPM_REPO=http://`hostname -f`$BASEDIR/build/packages
 else
 	export SELENIUM_RC_URL=${HUDSON_URL}job/$JOB_NAME/ws/reports
-	export FFORGE_RPM_REPO=${HUDSON_URL}job/$JOB_NAME/ws/build/packages
 fi
 
 export DB_NAME=fforge
@@ -37,62 +35,85 @@ export CONFIGURED=true
 
 export BUILDRESULT=$WORKSPACE/build/packages
 
-[ ! -d $WORKSPACE/build/packages ] || rm -fr $WORKSPACE/build/packages
+# Create place to build package if necessary
+[ ! -d $WORKSPACE/build/packages ] || mkdir -p $WORKSPACE/build/packages
+
+# Erase config
 [ ! -d $WORKSPACE/build/config ] || rm -fr $WORKSPACE/build/config
+mkdir -p $WORKSPACE/build/config
+
+# Erase reports
 [ ! -d $WORKSPACE/reports ] || rm -fr $WORKSPACE/reports
+mkdir -p $WORKSPACE/reports/coverage
+
+# Erase apidocs
 [ ! -d $WORKSPACE/apidocs ] || rm -fr $WORKSPACE/apidocs
-mkdir -p $WORKSPACE/build/packages $WORKSPACE/build/config $WORKSPACE/reports/coverage $WORKSPACE/apidocs
+mkdir -p $WORKSPACE/apidocs
 
 #[ ! -e $HOME/doxygen-1.6.3/bin/doxygen ] || make build-doc DOCSDIR=$WORKSPACE/apidocs DOXYGEN=$HOME/doxygen-1.6.3/bin/doxygen
 #make BUILDRESULT=$WORKSPACE/build/packages buildtar
 #make -f Makefile.rh BUILDRESULT=$WORKSPACE/build/packages src
 
-(cd 3rd-party/selenium ; make getselenium)
-
 if $KEEPVM
 then
+	# VM can already exist
+	echo "Starting vm $HOST"
+	tests/scripts/start_vm $HOST
+else 
+	# Destroy the VM if found
 	echo "Destroying vm $HOST"
 	tests/scripts/stop_vm $HOST || true
 fi
 
-tests/scripts/start_vm $HOST
+# BUILD FUSIONFORGE REPO
+echo "Build FUSIONFORGE REPO"
+make -C 3rd-party -f Makefile.rh BUILDRESULT=$WORKSPACE/build/packages
 
 # FUSIONFORGE REPO
-cp src/rpm-specific/fusionforge.repo $WORKSPACE/build/packages/fusionforge.repo
-sed -i "s#http://fusionforge.org/#${HUDSON_URL}#" $WORKSPACE/build/packages/fusionforge.repo
 if [ ! -z "$FFORGE_RPM_REPO" ]
 then
+	echo "Installing specific FUSIONFORGE REPO $FFORGE_RPM_REPO"
+	cp src/rpm-specific/fusionforge.repo $WORKSPACE/build/packages/fusionforge.repo
+	sed -i "s#http://fusionforge.org/#${HUDSON_URL}#" $WORKSPACE/build/packages/fusionforge.repo
 	sed -i "s#baseurl = .*#baseurl = ${FFORGE_RPM_REPO}/#" $WORKSPACE/build/packages/fusionforge.repo
+	scp $WORKSPACE/build/packages/fusionforge.repo root@$HOST:/etc/yum.repos.d/
+else
+	rsync -a --delete $WORKSPACE/build/packages/ root@$HOST:/root/fusionforge_repo/
+	echo "Installing standart FUSIONFORGE REPO from src/rpm-specific/fusionforge.repo"
+	scp src/rpm-specific/fusionforge.repo root@$HOST:/etc/yum.repos.d/
 fi
-scp $WORKSPACE/build/packages/fusionforge.repo root@$HOST:/etc/yum.repos.d/
 
 # DAG
-cp src/rpm-specific/dag-rpmforge.repo $WORKSPACE/build/packages/dag-rpmforge.repo
 if [ ! -z "$DAG_RPMFORGE_REPO" ] ; then
+	echo "Installing specific DAG REPO $DAG_RPMFORGE_REPO"
+	cp src/rpm-specific/dag-rpmforge.repo $WORKSPACE/build/packages/dag-rpmforge.repo
         sed -i "s#http://apt.sw.be/redhat#${DAG_RPMFORGE_REPO}#" $WORKSPACE/build/packages/dag-rpmforge.repo
+	scp $WORKSPACE/build/packages/dag-rpmforge.repo root@$HOST:/etc/yum.repos.d/
+else
+	echo "Installing standart DAG REPO from src/rpm-specific/dag-rpmforge.repo"
+	scp src/rpm-specific/dag-rpmforge.repo root@$HOST:/etc/yum.repos.d/
 fi
-scp $WORKSPACE/build/packages/dag-rpmforge.repo root@$HOST:/etc/yum.repos.d/
 
 cat > $WORKSPACE/build/config/phpunit <<-EOF
 HUDSON_URL=$HUDSON_URL
 JOB_NAME=$JOB_NAME
 EOF
 
-rsync -av ./ root@$HOST:/opt/fusionforge/
+rsync -a --delete src/ root@$HOST:/opt/gforge/
 
-ssh root@$HOST "cd /opt/fusionforge/ ; FFORGE_RPM_REPO=$FFORGE_RPM_REPO DAG_RPMFORGE_REPO=$DAG_RPMFORGE_REPO FFORGE_DB=fforge FFORGE_USER=gforge FFORGE_ADMIN_USER=ffadmin FFORGE_ADMIN_PASSWORD=ffadmin ./install-ng $HOST"
+ssh root@$HOST "/opt/gforge/install-ng"
 
 ssh root@$HOST "su - postgres -c \"pg_dumpall\" > /root/dump"
 
 ssh root@$HOST "(echo [core];echo use_ssl=no) > /etc/gforge/config.ini.d/zzz-zbuildbot.ini"
-ssh root@$HOST "cd /opt/fusionforge/tests/func; CONFIGURED=true CONFIG_PHP=config.php.buildbot DB_NAME=$DB_NAME php db_reload.php"
+ssh root@$HOST "cd /opt/gforge/tests/func; CONFIGURED=true CONFIG_PHP=config.php.buildbot DB_NAME=$DB_NAME php db_reload.php"
 #  Install a fake sendmail to catch all outgoing emails.
 # ssh root@$HOST "perl -spi -e s#/usr/sbin/sendmail#/opt/tests/scripts/catch_mail.php# /etc/gforge/local.inc"
 ssh root@$HOST "service crond stop" || true
 
 retcode=0
 echo "Run phpunit test on $HOST"
-ssh -X root@$HOST "/opt/fusionforge/tests/scripts/phpunit.sh TarCentos52Tests.php" || retcode=$?
+ssh -X root@$HOST "/opt/gforge/tests/scripts/phpunit.sh TarCentos52Tests.php" || retcode=$?
 
 if [ "x$SELENIUM_RC_DIR" != "x" ]
 then
