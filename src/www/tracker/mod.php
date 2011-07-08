db090c9d528af992fed52b6ca084dc1ca26c0afc
Franck VILLAUME <franck.villaume@capgemini.com>
2011-07-08 13:44:18 +0000

    add group_id in hookParams

--- src/www/tracker/mod.php
+++ src/www/tracker/mod.php
@@ -171,7 +171,7 @@ echo html_build_select_box ($res,'new_artifact_type_id',$ath->getID(),false);
 	<tr>
 		<td><strong><?php echo _('Summary')?><?php echo utils_requiredField(); ?>:</strong><br />
 		<input id="tracker-summary" title="<?php echo util_html_secure(_('The summary text-box represents a short tracker item summary. Useful when browsing through several tracker items.')) ?>" type="text" name="summary" size="70" value="<?php
-			echo $ah->getSummary(); 
+			echo $ah->getSummary();
 			?>" maxlength="255" />
 		</td>
 		<td>
@@ -248,7 +248,8 @@ if ($group->usesPM()) {
 <table border="0" width="80%">
 <tr><td colspan="2"><!-- dummy in case the hook is empty --></td></tr>
 	<?php
-		$hookParams['artifact_id']=$aid;
+		$hookParams['artifact_id'] = $aid;
+		$hookParams['group_id'] = $group_id;
 		plugin_hook("artifact_extra_detail",$hookParams);
 	?>
 </table>
@@ -258,8 +259,8 @@ if ($group->usesPM()) {
 	<tr>
 	<td colspan="2">
 		<h2><?php echo _('Changes') ?>:</h2>
-		<?php 
-			echo $ah->showHistory(); 
+		<?php
+			echo $ah->showHistory();
 		?>
 	</td>
 	</tr>
