7189e7db2878afdc5a35cd70bf1535ec54288bf9
Franck VILLAUME <franck.villaume@capgemini.com>
2011-07-08 14:06:14 +0000

    wrong index again...

--- src/plugins/scmhook/common/scmhookPlugin.class.php
+++ src/plugins/scmhook/common/scmhookPlugin.class.php
@@ -257,7 +257,7 @@ class scmhookPlugin extends Plugin {
 		foreach ($hooksAvailable as $hookAvailable) {
 			if (in_array($hookAvailable->getClassname(), $hooksEnabled)) {
 				if (method_exists($hookAvailable,'artifact_extra_detail')) {
-					return $hookAvailable->artifact_extra_detail($params['aid']);
+					return $hookAvailable->artifact_extra_detail($params['artifact_id']);
 				}
 			}
 		}
