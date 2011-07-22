9ed5356d17761e9e06aa149d1117e5b78ff39eb1
Franck VILLAUME <franck.villaume@capgemini.com>
2011-07-22 12:42:32 +0000

    fix wrong comment

--- src/common/docman/views/listfile.php
+++ src/common/docman/views/listfile.php
@@ -104,7 +104,7 @@ jQuery(document).ready(function() {
 		buttonAddItem:		jQuery('#docman-additem'),
 		buttonEditDirectory:	jQuery('#docman-editdirectory'),
 		docManURL:		'<?php util_make_uri("docman") ?>',
-		lockIntervalDelay:	60000, //in microsecond and if you change this value, please update the check value 600
+		lockIntervalDelay:	60000, //in millisecond and if you change this value, please update the check value 600
 		divLeft:		jQuery('#left'),
 		divHandle:		jQuery('#handle'),
 		divRight:		jQuery('#right'),
