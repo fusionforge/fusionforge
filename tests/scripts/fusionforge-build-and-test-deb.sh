73e3986a1c7e2d07c96091503ec8652e2a7e45e9
Roland Mas <lolando@debian.org>
2011-08-17 12:49:54 +0000

    Testsuite fixes

--- tests/scripts/fusionforge-build-and-test-deb.sh
+++ tests/scripts/fusionforge-build-and-test-deb.sh
@@ -83,7 +83,7 @@ EOF
 scp -r tests root@$HOST:/root
 scp -r $WORKSPACE/build/config  root@$HOST:/root
 scp 3rd-party/selenium/binary/selenium-server-current/selenium-server.jar root@$HOST:/root
-ssh root@$HOST "cat /root/tests/preseed/* | sed s/@ADMIN_PASSWORD@/$ADMIN_PASSWORD/ | LANG=C debconf-set-selections"
+ssh root@$HOST "cat /root/tests/preseed/* | sed s/@FORGE_ADMIN_PASSWORD@/$FORGE_ADMIN_PASSWORD/ | LANG=C debconf-set-selections"
 ssh root@$HOST "echo \"deb $DEBMIRROR $DIST main\" > /etc/apt/sources.list"
 ssh root@$HOST "echo \"deb $DEBMIRRORSEC $DIST/updates main\" > /etc/apt/sources.list.d/security.list"
 
@@ -110,7 +110,7 @@ sleep 5
 ssh root@$HOST "apt-get update"
 ssh root@$HOST "UCF_FORCE_CONFFNEW=yes DEBIAN_FRONTEND=noninteractive LANG=C apt-get -y --force-yes install postgresql-contrib fusionforge-full"
 echo "Set forge admin password"
-ssh root@$HOST "/usr/share/gforge/bin/forge_set_password admin $ADMIN_PASSWORD"
+ssh root@$HOST "/usr/share/gforge/bin/forge_set_password admin $FORGE_ADMIN_PASSWORD"
 #ssh root@$HOST "LANG=C a2dissite default ; LANG=C invoke-rc.d apache2 reload ; LANG=C touch /tmp/fusionforge-use-pfo-rbac"
 ssh root@$HOST "(echo [core];echo use_ssl=no) > /etc/gforge/config.ini.d/zzz-builbot.ini"
 #ssh root@$HOST "su - postgres -c \"pg_dump -Fc $DB_NAME\" > /root/dump"
