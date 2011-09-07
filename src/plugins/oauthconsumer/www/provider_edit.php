c30fb2bf2056686212869c513cfe14fb97bbbe94
Madhumita Dhar <madhumita.dhar@it-sudparis.eu>
2011-09-07 16:20:46 +0000

    removing isset from if condition blocks

--- src/plugins/oauthconsumer/www/provider_edit.php
+++ src/plugins/oauthconsumer/www/provider_edit.php
@@ -32,37 +32,37 @@ $i = 0;
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Name') ?></td>
-<td><input name="provider_name" maxlength="128" size="60" value="<?php echo isset($f_provider_name)?$f_provider_name:$provider->get_name() ?>"/></td>
+<td><input name="provider_name" maxlength="128" size="60" value="<?php echo ($f_provider_name)?$f_provider_name:$provider->get_name() ?>"/></td>
 </tr>
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Description') ?></td>
-<td><input name="provider_desc" maxlength="250" size="60" value="<?php echo isset($f_provider_desc)?$f_provider_desc:$provider->get_description() ?>"/></td>
+<td><input name="provider_desc" maxlength="250" size="60" value="<?php echo ($f_provider_desc)?$f_provider_desc:$provider->get_description() ?>"/></td>
 </tr>
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Consumer Key') ?></td>
-<td><input name="consumer_key" maxlength="250" size="60" value="<?php echo isset($f_consumer_key)?$f_consumer_key:$provider->get_consumer_key() ?>"/></td>
+<td><input name="consumer_key" maxlength="250" size="60" value="<?php echo ($f_consumer_key)?$f_consumer_key:$provider->get_consumer_key() ?>"/></td>
 </tr>
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Consumer Secret') ?></td>
-<td><input name="consumer_secret" maxlength="250" size="60" value="<?php echo isset($f_consumer_secret)?$f_consumer_secret:$provider->get_consumer_secret() ?>"/></td>
+<td><input name="consumer_secret" maxlength="250" size="60" value="<?php echo ($f_consumer_secret)?$f_consumer_secret:$provider->get_consumer_secret() ?>"/></td>
 </tr>
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Request Token URL') ?></td>
-<td><input name="request_token_url" maxlength="250" size="60" value="<?php echo isset($f_request_token_url)?$f_request_token_url:$provider->get_request_token_url() ?>"/></td>
+<td><input name="request_token_url" maxlength="250" size="60" value="<?php echo ($f_request_token_url)?$f_request_token_url:$provider->get_request_token_url() ?>"/></td>
 </tr>
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Authorization URL') ?></td>
-<td><input name="authorize_url" maxlength="250" size="60" value="<?php echo isset($f_authorize_url)?$f_authorize_url:$provider->get_authorize_url() ?>"/></td>
+<td><input name="authorize_url" maxlength="250" size="60" value="<?php echo ($f_authorize_url)?$f_authorize_url:$provider->get_authorize_url() ?>"/></td>
 </tr>
 
 <tr <?php echo $HTML->boxGetAltRowStyle($i++) ?>>
 <td class="category"><?php echo _('Access Token URL') ?></td>
-<td><input name="access_token_url" maxlength="250" size="60" value="<?php echo isset($f_access_token_url)?$f_access_token_url:$provider->get_access_token_url() ?>"/></td>
+<td><input name="access_token_url" maxlength="250" size="60" value="<?php echo ($f_access_token_url)?$f_access_token_url:$provider->get_access_token_url() ?>"/></td>
 </tr>
 
 <tr>
