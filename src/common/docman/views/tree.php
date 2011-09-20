502ca848df34bdd845b099b45a924226795ec325
Alain Peyrat <aljeux@free.fr>
2011-09-20 09:03:58 +0000

    Fix docman tree in IE

--- src/common/docman/views/tree.php
+++ src/common/docman/views/tree.php
@@ -56,7 +56,7 @@ echo '<div id="documenttree" style="height:100%">';
 <script language="JavaScript" type="text/javascript">/* <![CDATA[ */
 	var myMenu =
 		[
-			['<span class="JSCookTreeFolderClosed"><i><img alt="" src="' + myThemeXPBase + 'folder1.gif" /></i></span><span id="ctItemID0" class="JSCookTreeFolderOpen"><i><img alt="" src="' + myThemeXPBase + 'folderopen1.gif" /></i></span>', '/', '#', '', '', <?php docman_recursive_display(0); ?>
+			['<span class="JSCookTreeFolderClosed"><i><img alt="" src="' + myThemeXPBase + 'folder1.gif" /></i></span><span class="JSCookTreeFolderOpen"><i><img alt="" src="' + myThemeXPBase + 'folderopen1.gif" /></i></span>', '/', '#', '', '', <?php docman_recursive_display(0); ?>
 			]
 		];
 
