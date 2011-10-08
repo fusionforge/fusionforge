1430927b9127f56cd02382b34580053c40471d06
Roland Mas <lolando@debian.org>
2011-10-08 18:47:24 +0000

    Rewrite ff_tsvector_add in plpgsql (rather than plain SQL) to avoid parsing problems

--- src/db/20111007-fti-aggregate-functions.sql
+++ src/db/20111007-fti-aggregate-functions.sql
@@ -1,8 +1,10 @@
-CREATE OR REPLACE FUNCTION ff_tsvector_add (tsvector, tsvector)
+CREATE OR REPLACE FUNCTION ff_tsvector_add (v1 tsvector, v2 tsvector)
 RETURNS tsvector
-AS '
-   SELECT $1 || $2
-' LANGUAGE SQL
+AS $$
+BEGIN
+  RETURN v1 || v2 ;
+END;
+$$ LANGUAGE plpgsql
 IMMUTABLE
 RETURNS NULL ON NULL INPUT;
 
