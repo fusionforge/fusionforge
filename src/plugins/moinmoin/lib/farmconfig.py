7dde193cd37db0093948f7cf4b549948c8094765
Roland Mas <lolando@debian.org>
2011-10-14 16:08:06 +0000

    Move all of the project initialization of the instance Config object to the generic farmconfig module, so that it is properly shared across instances.

--- src/plugins/moinmoin/lib/farmconfig.py
+++ src/plugins/moinmoin/lib/farmconfig.py
@@ -6,22 +6,39 @@ import string
 
 class FarmConfig(multiconfig.DefaultConfig):
 
-    # basic options (you normally need to change these)
-    sitename = u'ForgeWiki' # [Unicode]
-    interwikiname = u'ForgeWiki' # [Unicode]
+    # Common declarations
 
     page_front_page = u"FrontPage"
+    theme_default = 'mentalwealth'
 
     ffsa = fusionforge.FusionForgeSessionAuth()
     ff_host = fusionforge.FusionForgeLink().get_config('web_host')
 
     auth = [ffsa]
-    theme_default = 'mentalwealth'
+
+    # Defaults (overridden per project)
+
+    sitename = u'ForgeWiki' # [Unicode]
+    interwikiname = u'ForgeWiki' # [Unicode]
 
     acl_rights_default = \
       string.join (map (lambda u: u+":read,write,delete,revert,admin",
                         ffsa.admins)
                    + ["All:"])
+
+    def __init__(self, project_name):
+        self.project_name = project_name
+        self.sitename = u'%s' % project_name
+        self.interwikiname = u'%s' % project_name
+        self.data_dir = '/var/lib/gforge/plugins/moinmoin/wikidata/%s/data' % project_name
+        self.data_underlay_dir = '/var/lib/gforge/plugins/moinmoin/wikidata/%s/underlay' % project_name
+
+        self.acl_rights_default = self.ffsa.get_moinmoin_acl_string(project_name)
+
+        # Call inherited constructor once instance specific variables
+        # have been set.
+
+        multiconfig.DefaultConfig.__init__(self, project_name)
 
     def groups (self, request):
         from MoinMoin.datastruct import WikiGroups, CompositeGroups
