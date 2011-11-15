a1e9ab8543f859a0cadfcdba351393ec0282e402
Alain Peyrat <aljeux@free.fr>
2011-11-15 09:31:43 +0000

    Initial script to install all required tools on a vm to run the suite

---
+++ tests/scripts/fusionforge-prepare-vm-centos5.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+. tests/scripts/common-functions
+. tests/scripts/common-vm
+
+get_config $@
+destroy_vm -t centos5 $@
+start_vm_if_not_keeped -t centos5 $@
+
+setup_dag_repo $@
+
+sleep 5
+
+ssh root@$HOST 'yum upgrade -y'
+
+ssh root@$HOST 'yum install -y php php-cli php-pear php-xml java firefox vnc-server'
+
+# Install phpunit inside the vps.
+ssh root@$HOST 'pear config-set auto_discover 1'
+ssh root@$HOST 'pear upgrade pear'
+ssh root@$HOST 'pear install pear.phpunit.de/PHPUnit'
+
+ssh root@$HOST 'pear upgrade --force PEAR'
+ssh root@$HOST 'pear install --alldeps phpunit/PHPUnit-3.4.1'
+
+ssh root@$HOST 'yum clean all'
+
+ssh root@$HOST 'perl -spi -e "s/^X11Forwarding no/X11Forwarding yes/" /etc/ssh/sshd_config'
+
+sudo /root/save_as_template_vz.sh
+
+stop_vm_if_not_keeped -t centos5 $@
