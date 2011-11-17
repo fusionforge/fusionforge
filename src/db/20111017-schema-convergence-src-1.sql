9d720450468b3afbe98a53139fb68942ca6c32b6
Roland Mas <lolando@debian.org>
2011-11-17 19:53:50 +0000

    More convergence SQL

--- src/db/20111017-schema-convergence-src-1.sql
+++ src/db/20111017-schema-convergence-src-1.sql
@@ -11,3 +11,12 @@ ALTER TABLE ONLY forum_attachment ADD CONSTRAINT forum_attachment_userid_fkey FO
 
 ALTER TABLE forum_attachment DROP CONSTRAINT "$1";
 ALTER TABLE forum_attachment DROP CONSTRAINT "$2";
+
+ALTER SEQUENCE supported_langu_language_id_seq RENAME TO supported_languages_pk_seq;
+ALTER SEQUENCE group_cvs_history_id_seq RENAME TO group_cvs_history_pk_seq;
+ALTER SEQUENCE project_messa_project_messa_seq RENAME TO project_messages_project_message_id_seq;
+ALTER TABLE activity_log ALTER COLUMN ver SET DEFAULT 0::double precision;
+ALTER TABLE artifact_extra_field_data ALTER COLUMN data_id SET DEFAULT nextval('artifact_extra_field_data_data_id_seq'::regclass);
+ALTER TABLE artifact_extra_field_list ALTER COLUMN extra_field_id SET DEFAULT nextval('artifact_extra_field_list_extra_field_id_seq'::regclass);
+ALTER TABLE db_images ALTER COLUMN upload_date SET DEFAULT 0;
+ALTER TABLE db_images ALTER COLUMN version SET DEFAULT 0;
--- src/db/schema-convergence-deb-1.sql
+++ src/db/schema-convergence-deb-1.sql
@@ -2,3 +2,10 @@ ALTER TABLE ONLY doc_data ADD CONSTRAINT docdata_languageid_fk FOREIGN KEY (lang
 ALTER TABLE ONLY forum ADD CONSTRAINT forum_posted_by_fk FOREIGN KEY (posted_by) REFERENCES users(user_id) MATCH FULL;
 
 DROP TABLE forum_attachment_type;
+DROP FUNCTION forums_search(text, integer, text, boolean);
+DROP TABLE foundry_data;
+DROP TABLE foundry_news;
+DROP TABLE foundry_preferred_projects;
+DROP TABLE foundry_project_downloads_agg;
+DROP TABLE foundry_project_rankings_agg;
+DROP TABLE foundry_projects;
