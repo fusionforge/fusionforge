ca6a4b207be55210fa20e2f86f51a391b5566956
Roland Mas <lolando@debian.org>
2011-11-17 19:53:58 +0000

    More convergence SQL

--- src/db/20111017-schema-convergence-src-1.sql
+++ src/db/20111017-schema-convergence-src-1.sql
@@ -20,3 +20,26 @@ ALTER TABLE artifact_extra_field_data ALTER COLUMN data_id SET DEFAULT nextval('
 ALTER TABLE artifact_extra_field_list ALTER COLUMN extra_field_id SET DEFAULT nextval('artifact_extra_field_list_extra_field_id_seq'::regclass);
 ALTER TABLE db_images ALTER COLUMN upload_date SET DEFAULT 0;
 ALTER TABLE db_images ALTER COLUMN version SET DEFAULT 0;
+
+ALTER TABLE group_join_request DROP CONSTRAINT "$1";
+ALTER TABLE group_join_request DROP CONSTRAINT "$2";
+ALTER TABLE group_join_request ADD CONSTRAINT group_join_request_group_id_fkey FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE CASCADE;
+ALTER TABLE group_join_request ADD CONSTRAINT group_join_request_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(user_id);
+ALTER TABLE groups ADD CONSTRAINT groups_license FOREIGN KEY (license) REFERENCES licenses(license_id) MATCH FULL;
+
+DROP INDEX plugins_plugin_name_key;
+ALTER TABLE plugins ADD CONSTRAINT plugins_plugin_name_key UNIQUE (plugin_name);
+ALTER TABLE project_category ADD CONSTRAINT project_category_pkey PRIMARY KEY (category_id);
+ALTER TABLE project_tags ADD CONSTRAINT project_tags_group_id_fkey FOREIGN KEY (group_id) REFERENCES groups(group_id) MATCH FULL;
+ALTER TABLE project_task ADD CONSTRAINT project_task_group_project_id_f FOREIGN KEY (group_project_id) REFERENCES project_group_list(group_project_id) MATCH FULL;
+
+ALTER TABLE project_task_external_order DROP CONSTRAINT "$1";
+ALTER TABLE project_task_external_order ADD CONSTRAINT project_task_external_order_project_task_id_fkey FOREIGN KEY (project_task_id) REFERENCES project_task(project_task_id) MATCH FULL ON DELETE CASCADE;
+
+ALTER TABLE project_weekly_metric ADD CONSTRAINT project_weekly_metric_pkey PRIMARY KEY (ranking);
+
+ALTER TABLE role DROP CONSTRAINT "$1";
+ALTER TABLE role ADD CONSTRAINT role_group_id_fkey FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY themes ADD CONSTRAINT themes_pkey PRIMARY KEY (theme_id);
+ALTER TABLE project_messages ALTER COLUMN project_message_id SET DEFAULT nextval('project_messages_project_message_id_seq'::regclass);
--- src/db/schema-convergence-deb-1.sql
+++ src/db/schema-convergence-deb-1.sql
@@ -9,3 +9,10 @@ DROP TABLE foundry_preferred_projects;
 DROP TABLE foundry_project_downloads_agg;
 DROP TABLE foundry_project_rankings_agg;
 DROP TABLE foundry_projects;
+DROP TABLE group_type;
+
+ALTER TABLE ONLY project_task_external_order DROP CONSTRAINT roject_task_external_order_pkey;
+ALTER TABLE ONLY project_task_external_order ADD CONSTRAINT project_task_external_order_pkey PRIMARY KEY (project_task_id);
+
+DROP SEQUENCE foundry_projects_pk_seq;
+DROP SEQUENCE foundry_preferred_projec_pk_seq;
