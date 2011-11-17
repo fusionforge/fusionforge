1b80a6f322b4b4b47ddb050a6d4ade47650f2b7e
Roland Mas <lolando@debian.org>
2011-11-17 19:51:23 +0000

    Use common SQL parser

--- src/db/upgrade-db.php
+++ src/db/upgrade-db.php
@@ -8,6 +8,7 @@ echo "Entering  upgrade-db.php\n";
 
 require_once dirname(__FILE__).'/../www/env.inc.php';
 require_once $gfcommon.'include/pre.php';
+require_once $gfcommon.'include/sqlparser.php';
 
 $db_path = dirname(__FILE__).'/';
 $date = -1;
@@ -16,34 +17,24 @@ $version = '';
 // a huge message will warn not to run it without doing a backup first // warning.
 
 // Check if table 'database_startpoint' exists
-$res = db_query_params('SELECT COUNT(*) AS proceed FROM pg_class WHERE relname = $1 AND relkind = $2',
-			array('database_startpoint',
-			'r'));
+if (!db_check_table_exists('database_startpoint')) {
+	show("ERROR: table 'database_startpoint' does not exist.\nRun startpoint.php first.\n");
+	exit();
+}
+
+// Check if table 'database_startpoint' has proper values
+$res = db_query_params('SELECT * FROM database_startpoint',
+		       array());
 
 if (!$res) { // db error
-	show("DB-ERROR-2: ".db_error()."\n");
+	show("DB-ERROR-3: ".db_error()."\n");
 	exit();
-} else {
-	$proceed = db_result($res, 0, 'proceed');
-	if (!$proceed) { // table does not exist
-		show("ERROR: table 'database_startpoint' does not exist.\nRun startpoint.php first.\n");
-		exit();
-	} else {
-		// Check if table 'database_startpoint' has proper values
-		$res = db_query_params('SELECT * FROM database_startpoint',
-			array());
-
-		if (!$res) { // db error
-			show("DB-ERROR-3: ".db_error()."\n");
-			exit();
-		} else if (db_numrows($res) == 0) { // table 'database_startpoint' is empty
-			show("ERROR: table 'database_startpoint' is empty.\nRun startpoint.php first.\n");
-			exit();
-		} else { // get the start date from the db
-			$date = (int) db_result($res, 0, 'db_start_date');
-			$version = db_result($res, 0, 'db_version');
-		}
-	}
+} else if (db_numrows($res) == 0) { // table 'database_startpoint' is empty
+	show("ERROR: table 'database_startpoint' is empty.\nRun startpoint.php first.\n");
+	exit();
+} else { // get the start date from the db
+	$date = (int) db_result($res, 0, 'db_start_date');
+	$version = db_result($res, 0, 'db_version');
 }
 
 if (!apply_fixes($version)) {
@@ -192,116 +183,26 @@ function run_sql_script($filename) {
 	if (!$file) {
 		return false;
 	}
-	$content = fread($file, filesize($sql_file));
-	fclose($file);
-
-	$content = preg_replace("/--(.*)/", '', $content);
-
-	$parts = explode(";\n", $content);
-	$queries = array();
-	$query_temp = '';
-	$is_function = false;
-	$is_copy_stdin = false;
-
-	for ($i=0;$i<count($parts);$i++) {
-		$q = $parts[$i];
-		// Check if it's a function
-		if ((in_string($q, 'create function') || in_string($q, 'create or replace function') ||
-			in_string($q, 'replace function')) && !in_string($q, 'language plpgsql') &&
-			!in_string($q, 'language \'plpgsql\'')&& !in_string($q, 'language \'c\'') &&
-			!in_string($q, 'language c')) {
-
-			while (!in_string($q, 'language plpgsql') && !in_string($q, 'language \'plpgsql\'')) {
-				$i++;
-				$q = $q.';'.$parts[$i];
-			}
-			$queries[] = trim($q);
-		// Check if it is a COPY FROM stdin
-		} else if (in_string($q, 'copy') && in_string($q, 'from stdin')) {
-			while (!in_string($q, '\.')) {
-				$i++;
-				$q = $q.";\n".$parts[$i];
-			}
-			$aux = explode('\.', $q, 2);
-			$queries[] = ltrim($aux[0]."\\.\n");
-			if (trim($aux[1]) != '') {
-				$queries[] = trim($aux[1]);
-			}
-		// Else, we just add it up
-		} else {
-			if (trim($q) != '') {
-				$queries[] = trim(preg_replace("/\s+/", ' ', str_replace("\n", ' ', $q)));
-			}
-		}
-	}
+	$queries = parse_sql_file($db_path.$filename);
 
 	$i = 0;
 
 	//db_begin();
 
 	foreach ($queries as $query) {
-		// Check if it is a DROP TABLE
-		if (in_string($query, 'drop table')) {
-			$aux = explode(' ', trim($query));
-			if (count($aux) == 3 || count($aux) == 4) { // PERFECT!
-				drop_table_if_exists($aux[2], (count($aux) == 4) && preg_match('/CASCADE/i', trim($aux[3])));
+		$res = db_query_params($query, array());
+		if (!$res) {
+			show(db_error()."\n");
+			show("QUERY: $query\n");
+			show("Continue executing ([Y]es/[N]o)?\n");
+			// Read the input
+			$answer = strtolower(trim(fgets(STDIN)));
+			if ($answer != 'y' && $answer != 'yes') {
+				//db_rollback();
+				return false;
 			} else {
-				print_r($aux);
-			}
-		// Check if it is a DROP SEQUENCE
-		} else if (in_string($query, 'drop sequence')) {
-			$aux = explode(' ', trim($query));
-			if (count($aux) == 3) { // PERFECT!
-				drop_seq_if_exists($aux[2]);
-			} else {
-				print_r($aux);
-			}
-		// Check if it is a DROP TRIGGER
-		} else if (in_string($query, 'drop trigger')) {
-			$aux = explode(' ', trim($query));
-			if (count($aux) == 5 || count($aux) == 6) { // PERFECT!
-				drop_trigger_if_exists($aux[2], $aux[4]);
-			} else {
-				print_r($aux);
-			}
-		// Check if it is a DROP VIEW
-		} else if (in_string($query, 'drop view')) {
-			$aux = explode(' ', trim($query));
-			if (count($aux) == 3 || count($aux) == 4) { // PERFECT!
-				drop_view_if_exists($aux[2]);
-			} else {
-				print_r($aux);
-			}
-		// Check if it is a DROP INDEX
-		} else if (in_string($query, 'drop index')) {
-			$aux = explode(' ', trim($query));
-			if (count($aux) == 3 || count($aux) == 4) { // PERFECT!
-				drop_index_if_exists($aux[2]);
-			} else {
-				print_r($aux);
-			}
-		// Check if it is a DROP CONSTRAINT
-		} else if (in_string($query, 'alter table') && in_string($query, 'drop constraint')) {
-			$aux = explode(' ', trim($query));
-			$table = trim($aux[2], "\" ");
-			$constraint = trim($aux[5], "\" ");
-
-			drop_constraint_if_exists($table, $constraint, $query);
-		} else {
-			$res = db_query_params($query, array());
-			if (!$res) {
-				show(db_error()."\n");
-				show("QUERY: $query\n");
-				show("Continue executing ([Y]es/[N]o)?\n");
-				// Read the input
-				$answer = strtolower(trim(fgets(STDIN)));
-				if ($answer != 'y' && $answer != 'yes') {
-					//db_rollback();
-					return false;
-				} else {
-					//db_commit();
-					//db_begin();
-				}
+				//db_commit();
+				//db_begin();
 			}
 		}
 	}
@@ -399,89 +300,6 @@ function apply_fixes($version) {
 		}
 	}
 	//db_commit();
-	return true;
-}
-
-function drop_view_if_exists($name) {
-	$result = drop_if_exists($name, 'DROP VIEW', 'v');
-	return $result;
-}
-
-function drop_seq_if_exists($name) {
-	$result = drop_if_exists($name, 'DROP SEQUENCE', 'S');
-	return $result;
-}
-
-function drop_index_if_exists($name) {
-	$result = drop_if_exists($name, 'DROP INDEX', 'i');
-	return $result;
-}
-
-function drop_table_if_exists($name, $cascade) {
-	if($cascade)  {
-		$result = drop_if_exists($name, 'DROP TABLE', 'r', 'CASCADE');
-	} else {
-		$result = drop_if_exists($name, 'DROP TABLE', 'r');
-	}
-	return $result;
-}
-
-function drop_if_exists($name, $command, $kind, $commandSuffix = '') {
-	// Strip "name" => name
-	if (preg_match('/^"(.*)"$/', $name, $match)) {
-		$name = $match[1];
-	}
-	$res = db_query_params('SELECT COUNT(*) AS exists FROM pg_class WHERE relname=$1 AND relkind=$2',
-				array ($name,
-				       $kind));
-	if (!$res) {
-		show("ERROR:".db_error()."\n");
-		return false;
-	}
-	if (db_result($res, 0, 'exists') != '0') {
-		$res = db_query_params("$command $name $commandSuffix", array());
-		if (!$res) {
-			show("ERROR:".db_error()."\n");
-			//db_rollback();
-			//exit();
-		}
-	}
-	return true;
-}
-
-function drop_constraint_if_exists($table, $constraint, $query) {
-	$res = db_query_params('SELECT COUNT(*) AS exists FROM information_schema.constraint_table_usage WHERE table_name=$1 AND constraint_name=$2',
-			array($table,
-			$constraint));
-
-	if (!$res) {
-		show("ERROR:".db_error()."\n");
-		return false;
-	}
-	if (db_result($res, 0, 'exists') != '0') {
-		$res = db_query_params($query, array());
-		if (!$res) {
-			show("ERROR:".db_error()."\n");
-		}
-	}
-	return true;
-}
-
-function drop_trigger_if_exists($name, $on) {
-	$res = db_query_params('SELECT COUNT(*) AS exists FROM pg_trigger WHERE tgname=$1',
-				array ($name));
-	if (!$res) {
-		show("ERROR:".db_error()."\n");
-		return false;
-	}
-	if (db_result($res, 0, 'exists') != '0') {
-		$res = db_query_params("DROP TRIGGER $1 ON $2", array($name, $on));
-		if (!$res) {
-			show("ERROR:".db_error()."\n");
-			//db_rollback();
-			//exit();
-		}
-	}
 	return true;
 }
 
