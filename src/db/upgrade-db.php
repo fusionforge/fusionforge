6402b90a34a2ae921643d2b635bcbb9d12cccd18
Roland Mas <lolando@debian.org>
2011-11-17 19:51:16 +0000

    Removed PHP4 compat code

--- src/db/upgrade-db.php
+++ src/db/upgrade-db.php
@@ -9,32 +9,6 @@ echo "Entering  upgrade-db.php\n";
 require_once dirname(__FILE__).'/../www/env.inc.php';
 require_once $gfcommon.'include/pre.php';
 
-/**
-* Sets up CLI environment based on SAPI and PHP version
-*/
-if (version_compare(phpversion(), '4.3.0', '<') || php_sapi_name() == 'cgi') {
-   // Handle output buffering
-   while (@ob_end_flush());
-   ob_implicit_flush(TRUE);
-
-   // PHP ini settings
-   set_time_limit(0);
-   ini_set('track_errors', TRUE);
-   ini_set('html_errors', FALSE);
-   ini_set('magic_quotes_runtime', FALSE);
-
-   // Define stream constants
-   define('STDIN', fopen('php://stdin', 'r'));
-   define('STDOUT', fopen('php://stdout', 'w'));
-   define('STDERR', fopen('php://stderr', 'w'));
-
-   // Close the streams on script termination
-   register_shutdown_function(
-       create_function('',
-       'fclose(STDIN); fclose(STDOUT); fclose(STDERR); return true;')
-       );
-}
-
 $db_path = dirname(__FILE__).'/';
 $date = -1;
 $version = '';
