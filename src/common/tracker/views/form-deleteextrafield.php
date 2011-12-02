e114547675c265bb13793b373a3c8c44d23d9661
Alain Peyrat <aljeux@free.fr>
2011-12-02 09:50:24 +0000

    Add fieldset in tracker around forms

--- src/common/tracker/views/form-deleteextrafield.php
+++ src/common/tracker/views/form-deleteextrafield.php
@@ -24,9 +24,11 @@
 
 $ath->adminHeader(array ('title'=>sprintf(_('Delete a custom field for %s'),$ath->getName())));
 
-		$id = getStringFromRequest('id');
+$id = getStringFromRequest('id');
 
-		?>
+?>
+		<fieldset>
+		<legend><?php echo _("Delete Custom Field") ?></legend>
 		<form action="<?php echo getStringFromServer('PHP_SELF').'?group_id='.$group_id.'&amp;atid='.$ath->getID(); ?>" method="post">
 		<p>
 		<input type="hidden" name="deleteextrafield" value="y" />
@@ -34,12 +36,13 @@ $ath->adminHeader(array ('title'=>sprintf(_('Delete a custom field for %s'),$ath
 		<?php echo _('You are about to permanently and irretrievably delete this custom field and all its contents!'); ?>
 		</p>
 		<p>
-		<input type="checkbox" name="sure" value="1" /><?php echo _("I'm Sure.") ?><br />
-		<input type="checkbox" name="really_sure" value="1" /><?php echo _("I'm Really Sure.") ?>
+		<input type="checkbox" name="sure" value="1" /><?php echo _("I'm Sure") ?><br />
+		<input type="checkbox" name="really_sure" value="1" /><?php echo _("I'm Really Sure") ?>
 		</p>
 		<p>
 		<input type="submit" name="post_changes" value="<?php echo _('Delete') ?>" /></p>
 		</form>
+		</fieldset>
 		<?php
 
 		$ath->footer(array());
--- src/common/tracker/views/form-deletetracker.php
+++ src/common/tracker/views/form-deletetracker.php
@@ -24,15 +24,18 @@
 
 $ath->adminHeader(array ('title'=>sprintf(_('Permanently Delete Tracker %s'), $ath->getName())));
 		?>
+		<fieldset>
+		<legend><?php echo _("Delete Tracker") ?></legend>
 		<form action="<?php echo getStringFromServer('PHP_SELF').'?group_id='.$group_id.'&amp;atid='.$ath->getID(); ?>" method="post">
 		<input type="hidden" name="delete" value="y" /><br />
 		<?php echo _('You are about to permanently and irretrievably delete this tracker and all its contents!'); ?>
 		<p>
-		<input type="checkbox" name="sure" value="1" /><?php echo _("I'm Sure.") ?><br />
-		<input type="checkbox" name="really_sure" value="1" /><?php echo _("I'm Really Sure.") ?></p>
+		<input type="checkbox" name="sure" value="1" /><?php echo _("I'm Sure") ?><br />
+		<input type="checkbox" name="really_sure" value="1" /><?php echo _("I'm Really Sure") ?></p>
 		<p>
 		<input type="submit" name="post_changes" value="<?php echo _('Delete') ?>" /></p>
 		</form>
+		</fieldset>
 		<?php
 
 		$ath->footer(array());
