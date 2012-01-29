aaf92ee8567cae9b6ddc2a86f1199e89cec86ae2
Franck VILLAUME <franck.villaume@capgemini.com>
2012-01-29 18:04:39 +0000

    fix indent

--- src/common/include/Role.class.php
+++ src/common/include/Role.class.php
@@ -152,17 +152,17 @@ class Role extends RoleExplicit implements PFO_RoleExplicit {
 	 */
 	function create($role_name, $data, $newproject=false) {
 		if ($this->Group == NULL) {
-			if (!forge_check_global_perm ('forge_admin')) {
+			if (!forge_check_global_perm('forge_admin')) {
 				$this->setPermissionDeniedError();
 				return false;
 			}
 		}
 		if ($newproject) {
-			if (!forge_check_global_perm ('approve_projects')) {
+			if (!forge_check_global_perm('approve_projects')) {
 				$this->setPermissionDeniedError();
 				return false;
 			}
-		} elseif (!forge_check_perm ('project_admin', $this->Group->getID())) {
+		} elseif (!forge_check_perm('project_admin', $this->Group->getID())) {
 			$this->setPermissionDeniedError();
 			return false;
 		}
