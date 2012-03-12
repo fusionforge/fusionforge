474b49a90370377c834f9b1468b82ef7ae83ce69
Roland Mas <lolando@debian.org>
2012-03-12 09:33:54 +0000

    Merged from 5.1: More error-proof RBAC upgrade path

--- src/db/20100518-pfo-rbac.sql
+++ src/db/20100518-pfo-rbac.sql
@@ -191,7 +191,6 @@ BEGIN
 			   PERFORM insert_pfo_role_setting (1, 'tracker', t.group_artifact_id, 1) ;
 			END IF ;
 
-			PERFORM insert_pfo_role_setting (1, 'tracker', t.group_artifact_id, 1) ;
 			PERFORM insert_pfo_role_setting (2, 'tracker', t.group_artifact_id, 1) ;
 		END LOOP ;
 
@@ -413,7 +412,7 @@ BEGIN
 
 	INSERT INTO pfo_role (SELECT nextval ('pfo_role_seq'), role_name, 1, group_id, false, role_id FROM role) ;
 
-	INSERT INTO pfo_user_role (SELECT ug.user_id, r.role_id FROM user_group ug, pfo_role r WHERE ug.role_id = r.old_role_id) ;
+	INSERT INTO pfo_user_role (SELECT DISTINCT ug.user_id, r.role_id FROM user_group ug, pfo_role r WHERE ug.role_id = r.old_role_id) ;
 
 	PERFORM migrate_rbac_permissions_to_pfo_rbac () ;
 	PERFORM migrate_role_observer_to_pfo_rbac () ;
