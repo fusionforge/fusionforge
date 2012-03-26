afa5c0a6627556fcf912378773518ab790c42739
Roland Mas <lolando@debian.org>
2012-03-26 18:10:52 +0000

    Bumped version numbers after the branching

--- src/common/include/FusionForge.class.php
+++ src/common/include/FusionForge.class.php
@@ -49,7 +49,7 @@ class FusionForge extends Error {
 		if (isset($forge_pkg_version)) {
 			$this->software_version = $forge_pkg_version;
 		} else {
-			$this->software_version = '5.1.50' ;
+			$this->software_version = '5.2.50' ;
 		}
 
 		if (isset($forge_pkg_type)) {
