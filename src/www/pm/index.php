c48438876cbc72833183761fbcce0f9beb11f890
Alain Peyrat <aljeux@free.fr>
2012-05-12 11:44:58 +0000

    Prefer util_make_uri to util_make_url when possible.

--- src/www/pm/index.php
+++ src/www/pm/index.php
@@ -169,7 +169,7 @@ if (count($pg_arr) < 1 || $pg_arr == false) {
 		} else {
 		echo '
 		<tr '. $HTML->boxGetAltRowStyle($j) . '>
-			<td><a href="'.util_make_url ('/pm/task.php?group_project_id='. $pg_arr[$j]->getID().'&amp;group_id='.$group_id.'&amp;func=browse').'">' .
+			<td><a href="'.util_make_uri('/pm/task.php?group_project_id='. $pg_arr[$j]->getID().'&amp;group_id='.$group_id.'&amp;func=browse').'">' .
 			html_image("ic/taskman20w.png","20","20") . ' &nbsp;'.$pg_arr[$j]->getID() .'</a></td>
 			<td><a href="'.util_make_url ('/pm/task.php?group_project_id='. $pg_arr[$j]->getID().'&amp;group_id='.$group_id.'&amp;func=browse').'">' .
 		$pg_arr[$j]->getName() .'</a></td>
