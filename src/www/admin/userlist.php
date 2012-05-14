4e2d19f1b31260077949723d83b40096bd5206cd
Alain Peyrat <aljeux@free.fr>
2012-05-14 10:04:32 +0000

    Fix memory issues in userlist with large number of users - Filtering & list users per plugin added.

--- src/www/admin/userlist.php
+++ src/www/admin/userlist.php
@@ -28,8 +28,6 @@ require_once $gfwww.'admin/admin_utils.php';
 // user must be forge admin to proceed
 session_require_global_perm ('forge_admin');
 
-$HTML->header(array('title'=>_('User List')));
-
 /**
  * performAction() - Updates the indicated user status
  *
@@ -38,6 +36,8 @@ $HTML->header(array('title'=>_('User List')));
  * @param               string  $user_id - the user id to act upon
  */
 function performAction($newStatus, $statusString, $user_id) {
+	global $feedback;
+
 	$u = user_get_object($user_id);
 	if (!$u || !is_object($u)) {
 		exit_error(_('Could Not Get User'),'home');
@@ -58,21 +58,21 @@ function performAction($newStatus, $statusString, $user_id) {
 		if(!$u->setUnixStatus($newStatus)) {
 			exit_error($u->getErrorMessage(),'home');
 		}
-
 	}
-	echo '<p class="feedback">' .sprintf(_('User updated to %1$s status'), $statusString)."</p>";
+	$feedback = sprintf(_('User updated to %1$s status'), $statusString);
 }
 
-function show_users_list ($users, $filter='') {
+function show_users_list ($users, $filter='', $sortorder='realname') {
+	echo '<p>' ._('Status') .': '.
+		util_make_link('/admin/userlist.php', _('All')). '
+		<span class="active">'.util_make_link('/admin/userlist.php?status=A'.'&amp;sortorder='.$sortorder,_('Active')). '</span>
+		<span class="deleted">'.util_make_link('/admin/userlist.php?status=D'.'&amp;sortorder='.$sortorder,_('Deleted')).'</span>
+		<span class="suspended">'.util_make_link('/admin/userlist.php?status=S'.'&amp;sortorder='.$sortorder,_('Suspended')).'</span>
+		<span class="pending">'.util_make_link('/admin/userlist.php?status=P'.'&amp;sortorder='.$sortorder,_('(*)Pending')).'</span>'.'</p>';
+
 	if (!count($users)) {
 		return;
 	}
-
-	echo '<p>' ._('Key') .':
-		<span class="active">'._('Active'). '</span>
-		<span class="deleted">' ._('Deleted') .'</span>
-		<span class="suspended">' ._('Suspended'). '</span>
-		<span class="pending">' ._('(*)Pending'). '</span>'.'</p>';
 
 	$headers = array(
 		_('Login'),
@@ -85,19 +85,22 @@ function show_users_list ($users, $filter='') {
 	);
 
 	$headerLinks = array(
-	  '/admin/userlist.php?sortorder=user_name',
-	  '/admin/userlist.php?sortorder=add_date',
-	  '/admin/userlist.php?sortorder=user_name',
-	  '/admin/userlist.php?sortorder=user_name',
-	  '/admin/userlist.php?sortorder=user_name',
-	  '/admin/userlist.php?sortorder=user_name',
-	  '/admin/userlist.php?sortorder=user_name'
+		'/admin/userlist.php?sortorder=user_name'.$filter,
+		'/admin/userlist.php?sortorder=add_date'.$filter,
+		'',
+		'',
+		'',
+		'',
+		''
 	);
 
 	echo $GLOBALS['HTML']->listTableTop($headers, $headerLinks);
 
+	$res = db_query_params('SELECT * FROM users WHERE user_id = ANY ($1) ORDER BY '.$sortorder,
+					array(db_int_array_to_any_clause($users)));
 	$count = 0;
-	foreach ($users as $u) {
+	while ($arr = db_fetch_array($res)) {
+		$u = new GFUser($arr['user_id'], $arr);
 		print '<tr '. $GLOBALS['HTML']->boxGetAltRowStyle($count) . '><td class="';
 		if ($u->getStatus() == 'A') print "active";
 		if ($u->getStatus() == 'D') print "deleted";
@@ -107,7 +110,7 @@ function show_users_list ($users, $filter='') {
 		if ($u->getStatus() == 'P') print "*";
 		echo $u->getRealName().' ('.$u->getUnixName().')</a>';
 		echo '</td>';
-		echo '<td width="15%" style="text-align:center">';
+		echo '<td width="15%" class="align-center">';
 		echo ($u->getAddDate() ? date(_('Y-m-d H:i'), $u->getAddDate()) : '-');
 		echo '</td>';
 		echo '<td width="12%" style="text-align:center">'.util_make_link ('/developer/?form_dev='.$u->getID(),_('[DevProfile]')).'</td>';
@@ -129,6 +132,7 @@ $group_id = getIntFromRequest('group_id');
 $action = getStringFromRequest('action');
 $user_id = getIntFromRequest('user_id');
 $status = getStringFromRequest('status');
+$usingplugin = getStringFromRequest('usingplugin');
 
 if ($action=='delete') {
 	performAction('D', "DELETED", $user_id);
@@ -138,40 +142,67 @@ if ($action=='delete') {
 	performAction('S', "SUSPENDED", $user_id);
 }
 
+$HTML->header(array('title'=>_('User List')));
+
 //	Show list of users
-print "<p>" ._('User list for project: ');
-if (!$group_id) {
+if ($usingplugin) {
+	echo "<p>"._('Users that use plugin'). " <strong>".$usingplugin."</strong></p>\n";
+	$res = db_query_params ('SELECT u.user_id FROM plugins p, user_plugin up, users u WHERE p.plugin_name = $1 and up.user_id = u.user_id and p.plugin_id = up.plugin_id', array($usingplugin));
+	show_users_list(user_get_objects(util_result_column_to_array($res,0)));
+
+} else if (!$group_id) {
 	$user_name_search = getStringFromRequest('user_name_search');
 
-	print "<strong>" ._('All Projects'). "</strong>";
-	print "\n</p>";
+	print "<p>" ._('User list for ') . "<strong>" ._('All Projects'). "</strong></p>";
 
 	if ($user_name_search) {
-		$res = db_query_params ('SELECT user_id FROM users WHERE lower(user_name) LIKE $1 OR lower(lastname) LIKE $1 ORDER BY realname',
+		$res = db_query_params ('SELECT user_id FROM users WHERE lower(user_name) LIKE $1 OR lower(lastname) LIKE $1',
 					   array (strtolower("$user_name_search%")));
-	} elseif ($status) {
-		$res = db_query_params ('SELECT user_id FROM users WHERE status = $1 ORDER BY realname',
+		$list_id = util_result_column_to_array($res,0);
+	}
+	if ($status) {
+		$res = db_query_params ('SELECT user_id FROM users WHERE status = $1',
 					   array ($status));
-	} else {
-		$sortorder = getStringFromRequest('sortorder', 'realname');
-		util_ensure_value_in_set ($sortorder,
-					  array('realname','user_name','lastname','firstname','user_id','status','add_date')) ;
-		$res = db_query_params('SELECT user_id FROM users ORDER BY '.$sortorder,
-					  array ());
+		if (isset($list_id)) {
+			$list_id = array_merge($list_id, util_result_column_to_array($res,0));
+		}
+		else {
+			$list_id = util_result_column_to_array($res,0);
+		}
+	}
+	if (! isset($list_id)) {
+		$res = db_query_params('SELECT user_id FROM users',
+				array ());
+		$list_id = util_result_column_to_array($res,0);
 	}
 	$filter='';
 	if (in_array($status,array('D','A','S','P'))) {
 		$filter = '&amp;status='.$status;
 	}
-	show_users_list (user_get_objects(util_result_column_to_array($res,0)),$filter);
+	$sortorder = getStringFromRequest('sortorder', 'realname');
+	util_ensure_value_in_set ($sortorder,
+					  array('realname','user_name','lastname','firstname','user_id','status','add_date')) ;
+	show_users_list ($list_id, $filter, $sortorder);
 } else {
 	/*
 		Show list for one project
 	*/
 	$project = group_get_object($group_id) ;
-	print "<strong>" . $project->getPublicName() . "</strong></p>";
-
-	show_users_list ($project->getUsers());
+	print "<p>" ._('User list for project: ') . "<strong>" . $project->getPublicName() . "</strong></p>";
+	$users = $project->getUsers();
+	if ($users) {
+		foreach ($project->getUsers() as $user) {
+			$users_id[] = $user->getID();
+		}
+		$filter = '&amp;group_id='.$group_id;
+		$sortorder = getStringFromRequest('sortorder', 'realname');
+		util_ensure_value_in_set ($sortorder,
+					array('realname','user_name','lastname','firstname','user_id','status','add_date')) ;
+		show_users_list ($users_id, $filter, $sortorder);
+	}
+	else {
+		echo '<p>'._('No user in this project').'</p>';
+	}
 }
 
 $HTML->footer(array());
