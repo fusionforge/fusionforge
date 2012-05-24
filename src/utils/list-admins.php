914ae39e633c0e2b62d2c882caf7db84e07e5402
Roland Mas <lolando@debian.org>
2012-05-24 13:59:13 +0000

    Stop relying on hardcoded path in utility script

--- src/utils/list-admins.php
+++ src/utils/list-admins.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php -f
 <?php
-require "/usr/share/gforge/common/include/env.inc.php";
+require (dirname(__FILE__).'/../common/include/env.inc.php');
 require_once $gfcommon."include/pre.php";
 $admins = RBACEngine::getInstance()->getUsersByAllowedAction("forge_admin", -1);
 $anames = array_map(create_function("\$x", "return \$x->getUnixName();"), $admins);
