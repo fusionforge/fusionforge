22bbcee2e14570f14a5312e73d1826dab1565925
Roland Mas <lolando@debian.org>
2012-05-31 07:58:49 +0000

    Merged from 5.1/5.2: fixes to Mediawiki and CVS plugins, stop VMs after use on vladimir

--- src/plugins/scmcvs/common/CVSPlugin.class.php
+++ src/plugins/scmcvs/common/CVSPlugin.class.php
@@ -221,16 +221,15 @@ class CVSPlugin extends SCMPlugin {
 
 			system ("chgrp -R $unix_group $repo $locks_dir") ;
 			system ("chmod 3777 $locks_dir") ;
+			system ("echo \"SystemAuth=no\" > $repo/CVSROOT/config");
+			system ("echo \"LockDir=$locks_dir\" >> $repo/CVSROOT/config");
+			system ("echo \"UseNewInfoFmtStrings=yes\" >> $repo/CVSROOT/config");
 			if ($project->enableAnonSCM()) {
 				system ("chmod -R g+wXs,o+rX-w $repo") ;
-				system ("echo \"SystemAuth=no\" > $repo/CVSROOT/config");
-				system ("echo \"LockDir=$locks_dir\" >> $repo/CVSROOT/config");
 				system ("echo \"anonymous\" > $repo/CVSROOT/readers");
 				system ("echo \"anonymous:\" > $repo/CVSROOT/passwd");
 			} else {
 				system ("chmod -R g+wXs,o-rwx $repo") ;
-				system ("echo \"SystemAuth=no\" > $repo/CVSROOT/config");
-				system ("echo \"LockDir=$locks_dir\" >> $repo/CVSROOT/config");
 				system ("echo \"\" > $repo/CVSROOT/readers");
 				system ("echo \"\" > $repo/CVSROOT/passwd");
 			}
