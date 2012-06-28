5a94d23034af68123daa6c864c94328b248b4f51
Roland Mas <lolando@debian.org>
2012-06-28 16:24:07 +0000

    Added a script to create and/or update the cowbuilder environments for Debian

---
+++ tests/scripts/manage-cowbuilder.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# Prepare and/or update cowbuilder caches
+
+COWBUILDERBASE=/var/lib/jenkins/builder/
+
+for DIST in squeeze wheezy ; do
+    COWBUILDERCONFIG=$COWBUILDERBASE/config/$DIST.config
+
+    cat > $COWBUILDERCONFIG <<EOF
+PDEBUILD_PBUILDER=cowbuilder
+BASEPATH=$COWBUILDERBASE/cow/base-$DIST-amd64.cow
+BUILDPLACE=$COWBUILDERBASE/buildplace
+APTCACHEHARDLINK="no"
+APTCACHE="/var/cache/pbuilder/aptcache"
+PBUILDERROOTCMD="sudo HOME=${HOME}"
+BUILDRESULT=$BUILDRESULT
+EOF
+   
+    if [ -d $COWBUILDERBASE/cow/base-$DIST-amd64.cow ] ; then
+	sudo cowbuilder --update --configfile $COWBUILDERCONFIG
+    else
+	sudo cowbuilder --create --distribution $DIST --configfile $COWBUILDERCONFIG --mirror http://ftp.fr.debian.org/
+    fi
+done
