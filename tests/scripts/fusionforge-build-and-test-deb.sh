506f62093806fd4469bf5c7e7d9047e391a4724a
Roland Mas <lolando@debian.org>
2012-06-28 12:46:17 +0000

    Work in progress

--- tests/scripts/fusionforge-build-and-test-deb.sh
+++ tests/scripts/fusionforge-build-and-test-deb.sh
@@ -34,7 +34,7 @@ cd $CHECKOUTPATH/src
 PKGNAME=$(dpkg-parsechangelog | awk '/^Source:/ { print $2 }')
 PKGVERS=$(dpkg-parsechangelog | awk '/^Version:/ { print $2 }')
 MAJOR=$(echo $PKGVERS | sed 's,([^-]+).*,\\1,')
-SMAJOR=$(echo $(MAJOR) | sed 's/^.://')
+SMAJOR=$(echo $MAJOR | sed 's/^.://')
 if [ -d $CHECKOUTPATH/.svn ] ; then
     MINOR=svn$(svn info | awk '/^Revision:/ { print $2 }')
 elif [ -d $CHECKOUTPATH/.bzr ] ; then
