807a0820d99ebe04d733a0d688c77ce90c731445
Franck VILLAUME <franck.villaume@capgemini.com>
2012-07-29 10:56:41 +0000

    fix missing column code

--- src/plugins/webanalytics/db/webanalytics-init.sql
+++ src/plugins/webanalytics/db/webanalytics-init.sql
@@ -2,5 +2,6 @@ CREATE TABLE plugin_webanalytics (
 	id_webanalytics	serial PRIMARY KEY,
 	url		text,
 	name		character varying(255),
-	is_enable	integer DEFAULT 0
+	is_enable	integer DEFAULT 0,
+	code        text
 );
