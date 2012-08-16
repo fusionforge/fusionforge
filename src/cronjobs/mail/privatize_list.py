2f3a70e7eda88e596d10eb87cf48627828d7c8c2
Franck VILLAUME <franck.villaume@capgemini.com>
2012-08-16 09:00:26 +0000

    port from branch 5.1: fix general behavior of mailing-list status and enable passwd reset

--- src/cronjobs/mail/privatize_list.py
+++ src/cronjobs/mail/privatize_list.py
@@ -5,5 +5,7 @@ mlist.subscribe_policy=3
 mlist.member_posting_only=1
 mlist.private_roster=1
 mlist.archive_private=1
+mlist.forward_auto_discards=0
+mlist.generic_nonmember_action=2
 mlist.Save()
 
