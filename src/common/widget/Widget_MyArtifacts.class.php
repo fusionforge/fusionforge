e84013be30c4b8ebfdedb91e91a1c48c01993a2d
Franck VILLAUME <franck.villaume@capgemini.com>
2012-08-25 16:32:30 +0000

    fix display: same as other widgets

--- src/common/widget/Widget_MyArtifacts.class.php
+++ src/common/widget/Widget_MyArtifacts.class.php
@@ -89,7 +89,7 @@ class Widget_MyArtifacts extends Widget {
 	}
 
 	function getContent() {
-		$html_my_artifacts = '<table style="width:100%">';
+
 		$atf = new ArtifactsForUser(@UserManager::instance()->getCurrentUser());
 		$assigned = $atf->getAssignedArtifactsByGroup();
 		$submitted = $atf->getSubmittedArtifactsByGroup();
@@ -107,13 +107,14 @@ class Widget_MyArtifacts extends Widget {
 		}
 
 		if (count($my_artifacts) > 0) {
+			$html_my_artifacts = '<table style="width:100%">';
 			$html_my_artifacts .= $this->_display_artifacts($my_artifacts, 1);
+			$html_my_artifacts .= '<tr><td colspan="3">'.(($this->_artifact_show == 'N' || count($my_artifacts) > 0)?' ':_("None")).'</td></tr>';
+			$html_my_artifacts .= '</table>';
 		} else {
-			$html_my_artifacts .= '<tr><td colspan="3">' .
-			    _("You have no artifacts") . '</td></tr>';
+			$html_my_artifacts = '<div class="warning">'. _("You have no artifacts") . '</div>';
 		}
-		$html_my_artifacts .= '<tr><td colspan="3">'.(($this->_artifact_show == 'N' || count($my_artifacts) > 0)?' ':_("None")).'</td></tr>';
-		$html_my_artifacts .= '</table>';
+
 		return $html_my_artifacts;
 	}
 
