0717b7e8c7bbabc9dd8c72e6f90f4267a1fbc940
Franck VILLAUME <franck.villaume@capgemini.com>
2012-08-26 09:20:29 +0000

    port from Branch_5_2: fix displayed group: only group using docman

--- src/common/widget/Widget_MyProjectsLastDocuments.class.php
+++ src/common/widget/Widget_MyProjectsLastDocuments.class.php
@@ -58,67 +58,69 @@ class Widget_MyProjectsLastDocuments extends Widget {
 			$html_my_projects .= '<table style="width:100%">';
 			$i = 0;
 			foreach ($groups as $g) {
-				$i++;
-
-				$vWhiteList = new Valid_WhiteList('hide_dmproj', array(0, 1));
-				$vWhiteList->required();
-				if($request->valid($vWhiteList)) {
-					$hide_docmanproject = $request->get('hide_dmproj');
-				} else {
-					$hide_docmanproject = null;
-				}
-
-				$df = new DocumentFactory($g);
-				$df->setLimit(5);
-				$df->setOrder(array('updatedate','createdate'));
-				$df->setSort('DESC');
-				$df->getDocuments();
-
-				list($hide_now,$count_diff,$hide_url) = my_hide_url('dmproj', $g->getID(), $hide_item_id, count($df->Documents), $hide_docmanproject);
-				$html_hdr = ($i ? '<tr class="boxitem"><td colspan="4">' : '').
-					$hide_url.'<a href="/docman/?group_id='.$g->getID().'">'.
-					$g->getPublicName().'</a>&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>';
-
-				$html = '';
-				if (!$hide_now) {
-					$keys = array_keys($df->Documents);
-					$j = 0;
-					if (!count($keys)) {
-						$html .= '<tr><td colspan="4"><div class="warning">'._('No documents').'</div></td></tr>';
+				if ($g->usesDocman()) {
+					$i++;
+					
+					$vWhiteList = new Valid_WhiteList('hide_dmproj', array(0, 1));
+					$vWhiteList->required();
+					if($request->valid($vWhiteList)) {
+						$hide_docmanproject = $request->get('hide_dmproj');
+					} else {
+						$hide_docmanproject = null;
 					}
-					foreach ($keys as $key) {
-						$dg = new DocumentGroup($g, $key);
-						$html .= '<tr><td colspan="4"><a href="'.util_make_url('/docman/?group_id='.$g->getID().'&view=listfile&dirid='.$key).'">'.$dg->getPath().'</a></td></tr>';
-						$j++;
-						$count = count($df->Documents[$key]);
-						for ($i=0; $i < $count; $i++) {
-							$doc =& $df->Documents[$key][$i];
-							$html .= '<tr '. $HTML->boxGetAltRowStyle($j) .'>';
-							switch ($doc->getFileType()) {
-								case "URL": {
-									$docurl = $doc->getFileName();
-									break;
-								}
-								default: {
-									$docurl = util_make_url('/docman/view.php/'.$g->getID().'/'.$doc->getID().'/'.urlencode($doc->getFileName()));
-								}
-							}
-							$html .= '<td><a href="'.$docurl.'">'.html_image($doc->getFileTypeImage(), '22', '22', array('alt'=>$doc->getFileType())).'</a></td>';
-							$html .= '<td>'.$doc->getFilename().'</td>';
-							$html .= '<td>'.make_user_link($doc->getCreatorUserName(), $doc->getCreatorRealName()).'</td>';
-							if ( $doc->getUpdated() ) {
-								$html .= '<td>'.date(_('Y-m-d H:i'), $doc->getUpdated()).'</td>';
-							} else {
-								$html .= '<td>'.date(_('Y-m-d H:i'), $doc->getCreated()).'</td>';
-							}
-							$html .= '</tr>';
-							$j++;
+
+					$df = new DocumentFactory($g);
+					$df->setLimit(5);
+					$df->setOrder(array('updatedate','createdate'));
+					$df->setSort('DESC');
+					$df->getDocuments();
+
+					list($hide_now,$count_diff,$hide_url) = my_hide_url('dmproj', $g->getID(), $hide_item_id, count($df->Documents), $hide_docmanproject);
+					$html_hdr = ($i ? '<tr class="boxitem"><td colspan="4">' : '').
+						$hide_url.'<a href="/docman/?group_id='.$g->getID().'">'.
+						$g->getPublicName().'</a>&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>';
+
+					$html = '';
+					if (!$hide_now) {
+						$keys = array_keys($df->Documents);
+						$j = 0;
+						if (!count($keys)) {
+							$html .= '<tr><td colspan="4"><div class="warning">'._('No documents').'</div></td></tr>';
 						}
-						$j--;
+						foreach ($keys as $key) {
+							$dg = new DocumentGroup($g, $key);
+							$html .= '<tr><td colspan="4"><a href="'.util_make_url('/docman/?group_id='.$g->getID().'&view=listfile&dirid='.$key).'">'.$dg->getPath().'</a></td></tr>';
+							$j++;
+							$count = count($df->Documents[$key]);
+							for ($i=0; $i < $count; $i++) {
+								$doc =& $df->Documents[$key][$i];
+								$html .= '<tr '. $HTML->boxGetAltRowStyle($j) .'>';
+								switch ($doc->getFileType()) {
+									case "URL": {
+										$docurl = $doc->getFileName();
+										break;
+									}
+									default: {
+										$docurl = util_make_url('/docman/view.php/'.$g->getID().'/'.$doc->getID().'/'.urlencode($doc->getFileName()));
+									}
+								}
+								$html .= '<td><a href="'.$docurl.'">'.html_image($doc->getFileTypeImage(), '22', '22', array('alt'=>$doc->getFileType())).'</a></td>';
+								$html .= '<td>'.$doc->getFilename().'</td>';
+								$html .= '<td>'.make_user_link($doc->getCreatorUserName(), $doc->getCreatorRealName()).'</td>';
+								if ( $doc->getUpdated() ) {
+									$html .= '<td>'.date(_('Y-m-d H:i'), $doc->getUpdated()).'</td>';
+								} else {
+									$html .= '<td>'.date(_('Y-m-d H:i'), $doc->getCreated()).'</td>';
+								}
+								$html .= '</tr>';
+								$j++;
+							}
+							$j--;
+						}
 					}
-				}
 
-				$html_my_projects .= $html_hdr.$html;
+					$html_my_projects .= $html_hdr.$html;
+				}
 			}
 			$html_my_projects .= '</table>';
 		}
