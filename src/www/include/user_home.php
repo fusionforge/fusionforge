cfede023fc764d1193de0440166a9a27526e8b02
Franck VILLAUME <franck.villaume@capgemini.com>
2012-08-31 09:15:16 +0000

    fix display of private projects in user profile

--- src/www/include/user_home.php
+++ src/www/include/user_home.php
@@ -5,6 +5,7 @@
  * Copyright 1999-2001 (c) VA Linux Systems
  * Copyright 2010, FusionForge Team
  * Copyright (C) 2011 Alain Peyrat - Alcatel-Lucent
+ * Copyright 2012, Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -104,42 +105,58 @@ if (count ($projects) < 1) {
 	print "<p>"._('This developer is a member of the following projects:')."</p>\n";
 
 	foreach ($projects as $p) {
-		$project_link = util_make_link_g ($p->getUnixName(),$p->getID(),$p->getPublicName());
-		$project_uri = util_make_url_g ($p->getUnixName(),$p->getID());
-		// sioc:UserGroups for all members of a project are named after /projects/A_PROJECT/members/
-		$usergroup_uri = $project_uri .'members/';
-
-		print '<div rel="sioc:member_of">'."\n"
-			.'<div about="'. $usergroup_uri .'" typeof="sioc:UserGroup">'."\n"
-			.'<div rel="sioc:usergroup_of">'."\n"
-			.'<div about="'. $project_uri .'" typeof="sioc:Space">';
-		$role_names = array () ;
-		$sioc_has_function_close = "";
-		foreach ($roles as $r) {
-			if ($r instanceof RoleExplicit
-			    && $r->getHomeProject() != NULL
-			    && $r->getHomeProject()->getID() == $p->getID()) {
-				$role_names[] = $r->getName() ;
-				print '<div property="sioc:has_function" content= "'.$r->getName().'">';
-				$sioc_has_function_close .= "</div>";
+		$display = 0;
+		if (!$p->isPublic()) {
+			$currentUser = session_get_user();
+			if ($currentUser) {
+				$pMembers = $p->getMembers();
+				foreach ($pMembers as $pMember) {
+					if ($pMember->getID() == $currentUser->getID()) {
+						$display = 1;
+					}
+				}
 			}
+		} else {
+			$display = 1;
 		}
+		if ($display) {
+			$project_link = util_make_link_g ($p->getUnixName(),$p->getID(),$p->getPublicName());
+			$project_uri = util_make_url_g ($p->getUnixName(),$p->getID());
+			// sioc:UserGroups for all members of a project are named after /projects/A_PROJECT/members/
+			$usergroup_uri = $project_uri .'members/';
 
-		print ('<br />' . $project_link .' ('.htmlspecialchars (implode (', ', $role_names)).')');
-		print "\n";
+			print '<div rel="sioc:member_of">'."\n"
+				.'<div about="'. $usergroup_uri .'" typeof="sioc:UserGroup">'."\n"
+				.'<div rel="sioc:usergroup_of">'."\n"
+				.'<div about="'. $project_uri .'" typeof="sioc:Space">';
+			$role_names = array () ;
+			$sioc_has_function_close = "";
+			foreach ($roles as $r) {
+				if ($r instanceof RoleExplicit
+				&& $r->getHomeProject() != NULL
+				&& $r->getHomeProject()->getID() == $p->getID()) {
+					$role_names[] = $r->getName() ;
+					print '<div property="sioc:has_function" content= "'.$r->getName().'">';
+					$sioc_has_function_close .= "</div>";
+				}
+			}
 
-		if (forge_check_perm_for_user ($user, 'project_admin', $p->getID())) {
-			print '<span rev="doap:maintainer" resource="#me"></span>';
+			print ('<br />' . $project_link .' ('.htmlspecialchars (implode (', ', $role_names)).')');
+			print "\n";
+
+			if (forge_check_perm_for_user ($user, 'project_admin', $p->getID())) {
+				print '<span rev="doap:maintainer" resource="#me"></span>';
+			}
+			else {
+				print '<span rev="doap:developer" resource="#me"></span>';
+			}
+
+			echo $sioc_has_function_close."\n";  // sioc:has_function
+			echo "</div>\n";  // sioc:Space .../projects/A_PROJECT/
+			echo "</div>\n"; // sioc:usergroup_of
+			echo "</div>\n";  // sioc:UserGroup .../projects/A_PROJECT/members
+			echo "</div>\n"; // sioc:member_of
 		}
-		else {
-			print '<span rev="doap:developer" resource="#me"></span>';
-		}
-
-		echo $sioc_has_function_close."\n";  // sioc:has_function
-		echo "</div>\n";  // sioc:Space .../projects/A_PROJECT/
-		echo "</div>\n"; // sioc:usergroup_of
-		echo "</div>\n";  // sioc:UserGroup .../projects/A_PROJECT/members
-		echo "</div>\n"; // sioc:member_of
 	}
 } // end if groups
 echo "</div>\n"; // prefixes
