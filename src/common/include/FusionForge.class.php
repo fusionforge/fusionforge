9d7803051c73b40839f60a86ab36efa427142a5f
Roland Mas <lolando@debian.org>
2012-08-31 12:22:57 +0000

    5.2rc2

--- src/common/include/FusionForge.class.php
+++ src/common/include/FusionForge.class.php
@@ -49,7 +49,7 @@ class FusionForge extends Error {
 		if (isset($forge_pkg_version)) {
 			$this->software_version = $forge_pkg_version;
 		} else {
-			$this->software_version = '5.1.91' ; // 5.2~rc1
+			$this->software_version = '5.1.92' ; // 5.2~rc2
 		}
 
 		if (isset($forge_pkg_type)) {
