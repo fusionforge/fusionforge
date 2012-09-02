1ae122907023c984a3cbbfa7fe020a4d5b297384
Franck VILLAUME <franck.villaume@capgemini.com>
2012-09-02 16:28:35 +0000

    port from Branch_5_2: add licence

--- src/utils/list-admins.php
+++ src/utils/list-admins.php
@@ -1,7 +1,27 @@
 #!/usr/bin/php -f
 <?php
+/**
+ * Copyright 2012, Thorsten Glaser
+ * Copyright 2012, Roland Mas
+ *
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 require (dirname(__FILE__).'/../common/include/env.inc.php');
 require_once $gfcommon."include/pre.php";
 $admins = RBACEngine::getInstance()->getUsersByAllowedAction("forge_admin", -1);
 $anames = array_map(create_function("\$x", "return \$x->getUnixName();"), $admins);
 sort($anames); echo join(" ", $anames) . "\n";
+?>
