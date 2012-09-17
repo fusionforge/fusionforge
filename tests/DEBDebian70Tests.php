50be87179ac414ba336846aba5a2520f42ef953b
Roland Mas <lolando@debian.org>
2012-09-17 08:06:16 +0000

    Run plugin-moinmoin tests in Debian testsuite

--- tests/DEBDebian70Tests.php
+++ tests/DEBDebian70Tests.php
@@ -30,6 +30,7 @@ class DEBDebian60Tests
 		$suite->addTestFiles(glob("func/Forums/*Test.php"));
 		$suite->addTestFiles(glob("func/News/*Test.php"));
 		$suite->addTestFiles(glob("func/PluginsBlocks/*Test.php"));
+		$suite->addTestFiles(glob("func/PluginsMoinMoin/*Test.php"));
 		$suite->addTestFiles(glob("func/RBAC/*Test.php"));
 		$suite->addTestFiles(glob("func/Surveys/*Test.php"));
 		$suite->addTestFiles(glob("func/Search/*Test.php"));
