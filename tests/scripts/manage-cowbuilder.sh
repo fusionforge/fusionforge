c34989bc13cbec8fe463f010c84a89f90402186b
Christian Bayle <>
2012-09-24 20:25:58 +0000

    Replace COWBUILDERBASE with calsulated BUILDERDIR

--- tests/scripts/manage-cowbuilder.sh
+++ tests/scripts/manage-cowbuilder.sh
@@ -2,23 +2,33 @@
 
 # Prepare and/or update cowbuilder caches
 
-COWBUILDERBASE=/var/lib/jenkins/builder/
+. tests/scripts/common-functions
+. tests/scripts/common-vm
 
-for DIST in squeeze wheezy ; do
-    COWBUILDERCONFIG=$COWBUILDERBASE/config/$DIST.config
+get_config
+
+BUILDERDIR=${BUILDERDIR:-/var/lib/jenkins/builder/}
+DEBMIRROR=${DEBMIRROR:-http://cdn.debian.net/debian/}
+DISTROLIST=${DISTROLIST:-"squeeze wheezy"}
+
+[ -d $BUILDERDIR/config ] || mkdir $BUILDERDIR/config
+
+for DIST in $DISTROLIST ; do
+    COWBUILDERCONFIG=$BUILDERDIR/config/$DIST.config
 
     cat > $COWBUILDERCONFIG <<EOF
 PDEBUILD_PBUILDER=cowbuilder
-BASEPATH=$COWBUILDERBASE/cow/base-$DIST-amd64.cow
-BUILDPLACE=$COWBUILDERBASE/buildplace
+BASEPATH=$BUILDERDIR/cow/base-$DIST-amd64.cow
+BUILDPLACE=$BUILDERDIR/buildplace
+BUILDRESULT=$BUILDERDIR/result
 APTCACHEHARDLINK="no"
 APTCACHE="/var/cache/pbuilder/aptcache"
 PBUILDERROOTCMD="sudo HOME=${HOME}"
 EOF
    
-    if [ -d $COWBUILDERBASE/cow/base-$DIST-amd64.cow ] ; then
-	sudo cowbuilder --update --configfile $COWBUILDERCONFIG
+    if [ -d $BUILDERDIR/cow/base-$DIST-amd64.cow ] ; then
+	sudo cowbuilder --update --configfile $COWBUILDERCONFIG --debootstrapopts --variant=buildd
     else
-	sudo cowbuilder --create --distribution $DIST --configfile $COWBUILDERCONFIG --mirror http://ftp.fr.debian.org/
+	sudo cowbuilder --create --distribution $DIST --configfile $COWBUILDERCONFIG --mirror $DEBMIRROR --debootstrapopts --variant=buildd
     fi
 done
