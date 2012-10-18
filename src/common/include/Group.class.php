a32b2b5ca6c0cfc52a0d03937cb02aaef8fa5c30
Roland Mas <lolando@debian.org>
2012-10-18 14:32:10 +0200

    Unlink external roles from a project when deleting it (with non-regression test)

--- src/common/include/Group.class.php
+++ src/common/include/Group.class.php
@@ -1661,11 +1661,12 @@ class Group extends Error {
 					array ($this->getID())) ;
 
 		// unlink roles from this project
-		$ra = RoleAnonymous::getInstance();
-		$rl = RoleLoggedIn::getInstance();
-		$ra->unlinkProject($this);
-		$rl->unlinkProject($this);
-		// @todo : unlink all the other roles created in the project...
+		foreach ($this->getRoles() as $r) {
+			if ($r->getHomeProject() == NULL
+			    || $r->getHomeProject()->getID() != $this->getID()) {
+				$r->unlinkProject($this);
+			}
+		}
 
 		//
 		//	Delete Trackers
