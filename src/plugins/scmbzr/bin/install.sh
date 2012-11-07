77e65871c239e2a5092bed947ae8a928f60c10b2
Roland Mas <lolando@debian.org>
2012-11-07 17:33:46 +0100

    Backport from trunk: use Loggerhead as WSGI rather than as a daemon, and testsuite for scmbzr

---
+++ src/plugins/scmbzr/bin/install.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+# 
+# Configure Loggerhead for FusionForge
+# Roland Mas
+
+set -e
+
+if [ `id -u` != 0 ] ; then
+    echo "You must be root to run this, please enter passwd"
+    exec su -c "$0 $1"
+fi
+
+configfile=~gforge/.bazaar/bazaar.conf
+cachedir=/var/cache/gforge/loggerhead
+
+case "$1" in
+    configure)
+	PATH=/usr/share/gforge/bin:/usr/share/fusionforge/bin:$PATH
+	repos_path=$(forge_get_config repos_path scmbzr)
+	web_host=$(forge_get_config web_host)
+	url_prefix=$(forge_get_config url_prefix)
+
+	a2enmod wsgi
+	if [ ! -e $configfile ] ; then
+	    mkdir -p $(dirname $configfile)
+	    cat > $configfile <<EOF
+# Directory to serve bzr branches from
+# Non-bzr directories under this path will also be visible in loggerhead
+http_root_dir = '${repos_path}'
+
+# The url prefix for the bzr branches.
+http_user_prefix = 'http://${web_host}${url_prefix}scm/loggerhead'
+
+# Directory to put cache files in
+http_sql_dir = '/var/cache/gforge/loggerhead'
+EOF
+	    mkdir -p $cachedir
+	    chown gforge $cachedir
+	fi
+        ;;
+
+    purge)
+	rm -rf $configfile $cachedir
+	rmdir $(dirname $configfile) || true
+        ;;
+
+    *)
+        echo "Usage: $0 {configure|purge}"
+        exit 1
+esac
