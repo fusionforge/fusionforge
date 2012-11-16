f44b60b6025ac1f4ee3da8e1cb38a0390e909474
Christian Bayle <chris@bayle.eu>
2012-11-16 21:28:55 +0100

    Make code much simpler

--- tests/scripts/redhat-3rd-party.sh
+++ tests/scripts/redhat-3rd-party.sh
@@ -4,23 +4,8 @@
 relativepath=`dirname $0`
 absolutesourcepath=`cd $relativepath/../..; pwd`
 cd $absolutesourcepath
-
-echo "Read config from tests/config/default"
-. tests/config/default
-if [ -f tests/config/`hostname` ]
-then
-        echo "Read config from tests/config/`hostname`"
-        . tests/config/`hostname`
-fi
-BUILDERDIR=${BUILDERDIR:-$HOME/builder/}
-DIST=wheezy
-COWBUILDERCONFIG=$BUILDERDIR/config/$DIST.config
-
-# Setup Repo
-WORKDIR=$(cd $absolutesourcepath/..; pwd)
-# Jenkins will set WORKSPACE
-WORKSPACE=${WORKSPACE:-$WORKDIR}
+BUILDERDIR=$(./tests/scripts/builder_get_config.sh BUILDERDIR)
 
 # Build selenium
-make -C 3rd-party/selenium COWBUILDERCONFIG=$COWBUILDERCONFIG REPOPATH=$REPOPATH rpm
+make -C 3rd-party/selenium -f Makefile.rpm
 
