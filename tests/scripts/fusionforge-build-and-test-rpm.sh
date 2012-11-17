c681c416cde4c2d4436c2372ba1a8edf2155ef0f
Christian Bayle <chris@bayle.eu>
2012-11-17 16:04:34 +0100

    Normalize selenium tests

--- tests/scripts/fusionforge-build-and-test-rpm.sh
+++ tests/scripts/fusionforge-build-and-test-rpm.sh
@@ -44,15 +44,17 @@ ssh root@$HOST "su - postgres -c \"pg_dumpall\" > /root/dump"
 # Install a fake sendmail to catch all outgoing emails.
 ssh root@$HOST "perl -spi -e s#/usr/sbin/sendmail#$FORGE_HOME/tests/scripts/catch_mail.php# /etc/gforge/config.ini.d/defaults.ini"
 
-# Stop cron
+echo "Stop cron daemon"
 ssh root@$HOST "service crond stop" || true
 
+# Install selenium
+ssh root@$HOST "yum -y install selenium"
+
 # Install selenium tests
-ssh root@$HOST mkdir $FORGE_HOME/tests
-make -C 3rd-party/selenium selenium-server.jar
-cp 3rd-party/selenium/selenium-server.jar tests/
+ssh root@$HOST "[ -d $FORGE_HOME ] || mkdir -p $FORGE_HOME"
 rsync -a --delete tests/ root@$HOST:$FORGE_HOME/tests/
 
+# Transfer hudson config
 ssh root@$HOST "cat > $FORGE_HOME/tests/config/phpunit" <<-EOF
 HUDSON_URL=$HUDSON_URL
 JOB_NAME=$JOB_NAME
@@ -61,29 +63,10 @@ EOF
 # Run tests
 retcode=0
 echo "Run phpunit test on $HOST in $FORGE_HOME"
-
-ssh root@$HOST "yum install -y vnc-server ; mkdir -p /root/.vnc"
-ssh root@$HOST "cat > /root/.vnc/xstartup ; chmod +x /root/.vnc/xstartup" <<EOF
-#! /bin/bash
-: > /root/phpunit.exitcode
-$FORGE_HOME/tests/scripts/phpunit.sh RPMCentosTests.php &> /var/log/phpunit.log &
-echo \$! > /root/phpunit.pid
-wait %1
-echo \$? > /root/phpunit.exitcode
-EOF
-ssh root@$HOST vncpasswd <<EOF
-password
-password
-EOF
-ssh root@$HOST "vncserver :1"
-sleep 5
-pid=$(ssh root@$HOST cat /root/phpunit.pid)
-ssh root@$HOST "tail -f /var/log/phpunit.log --pid=$pid"
-sleep 5
-retcode=$(ssh root@$HOST cat /root/phpunit.exitcode)
+ssh root@$HOST "$FORGE_HOME/tests/func/vncxstartsuite.sh $FILTER"
+retcode=$?
 rsync -av root@$HOST:/var/log/ $WORKSPACE/reports/
-ssh root@$HOST "vncserver -kill :1" || retcode=$?
+scp root@$HOST:/tmp/gforge-*.log $WORKSPACE/reports/
 
 stop_vm_if_not_keeped -t centos5 $@
 exit $retcode
-
