2ff187741108177f035471a8a39e1b441cf7548f
Roland Mas <lolando@debian.org>
2012-11-23 15:38:02 +0100

    Port to the shared tables

--- src/plugins/scmgit/www/index.php
+++ src/plugins/scmgit/www/index.php
@@ -10,15 +10,19 @@ require_once '../../../www/env.inc.php';
 require_once $gfcommon.'include/pre.php';
 require_once $gfwww.'scm/include/scm_utils.php';
 
+$plugin = plugin_get_object('scmgit');
+$plugin_id = $plugin->getID();
+
 $func = getStringFromRequest ('func') ;
 switch ($func) {
 case 'request-personal-repo':
 	$group_id = getIntFromRequest ('group_id') ;
 	session_require_perm ('scm', $group_id, 'write') ;
 	$user = session_get_user(); // get the session user
-	$result = db_query_params ('SELECT * FROM plugin_scmgit_personal_repos p WHERE p.group_id=$1 AND p.user_id=$2',
+	$result = db_query_params ('SELECT * FROM scm_personal_repos WHERE group_id=$1 AND user_id=$2 AND plugin_id=$3',
 				   array ($group_id,
-					  $user->getID())) ;
+					  $user->getID(),
+					  $plugin_id)) ;
 	if ($result && db_numrows ($result) == 1) {
 		scm_header (array ('title' => _('SCM Repository'), 'group' => $group_id)) ;
 		echo _('You have already requested a personal Git repository for this project.  If it does not exist yet, it will be created shortly.') ;
@@ -29,9 +33,10 @@ case 'request-personal-repo':
 	$glist = $user->getGroups() ;
 	foreach ($glist as $g) {
 		if ($g->getID() == $group_id) {
-			$result = db_query_params ('INSERT INTO plugin_scmgit_personal_repos (group_id, user_id) VALUES ($1,$2)',
+			$result = db_query_params ('INSERT INTO scm_personal_repos (group_id, user_id, plugin_id) VALUES ($1,$2,$3)',
 						   array ($group_id,
-							  $user->getID())) ;
+							  $user->getID(),
+							  $plugin_id)) ;
 
 			scm_header (array ('title' => _('SCM Repository'), 'group' => $group_id)) ;
 			echo _('You have now requested a personal Git repository for this project.  If will be created shortly.') ;
