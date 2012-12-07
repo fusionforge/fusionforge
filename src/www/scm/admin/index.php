560b9e825a1c185d8961190bdc9841b9731e7724
Alain Peyrat <alain.peyrat@alcatel-lucent.com>
2012-12-07 16:56:45 +0100

    Ident

--- src/www/scm/admin/index.php
+++ src/www/scm/admin/index.php
@@ -49,35 +49,36 @@ if (getStringFromRequest('form_create_repo')) {
 }
 
 if (getStringFromRequest('create_repository') && getStringFromRequest('submit')) {
-       $repo_name = trim(getStringFromRequest('repo_name'));
-       $description = preg_replace('/[\r\n]/', ' ', getStringFromRequest('description'));
-       $clone = getStringFromRequest('clone');
-       $hook_params = array () ;
-       $hook_params['group_id'] = $group_id;
-       $hook_params['repo_name'] = $repo_name;
-       $hook_params['description'] = $description;
-       $hook_params['clone'] = $clone;
-       $hook_params['error_msg'] = '';
-       plugin_hook_by_reference('scm_add_repo', $hook_params);
-       if ($hook_params['error_msg']) {
-               $error_msg = $hook_params['error_msg'];
-       }
-       else {
-               $feedback = sprintf(_('New repository %s registered, will be created shortly.'), $repo_name);
-       }
+	$repo_name = trim(getStringFromRequest('repo_name'));
+	$description = preg_replace('/[\r\n]/', ' ', getStringFromRequest('description'));
+	$clone = getStringFromRequest('clone');
+	$hook_params = array () ;
+	$hook_params['group_id'] = $group_id;
+	$hook_params['repo_name'] = $repo_name;
+	$hook_params['description'] = $description;
+	$hook_params['clone'] = $clone;
+	$hook_params['error_msg'] = '';
+	plugin_hook_by_reference('scm_add_repo', $hook_params);
+	if ($hook_params['error_msg']) {
+		$error_msg = $hook_params['error_msg'];
+	}
+	else {
+		$feedback = sprintf(_('New repository %s registered, will be created shortly.'), $repo_name);
+	}
 } elseif (getStringFromRequest('delete_repository') && getStringFromRequest('submit')) {
-       $repo_name = trim(getStringFromRequest('repo_name'));
-       $hook_params = array () ;
-       $hook_params['group_id'] = $group_id;
-       $hook_params['repo_name'] = $repo_name;
-       $hook_params['error_msg'] = '';
-       plugin_hook_by_reference('scm_delete_repo', $hook_params);
-       if ($hook_params['error_msg']) {
-               $error_msg = $hook_params['error_msg'];
-       }
-       else {
-               $feedback = sprintf(_('Repository %s is marked for deletion (actual deletion will happen shortly).'), $repo_name);
-       }
+	$repo_name = trim(getStringFromRequest('repo_name'));
+
+	$hook_params = array () ;
+	$hook_params['group_id'] = $group_id;
+	$hook_params['repo_name'] = $repo_name;
+	$hook_params['error_msg'] = '';
+	plugin_hook_by_reference('scm_delete_repo', $hook_params);
+	if ($hook_params['error_msg']) {
+		$error_msg = $hook_params['error_msg'];
+	}
+	else {
+		$feedback = sprintf(_('Repository %s is marked for deletion (actual deletion will happen shortly).'), $repo_name);
+	}
 } elseif (getStringFromRequest('submit')) {
 	$hook_params = array();
 	$hook_params['group_id'] = $group_id;
