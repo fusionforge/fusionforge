a1d6fe5e4eed82cf094b8924635041f671592c29
Roland Mas <lolando@debian.org>
2013-01-04 15:10:47 +0000

    [#522] Patch by Franck Villaume to fix behaviour of mailing lists in Debian

--- src/deb-specific/create-mailing-lists.pl
+++ src/deb-specific/create-mailing-lists.pl
@@ -4,6 +4,7 @@
 # Copyright 2001-2003, 2009 Roland Mas <lolando@debian.org>
 # Copyright 2003, 2004, Christian Bayle <bayle@debian.org>
 # Copyright 2005, INRIA (David Margery and Soraya Arias)
+# Copyright 2012, Franck Villaume - TrivialDev
 
 use DBI ;
 use strict ;
@@ -132,6 +133,63 @@ eval {
 
 	my $cmd = "/usr/lib/mailman/bin/change_pw -l $listname >/dev/null 2>&1" ;
 	system ($cmd) ;
+
+	$query = "UPDATE mail_group_list SET status = 3 where group_list_id = group_list_id" ; # Status = 3: list configured on Mailman
+	$sth = $dbh->prepare ($query) ;
+	$sth->execute () ;
+	$sth->finish () ;
+
+	#debug "Committing." ;
+	$dbh->commit () ;
+    }
+
+    $query = "SELECT mail_group_list.group_list_id,
+                     mail_group_list.list_name,
+                     users.user_name,
+                     mail_group_list.password,
+                     mail_group_list.description,
+                     mail_group_list.is_public
+              FROM mail_group_list, users
+              WHERE mail_group_list.status = 5
+                    AND mail_group_list.list_admin = users.user_id" ; # Status = 5: configuration change requested
+    $sth = $dbh->prepare ($query) ;
+    $sth->execute () ;
+    @lines = () ;
+    while (my @myarray = $sth->fetchrow_array ()) {
+	push @lines, \@myarray ;
+    }
+    $sth->finish () ;
+    foreach $line (@lines) {
+	@array = @{$line} ;
+	my ($group_list_id, $listname, $user_name, $password, $description, $is_public) ;
+	my ($tmp) ;
+    	$tmp = mktemp ("/tmp/XXXXXX") ;
+	$cmd = "/usr/lib/mailman/bin/config_list -o $tmp $listname" ;
+	#print "cmd = <$cmd>\n" ;
+# Commented out on Matt Hope <dopey@debian.org> advice
+# To be revised by Roland Mas
+#	system ($cmd) ;
+	open CONFIG, ">>$tmp" ;
+	print CONFIG "description = \"$description\"\n" ;
+	print CONFIG "host_name = '$sys_lists_host'\n" ;
+	if (!$is_public) {
+	    print CONFIG "archive_private = True\n" ;
+	    print CONFIG "advertised = False\n" ;
+	    print CONFIG "subscribe_policy = 3\n" ;
+	    ## Reject mails sent by non-members
+	    print CONFIG "generic_nonmember_action = 2\n";
+	    ## Do not forward auto discard message
+	    print CONFIG "forward_auto_discards = 0\n";
+	} else {
+	    print CONFIG "archive_private = False\n" ;
+	    print CONFIG "advertised = True\n" ;
+	    print CONFIG "subscribe_policy = 1\n" ;
+	}
+	close CONFIG ;
+	$cmd = "/usr/lib/mailman/bin/config_list -i $tmp $listname" ;
+	#print "cmd = <$cmd>\n" ;
+	system ($cmd) ;
+	unlink $tmp ;
 
 	$query = "UPDATE mail_group_list SET status = 3 where group_list_id = group_list_id" ; # Status = 3: list configured on Mailman
 	$sth = $dbh->prepare ($query) ;
