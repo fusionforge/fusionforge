3299f45474c636760f7606683afea77fe27d4e53
Franck Villaume <franck.villaume@trivialdev.com>
2013-03-18 20:17:37 +0100

    widget: fix survey widget when project does not use survey

--- src/common/widget/Widget_MySurveys.class.php
+++ src/common/widget/Widget_MySurveys.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright 2013, Franck Villaume - TrivialDev
  *
- * This file is a part of Codendi.
+ * This file is a part of Fusionforge.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Fusionforge is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Fusionforge is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Fusionforge. If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once('Widget.class.php');
@@ -43,10 +44,12 @@ class Widget_MySurveys extends Widget {
 		sortProjectList ($projects) ;
 		$tmp = array () ;
 		foreach ($projects as $p) {
-			$sf = new SurveyFactory($p);
-			foreach ($sf->getSurveys() as $s) {
-				$tmp[] = $p ;
-				break ;
+			if ($p->usesSurvey()) {
+				$sf = new SurveyFactory($p);
+				foreach ($sf->getSurveys() as $s) {
+					$tmp[] = $p ;
+					break ;
+				}
 			}
 		}
 		$projects = $tmp ;
