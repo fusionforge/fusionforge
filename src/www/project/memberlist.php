0e76ca9d18ade20bc975810851047d8b42af103d
Alain Peyrat <alain.peyrat@alcatel-lucent.com>
2013-05-03 16:06:15 +0200

    Rename: developer => member, more generic

--- src/www/project/memberlist.php
+++ src/www/project/memberlist.php
@@ -34,7 +34,7 @@ if (!$group_id && $form_grp) {
 
 site_project_header(array('title'=>_('Project Member List'),'group'=>$group_id,'toptab'=>'memberlist'));
 
-echo '<p>' . _('If you would like to contribute to this project by becoming a developer, contact one of the project admins, designated in bold text below.') . '</p>';
+echo '<p>' . _('If you would like to contribute to this project by becoming a member, contact one of the project admins, designated in bold text below.') . '</p>';
 
 // beginning of the user descripion block
 $project =& group_get_object($group_id);
