34003c41842c8b559d4513417ee0ebaf82cf929b
Franck Villaume <franck.villaume@trivialdev.com>
2013-06-14 13:58:22 +0200

    scmhook: migration script fix since plugin code has been removed

--- src/db/20130531-svntrackerToScmhookCommitTrackerMigration.php
+++ src/db/20130531-svntrackerToScmhookCommitTrackerMigration.php
@@ -28,76 +28,74 @@ require_once $gfplugins.'scmhook/library/scmsvn/cronjobs/updateScmRepo.php';
 
 $pm = plugin_manager_get_object();
 
-if ($pm->PluginIsInstalled('svntracker')) {
-	$used = false;
-	$groupnames = db_query_params('SELECT g.group_name FROM plugins p, group_plugin gp, groups g WHERE plugin_name = $1 and gp.group_id = g.group_id and p.plugin_id = gp.plugin_id',
-		array('svntracker'));
+$used = false;
+$groupnames = db_query_params('SELECT g.group_name FROM plugins p, group_plugin gp, groups g WHERE plugin_name = $1 and gp.group_id = g.group_id and p.plugin_id = gp.plugin_id',
+	array('svntracker'));
 
-	if ($groupnames) {
-		if (db_numrows($groupnames) > 0) {
-			$used = true;
-		}
+if ($groupnames) {
+	if (db_numrows($groupnames) > 0) {
+		$used = true;
+	}
+}
+
+if ($used) {
+	// is scmhook activated ?
+	// no ... -> activate the plugin
+	if (!$pm->PluginIsInstalled('scmhook')) {
+		$pm->activate('scmhook');
 	}
 
-	if ($used) {
-		// is scmhook activated ?
-		// no ... -> activate the plugin
-		if (!$pm->PluginIsInstalled('scmhook')) {
-			$pm->activate('scmhook');
-		}
+	$pm->LoadPlugin('scmhook');
+	$pluginScmHook = $pm->GetPluginObject('scmhook');
+	$scmsvncronjob = new ScmSvnUpdateScmRepo();
 
-		$pm->LoadPlugin('scmhook');
-		$pluginScmHook = $pm->GetPluginObject('scmhook');
-		$scmsvncronjob = new ScmSvnUpdateScmRepo();
-
-		while ($arrGroupNames = db_fetch_array($groupnames)) {
-			// -> register scmhook for these groups
-			$projectObject = group_get_object_by_publicname($arrGroupNames['group_name']);
-			if (!$projectObject->usesTracker()) {
-				$projectObject->setUseTracker(true);
-			}
-			$projectObject->setPluginUse('scmhook');
-			$projectId = $projectObject->getID();
-			$group = $projectObject; // need to be set due to use of global vars in commitEmail class
-			$pluginScmHook->add($projectId);
-			// -> add committracker hook
-			$params = array();
-			$params['group_id'] = $projectId;
-			$params['scmsvn_commitTracker'] = 1;
-			$pluginScmHook->update($params);
-			$hooksArray = $pluginScmHook->getEnabledHooks($projectId);
-			unset($pluginScmHook);
-			$params['hooksString'] = implode('|',$hooksArray);
-			$params['scm_root'] = forge_get_config('repos_path', 'scmsvn') . '/' . $projectObject->getUnixName();
-			if ($scmsvncronjob->updateScmRepo($params)) {
-				db_query_params('UPDATE plugin_scmhook set need_update = $1 where id_group = $2', array(0, $projectId));
-			}
-			$projectObject->setPluginUse('svntracker', false);
+	while ($arrGroupNames = db_fetch_array($groupnames)) {
+		// -> register scmhook for these groups
+		$projectObject = group_get_object_by_publicname($arrGroupNames['group_name']);
+		if (!$projectObject->usesTracker()) {
+			$projectObject->setUseTracker(true);
 		}
-		// -> retrieve old data if any
-		$res = db_query_params('insert into plugin_scmhook_scmsvn_committracker_data_artifact (kind, group_artifact_id, project_task_id)
-					select kind, group_artifact_id, project_task_id FROM plugin_svntracker_data_artifact', array());
-		$res = db_query_params('insert into plugin_scmhook_scmsvn_committracker_data_master (holder_id, svn_date, log_text, file, prev_version, actual_version, author)
-					select holder_id, svn_date, log_text, file, prev_version, actual_version, author FROM plugin_svntracker_data_master', array());
+		$projectObject->setPluginUse('scmhook');
+		$projectId = $projectObject->getID();
+		$group = $projectObject; // need to be set due to use of global vars in commitEmail class
+		$pluginScmHook->add($projectId);
+		// -> add committracker hook
+		$params = array();
+		$params['group_id'] = $projectId;
+		$params['scmsvn_commitTracker'] = 1;
+		$pluginScmHook->update($params);
+		$hooksArray = $pluginScmHook->getEnabledHooks($projectId);
+		unset($pluginScmHook);
+		$params['hooksString'] = implode('|',$hooksArray);
+		$params['scm_root'] = forge_get_config('repos_path', 'scmsvn') . '/' . $projectObject->getUnixName();
+		if ($scmsvncronjob->updateScmRepo($params)) {
+			db_query_params('UPDATE plugin_scmhook set need_update = $1 where id_group = $2', array(0, $projectId));
+		}
+		$projectObject->setPluginUse('svntracker', false);
 	}
-	
-	$pm->deactivate('svntracker');
-	// Remove the symbolic link made if plugin has a www.
-	if (is_dir(forge_get_config('plugins_path') . '/svntracker/www')) { // if the plugin has a www dir delete the link to it
-		if (file_exists('../www/plugins/svntracker')) {
-			$result = unlink('../www/plugins/svntracker');
-			if (!$result) {
-				$feedback .= "\n"._('Soft link wasn\'t removed in www/plugins folder, please do so manually.');
-			}
-		}
-	}
+	// -> retrieve old data if any
+	$res = db_query_params('insert into plugin_scmhook_scmsvn_committracker_data_artifact (kind, group_artifact_id, project_task_id)
+				select kind, group_artifact_id, project_task_id FROM plugin_svntracker_data_artifact', array());
+	$res = db_query_params('insert into plugin_scmhook_scmsvn_committracker_data_master (holder_id, svn_date, log_text, file, prev_version, actual_version, author)
+				select holder_id, svn_date, log_text, file, prev_version, actual_version, author FROM plugin_svntracker_data_master', array());
+}
 
-	// Remove the symbolic link made if plugin has a config.
-	if (file_exists(forge_get_config('config_path'). '/plugins/svntracker')) {
-		$result = unlink(forge_get_config('config_path'). '/plugins/svntracker'); // the apache group or user should have write perms in forge_get_config('config_path')/plugins folder...
+$pm->deactivate('svntracker');
+// Remove the symbolic link made if plugin has a www.
+if (is_dir(forge_get_config('plugins_path') . '/svntracker/www')) { // if the plugin has a www dir delete the link to it
+	if (file_exists('../www/plugins/svntracker')) {
+		$result = unlink('../www/plugins/svntracker');
 		if (!$result) {
-			$feedback .= _('Soft link wasn\'t removed in config folder, please do so manually.');
+			$feedback .= "\n"._('Soft link wasn\'t removed in www/plugins folder, please do so manually.');
 		}
+	}
+}
+
+// Remove the symbolic link made if plugin has a config.
+if (file_exists(forge_get_config('config_path'). '/plugins/svntracker')) {
+	$result = unlink(forge_get_config('config_path'). '/plugins/svntracker'); // the apache group or user should have write perms in forge_get_config('config_path')/plugins folder...
+	if (!$result) {
+		$feedback .= _('Soft link wasn\'t removed in config folder, please do so manually.');
 	}
 }
 echo "SUCCESS\n";
--- src/db/20130602-svncommitemailToScmHookCommitEmailMigration.php
+++ src/db/20130602-svncommitemailToScmHookCommitEmailMigration.php
@@ -28,71 +28,70 @@ require_once $gfplugins.'scmhook/library/scmsvn/cronjobs/updateScmRepo.php';
 
 $pm = plugin_manager_get_object();
 
-if ($pm->PluginIsInstalled('svncommitemail')) {
-	$used = false;
-	$groupnames = db_query_params('SELECT g.group_name FROM plugins p, group_plugin gp, groups g WHERE plugin_name = $1 and gp.group_id = g.group_id and p.plugin_id = gp.plugin_id',
-		array('svncommitemail'));
 
-	if ($groupnames) {
-		if (db_numrows($groupnames) > 0) {
-			$used = true;
-		}
+$used = false;
+$groupnames = db_query_params('SELECT g.group_name FROM plugins p, group_plugin gp, groups g WHERE plugin_name = $1 and gp.group_id = g.group_id and p.plugin_id = gp.plugin_id',
+	array('svncommitemail'));
+
+if ($groupnames) {
+	if (db_numrows($groupnames) > 0) {
+		$used = true;
+	}
+}
+
+if ($used) {
+	// is scmhook activated ?
+	// no ... -> activate the plugin
+	if (!$pm->PluginIsInstalled('scmhook')) {
+		$pm->activate('scmhook');
 	}
 
-	if ($used) {
-		// is scmhook activated ?
-		// no ... -> activate the plugin
-		if (!$pm->PluginIsInstalled('scmhook')) {
-			$pm->activate('scmhook');
-		}
+	$pm->LoadPlugin('scmhook');
+	$pluginScmHook = $pm->GetPluginObject('scmhook');
+	$scmsvncronjob = new ScmSvnUpdateScmRepo();
 
-		$pm->LoadPlugin('scmhook');
-		$pluginScmHook = $pm->GetPluginObject('scmhook');
-		$scmsvncronjob = new ScmSvnUpdateScmRepo();
-
-		while ($arrGroupNames = db_fetch_array($groupnames)) {
-			// -> register scmhook for these groups
-			$projectObject = group_get_object_by_publicname($arrGroupNames['group_name']);
-			if (!$projectObject->usesMail()) {
-				$projectObject->setUseMail(true);
-			}
-			$projectObject->setPluginUse('scmhook');
-			$projectId = $projectObject->getID();
-			$group = $projectObject; // need to be set due to use of global vars in commitEmail class
-			$pluginScmHook->add($projectId);
-			// -> add commitEmail hook
-			$params = array();
-			$params['group_id'] = $projectId;
-			$params['scmsvn_commitEmail'] = 1;
-			$pluginScmHook->update($params);
-			$hooksArray = $pluginScmHook->getEnabledHooks($projectId);
-			unset($pluginScmHook);
-			$params['hooksString'] = implode('|',$hooksArray);
-			$params['scm_root'] = forge_get_config('repos_path', 'scmsvn') . '/' . $projectObject->getUnixName();
-			if ($scmsvncronjob->updateScmRepo($params)) {
-				db_query_params('UPDATE plugin_scmhook set need_update = $1 where id_group = $2', array(0, $projectId));
-			}
-			$projectObject->setPluginUse('svncommitemail', false);
+	while ($arrGroupNames = db_fetch_array($groupnames)) {
+		// -> register scmhook for these groups
+		$projectObject = group_get_object_by_publicname($arrGroupNames['group_name']);
+		if (!$projectObject->usesMail()) {
+			$projectObject->setUseMail(true);
 		}
+		$projectObject->setPluginUse('scmhook');
+		$projectId = $projectObject->getID();
+		$group = $projectObject; // need to be set due to use of global vars in commitEmail class
+		$pluginScmHook->add($projectId);
+		// -> add commitEmail hook
+		$params = array();
+		$params['group_id'] = $projectId;
+		$params['scmsvn_commitEmail'] = 1;
+		$pluginScmHook->update($params);
+		$hooksArray = $pluginScmHook->getEnabledHooks($projectId);
+		unset($pluginScmHook);
+		$params['hooksString'] = implode('|',$hooksArray);
+		$params['scm_root'] = forge_get_config('repos_path', 'scmsvn') . '/' . $projectObject->getUnixName();
+		if ($scmsvncronjob->updateScmRepo($params)) {
+			db_query_params('UPDATE plugin_scmhook set need_update = $1 where id_group = $2', array(0, $projectId));
+		}
+		$projectObject->setPluginUse('svncommitemail', false);
 	}
-	
-	$pm->deactivate('svncommitemail');
-	// Remove the symbolic link made if plugin has a www.
-	if (is_dir(forge_get_config('plugins_path') . '/svncommitemail/www')) { // if the plugin has a www dir delete the link to it
-		if (file_exists('../www/plugins/svncommitemail')) {
-			$result = unlink('../www/plugins/svncommitemail');
-			if (!$result) {
-				$feedback .= "\n"._('Soft link wasn\'t removed in www/plugins folder, please do so manually.');
-			}
-		}
-	}
+}
 
-	// Remove the symbolic link made if plugin has a config.
-	if (file_exists(forge_get_config('config_path'). '/plugins/svncommitemail')) {
-		$result = unlink(forge_get_config('config_path'). '/plugins/svncommitemail'); // the apache group or user should have write perms in forge_get_config('config_path')/plugins folder...
+$pm->deactivate('svncommitemail');
+// Remove the symbolic link made if plugin has a www.
+if (is_dir(forge_get_config('plugins_path') . '/svncommitemail/www')) { // if the plugin has a www dir delete the link to it
+	if (file_exists('../www/plugins/svncommitemail')) {
+		$result = unlink('../www/plugins/svncommitemail');
 		if (!$result) {
-			$feedback .= _('Soft link wasn\'t removed in config folder, please do so manually.');
+			$feedback .= "\n"._('Soft link wasn\'t removed in www/plugins folder, please do so manually.');
 		}
+	}
+}
+
+// Remove the symbolic link made if plugin has a config.
+if (file_exists(forge_get_config('config_path'). '/plugins/svncommitemail')) {
+	$result = unlink(forge_get_config('config_path'). '/plugins/svncommitemail'); // the apache group or user should have write perms in forge_get_config('config_path')/plugins folder...
+	if (!$result) {
+		$feedback .= _('Soft link wasn\'t removed in config folder, please do so manually.');
 	}
 }
 echo "SUCCESS\n";
