6443046afa3a8af1fbd283b4fffe181a50bb8bbc
Franck Villaume <franck.villaume@trivialdev.com>
2013-07-01 23:23:59 +0200

    db: drop wrong constraint if any exists on stats_agg

---
+++ src/db/20130701-delete-constraints-on-stats_agg.sql
@@ -0,0 +1,2 @@
+ALTER TABLE stats_agg_site_by_group DROP CONSTRAINT IF EXISTS stats_agg_site_by_group_group_id_fkey;
+ALTER TABLE stats_agg_logo_by_group DROP CONSTRAINT IF EXISTS stats_agg_logo_by_group_group_id_fkey;
