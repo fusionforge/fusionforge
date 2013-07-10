941cce3ae3f0258144857cc4a3d433b0595155a7
Roland Mas <lolando@debian.org>
2013-07-10 11:33:53 +0200

    Fixed PHP warning

--- src/plugins/scmcvs/common/CVSPlugin.class.php
+++ src/plugins/scmcvs/common/CVSPlugin.class.php
@@ -304,7 +304,7 @@ class CVSPlugin extends SCMPlugin {
 				return false ;
 			}
 
-			$hist_file =& fopen( $hist_file_path, 'r' );
+			$hist_file = fopen( $hist_file_path, 'r' );
 			if ( ! $hist_file ) {
 				echo "Unreadable history\n" ;
 				db_rollback () ;
