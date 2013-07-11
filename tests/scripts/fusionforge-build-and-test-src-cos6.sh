e8544cd7efe57de8d7961fcb8f3e1e0a463aab7f
Roland Mas <lolando@debian.org>
2013-07-11 10:44:10 +0200

    Save logs even if the build fails

--- tests/scripts/fusionforge-build-and-test-src-cos6.sh
+++ tests/scripts/fusionforge-build-and-test-src-cos6.sh
@@ -66,8 +66,7 @@ EOF
 # Run tests
 retcode=0
 echo "Run phpunit test on $HOST in $FORGE_HOME"
-ssh root@$HOST "$FORGE_HOME/tests/func/vncxstartsuite.sh $FILTER"
-retcode=$?
+ssh root@$HOST "$FORGE_HOME/tests/func/vncxstartsuite.sh $FILTER" || retcode=$?
 rsync -av root@$HOST:/var/log/ $WORKSPACE/reports/
 scp root@$HOST:/tmp/gforge-*.log $WORKSPACE/reports/
 
