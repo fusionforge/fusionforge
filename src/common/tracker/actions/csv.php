2103627187c57ffe6195c03367bb5fd2e0a2fa9a
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-07-19 08:31:34 +0000

    Typo

--- src/common/tracker/actions/csv.php
+++ src/common/tracker/actions/csv.php
@@ -84,7 +84,7 @@ $format .= " using '$sep' as separator.";
 <p><?php echo _('This page allows you to export the items using a CSV (<a href="http://en.wikipedia.org/wiki/Comma-separated_values">Comma Separated Values</a>) File. This format can be used to view your entries using MS Excel.'); ?></p>
 <h2><?php echo _('Export as a CSV file'); ?></h2>
 
-<strong><?php echo _('Selected CSV Format :'); ?></strong> CSV<?php echo $format ?> <a href="<?php echo $url_set_format ?>">(Change)</a>
+<strong><?php echo _('Selected CSV Format:'); ?></strong> CSV<?php echo $format ?> <a href="<?php echo $url_set_format ?>">(Change)</a>
 
 <p><a href="<?php echo $url_export ?>"><?php echo _('Download CSV file'); ?></a></p>
 
