9b5995cb560904f5549c4f98a86d46f51aa4abc2
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-08-21 07:54:59 +0000

    Add <div class="info-box">

--- src/www/reporting/index.php
+++ src/www/reporting/index.php
@@ -31,25 +31,35 @@ require_once $gfcommon.'reporting/ReportSetup.class.php';
 session_require_global_perm ('forge_stats', 'read') ;
 
 report_header(_('Main Page'));
-
 ?>
+
+<div class="info-box">
 <h2><?php echo _('Users'); ?></h2>
 <p>
 <a href="useradded.php?SPAN=1"><?php echo _('Users Added (graph)'); ?></a><br />
 <a href="usercum.php?SPAN=1"><?php echo _('Cumulative Users (graph)'); ?></a><br />
 <?php echo _('User-specific reports: Tracker, Task Mgr, Forums, Doc Mgr, Downloads'); ?><br />
 <a href="useract.php"><?php echo _('Activity (graph)'); ?></a></p>
+</div>
+
+<div class="info-box">
 <h2><?php echo _('Projects'); ?></h2>
 <p>
 <a href="groupadded.php?SPAN=1"><?php echo _('Projects Added (graph)'); ?></a><br />
 <a href="groupcum.php?SPAN=1"><?php echo _('Cumulative Projects (graph)'); ?></a><br />
 <?php echo _('Project-specific reports: Tracker, Task Mgr, Forums, Doc Mgr, Downloads'); ?><br />
 <a href="projectact.php"><?php echo _('Activity (graph)'); ?></a></p>
+</div>
+
+<div class="info-box">
 <h2><?php echo _('Site-Wide'); ?></h2>
 <p>
 <?php echo _('Site-wide reports: Tracker, Task Mgr, Forums, Doc Mgr, Downloads'); ?><br />
 <a href="toolspie.php"><?php echo _('Pie (graph)'); ?></a><br />
 <a href="siteact.php"><?php echo _('Line (graph)'); ?></a></p>
+</div>
+
+<div class="info-box">
 <h2><?php echo _('Time-Tracking'); ?></h2>
 <p>
 <a href="usertime.php"><?php echo _('Individual User Time Report (graph)'); ?></a> <a href="usertime.php?typ=r">(<?php echo _('report'); ?>)</a><br />
@@ -57,12 +67,16 @@ report_header(_('Main Page'));
 <a href="sitetime.php"><?php echo _('Site-Wide Time Report (graph)'); ?></a> <a href="sitetime.php?typ=r">(<?php echo _('report'); ?>)</a><br />
 <a href="sitetimebar.php"><?php echo _('Site-Wide Total Hours Graph (graph)'); ?></a> <a href="sitetimebar.php?typ=r">(<?php echo _('report'); ?>)</a><br />
 <a href="usersummary.php"><?php echo _('Site-Wide Task &amp; Hours (report)'); ?></a></p>
+</div>
 
 <?php if (forge_check_global_perm ('forge_stats', 'admin')) { ?>
+
+<div class="info-box">
 <h2><?php echo _('Administrative'); ?></h2>
 <p>
 <a href="rebuild.php"><?php echo _('Initialize / Rebuild Reporting Tables'); ?></a><br />
 <a href="timecategory.php"><?php echo _('Manage Time Tracker Categories'); ?></a></p>
+</div>
 <?php
 }
 
