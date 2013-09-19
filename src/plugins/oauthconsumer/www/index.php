6552c2d09851d6d6948150c675a36322c00fbaaf
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-09-19 14:10:26 +0000

    <br />; whitespace

--- src/plugins/oauthconsumer/www/index.php
+++ src/plugins/oauthconsumer/www/index.php
@@ -17,7 +17,7 @@ if(count($providers)>0)	{
 	<b><?php echo _('Providers'); ?></b>
 	<?php $f_provider_id = getStringFromPost('providers'); ?>
 	<select name=providers>
-	<?php foreach ($providers as $provider) 	{
+	<?php foreach ($providers as $provider) {
 		echo '<option value="'.$provider->get_id().'" ';
 		if($provider->get_id()==$f_provider_id)	{
 			echo 'selected';
@@ -28,14 +28,14 @@ if(count($providers)>0)	{
 		<input type="submit" value="<?php echo _('Select') ?>"/>
 		</form>
 	<?php
-	
+
 	if($f_provider_id)	{
 		$access_tokens = OAuthAccessToken::get_all_access_tokens_by_provider($f_provider_id, $userid);
 		if(count($access_tokens)>0)	{
 			?>
 			<form action="response.php" method="post">
 			<table class="width50" align="center" cellspacing="1">
-							
+
 			<tr>
 			<td class="category"><?php echo _('Access Tokens');?></td>
 			<td><select name=tokens>
@@ -44,12 +44,12 @@ if(count($providers)>0)	{
 			}?>
 			</select></td>
 			</tr>
-			
+
 			<tr>
 			<td class="category"><?php echo _('Resource URL') ?></td>
 			<td><input name="resource_url" maxlength="250" size="60" value=""/></td>
 			</tr>
-			
+
 			<tr>
 			<td class="category"><?php echo _('HTTP Request');?></td>
 			<td><select name=http>
@@ -57,31 +57,31 @@ if(count($providers)>0)	{
 				<option value="post">POST</option>
 			</select></td>
 			</tr>
-			
+
 			<tr>
 			<td class="category"><?php echo _('POST data') ?></td>
 			<td><input name="post_data" maxlength="250" size="60" value=""/></td>
 			</tr>
-			
+
 			<tr>
 			<td class="center" colspan="2"><input type="submit" value="<?php echo _('Go') ?>"/></td>
 			</tr>
-			
-			</table>			
+
+			</table>
 			</form>
-			
-			<br><br>
-			<?php 
-			
-		}else {
+
+			<br /><br />
+			<?php
+
+		} else {
 			echo '<p>'. _('No access tokens have been created for this provider').'</p>';
 		}
 	}
-}else 	{
+} else {
 	echo '<p>'. _('There are no OAuth Providers registered in the database currently. Please ask your forge administer to create one.').'</p>';
 }
 
-echo'<br><br><p>'._("If no OAuth Providers or Access Tokens have been created yet, follow the links below to get started").'</p>';
+echo'<br /><br /><p>'._("If no OAuth Providers or Access Tokens have been created yet, follow the links below to get started").'</p>';
 
 echo util_make_link('/plugins/'.$pluginname.'/providers.php', _('OAuth Providers')). ' <br />';
 echo util_make_link('/plugins/'.$pluginname.'/get_access_token.php', _('Get Access tokens')).'<br /> ';
