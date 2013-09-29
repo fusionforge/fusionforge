9343478685456ca4c8d0c74c6cd94c3688f5043a
Franck Villaume <franck.villaume@trivialdev.com>
2013-09-29 14:22:28 +0200

    bug #581: really include the js in the page

--- src/plugins/scmhook/common/scmhookPlugin.class.php
+++ src/plugins/scmhook/common/scmhookPlugin.class.php
@@ -127,6 +127,7 @@ class scmhookPlugin extends Plugin {
 	function displayScmHook($group_id) {
 		global $HTML;
 		use_javascript('/js/sortable.js');
+		echo $HTML->getJavascripts();
 		$hooksAvailable = $this->getAvailableHooks($group_id);
 		$statusDeploy = $this->getStatusDeploy($group_id);
 		$hooksEnabled = $this->getEnabledHooks($group_id);
