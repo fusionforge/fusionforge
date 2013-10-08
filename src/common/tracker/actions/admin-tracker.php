54c17afee2cd32d42095564771d61a1d3987262c
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-10-08 12:48:44 +0000

    Better header

--- src/common/tracker/actions/admin-tracker.php
+++ src/common/tracker/actions/admin-tracker.php
@@ -28,47 +28,53 @@ global $ath;
 //
 //  SHOW LINKS TO FUNCTIONS
 //
+$ath->header(array ('title'=>_('Administration of tracker').': '.$ath->getName()));
 
-$ath->header(array ('title'=>_('Administration').': '.$ath->getName()));
-
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id,
-			     '<strong>'._('New Tracker').'</strong>').'<br />' ;
-echo _('Create a new tracker.') ;
+echo '<p>';
+echo util_make_link('/tracker/admin/?group_id='.$group_id, '<strong>'._('New Tracker').'</strong>');
+echo '<br />';
+echo _('Create a new tracker.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;update_type=1',
-			     '<strong>'._('Update Settings').'</strong>').'<br />' ;
-echo _('Set up preferences like expiration times, email addresses.') ;
+echo '<p>';
+echo util_make_link('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;update_type=1', '<strong>'._('Update Settings').'</strong>');
+echo '<br />';
+echo _('Set up preferences like expiration times, email addresses.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;add_extrafield=1',
-			     '<strong>'._('Manage Custom Fields').'</strong>').'<br />' ;
-echo _('Add new boxes like Phases, Quality Metrics, Components, etc.  Once added they can be used with other selection boxes (for example, Categories or Groups) to describe and browse bugs or other artifact types.') ;
+echo '<p>';
+echo util_make_link('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;add_extrafield=1', '<strong>'._('Manage Custom Fields').'</strong>');
+echo '<br />';
+echo _('Add new boxes like Phases, Quality Metrics, Components, etc.  Once added they can be used with other selection boxes (for example, Categories or Groups) to describe and browse bugs or other artifact types.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;workflow=1',
-			     '<strong>'._('Manage Workflow').'</strong>').'<br />' ;
-echo _('Edit tracker workflow.') ;
+echo '<p>';
+echo util_make_link('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;workflow=1', '<strong>'._('Manage Workflow').'</strong>');
+echo '<br />';
+echo _('Edit tracker workflow.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;customize_list=1',
-			     '<strong>'._('Customize List').'</strong>').'<br />' ;
-echo _('Customize display for the tracker.') ;
+echo '<p>';
+echo util_make_link('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;customize_list=1', '<strong>'._('Customize List').'</strong>');
+echo '<br />';
+echo _('Customize display for the tracker.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;add_canned=1',
-			     '<strong>'._('Add/Update Canned Responses').'</strong>').'<br />' ;
-echo _('Create/change generic response messages for the tracker.') ;
+echo '<p>';
+echo util_make_link('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;add_canned=1', '<strong>'._('Manage Canned Responses').'</strong>');
+echo '<br />';
+echo _('Create/change generic response messages for the tracker.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;clone_tracker=1',
-			     '<strong>'._('Apply Template Tracker').'</strong>').'<br />' ;
-echo _('Duplicate parameters and fields from a template trackers in this one.') ;
+echo '<p>';
+echo util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;clone_tracker=1', '<strong>'._('Apply Template Tracker').'</strong>');
+echo '<br />';
+echo _('Duplicate parameters and fields from a template trackers in this one.');
 echo '</p>';
 
-echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;delete=1',
-			     '<strong>'._('Delete').'</strong>').'<br />' ;
-echo _('Permanently delete this tracker.') ;
+echo '<p>' . util_make_link ('/tracker/admin/?group_id='.$group_id.'&amp;atid='.$ath->getID().'&amp;delete=1', '<strong>'._('Delete').'</strong>');
+echo '<br />';
+echo _('Permanently delete this tracker.');
 echo '</p>';
 
 $ath->footer(array());
