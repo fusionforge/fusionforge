90dfd84ad3532cc991c9713886b8f034b51b77d1
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-10-15 17:00:46 +0000

    Codendi --> Fusionforge

--- src/common/dao/CodendiDataAccess.class.php
+++ src/common/dao/CodendiDataAccess.class.php
@@ -4,14 +4,14 @@
  *
  * Originally written by Nicolas Terray, 2005. Xerox Codendi Team.
  *
- * This file is a part of Codendi.
+ * This file is a part of Fusionforge.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Fusionforge is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Fusionforge is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
