3a3bbbb2263d989132cb1d5b21cb3787d707c1b8
Franck Villaume <franck.villaume@trivialdev.com>
2013-10-17 22:53:08 +0200

    docman: use correct element : use #maindiv, fix replace

--- src/www/docman/scripts/DocManController.js
+++ src/www/docman/scripts/DocManController.js
@@ -62,21 +62,16 @@ DocManListFileController.prototype =
 
 	resizableDiv: function() {
 		var splitterPosition = '30%';
-		var mainwidth = jQuery('#views').width();
+		var mainwidth = jQuery('#maindiv').width();
 		if (jQuery.Storage.get('splitterStyle') !== undefined) {
-			storedSplitterPosition = jQuery.Storage.get('splitterStyle').replace(/\D/g, '');
+			storedSplitterPosition = jQuery.Storage.get('splitterStyle').replace(/px;/g, '').replace(/left: /g, '');
 			splitterPosition = Math.round(storedSplitterPosition * 100 / mainwidth )+'%';
 		}
-		(jQuery(this.params.divLeft).height() > jQuery(this.params.divRight).height()) ? mainheight = jQuery(this.params.divLeft).height() : mainheight = jQuery('#rightdiv').height();
-		jQuery('#views')
-				.height(mainheight)
+		(jQuery(this.params.divLeft).height() > jQuery(this.params.divRight).height()) ? mainheight = jQuery(this.params.divLeft).height() : mainheight = jQuery(this.params.divRight).height();
+		jQuery('#views').height(mainheight)
 				.split({orientation:'vertical', limit:100, position: splitterPosition});
 		jQuery('.vsplitter').mouseup(function(){
 			jQuery.Storage.set('splitterStyle',''+jQuery('.vsplitter').attr('style'));
-		});
-		jQuery(window).resize(function(){
-			(jQuery(this.params.divLeft).height() > jQuery(this.params.divRight)) ? mainheight = jQuery(this.params.divLeft).height() : mainheight = jQuery('#rightdiv').height();
-			jQuery('#views').height(mainheight);
 		});
 	},
 
