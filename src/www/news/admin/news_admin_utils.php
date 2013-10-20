5d46194f35b0096619b5693ef522853c85cc6291
Franck Villaume <franck.villaume@trivialdev.com>
2013-10-20 14:40:02 +0200

    news: indent file

--- src/www/news/admin/news_admin_utils.php
+++ src/www/news/admin/news_admin_utils.php
@@ -5,7 +5,7 @@
  * Copyright 1999-2001 (c) VA Linux Systems
  * Copyright 2002-2004 (c) GForge Team
  * Copyright (C) 2012 Alain Peyrat - Alcatel-Lucent
- * Copyright 2012, Franck Villaume - TrivialDev
+ * Copyright 2012-2013, Franck Villaume - TrivialDev
  * http://fusionforge.org/
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -25,118 +25,117 @@
  */
 
 function show_news_approve_form($qpa_pending, $qpa_rejected, $qpa_approved) {
-        /*
-       		Show list of waiting news items
-       	*/
+	/*
+		Show list of waiting news items
+	*/
 
-       	// function to show single news item
-       	// factored out because called 3 time below
-       	function show_news_item($row,$i,$approved,$selectable) {
-	        global $HTML;
+	// function to show single news item
+	// factored out because called 3 time below
+	function show_news_item($row, $i, $approved, $selectable) {
+		global $HTML;
 
 		echo '<tr '. $HTML->boxGetAltRowStyle($i) . '><td>';
-       		if ($selectable) {
-       			echo '<input type="checkbox" '
-       			     .'name="news_id[]" value="'
-       			     .$row['id'].'" />';
-       		}
-       		echo date(_('Y-m-d'), $row['post_date']).'</td>
-       		<td width="45%">';
-       		echo '
-       		<a href="'.getStringFromServer('PHP_SELF').'?approve=1&amp;id='.$row['id'].'">'.$row['summary'].'</a>
-       		</td>
+		if ($selectable) {
+			echo '<input type="checkbox" '
+			.'name="news_id[]" value="'
+			.$row['id'].'" />';
+		}
+		echo date(_('Y-m-d'), $row['post_date']).'</td>
+		<td width="45%">';
+		echo '
+		<a href="'.getStringFromServer('PHP_SELF').'?approve=1&amp;id='.$row['id'].'">'.$row['summary'].'</a>
+		</td>
 
-       		<td class="onethirdwidth">'
-		.util_make_link_g ($row['unix_group_name'],$row['group_id'],$row['group_name'].' ('.$row['unix_group_name'].')')
-       		.'</td>
-       		</tr>'
-       		;
-       	}
+		<td class="onethirdwidth">'
+		.util_make_link_g ($row['unix_group_name'], $row['group_id'], $row['group_name'].' ('.$row['unix_group_name'].')')
+		.'</td>
+		</tr>'
+		;
+	}
 
-       	$title_arr = array(
-       		_('Date'),
-       		_('Subject'),
-       		_('Project')
-       	);
+	$title_arr = array(
+		_('Date'),
+		_('Subject'),
+		_('Project')
+	);
 
-	$ra = RoleAnonymous::getInstance() ;
+	$ra = RoleAnonymous::getInstance();
 
-       	$result = db_query_qpa($qpa_pending);
+	$result = db_query_qpa($qpa_pending);
 	$items = array();
 	while ($row_item = db_fetch_array($result)) {
 		if ($ra->hasPermission('project_read', $row_item['group_id'])) {
 			$items[] = $row_item;
 		}
 	}
-       	$rows = count($items);
+	$rows = count($items);
 
-       	echo '<form action="'. getStringFromServer('PHP_SELF') .'" method="post">';
-       	echo '<input type="hidden" name="mass_reject" value="1" />';
-       	echo '<input type="hidden" name="post_changes" value="y" />';
+	echo '<form action="'. getStringFromServer('PHP_SELF') .'" method="post">';
+	echo '<input type="hidden" name="mass_reject" value="1" />';
+	echo '<input type="hidden" name="post_changes" value="y" />';
 
-       	if ($rows < 1) {
-       		echo '
-       			<h2>'._('No Queued Items Found').'</h2>';
-       	} else {
-       		echo '<h2>'.sprintf(_('These items need to be approved (total: %1$s)'), $rows).'</h2>';
-       		echo $GLOBALS['HTML']->listTableTop($title_arr);
-       		for ($i=0; $i<$rows; $i++) {
-       			show_news_item($items[$i],$i,false,true);
-       		}
-       		echo $GLOBALS['HTML']->listTableBottom();
-       		echo '<br /><input type="submit" name="submit" value="'._('Reject Selected').'" />';
-       	}
-       	echo '</form>';
+	if ($rows < 1) {
+		echo '
+			<h2>'._('No Queued Items Found').'</h2>';
+	} else {
+		echo '<h2>'.sprintf(_('These items need to be approved (total: %1$s)'), $rows).'</h2>';
+		echo $GLOBALS['HTML']->listTableTop($title_arr);
+		for ($i=0; $i < $rows; $i++) {
+			show_news_item($items[$i], $i, false,true);
+		}
+		echo $GLOBALS['HTML']->listTableBottom();
+		echo '<br /><input type="submit" name="submit" value="'._('Reject Selected').'" />';
+	}
+	echo '</form>';
 
-       	/*
-       		Show list of rejected news items for this week
-       	*/
+	/*
+		Show list of rejected news items for this week
+	*/
 
-       	$result = db_query_qpa($qpa_rejected);
+	$result = db_query_qpa($qpa_rejected);
 	$items = array();
 	while ($row_item = db_fetch_array($result)) {
 		if ($ra->hasPermission('project_read', $row_item['group_id'])) {
 			$items[] = $row_item;
 		}
 	}
-       	$rows = count($items);
+	$rows = count($items);
 
-       	if ($rows < 1) {
-       		echo '
-       			<h2>'._('No rejected items found for this week').'</h2>';
-       	} else {
-       		echo '<h2>'.sprintf(_('These items were rejected this past week or were not intended for front page (total: %1$s)'), $rows).'</h2>';
-       		echo $GLOBALS['HTML']->listTableTop($title_arr);
-       		for ($i=0; $i<$rows; $i++) {
-       			show_news_item($items[$i],$i,false,false);
-       		}
-       		echo $GLOBALS['HTML']->listTableBottom();
-       	}
+	if ($rows < 1) {
+		echo '
+			<h2>'._('No rejected items found for this week').'</h2>';
+	} else {
+		echo '<h2>'.sprintf(_('These items were rejected this past week or were not intended for front page (total: %1$s).'), $rows).'</h2>';
+		echo $GLOBALS['HTML']->listTableTop($title_arr);
+		for ($i=0; $i<$rows; $i++) {
+			show_news_item($items[$i],$i,false,false);
+		}
+		echo $GLOBALS['HTML']->listTableBottom();
+	}
 
-       	/*
-       		Show list of approved news items for this week
-       	*/
+	/*
+		Show list of approved news items for this week
+	*/
 
-       	$result = db_query_qpa($qpa_approved);
+	$result = db_query_qpa($qpa_approved);
 	$items = array();
 	while ($row_item = db_fetch_array($result)) {
 		if ($ra->hasPermission('project_read', $row_item['group_id'])) {
 			$items[] = $row_item;
 		}
 	}
-       	$rows = count($items);
-       	if ($rows < 1) {
-       		echo '
-       			<h2>'._('No approved items found for this week').'</h2>';
-       	} else {
-       		echo '<h2>'.sprintf(_('These items were approved this past week (total: %1$s)'), $rows).'</h2>';
-       		echo $GLOBALS['HTML']->listTableTop($title_arr);
-       		for ($i=0; $i<$rows; $i++) {
-       			show_news_item($items[$i],$i,false,false);
-       		}
-       		echo $GLOBALS['HTML']->listTableBottom();
-       	}
-
+	$rows = count($items);
+	if ($rows < 1) {
+		echo '
+			<h2>'._('No approved items found for this week').'</h2>';
+	} else {
+		echo '<h2>'.sprintf(_('These items were approved this past week (total: %1$s).'), $rows).'</h2>';
+		echo $GLOBALS['HTML']->listTableTop($title_arr);
+		for ($i=0; $i < $rows; $i++) {
+			show_news_item($items[$i], $i, false, false);
+		}
+		echo $GLOBALS['HTML']->listTableBottom();
+	}
 }
 
 // Local Variables:
