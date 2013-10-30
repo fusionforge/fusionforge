b7860b2842678a1e0e4c1828738740e6d90d4062
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-10-30 08:50:08 +0000

    Fix syntax error

--- src/www/news/submit.php
+++ src/www/news/submit.php
@@ -143,10 +143,10 @@ if (session_loggedin()) {
 		<input type="hidden" name="form_key" value="'. form_generate_key() .'" />
 		<p><strong>'._('For project')._(': ').$group->getPublicName().'</strong></p>
 		<p>
-		<strong>'._('Subject')_(': ').'</strong>'.utils_requiredField().'<br />
+		<strong>'._('Subject')._(': ').'</strong>'.utils_requiredField().'<br />
 		<input required="required" type="text" name="summary" value="'.$summary.'" size="80" /></p>
 		<p>
-		<strong>'._('Details')_(': ').'</strong>'.notepad_button('document.forms.newssubmitform.details').utils_requiredField().'</p>';
+		<strong>'._('Details')._(': ').'</strong>'.notepad_button('document.forms.newssubmitform.details').utils_requiredField().'</p>';
 
 	$params = array();
 	$params['name'] = 'details';
