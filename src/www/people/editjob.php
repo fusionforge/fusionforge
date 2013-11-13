eb37eb7cc1a305fa6e370e80f8a95d7d3cefdf43
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-11-13 15:35:49 +0000

    Translate ":"

--- src/www/people/editjob.php
+++ src/www/people/editjob.php
@@ -166,10 +166,10 @@ array($group_id, user_getid(), htmlspecialchars($title), htmlspecialchars($descr
 		<strong>'._('Status').'</strong><br />
 		'. people_job_status_box('status_id',db_result($result,0,'status_id')) .'</p>
 		<p>
-		<strong>'._('Short Description').':</strong><br />
+		<strong>'._('Short Description')._(':').'</strong><br />
 		<input type="text" name="title" value="'. db_result($result,0,'title') .'" size="40" maxlength="60" /></p>
 		<p>
-		<strong>'._('Long Description').':</strong><br />
+		<strong>'._('Long Description')._(':').'</strong><br />
 		<textarea name="description" rows="10" cols="60">'. db_result($result,0,'description') .'</textarea></p>
 		<p>
 		<input type="submit" name="update_job" value="'._('Update Descriptions').'" /></p>
