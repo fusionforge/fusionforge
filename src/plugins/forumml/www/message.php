a4724641f37b9cee40a36db1ca0e279b584c0025
Franck Villaume <franck.villaume@trivialdev.com>
2013-11-27 10:04:33 +0100

    Forumml plugin: codendi -> Fusionforge

--- src/plugins/forumml/www/message.php
+++ src/plugins/forumml/www/message.php
@@ -4,14 +4,14 @@
  *
  * Originally written by Jean-Philippe Giola, 2005
  *
- * This file is a part of codendi.
+ * This file is a part of Fusionforge.
  *
- * codendi is free software; you can redistribute it and/or modify
+ * Fusionforge is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * codendi is distributed in the hope that it will be useful,
+ * Fusionforge is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -20,7 +20,6 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * $Id$
  */
 
 /*
