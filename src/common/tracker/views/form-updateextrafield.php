bf11598e37515bc66e5d05c18f87455a47e2dd6c
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2013-12-04 11:43:59 +0000

    Add <label>

--- src/common/tracker/views/form-updateextrafield.php
+++ src/common/tracker/views/form-updateextrafield.php
@@ -39,45 +39,60 @@ if (!$ac || !is_object($ac)) {
 } else {
 ?>
 <p>
-<strong><?php echo _('Type of custom field').': '.$ac->getTypeName(); ?></strong></p>
+<strong><?php echo _('Type of custom field')._(': ').$ac->getTypeName(); ?></strong></p>
 
 <form action="<?php echo getStringFromServer('PHP_SELF').'?group_id='.$group_id.'&amp;id='.$id.'&amp;atid='.$ath->getID(); ?>" method="post">
 	<input type="hidden" name="update_box" value="y" />
 	<input type="hidden" name="id" value="<?php echo $ac->getID(); ?>" />
 	<p>
+		<label for="name">
 		<strong><?php echo _('Custom Field Name')._(':'); ?></strong><br />
-		<input type="text" name="name" value="<?php echo $ac->getName(); ?>" />
+		</label>
+		<input id="name" type="text" name="name" value="<?php echo $ac->getName(); ?>" />
 	</p>
 	<p>
 		<?php if ($ac->getType() == ARTIFACT_EXTRAFIELDTYPE_TEXTAREA) { ?>
+		<label for="attribute1">
 		<b><?php echo _('Text Area Rows'); ?></b><br />
-		<input type="text" name="attribute1" value="<?php echo $ac->getAttribute1(); ?>" size="2" maxlength="2" />
+		</label>
+		<input id="attribute1" type="text" name="attribute1" value="<?php echo $ac->getAttribute1(); ?>" size="2" maxlength="2" />
 	</p>
 	<p>
+		<label for="attribute2">
 		<b><?php echo _('Text Area Columns'); ?></b><br />
-		<input type="text" name="attribute2" value="<?php echo $ac->getAttribute2(); ?>" size="2" maxlength="2" />
-		<?php } elseif ($ac->getType() == ARTIFACT_EXTRAFIELDTYPE_TEXT || $ac->getType() == ARTIFACT_EXTRAFIELDTYPE_RELATION) {?>
+		</label>
+		<input id="attribute2" type="text" name="attribute2" value="<?php echo $ac->getAttribute2(); ?>" size="2" maxlength="2" />
+		<?php } elseif ($ac->getType() == ARTIFACT_EXTRAFIELDTYPE_TEXT ||
+					$ac->getType() == ARTIFACT_EXTRAFIELDTYPE_RELATION) {?>
+		<label for="attribute1">
 		<b><?php echo _('Text Field Size'); ?></b><br />
-		<input type="text" name="attribute1" value="<?php echo $ac->getAttribute1(); ?>" size="2" maxlength="2" />
+		</label>
+		<input id="attribute1" type="text" name="attribute1" value="<?php echo $ac->getAttribute1(); ?>" size="2" maxlength="2" />
 		</p>
 		<p>
+		<label for="attribute2">
 		<b><?php echo _('Text Field Maxlength'); ?></b><br />
-		<input type="text" name="attribute2" value="<?php echo $ac->getAttribute2(); ?>" size="2" maxlength="2" />
+		<input id="attribute2" type="text" name="attribute2" value="<?php echo $ac->getAttribute2(); ?>" size="2" maxlength="2" />
 		<?php } else { ?>
 		<input type="hidden" name="attribute1" value="0" />
 		<input type="hidden" name="attribute2" value="0" />
 		<?php } ?>
 	</p>
 	<p>
-		<strong><?php echo _('Field alias') ?>:</strong><br />
-		<input type="text" name="alias" value="<?php echo $ac->getAlias(); ?>" />
+		<label for="alias">
+		<strong><?php echo _('Field alias')._(':'); ?></strong><br />
+		</label>
+		<input id="alias" type="text" name="alias" value="<?php echo $ac->getAlias(); ?>" />
 	</p>
-	<p><input type="checkbox" name="is_required" <?php if ($ac->isRequired()) echo "checked=\"checked\""; ?> /><?php echo _('Field is mandatory')?></p>
-	<div class="warning"><?php echo _('It is not recommended that you change the custom field name because other things are dependent upon it. When you change the custom field name, all related items will be changed to the new name') ?>
-	</div>
+	<p><input id="is_required" type="checkbox" name="is_required" <?php if ($ac->isRequired()) echo 'checked="checked"'; ?> />
+	<?php echo _('Field is mandatory')?></p>
+	<label for="is_required">
+	<p class="warning"><?php echo _('It is not recommended that you change the custom field name because other things are dependent upon it. When you change the custom field name, all related items will be changed to the new name') ?>
+	</label>
+	</p>
 	<p>
 	<input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" />
-    </p>
+	</p>
 </form>
 <?php
 }
