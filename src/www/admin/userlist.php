753c32de86c76682ca9810c0a909ac8553fac8e5
Franck Villaume <franck.villaume@trivialdev.com>
2013-12-08 16:18:54 +0100

    fix bug [#613]: disable link when needed

--- src/www/admin/userlist.php
+++ src/www/admin/userlist.php
@@ -3,6 +3,7 @@
  *
  * Copyright 1999-2000 (c) The SourceForge Crew
  * Copyright 2010 (c) Franck Villaume - Capgemini
+ * Copyright 2013, Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -26,43 +27,43 @@ require_once $gfcommon.'include/pre.php';
 require_once $gfwww.'admin/admin_utils.php';
 
 // user must be forge admin to proceed
-session_require_global_perm ('forge_admin');
+session_require_global_perm('forge_admin');
 
 /**
  * performAction() - Updates the indicated user status
  *
- * @param               string  $newStatus - the new user status
- * @param               string  $statusString - the status string to display
- * @param               string  $user_id - the user id to act upon
+ * @param	string	$newStatus - the new user status
+ * @param	string	$statusString - the status string to display
+ * @param	string	$user_id - the user id to act upon
  */
 function performAction($newStatus, $statusString, $user_id) {
 	global $feedback;
 
 	$u = user_get_object($user_id);
 	if (!$u || !is_object($u)) {
-		exit_error(_('Could Not Get User'),'home');
+		exit_error(_('Could Not Get User'), 'home');
 	} elseif ($u->isError()) {
-		exit_error($u->getErrorMessage(),'home');
+		exit_error($u->getErrorMessage(), 'home');
 	}
 	if($newStatus=="D") {
 		if(!$u->setStatus($newStatus)) {
-			exit_error($u->getErrorMessage(),'home');
+			exit_error($u->getErrorMessage(), 'home');
 		}
 		if(!$u->delete(true)) {
-			exit_error($u->getErrorMessage(),'home');
+			exit_error($u->getErrorMessage(), 'home');
 		}
 	} else {
 		if(!$u->setStatus($newStatus)) {
-			exit_error($u->getErrorMessage(),'home');
+			exit_error($u->getErrorMessage(), 'home');
 		}
 		if(!$u->setUnixStatus($newStatus)) {
-			exit_error($u->getErrorMessage(),'home');
+			exit_error($u->getErrorMessage(), 'home');
 		}
 	}
 	$feedback = sprintf(_('User updated to %1$s status'), $statusString);
 }
 
-function show_users_list ($users, $filter='', $sortorder='realname') {
+function show_users_list($users, $filter = '', $sortorder = 'realname') {
 	echo '<p>' ._('Status') .': '.
 		util_make_link('/admin/userlist.php', _('All')). '
 		<span class="active">'.util_make_link('/admin/userlist.php?status=A'.'&amp;sortorder='.$sortorder,_('Active')). '</span>
@@ -114,11 +115,29 @@ function show_users_list ($users, $filter='', $sortorder='realname') {
 		echo '<td width="15%" class="align-center">';
 		echo ($u->getAddDate() ? date(_('Y-m-d H:i'), $u->getAddDate()) : '-');
 		echo '</td>';
-		echo '<td width="15%" style="text-align:center">'.util_make_link ('/developer/?form_dev='.$u->getID(),_('User Profile')).'</td>';
-		echo '<td width="15%" style="text-align:center">'.util_make_link ('/admin/userlist.php?action=activate&amp;user_id='.$u->getID().$filter,_('Activate')).'</td>';
-		echo '<td width="15%" style="text-align:center">'.util_make_link ('/admin/userlist.php?action=delete&amp;user_id='.$u->getID().$filter,_('Delete')).'</td>';
-		echo '<td width="15%" style="text-align:center">'.util_make_link ('/admin/userlist.php?action=suspend&amp;user_id='.$u->getID().$filter,_('Suspend')).'</td>';
-		echo '<td width="12%" style="text-align:center">'.util_make_link ('/admin/passedit.php?user_id='.$u->getID().$filter,_('Change Password')).'</td>';
+		echo '<td width="15%" style="text-align:center">';
+		if ($u->getStatus() != 'D') {
+			echo util_make_link('/developer/?form_dev='.$u->getID(),_('User Profile'));
+		} else {
+			echo '<strike>'._('User Profile').'</strike>';
+		}
+		echo '</td>';
+		echo '<td width="15%" style="text-align:center">';
+		if ($u->getStatus() != 'A') {
+			echo util_make_link('/admin/userlist.php?action=activate&amp;user_id='.$u->getID().$filter,_('Activate'));
+		} else {
+			echo '<strike>'._('Activate').'</strike>';
+		}
+		echo '</td>';
+		echo '<td width="15%" style="text-align:center">';
+		if ($u->getStatus() != 'D') {
+			echo util_make_link('/admin/userlist.php?action=delete&amp;user_id='.$u->getID().$filter,_('Delete'));
+		} else {
+			echo '<strike>'._('Delete').'</strike>';
+		}
+		echo '</td>';
+		echo '<td width="15%" style="text-align:center">'.util_make_link('/admin/userlist.php?action=suspend&amp;user_id='.$u->getID().$filter,_('Suspend')).'</td>';
+		echo '<td width="12%" style="text-align:center">'.util_make_link('/admin/passedit.php?user_id='.$u->getID().$filter,_('Change Password')).'</td>';
 		echo '</tr>';
 		$count ++;
 	}
@@ -148,15 +167,15 @@ $HTML->header(array('title'=>_('User List')));
 //	Show list of users
 if ($usingplugin) {
 	echo "<p>"._('Users that use plugin'). " <strong>".$usingplugin."</strong></p>\n";
-	$res = db_query_params ('SELECT u.user_id FROM plugins p, user_plugin up, users u WHERE p.plugin_name = $1 and up.user_id = u.user_id and p.plugin_id = up.plugin_id', array($usingplugin));
-	show_users_list(user_get_objects(util_result_column_to_array($res,0)));
+	$res = db_query_params('SELECT u.user_id FROM plugins p, user_plugin up, users u WHERE p.plugin_name = $1 and up.user_id = u.user_id and p.plugin_id = up.plugin_id', array($usingplugin));
+	show_users_list(user_get_objects(util_result_column_to_array($res, 0)));
 
 } elseif (!$group_id) {
 	$user_name_search = getStringFromRequest('user_name_search');
 
 	if ($user_name_search) {
-		$res = db_query_params ('SELECT user_id FROM users WHERE lower(user_name) LIKE $1 OR lower(lastname) LIKE $1',
-					   array (strtolower("$user_name_search%")));
+		$res = db_query_params('SELECT user_id FROM users WHERE lower(user_name) LIKE $1 OR lower(lastname) LIKE $1',
+					array(strtolower("$user_name_search%")));
 		$list_id = util_result_column_to_array($res,0);
 		$msg = sprintf(_('User list beginning with “%s” for all projects'), $user_name_search);
 	} else {
@@ -168,30 +187,30 @@ if ($usingplugin) {
 		$res = db_query_params ('SELECT user_id FROM users WHERE status = $1',
 					   array ($status));
 		if (isset($list_id)) {
-			$list_id = array_merge($list_id, util_result_column_to_array($res,0));
+			$list_id = array_merge($list_id, util_result_column_to_array($res, 0));
 		}
 		else {
-			$list_id = util_result_column_to_array($res,0);
+			$list_id = util_result_column_to_array($res, 0);
 		}
 	}
 	if (! isset($list_id)) {
 		$res = db_query_params('SELECT user_id FROM users',
-				array ());
-		$list_id = util_result_column_to_array($res,0);
+				array());
+		$list_id = util_result_column_to_array($res, 0);
 	}
 	$filter='';
 	if (in_array($status,array('D','A','S','P'))) {
 		$filter = '&amp;status='.$status;
 	}
 	$sort_order = getStringFromRequest('sortorder', 'realname');
-	util_ensure_value_in_set ($sort_order,
-					  array('realname','user_name','lastname','firstname','user_id','status','add_date')) ;
-	show_users_list ($list_id, $filter, $sort_order);
+	util_ensure_value_in_set($sort_order,
+				array('realname','user_name','lastname','firstname','user_id','status','add_date'));
+	show_users_list($list_id, $filter, $sort_order);
 } else {
 	/*
 		Show list for one project
 	*/
-	$project = group_get_object($group_id) ;
+	$project = group_get_object($group_id);
 	print "<p>" ._('User list for project: ') . "<strong>" . $project->getPublicName() . "</strong></p>";
 	$users = $project->getUsers();
 	if ($users) {
@@ -200,9 +219,9 @@ if ($usingplugin) {
 		}
 		$filter = '&amp;group_id='.$group_id;
 		$sort_order = getStringFromRequest('sortorder', 'realname');
-		util_ensure_value_in_set ($sort_order,
-					array('realname','user_name','lastname','firstname','user_id','status','add_date')) ;
-		show_users_list ($users_id, $filter, $sort_order);
+		util_ensure_value_in_set($sort_order,
+					array('realname','user_name','lastname','firstname','user_id','status','add_date'));
+		show_users_list($users_id, $filter, $sort_order);
 	}
 	else {
 		echo '<p>'._('No user in this project').'</p>';
