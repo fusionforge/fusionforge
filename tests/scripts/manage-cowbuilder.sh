50e9bc7c631bb041385cefc8c915c6c3a7588030
Roland Mas <lolando@debian.org>
2013-12-13 15:52:20 +0100

    Update cowbuilder options

--- tests/scripts/manage-cowbuilder.sh
+++ tests/scripts/manage-cowbuilder.sh
@@ -8,6 +8,7 @@ BUILDERDIR=$(./scripts/builder_get_config.sh BUILDERDIR)
 
 # Prepare and/or update cowbuilder caches
 DISTROLIST=${DISTROLIST:-"squeeze wheezy jessie"}
+DEBMIRROR=${DEBMIRROR:-"http://cdn.debian.net/debian"}
 
 [ -d $BUILDERDIR/config ] || mkdir $BUILDERDIR/config
 
