5484ef4852363176680a1c15b2cd93fad6258840
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-01-29 18:27:02 +0100

    VM: more precise dependencies for a APT::Install-Recommends=false box

--- tools/VM-scripts/install-gui.sh
+++ tools/VM-scripts/install-gui.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-apt-get install -y nodm xfce4
+apt-get install -y xorg nodm xfce4 tango-icon-theme
 sed -i -e 's/^NODM_ENABLED=.*/NODM_ENABLED=true/' -e 's/^NODM_USER=.*/NODM_USER=root/' /etc/default/nodm
 /etc/init.d/nodm restart
