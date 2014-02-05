f95a660d956f97d4408701140fdafdfc582f984c
Franck Villaume <franck.villaume@trivialdev.com>
2014-02-05 10:08:45 +0100

    plugin projects hierarchy: fix display for private projects

--- src/plugins/projects-hierarchy/view/home_project_link.php
+++ src/plugins/projects-hierarchy/view/home_project_link.php
@@ -5,6 +5,7 @@
  * Copyright 2006 (c) Fabien Regnier - Sogeti
  * Copyright 2010-2011, Franck Villaume - Capgemini
  * Copyright 2013, French Ministry of Education
+ * Copyright 2014, Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -29,6 +30,22 @@ $projectsHierarchy = plugin_get_object('projects-hierarchy');
 
 $parent = $projectsHierarchy->getFamily($group_id, 'parent', false, 'validated');
 $childs = $projectsHierarchy->getFamily($group_id, 'child', false, 'validated');
+
+if (sizeof($parent)) {
+	$parentGroup = group_get_object($parent[0]);
+	if (!forge_check_perm('project_read', $parentGroup->getID())) {
+		array_pop($parent);
+	}
+}
+if (sizeof($childs)) {
+	for ($i = 0; $i < count($childs); $i++) {
+		$childGroup = group_get_object($childs[$i]);
+		if (!forge_check_perm('project_read', $childGroup->getID())) {
+				unset($childs[$i]);
+		}
+	}
+	$childs = array_values($childs);
+}
 if (sizeof($parent) || sizeof($childs)) {
 	echo $HTML->boxTop(_('Linked projects'));
 	if (sizeof($parent)) {
