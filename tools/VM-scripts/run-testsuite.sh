14b41ca00a59f52a1729a4df67668cdc994f57fe
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-02-06 16:10:58 +0100

    VM: use new testsuite infra

--- tools/VM-scripts/run-testsuite.sh
+++ tools/VM-scripts/run-testsuite.sh
@@ -1,22 +1,27 @@
-#! /bin/sh
+#!/bin/bash
+
+# Selenium dependencies
+aptitude -y install default-jre iceweasel
+
+# Build selenium
+aptitude -y install cowbuilder
+mkdir -p ~/builder/cow/
+DISTROLIST=wheezy /root/fusionforge/tests/scripts/manage-cowbuilder.sh
+
+mkdir -p ~/build/
+aptitude -y install reprepro
+echo -e "Codename: wheezy\nArchitectures: amd64 source\nComponents: main" > ~/build/debian/conf/distributions
+cd /root/fusionforge/3rd-party/selenium
+make
+
 
 # Test dependencies
 aptitude -y install php5-cli phpunit phpunit-selenium
 
-# If available, install the JUnit OSLC provider test suite
-if [ -d src/plugins/oslc/tests ]; then
-    cd /root/fusionforge/src/plugins/oslc/tests
-    ./setup-provider-test.sh
-fi
+## If available, install the JUnit OSLC provider test suite
+#if [ -d src/plugins/oslc/tests ]; then
+#    cd /root/fusionforge/src/plugins/oslc/tests
+#    ./setup-provider-test.sh
+#fi
 
-export CONFIG_PHP=/root/fusionforge/tests/func/config.php.ffsandbox
-
-# Run the phpunit + Selenium functional tests
-cd /root/fusionforge/tests
-if [ "$*" = "" ] ; then
-    phpunit --verbose SeleniumTests.php
-else
-    for i in $* ; do
-	phpunit --verbose $i
-    done
-fi
+/root/fusionforge/tests/scripts/phpunit.sh DEBDebian70Tests.php
