ac45b58c1337f1df1d86c8c7a46b59a068b8f752
Roland Mas <lolando@debian.org>
2014-02-14 10:51:02 +0100

    Imported Debian packaging from Bazaar branch

---
+++ src/utils/make-debian-orig.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+# Creates the .orig tarball corresponding to the upstream archive,
+# base of the Debian packaging.
+
+if [ -e src/debian/changelog ] ; then        # We're in the parent dir
+    cd src
+elif [ -e debian/changelog ] ; then             # probably in src/ (or a renamed src/)
+    cd . # do nothing, but shell syntax requires an instruction in a then-block
+elif [ -e ../src/debian/changelog ] ; then   # in tools/ or tests/ or something
+    cd ../src
+elif [ -e ../debian/changelog ] ; then       # In a subdir of src/
+    cd ..
+else
+    echo "Couldn't find changelog..."
+    exit 1
+fi
+
+# package version including revision
+f=$(dpkg-parsechangelog | awk '/^Version:/ { print $2 }')
+# upstream version
+u=${f%-*}
+
+if [ -e ../fusionforge_$u.orig.tar.gz ] ; then
+    echo "../fusionforge_$u.orig.tar.gz already exists"
+    exit 1
+fi
+
+bzr export --root=fusionforge-$u --format=tar - . \
+    | tar -f - --delete fusionforge-$u/debian \
+    | gzip -c \
+    > ../fusionforge_$u.orig.tar.gz
