298a6fbb15f1b91986450c731a215455d8b6d953
Julien HEYMAN <julien.heyman@atos.net>
2014-02-27 18:49:24 +0100

    Revert "Merge branch 'Branch_5_2' into Branch_5_3"
    This reverts commit d30bf60dc906f620da690b21051fd325093cbf4e, reversing
    changes made to 247d404a78c4ce704cc543dbf181e352920c61e3.

--- src/www/themes/funky/Theme.class.php
+++ src/www/themes/funky/Theme.class.php
@@ -430,13 +430,7 @@ class Theme extends Layout {
 	 */
 	function headerJS()
 	{
-		$this->addjavascript('/js/common.js');
-		$this->addjavascript('/scripts/codendi/LayoutManager.js');
-		$this->addjavascript('/scripts/codendi/ReorderColumns.js');
-		$this->addjavascript('/scripts/codendi/codendi-1236793993.js');
-		$this->addjavascript('/scripts/codendi/validate.js');
-		$this->addjavascript('/scripts/codendi/Tooltip.js');
-
+		echo '<script type="text/javascript" src="'. util_make_uri('/js/common.js') .'"></script>';
 
 		plugin_hook("javascript_file", false);
 
