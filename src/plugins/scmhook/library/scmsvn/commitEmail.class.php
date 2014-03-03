83d0ef3a7b92c2548cddd243663c2d120c6a571a
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-03-03 11:17:25 +0100

    Improve indentation/readability

--- src/plugins/scmhook/library/scmsvn/commitEmail.class.php
+++ src/plugins/scmhook/library/scmsvn/commitEmail.class.php
@@ -37,8 +37,8 @@ class commitEmail extends scmhook {
 		$this->label = "scmsvn";
 		$this->unixname = "commitemail";
 		$this->needcopy = 0;
-		$this->command = '/usr/bin/php -d include_path='.ini_get('include_path').' '.forge_get_config('plugins_path').'/scmhook/library/'.
-		$this->label.'/hooks/'.$this->unixname.'/commit-email.php "$1" "$2" '.$this->group->getUnixName().'-commits@'.forge_get_config('lists_host');
+		$this->command = '/usr/bin/php -d include_path='.ini_get('include_path').' '.forge_get_config('plugins_path').'/scmhook/library/'
+			.$this->label.'/hooks/'.$this->unixname.'/commit-email.php "$1" "$2" '.$this->group->getUnixName().'-commits@'.forge_get_config('lists_host');
 	}
 
 	function isAvailable() {
