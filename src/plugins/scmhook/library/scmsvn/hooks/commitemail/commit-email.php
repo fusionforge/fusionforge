484fea262520c904ce3f92b929c378fb8f43364c
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-03-12 15:31:05 +0100

    scmhook: let commit-email.pl set the sender from the SVN username; forward return code to SVN so it displays errors if any

--- src/plugins/scmhook/library/scmsvn/hooks/commitemail/commit-email.php
+++ src/plugins/scmhook/library/scmsvn/hooks/commitemail/commit-email.php
@@ -36,15 +36,8 @@ if(!file_exists($svnlook) || !file_exists($commit_email_pl)) { die("Missing requ
 # Find who made the changes
 if($argc < 4) { die("Invalid arguments."); }
 
-$author = exec("$svnlook author \"$argv[1]\" -r $argv[2]");
-$res_db= db_query_params('SELECT email FROM users where user_name = lower($1) LIMIT 1',
-			array($author));
-if($res_db) {
-	$e = db_fetch_array($res_db);
-	if($e) {
-		passthru("$commit_email_pl --from $e[email] \"$argv[1]\" $argv[2] $argv[3]");
-		exit;
-	}
-}
-passthru("$commit_email_pl \"$argv[1]\" $argv[2] $argv[3]");
-?>
+$users_host = forge_get_config('users_host');
+if (!empty($users_host))
+  $hostname = "-h $users_host";
+passthru("$commit_email_pl \"$argv[1]\" $argv[2] $hostname $argv[3]", $ret);
+exit($ret);  // warn SVN if something went wrong
