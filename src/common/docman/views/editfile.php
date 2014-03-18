a5d17ae0059cb267a6a6667e1a7c15ae825f9ec3
Franck Villaume <franck.villaume@trivialdev.com>
2014-03-18 15:08:29 +0100

    docman: editfile, move to html_{ao,ac,ap,e}, more css, listTableTop, listTableBottom, multiTabRow

--- src/common/docman/views/editfile.php
+++ src/common/docman/views/editfile.php
@@ -2,7 +2,7 @@
 /**
  * FusionForge Documentation Manager
  *
- * Copyright 2012-2013, Franck Villaume - TrivialDev
+ * Copyright 2012-2014, Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -25,63 +25,58 @@
 /* global variables used */
 global $g; //group object
 global $group_id; // id of the group
+global $HTML;
 
 if (!forge_check_perm('docman', $group_id, 'approve')) {
 	$return_msg= _('Document Manager Access Denied');
 	session_redirect('/docman/?group_id='.$group_id.'&warning_msg='.urlencode($return_msg));
 }
 
-echo '<div id="editFile" >';
-echo '<form id="editdocdata" name="editdocdata" method="post" enctype="multipart/form-data">';
-echo '<table>';
-echo '	<tr>';
-echo '		<td><strong>'. _('Document Title').utils_requiredField()._(':').'</strong><br />';
-echo '		<input pattern=".{5,}" title="'.sprintf(_('(at least %s characters)'), 5).'" id="title" type="text" name="title" size="40" maxlength="255"/></td>';
-echo '	</tr>';
-echo '	<tr>';
-echo '		<td><strong>'. _('Description').utils_requiredField()._(':').'</strong><br />';
-echo '		<input pattern=".{10,}" title="'.sprintf(_('(at least %s characters)'), 10).'" id="description" type="text" name="description" size="40" maxlength="255"/></td>';
-echo '	</tr>';
+echo html_ao('div', array('id' => 'editFile'));
+echo html_ao('form', array('id' => 'editdocdata', 'name' => 'editdocdata', 'method' => 'post', 'enctype' => 'multipart/form-data'));
+echo $HTML->listTableTop(array());
+$cells = array();
+$cells[] = array(_('Document Title').utils_requiredField()._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('input', array('pattern' => '.{5,}', 'title' => sprintf(_('(at least %s characters)'), 5), 'id' => 'title', 'type' => 'text', 'name' => 'title', 'size' => '40', 'maxlength' => '255'));
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('Description').utils_requiredField()._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('input', array('pattern' => '.{10,}', 'title' => sprintf(_('(at least %s characters)'), 10), 'id' => 'description', 'type' => 'text', 'name' => 'description', 'size' => '40', 'maxlength' => '255'));
+echo $HTML->multiTableRow(array(), $cells);
 if ($g->useDocmanSearch()) {
-	echo '	<tr>';
-	echo '		<td>'. _('Both fields are used by the document search engine.') .'</td>';
-	echo '	</tr>';
+	$cells = array();
+	$cells[] =  array(_('Both fields are used by the document search engine.'), 'colspan' => 2);
+	echo $HTML->multiTableRow(array(), $cells);
 }
-echo '	<tr>';
-echo '		<td><strong>'. _('File')._(': ').'</strong>';
-echo '			<a id="filelink"></a>';
-echo '		</td>';
-echo '	</tr>';
+$cells = array();
+$cells[] = array(_('File')._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('a', array('id' => 'filelink'), '', false);
+echo $HTML->multiTableRow(array(), $cells);
 if ($g->useCreateOnline()) {
-	echo '	<tr id="editonlineroweditfile" >';
-	echo '		<td>'. _('Edit the contents to your desire or leave them as they are to remain unmodified.') .'<br />';
-	echo '			<textarea id="defaulteditzone" name="details" rows="15" cols="70"></textarea><br />';
-	echo '			<input id="defaulteditfiletype" type="hidden" name="filetype" value="text/plain" />';
-	echo '			<input id="editor" type="hidden" name="editor" value="online" />';
-	echo '		</td>';
-	echo '	</tr>';
+	$cells = array();
+	$cells[] = array(_('Edit the contents to your desire or leave them as they are to remain unmodified.').html_e('br').
+			html_e('textarea', array('id' => 'defaulteditzone', 'name' => 'details', 'rows' => '15', 'cols' => '70'), '', false).
+			html_e('input', array('id' => 'defaulteditfiletype', 'type' => 'hidden', 'name' => 'filetype', 'value' => 'text/plain')).
+			html_e('input', array('id' => 'editor', 'type' => 'hidden', 'name' => 'editor', 'value' => 'online')),
+			'colspan' => 2);
+	echo $HTML->multiTableRow(array('id' => 'editonlineroweditfile'), $cells);
 }
-echo '	<tr>';
-echo '		<td><strong>'. _('Folder that document belongs to:') .'</strong><br />';
-echo '			<select name="doc_group" id="doc_group"></select>';
-echo '		</td>';
-echo '	</tr>';
-echo '	<tr>';
-echo '		<td><strong>'. _('State')._(': ').'</strong><br />';
-echo '			<select name="stateid" id="stateid"></select>';
-echo '		</td>';
-echo '	</tr>';
-echo '	<tr id="fileurlroweditfile">';
-echo '		<td><strong>'. _('Specify an new outside URL where the file will be referenced:') .'</strong>'. utils_requiredField() .'<br />';
-echo '			<input id="fileurl" type="url" name="file_url" size="50" pattern="ftp://.+|https?://.+" />';
-echo '		</td>';
-echo '	</tr>';
-echo '	<tr id="uploadnewroweditfile">';
-echo '		<td><strong>'. _('OPTIONAL: Upload new file:') .'</strong><br />';
-echo '			<input type="file" name="uploaded_data" /><br />'.sprintf(_('(max upload size: %s)'),human_readable_bytes(util_get_maxuploadfilesize()));
-echo '		</td>';
-echo '	</tr>';
-echo '</table>';
-echo '<input type="hidden" id="docid" name="docid" />';
-echo '</form>';
-echo '</div>';
+$cells = array();
+$cells[] = array(_('Folder that document belongs to')._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('select', array('name' => 'doc_group', 'id' => 'doc_group'), '', false);
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('State')._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('select', array('name' => 'stateid', 'id' => 'stateid'), '', false);
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('Specify an new outside URL where the file will be referenced').utils_requiredField()._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('input', array('id' => 'fileurl', 'type' => 'url', 'name' => 'file_url', 'size' => '50', 'pattern' => 'ftp://.+|https?://.+'));
+echo $HTML->multiTableRow(array('id' => 'fileurlroweditfile'), $cells);
+$cells = array();
+$cells[] = array(_('OPTIONAL Upload new file')._(':'), 'class' => 'docman_editfile_title');
+$cells[][] = html_e('input', array('type' => 'file', 'name' => 'uploaded_data')).html_e('br').sprintf(_('(max upload size: %s)'),human_readable_bytes(util_get_maxuploadfilesize()));
+echo $HTML->multiTableRow(array('id' => 'uploadnewroweditfile'), $cells);
+echo $HTML->listTableBottom();
+echo html_e('input', array('type' => 'hidden', 'id' => 'docid', 'name' => 'docid'));
+echo html_ac(html_ap() -2);
