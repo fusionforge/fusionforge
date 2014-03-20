1451688a6948005cc53d11899cf152aa3a3fad48
Stéphane-Eymeric Bredthauer <sebredthauer@gmail.com>
2014-03-20 23:00:07 +0100

    My page/My Trackers Dashboard: use helpers
    Signed-off-by: Franck Villaume <franck.villaume@trivialdev.com>

--- src/www/my/dashboard.php
+++ src/www/my/dashboard.php
@@ -9,6 +9,7 @@
  * Copyright (C) 2012 Alain Peyrat - Alcatel-Lucent
  * Copyright (C) 2012-2013 Marc-Etienne Vargenau - Alcatel-Lucent
  * Copyright 2013, Franck Villaume - TrivialDev
+ * Copyright 2014, Stéphane-Eymeric Bredthauer
  *
  * This file is part of FusionForge. FusionForge is free software;
  * you can redistribute it and/or modify it under the terms of the
@@ -34,23 +35,22 @@ require_once $gfcommon.'tracker/ArtifactsForUser.class.php';
 require_once $gfcommon.'forum/ForumsForUser.class.php';
 require_once $gfcommon.'pm/ProjectTasksForUser.class.php';
 
+global $HTML; // Layout object
+
 if (!session_loggedin()) {
 	exit_not_logged_in();
 } else {
 	$u = session_get_user();
 	site_user_header(array('title'=>sprintf(_('Personal Page for %s'), $u->getRealName())));
-	?>
-
-	<table class="fullwidth">
-	<tr><td class="top">
-	<?php
-
+	echo html_ao('table', array('class' => 'fullwidth'));
+	echo html_ao('tr');
+	echo html_ao('td', array('class' => 'top'));
 	echo $HTML->boxTop(_('All trackers for my projects'), false, false);
 	// Include both groups and foundries; developers should be similarly
 	// aware of membership in either.
 	$projects = session_get_user()->getGroups();
 	if (count ($projects) < 1) {
-		echo '<p class="information">'._("You're not a member of any active projects").'</p>';
+		echo $HTML->information(_("You're not a member of any active projects"));
 	} else {
 		$display_col=array('summary'=>1,
 				'changed'=>1,
@@ -85,7 +85,7 @@ if (!session_loggedin()) {
 			$title_arr[]=_('Tasks');
 		}
 
-		echo $GLOBALS['HTML']->listTableTop($title_arr);
+		echo $HTML->listTableTop($title_arr);
 
 		foreach ($projects as $p) {
 			$admin_flags = forge_check_perm('project_admin', $p->getID()) ;
@@ -93,15 +93,15 @@ if (!session_loggedin()) {
 			//  get the Project object
 			//
 			if (!$p->usesTracker()) {
-				echo '
-				<tr>
-				<td colspan="' . (array_sum($display_col)+1) . '" align="left" style="background-color: #CADACA; padding-top: 4px; border-top: 1px dotted darkgreen; border-bottom: 1px solid darkgreen; font-size: larger; color: darkgreen;"><strong> • ' .
-						util_make_link('/tracker/?group_id='.$p->getID(),
-								$p->getPublicName())
-						. '</strong></td></tr>';
-				echo '
-				<tr>
-				<td colspan="' . (array_sum($display_col)+1) . '" align="left"><strong>'.('This project is not using the tracker feature.').'</strong></td></tr>';
+				$cell_text = html_e('strong', array(), ' • '. util_make_link('/tracker/?group_id='.$p->getID(), $p->getPublicName()));
+				$cell_attrs = array('colspan' => (array_sum($display_col)+1), 'align' => 'left', 'style' => 'background-color: #CADACA; padding-top: 4px; border-top: 1px dotted darkgreen; border-bottom: 1px solid darkgreen; font-size: larger; color: darkgreen;');
+				$cell_data = array(array_merge((array)$cell_text, $cell_attrs));
+				echo $HTML->multiTableRow(array(), $cell_data);
+				$cell_text =  html_e('strong', array(), _('This project is not using the tracker feature.'));
+				$cell_attrs = array('colspan' => (array_sum($display_col)+1), 'align' => 'left');
+				$cell_data = array(array_merge((array)$cell_text, $cell_attrs));
+				echo $HTML->multiTableRow(array(), $cell_data);
+				
 			} else {
 				$atf = new ArtifactTypeFactory($p);
 				$at_arr = $atf->getArtifactTypes();
@@ -109,12 +109,10 @@ if (!session_loggedin()) {
 				$art_found = 0;
 
 				if(count($at_arr) > 0) {
-					echo '
-					<tr>
-					<td colspan="' . (array_sum($display_col)+1) . '" align="left" style="background-color: #CADACA; padding-top: 4px; border-top: 1px dotted darkgreen; border-bottom: 1px solid darkgreen; font-size: larger; color: darkgreen;"><strong> • ' .
-						util_make_link('/tracker/?group_id='.$p->getID(),
-								$p->getPublicName())
-						. '</strong></td></tr>';
+					$cell_text = html_e('strong', array(), ' • '.util_make_link('/tracker/?group_id='.$p->getID(), $p->getPublicName()));
+					$cell_attrs = array('colspan' => (array_sum($display_col)+1), 'align' => 'left', 'style' => 'background-color: #CADACA; padding-top: 4px; border-top: 1px dotted darkgreen; border-bottom: 1px solid darkgreen; font-size: larger; color: darkgreen;');
+					$cell_data = array(array_merge((array)$cell_text, $cell_attrs));
+					echo $HTML->multiTableRow(array(), $cell_data);
 					foreach($at_arr as $at) {
 						$art_found = 1;
 						//
@@ -127,34 +125,57 @@ if (!session_loggedin()) {
 						// get artifacts from object
 						$art_arr = $af->getArtifacts();
 						if (count($art_arr) > 0) {
-							echo '<tr><td colspan="' . (array_sum($display_col)+1) . '" align="left" style="color: darkred; border-bottom: 1px solid #A0A0C0; border-top: 1px dotted #A0A0C0; background-color: #CACADA;"><strong> · '.
-								util_make_link ('/tracker/?group_id='.$at->Group->getID().'&atid='.$at->getID(),
-										$at->getName()) . '</strong></td></tr>';
+							$cell_text = html_e('strong', array(), ' · '. util_make_link ('/tracker/?group_id='.$at->Group->getID().'&atid='.$at->getID(), $at->getName()));
+							$cell_attrs = array('colspan' => (array_sum($display_col)+1), 'align' => 'left', 'style' => 'color: darkred; border-bottom: 1px solid #A0A0C0; border-top: 1px dotted #A0A0C0; background-color: #CACADA;');
+							$cell_data = array(array_merge((array)$cell_text, $cell_attrs));
+							echo $HTML->multiTableRow(array(), $cell_data);
 							$toggle=0;
+							
 							foreach($art_arr as $art) {
-								echo '<tr '. $HTML->boxGetAltRowStyle($toggle++) . '><td class="align-center">'. $art->getID() .'</td>';
-								if ($display_col['summary'])
-								echo '<td class="align-left">'.util_make_link('/tracker/?func=detail&aid='.$art->getID().'&group_id='.$p->getID().'&atid='.$ath->getID(), $art->getSummary()).'</td>';
-								if ($display_col['changed'])
-									echo '<td width="150">'
-										.date(_('Y-m-d'),$art->getLastModifiedDate()) .'</td>';
-								if ($display_col['status'])
-									echo '<td>'. $art->getStatusName() .'</td>';
-								if ($display_col['priority'])
-									echo '<td class="priority'.$art->getPriority() .'" align="center">'. $art->getPriority() .'</td>';
-								if ($display_col['assigned_to'])
-									echo '<td>'. $art->getAssignedRealName() .'</td>';
-								if ($display_col['submitted_by'])
-									echo '<td>'. $art->getSubmittedRealName() .'</td>';
+								$cell_data = array();
+								$row_attrs = array('class' => $HTML->boxGetAltRowStyle($toggle++, true));
+								$cell_attrs = array('class' => 'align-center');
+								$cell_text = $art->getID();
+								$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								if ($display_col['summary']) {
+									$cell_attrs = array('class' => 'align-left');
+									$cell_text = util_make_link('/tracker/?func=detail&aid='.$art->getID().'&group_id='.$p->getID().'&atid='.$ath->getID(), $art->getSummary());
+									$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								}
+								if ($display_col['changed']) {
+									$cell_attrs = array('width' => '150');
+									$cell_text = date(_('Y-m-d'),$art->getLastModifiedDate());
+									$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								}
+								if ($display_col['status']) {
+									$cell_attrs = array();
+									$cell_text = $art->getStatusName();
+									$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								}
+								if ($display_col['priority']) {
+									$cell_attrs = array('class' => 'priority'.$art->getPriority(), 'align' => 'center');
+									$cell_text = $art->getPriority();
+									$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								}
+								if ($display_col['assigned_to']) {
+									$cell_attrs = array();
+									$cell_text = $art->getAssignedRealName();
+									$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								}
+								if ($display_col['submitted_by']) {
+									$cell_attrs = array();
+									$cell_text = $art->getSubmittedRealName();
+									$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
+								}
 								if ($display_col['related_tasks']) {
 									$result_tasks = $art->getRelatedTasks();
 									if($result_tasks) {
+										$cell_text ='';
 										$taskcount = db_numrows($art->relatedtasks);
-										echo '<td>';
 										if ($taskcount > 0) {
 											for ($itask = 0; $itask < $taskcount; $itask++) {
 												if($itask>0)
-													echo '<br/>';
+													$cell_text .= html_e('br');
 
 												$taskinfo = db_fetch_array($art->relatedtasks, $itask);
 												$taskid = $taskinfo['project_task_id'];
@@ -162,42 +183,39 @@ if (!session_loggedin()) {
 												$groupid   = $taskinfo['group_id'];
 												$g = group_get_object($groupid);
 												$pg = new ProjectGroup($g, $projectid);
-												echo $pg->getName().'<br/>';
+												$cell_text .= $pg->getName().html_e('br');
 												$summary   = util_unconvert_htmlspecialchars($taskinfo['summary']);
-												echo util_make_link('/pm/task.php?func=detailtask&project_task_id='.$taskid.'&group_id='.$groupid.'&group_project_id='.$projectid, $summary);
+												$cell_text .= util_make_link('/pm/task.php?func=detailtask&project_task_id='.$taskid.'&group_id='.$groupid.'&group_project_id='.$projectid, $summary);
 											}
 										}
-										echo '</td>';
+										$cell_data [] = array_merge((array)$cell_text, $cell_attrs);
 									}
 								}
-								echo '</tr>';
+								echo $HTML->multiTableRow($row_attrs, $cell_data);
 							}
 						}
 					}
 					if (!$art_found) {
-						echo '
-						<tr>
-						<td colspan="' . (array_sum($display_col)+1) . '" align="left"><strong> --</strong></td></tr>';
+						$cell_text =  html_e('strong', array(), ' --');
+						$cell_attrs = array('colspan' => (array_sum($display_col)+1), 'align' => 'left');
+						$cell_data = array(array_merge((array)$cell_text, $cell_attrs));
+						echo $HTML->multiTableRow(array(), $cell_data);
 					}
 				}
 			}
 		}
-                echo $GLOBALS['HTML']->listTableBottom();
+		echo $HTML->listTableBottom();
 	}
 	echo $HTML->boxBottom();
-
-//second column of "my" page
-
-	echo '</td><td class="top"></td></tr>
-
-	<!--  Bottom Row   -->
-<!--
-	<tr><td colspan="2">';
+	echo html_ac(html_ap()-2);
+	
+// priority colors
+/*
+	echo html_ao('tr');
+	echo html_ao('td', array('colspan' => '2'));
 	show_priority_colors_key();
-	echo '
-	</td></tr>
--->
-	</table>';
-
+	echo html_ac(html_ap()-2);
+*/
+	echo html_ac(html_ap()-1);
 	site_user_footer();
 }
