66ef369c72beaf16a93999ef7786e57482e74696
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-03-25 18:47:49 +0100

    Fix project names

--- src/db/20140325-drop-scmcvs-from-default-groups.sql
+++ src/db/20140325-drop-scmcvs-from-default-groups.sql
@@ -3,4 +3,4 @@
 -- - scmcvs confusingly overrides scmsvn in the builtin viewvc browsing
 DELETE FROM group_plugin
   WHERE plugin_id=(SELECT plugin_id FROM plugins WHERE plugin_name='scmcvs')
-    AND group_id IN (SELECT group_id FROM groups WHERE unix_group_name IN ('siteadmin','news','stats','peers'));
+    AND group_id IN (SELECT group_id FROM groups WHERE unix_group_name IN ('siteadmin','newsadmin','stats','peerrating'));
