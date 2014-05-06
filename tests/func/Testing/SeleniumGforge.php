5408e017f51cd534929e9b3eee7da2e19e01e052
Roland Mas <lolando@debian.org>
2014-05-06 10:11:30 +0200

    Backport from master: uploadSshKey method

--- tests/func/Testing/SeleniumGforge.php
+++ tests/func/Testing/SeleniumGforge.php
@@ -420,6 +420,17 @@ class FForge_SeleniumTestCase extends PHPUnit_Extensions_SeleniumTestCase
 		$this->waitForPageToLoad("30000");
 		$this->assertTrue($this->isTextPresent("This is the public description for $project."));
 	}
+
+	protected function uploadSshKey () {
+		$keys = file(getenv('HOME').'/.ssh/id_rsa.pub');
+		$k = $keys[0];
+		$this->assertEquals(count($keys), 1);
+
+		$this->clickAndWait("link=My Account");
+		$this->clickAndWait("link=Edit Keys");
+		$this->type("authorized_key", $k);
+		$this->clickAndWait("submit");
+	}
 }
 
 // Local Variables:
