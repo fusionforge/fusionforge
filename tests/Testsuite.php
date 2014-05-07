65ddeaa02b5b83de925126876fd7119b0d379640
Roland Mas <lolando@debian.org>
2014-05-07 14:01:06 +0200

    Removed obsolete files

--- tests/Testsuite.php
+++ tests/Testsuite.php
@@ -1,9 +1,22 @@
 <?php
+if (!defined('PHPUnit_MAIN_METHOD')) {
+	define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+}
 
-require_once 'TestsuiteCommon.php';
+if (!@include_once 'PHPUnit/Autoload.php') {
+	include_once 'PHPUnit/Framework.php';
+	require_once 'PHPUnit/TextUI/TestRunner.php';
+}
 
-class Testsuite extends TestsuiteCommon
+require_once 'func/Testing/SeleniumRemoteSuite.php';
+
+class Testsuite
 {
+	public static function main()
+	{
+		PHPUnit_TextUI_TestRunner::run(self::suite());
+	}
+
 	public static function suite()
 	{
 		$suite = new SeleniumRemoteSuite('PHPUnit');
