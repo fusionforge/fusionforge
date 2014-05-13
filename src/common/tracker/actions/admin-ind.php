40d8059db51d89d56b49a8c2f944767c2992c38c
Marc-Etienne VARGENAU <Marc-Etienne.Vargenau@alcatel-lucent.com>
2014-05-13 08:05:36 +0000

    Less strings

--- src/common/tracker/actions/admin-ind.php
+++ src/common/tracker/actions/admin-ind.php
@@ -127,7 +127,7 @@ if (!isset($at_arr) || !$at_arr || count($at_arr) < 1) {
 //
 
 if (forge_check_perm ('tracker_admin', $group->getID())) { ?>
-	<h3><?php echo _('Create a new tracker') ?></h3>
+	<h3><?php echo _('Create a new tracker.') ?></h3>
 	<p><?php echo _('You can use this system to track virtually any kind of data, with each tracker having separate user, group, category, and permission lists. You can also easily move items between trackers when needed.') ?></p>
 	<p><?php echo _('Trackers are referred to as “Artifact Types” and individual pieces of data are “Artifacts”. “Bugs” might be an Artifact Type, whiles a bug report would be an Artifact. You can create as many Artifact Types as you want, but remember you need to set up categories, groups, and permission for each type, which can get time-consuming.') ?></p>
 
