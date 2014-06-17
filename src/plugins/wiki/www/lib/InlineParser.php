3ffa818fcac260826db4fb249cd5461c0a3cbae0
Marc-Etienne Vargenau <Marc-Etienne.Vargenau@alcatel-lucent.com>
2014-06-17 15:13:09 +0200

    'acronym' is deprecated in HTML 5, replace by 'abbr'

--- src/plugins/wiki/www/lib/InlineParser.php
+++ src/plugins/wiki/www/lib/InlineParser.php
@@ -464,8 +464,7 @@ function LinkBracketLink($bracketlink)
     if ($hash) {
         // It's an anchor, not a link...
         $id = MangleXmlIdentifier($link);
-        return HTML::a(array('name' => $id, 'id' => $id),
-            $bar ? $label : $link);
+        return HTML::a(array('id' => $id), $bar ? $label : $link);
     }
 
     if (preg_match("#^(" . ALLOWED_PROTOCOLS . "):#", $link)) {
@@ -912,10 +911,8 @@ class Markup_html_abbr extends BalancedMarkup
 
     function markup($match, $body)
     {
-        if (substr($match, 1, 4) == 'abbr')
-            $tag = 'abbr';
-        else
-            $tag = 'acronym';
+        // 'acronym' is deprecated in HTML 5, replace by 'abbr'
+        $tag = 'abbr';
         $rest = substr($match, 1 + strlen($tag), -1);
         $attrs = parse_attributes($rest);
         // Remove attributes other than title and lang
