511344b5b261f87e99e1db939ef207ae91c4071d
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-08-22 11:45:06 +0200

    rpm: instructions to easily rebuild libnss-pgsql and moin from Fedora

--- tests/scripts/rpm/install.sh
+++ tests/scripts/rpm/install.sh
@@ -20,6 +20,36 @@
 
 # moin: no available package for RHEL; though 'moin' is available in Fedora
 # libnss-pgsql: id., plus http://yum.postgresql.org/8.4/redhat/rhel-5-x86_64/
+cat <<'EOF' > /etc/yum.repos.d/fedora-source.repo
+[fedora]
+name=Fedora 20
+failovermethod=priority
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-20&arch=$basearch
+enabled=0
+gpgcheck=0
+[fedora-source]
+name=Fedora 20 - Source
+failovermethod=priority
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-source-20&arch=$basearch
+enabled=0
+gpgcheck=0
+EOF
+yum install -y yum-utils  # yumdownloader
+
+yumdownloader --enablerepo=fedora --source libnss-pgsql
+DEPS="python-devel"
+yum install -y $DEPS
+rpmbuild --rebuild moin-*.src.rpm
+yum remove -y $DEPS
+rpm -ivh ~/rpmbuild/RPMS/noarch/moin-1.9.7-2.el6.noarch.rpm
+
+yumdownloader --enablerepo=fedora --source libnss-pgsql
+DEPS="gcc postgresql-devel xmlto"
+yum install -y $DEPS
+rpmbuild --rebuild libnss-pgsql-*.src.rpm
+yum remove -y $DEPS
+rpm -ivh ~/rpmbuild/RPMS/x86_64/libnss-pgsql-1.5.0-0.9.beta.el6.x86_64.rpm
+
 
 # mediawiki (provided by mediawiki119): EPEL
 
