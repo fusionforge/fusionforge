b2c11cab6a246646593bf221a1f5106adc62e2fa
Matthieu Imbert <matthieu.imbert@inria.fr>
2014-08-27 14:20:48 +0200

    fix-svn-activity-log-shift-on-empty-commit-message
    Patch to fix a shift in Activity Subversion commit log messages when
    some commit log messages are empty.
    
    Signed-off-by: Matthieu Imbert <matthieu.imbert@inria.fr>
    Signed-off-by: Franck Villaume <franck.villaume@trivialdev.com>

--- src/plugins/scmsvn/common/SVNPlugin.class.php
+++ src/plugins/scmsvn/common/SVNPlugin.class.php
@@ -399,7 +399,7 @@ class SVNPlugin extends SCMPlugin {
 
 	function gatherStats($params) {
 		global $last_user, $last_time, $last_tag, $time_ok, $start_time, $end_time,
-			$adds, $deletes, $updates, $commits, $date_key,
+			$adds, $deletes, $updates, $date_key,
 			$usr_adds, $usr_deletes, $usr_updates;
 
 		$time_ok = true;
@@ -597,8 +597,8 @@ class SVNPlugin extends SCMPlugin {
 
 	function activity($params) {
 		global $last_user, $last_time, $last_tag, $time_ok, $start_time, $end_time,
-			$adds, $deletes, $updates, $commits, $date_key,
-			$usr_adds, $usr_deletes, $usr_updates, $old_commit,
+			$adds, $deletes, $updates, $date_key,
+			$usr_adds, $usr_deletes, $usr_updates,
 			$messages, $last_message, $times, $revisions, $users;
 		$group_id = $params['group'];
 		$project = group_get_object($group_id);
@@ -607,8 +607,6 @@ class SVNPlugin extends SCMPlugin {
 		}
 
 		if (in_array('scmsvn', $params['show']) || (count($params['show']) < 1)) {
-			$commits = 0;
-			$old_commit = -1;
 			$start_time = $params['begin'];
 			$end_time = $params['end'];
 			$d1 = date('Y-m-d', $start_time - 80000);
@@ -660,7 +658,7 @@ class SVNPlugin extends SCMPlugin {
 // End of class, helper functions now
 
 function SVNPluginCharData($parser, $chars) {
-	global $last_tag, $last_user, $last_time, $start_time, $end_time, $old_commit, $commits,
+	global $last_tag, $last_user, $last_time, $start_time, $end_time,
 		$time_ok, $user_list, $last_message, $messages, $times, $users;
 	switch ($last_tag) {
 		case "AUTHOR": {
@@ -680,15 +678,10 @@ function SVNPluginCharData($parser, $chars) {
 			break;
 		}
 		case "MSG": {
-			/* If commit id is the same, then concatenate the string with the previous
-+			 * (happen when the message contain accents).
-+			 */
-			if ($old_commit == $commits) {
-				$messages[count($messages)-1] .= $chars;
-			} else {
-				$messages[] = $chars;
-			}
-			$old_commit = $commits;
+                        $messages[count($messages)-1] .= $chars;
+                        /* note: there may be more than one msg
+			 * (happen when the message contain accents).
+			 */
 			break;
 		}
 	}
@@ -722,14 +715,15 @@ function SVNPluginStartElement($parser, $name, $attrs) {
 			}
 			break;
 		}
+                case "MSG": {
+                        $messages[] = "";
+			break;
+                }
 	}
 }
 
 function SVNPluginEndElement($parser, $name) {
-	global $time_ok, $last_tag, $commits;
-	if ($name == "LOGENTRY" && $time_ok) {
-		$commits++;
-	}
+	global $last_tag;
 	$last_tag = "";
 }
 
