6034e778f902e38e495460b03ae3dd596eb258f3
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-08-28 19:25:35 +0200

    rpm: snapshot support

--- tests/scripts/rpm/build.sh
+++ tests/scripts/rpm/build.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Build .rpm packages
+set -e
 
 # Install build dependencies
 yum install -y gettext tar bzip2 rpm-build  createrepo
+yum install -y php-cli  # rpm/gen_spec.sh
 
 # Build package
 cd /usr/src/fusionforge/src/
+version="$(make version)"
+snapshot="+$(date +%Y%m%d%H%M)"
+rpm/gen_spec.sh $version $snapshot
+make dist
 mkdir -p ../build/SOURCES/ ../build/SPECS/
-TAR_PREFIX=fusionforge-$(make version)
-make dist && mv $TAR_PREFIX.tar.bz2 ..
-ln -nfs ../../$TAR_PREFIX.tar.bz2 ../build/SOURCES/
-chown -h root: ../build/SOURCES/$TAR_PREFIX.tar.bz2
-chown root: ../$TAR_PREFIX.tar.bz2 # srpm
-#chown root: fusionforge.spec
-cp fusionforge.spec ../build/SPECS/ && rpmbuild ../build/SPECS/fusionforge.spec --define "_topdir $(pwd)/../build" -ba
+mv fusionforge-$(make version).tar.bz2 ../build/SOURCES/fusionforge-$version$snapshot.tar.bz2
+chown -h root: ../build/SOURCES/fusionforge-$version$snapshot.tar.bz2
+cp fusionforge.spec ../build/SPECS/
+rpmbuild ../build/SPECS/fusionforge.spec --define "_topdir $(pwd)/../build" -ba
 
 (cd ../build/RPMS/ && createrepo .)
 cat <<EOF > /etc/yum.repos.d/local.repo
