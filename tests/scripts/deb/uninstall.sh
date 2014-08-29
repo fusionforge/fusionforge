905ca5c1525f78d54c7a805c32cefdcfbfca7924
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-08-29 13:17:25 +0200

    scripts: remove testsuite db checkpoint on uninstall

--- tests/scripts/deb/uninstall.sh
+++ tests/scripts/deb/uninstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Aggressive desinstall for testing a clean re-install
 aptitude purge ~nforge ~npostgres ~nnss-pgsql ~napache2 ~nphp ~npostfix ~nexim4 && rm -rf /usr/share/gforge/ /etc/fusionforge/
+rm -rf /root/dump /var/lib/postgresql.backup/
