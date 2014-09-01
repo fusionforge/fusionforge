9b905dcefe4325e250ecdb207541f8c87eb5defe
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-09-01 17:51:59 +0200

    mediawiki: uninstall support

--- src/plugins/mediawiki/bin/install.sh
+++ src/plugins/mediawiki/bin/install.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash -e
 # MediaWiki post-install
 
-config_path=$(forge_get_config config_path)
+source_path=$(forge_get_config source_path)
 data_path=$(forge_get_config data_path)
 plugindir=$(forge_get_config source_path)/plugins/mediawiki
 
@@ -18,24 +18,24 @@ upgrade_mediawikis () {
 
 case "$1" in
     configure)
-        ln -nfs $mediawikidir/api.php              $plugindir/www/api.php
-        ln -nfs $mediawikidir/extensions           $plugindir/www/extensions
-        ln -nfs $mediawikidir/img_auth.php         $plugindir/www/img_auth.php
-        ln -nfs $mediawikidir/includes             $plugindir/www/includes
-        ln -nfs $mediawikidir/index.php            $plugindir/www/index.php
-        ln -nfs $mediawikidir/languages            $plugindir/www/languages
-        ln -nfs $mediawikidir/load.php             $plugindir/www/load.php
-        ln -nfs $mediawikidir/maintenance          $plugindir/www/maintenance
-        ln -nfs $mediawikidir/opensearch_desc.php  $plugindir/www/opensearch_desc.php
-        ln -nfs $mediawikidir/profileinfo.php      $plugindir/www/profileinfo.php
-        ln -nfs $mediawikidir/redirect.php         $plugindir/www/redirect.php
-        ln -nfs $mediawikidir/thumb.php            $plugindir/www/thumb.php
+        ln -nfs $mediawikidir/api.php              $plugindir/www/
+        ln -nfs $mediawikidir/extensions           $plugindir/www/
+        ln -nfs $mediawikidir/img_auth.php         $plugindir/www/
+        ln -nfs $mediawikidir/includes             $plugindir/www/
+        ln -nfs $mediawikidir/index.php            $plugindir/www/
+        ln -nfs $mediawikidir/languages            $plugindir/www/
+        ln -nfs $mediawikidir/load.php             $plugindir/www/
+        ln -nfs $mediawikidir/maintenance          $plugindir/www/
+        ln -nfs $mediawikidir/opensearch_desc.php  $plugindir/www/
+        ln -nfs $mediawikidir/profileinfo.php      $plugindir/www/
+        ln -nfs $mediawikidir/redirect.php         $plugindir/www/
+        ln -nfs $mediawikidir/thumb.php            $plugindir/www/
 
-	ln -nfs $mediawikidir/skins $plugindir/www/skins
-	ln -nfs $mediawikidir/skins/monobook/headbg.jpg $plugindir/www/themes/css/mw-headbg.jpg
+	ln -nfs $mediawikidir/skins $plugindir/www/
+	ln -nfs $mediawikidir/skins/monobook/headbg.jpg $source_path/www/themes/css/mw-headbg.jpg
 
 	ln -nfs $mediawikidir $data_path/plugins/mediawiki/master
-	ln -nfs $plugindir/mediawiki-skin/FusionForge.php $mediawikidir/skins/FusionForge.php
+	ln -nfs $plugindir/mediawiki-skin/FusionForge.php $mediawikidir/skins/
 	;;
     triggered)
 	case $2 in
@@ -43,6 +43,10 @@ case "$1" in
 	esac
 	;;
     remove)
+	find $plugindir/www/ -type l -print0 | xargs -r0 rm
+	rm -f $plugindir/www/themes/css/mw-headbg.jpg
+	rm -f $data_path/plugins/mediawiki/master
+	rm -f $mediawikidir/skins/FusionForge.php
 	;;
     *)
         echo "Usage: $0 {configure|triggered|remove}"
