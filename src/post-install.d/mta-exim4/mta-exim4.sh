17250d676a95663b78aa632cfa468ab01821c32d
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-09-02 18:02:53 +0200

    install: mta-exim4: rewrite without cryptic perl

--- src/post-install.d/mta-exim4/mta-exim4.sh
+++ src/post-install.d/mta-exim4/mta-exim4.sh
@@ -23,92 +23,51 @@
 set -e
 
 ####
-# Handle the three configuration setups
-
-cfg_exim4=/etc/exim4/exim4.conf
-cfg_exim4_templ=/etc/exim4/exim4.conf.template
-cfg_exim4_split_main=/etc/exim4/conf.d/main/01_exim4-config_listmacrosdefs
-cfg_exim4_split_router=/etc/exim4/conf.d/router/01_gforge_forwards
-
-cfg_exim4_main="$cfg_exim4_templ $cfg_exim4_split_main"
-cfg_exim4_router="$cfg_exim4_templ"
-
-if [ -e $cfg_exim4 ]; then
-  cfg_exim4_main="$cfg_exim4_main $cfg_exim4"
-  cfg_exim4_router="$cfg_exim4_router $cfg_exim4"
+# Handle the three configuration types (unsplit, split, manual)
+cfgs_exim4_main="/etc/exim4/exim4.conf.template /etc/exim4/conf.d/main/01_exim4-config_listmacrosdefs"
+cfgs_exim4_router="/etc/exim4/exim4.conf.template" # + /etc/exim4/conf.d/router/01_fusionforge_forwards
+if [ -e /etc/exim4/exim4.conf ]; then
+    cfgs_exim4_main="$cfgs_exim4_main /etc/exim4/exim4.conf"
+    cfgs_exim4_router="$cfgs_exim4_router /etc/exim4/exim4.conf"
 fi
 
-pattern=$(basename $0).XXXXXX
-
 case "$1" in
-  configure)
-    ####
-    # Configure aliases
+    configure)
+	users_host=$(forge_get_config users_host)
+	lists_host=$(forge_get_config lists_host)
+	pgsock='/var/run/postgresql/.s.PGSQL.5432'
+	if [ -e '/etc/redhat-release' ]; then pgsock='/tmp/.s.PGSQL.5432'; fi
+	database_name=$(forge_get_config database_name)
+	database_user=$(forge_get_config database_user)
 
-    # Redirect "noreply" mail to the bit bucket (if need be)
-    noreply_to_bitbucket=$(forge_get_config noreply_to_bitbucket)
-    if [ "$noreply_to_bitbucket" != "no" ] ; then
-      if ! grep -q "^noreply:" /etc/aliases; then
-	echo "### Next line inserted by GForge install" >> /etc/aliases
-	echo "noreply: :blackhole:" >> /etc/aliases
-      fi
-    fi
-    # Redirect "gforge" mail to the site admin
-    server_admin=$(forge_get_config admin_email)
-    if ! grep -q "^gforge:" /etc/aliases; then
-      echo "### Next line inserted by GForge install" >> /etc/aliases
-      echo "gforge: $server_admin" >> /etc/aliases
-    fi
-    [ -x /usr/bin/newaliases ] && newaliases
-    invoke-rc.d exim4 restart
+	# Redirect "noreply" mail to the bit bucket (if need be)
+	if [ "$(forge_get_config noreply_to_bitbucket)" != 'no' ] ; then
+	    if ! grep -q '^noreply:' /etc/aliases ; then
+		echo 'noreply: :blackhole:' >> /etc/aliases
+	    fi
+	fi
 
-    ####
-    # Modify exim4 configurations
+	# Main configuration: list of local domains
+	for i in $cfgs_exim4_main; do
+	    sed -i '/:FUSIONFORGE_DOMAINS/! s/^domainlist local_domains.*/&:FUSIONFORGE_DOMAINS/' $i
+	    if ! grep -q '^FUSIONFORGE_DOMAINS=' $i; then
+		sed -i '/^domainlist local_domains/ecat' $i <<EOF
+hide pgsql_servers = ($pgsock)/mail/Debian-exim/bogus:($pgsock)/$database_name/${database_user}_mta/${database_user}_mta
+FUSIONFORGE_DOMAINS=$users_host:$lists_host
+EOF
+	    fi
+	done
 
-    # First, get the list of local domains right
-
-    for m in $cfg_exim4_main; do
-      tmp1=$(mktemp /tmp/$pattern)
-
-      perl -e '
-my $source_path = `forge_get_config source_path`;
-chomp $source_path;
-
-require ("$source_path/lib/include.pl") ; # Include all the predefined functions 
-
-$seen_gf_domains = 0;
-while (($l = <>) !~ /^\s*domainlist\s*local_domains/) {
-  print $l;
-  $seen_gf_domains = 1 if ($l =~ /\s*GFORGE_DOMAINS=/);
-  $seen_pg_servers = 1 if ($l =~ m,hide pgsql_servers = .*./var/run/postgresql/.s.PGSQL.5432..*/${sys_dbuser}_mta,);
-};
-print "hide pgsql_servers = (/var/run/postgresql/.s.PGSQL.5432)/mail/Debian-exim/bogus:(/var/run/postgresql/.s.PGSQL.5432)/$sys_dbname/${sys_dbuser}_mta/${sys_dbuser}_mta\n" unless $seen_pg_servers;
-print "GFORGE_DOMAINS=$sys_users_host:$sys_lists_host\n" unless $seen_gf_domains;
-chomp $l;
-$l .= ":GFORGE_DOMAINS" unless ($l =~ /^[^#]*GFORGE_DOMAINS/);
-print "$l\n" ;
-while (<>) { print unless m/^hide pgsql_servers/; };
-' < $m > $tmp1
-
-      cat $tmp1 > $m
-      rm $tmp1
-    done
-
-    # Second, insinuate our forwarding rules in the directors section
-
-    perl -e '
-my $source_path = `forge_get_config source_path`;
-chomp $source_path;
-
-require ("$source_path/lib/include.pl") ; # Include all the predefined functions 
-
-my $gf_block = "# BEGIN GFORGE BLOCK -- DO NOT EDIT #
+	# Router configuration
+	# Stand-alone file:
+	cat <<EOF > /etc/exim4/conf.d/router/01_fusionforge_forwards
+### BEGIN FUSIONFORGE BLOCK -- DO NOT EDIT ###
 # You may move this block around to accomodate your local needs as long as you
 # keep it in the Directors Configuration section (between the second and the
 # third occurences of a line containing only the word \"end\")
 
 forward_for_gforge:
-  domains = $sys_users_host
+  domains = $users_host
   driver = redirect
   file_transport = address_file
   data = \${lookup pgsql {select email from mta_users where login=".chr(39)."\$local_part".chr(39)."}{\$value}}
@@ -116,7 +75,7 @@ forward_for_gforge:
   group = nogroup
 
 forward_for_gforge_lists:
-  domains = $sys_lists_host
+  domains = $lists_host
   driver = redirect
   pipe_transport = address_pipe
   data = \${lookup pgsql {select post_address from mta_lists where list_name=".chr(39)."\$local_part".chr(39)."}{\$value}}
@@ -124,7 +83,7 @@ forward_for_gforge_lists:
   group = nogroup
 
 forward_for_gforge_lists_owner:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -owner
   driver = redirect
   pipe_transport = address_pipe
@@ -133,7 +92,7 @@ forward_for_gforge_lists_owner:
   group = nogroup
 
 forward_for_gforge_lists_request:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -request
   driver = redirect
   pipe_transport = address_pipe
@@ -142,7 +101,7 @@ forward_for_gforge_lists_request:
   group = nogroup
 
 forward_for_gforge_lists_admin:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -admin
   driver = redirect
   pipe_transport = address_pipe
@@ -151,7 +110,7 @@ forward_for_gforge_lists_admin:
   group = nogroup
 
 forward_for_gforge_lists_bounces:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -bounces : -bounces+*
   driver = redirect
   pipe_transport = address_pipe
@@ -160,7 +119,7 @@ forward_for_gforge_lists_bounces:
   group = nogroup
 
 forward_for_gforge_lists_confirm:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -confirm : -confirm+*
   driver = redirect
   pipe_transport = address_pipe
@@ -169,7 +128,7 @@ forward_for_gforge_lists_confirm:
   group = nogroup
 
 forward_for_gforge_lists_join:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -join
   driver = redirect
   pipe_transport = address_pipe
@@ -178,7 +137,7 @@ forward_for_gforge_lists_join:
   group = nogroup
 
 forward_for_gforge_lists_leave:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -leave
   driver = redirect
   pipe_transport = address_pipe
@@ -187,7 +146,7 @@ forward_for_gforge_lists_leave:
   group = nogroup
 
 forward_for_gforge_lists_subscribe:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -subscribe
   driver = redirect
   pipe_transport = address_pipe
@@ -196,129 +155,51 @@ forward_for_gforge_lists_subscribe:
   group = nogroup
 
 forward_for_gforge_lists_unsubscribe:
-  domains = $sys_lists_host
+  domains = $lists_host
   local_part_suffix = -unsubscribe
   driver = redirect
   pipe_transport = address_pipe
   data = \${lookup pgsql {select unsubscribe_address from mta_lists where list_name=".chr(39)."\$local_part".chr(39)."}{\$value}}
   user = nobody
   group = nogroup
-# END GFORGE BLOCK #
-";
+### END FUSIONFORGE BLOCK -- DO NOT EDIT
+EOF
+	# Add the same in the unsplit big file(s)
+	for i in $cfgs_exim4_router; do
+	    if ! grep -q '^### BEGIN FUSIONFORGE BLOCK' $i; then
+		sed -i -e '/^begin routers$/ {' -e 'ecat' -e 'd }' $i <<-EOF
+		begin routers
+		### BEGIN FUSIONFORGE BLOCK -- DO NOT EDIT ###
+		### END FUSIONFORGE BLOCK ###
+		EOF
+	    fi
+	    sed -i -e '/^### BEGIN FUSIONFORGE BLOCK/,/^### END FUSIONFORGE BLOCK/ { ' \
+		-e 'ecat' -e 'd }' $i < /etc/exim4/conf.d/router/01_fusionforge_forwards
+	done
+	;;
+    
+    remove)
+	if [ "$(forge_get_config noreply_to_bitbucket)" != 'no' ] ; then
+	    sed -i -e '/^noreply:/d' /etc/aliases
+	fi
 
-print $gf_block;
-' > $cfg_exim4_split_router
+	# main conf
+	database_name=$(forge_get_config database_name)
+	for i in $cfgs_exim4_main; do
+	    sed -i -e '/^FUSIONFORGE_DOMAINS=/d' \
+		-e "/^hide pgsql_servers = .*$database_name.*/d" \
+		-e '/domainlist local_domains.*/ s/:FUSIONFORGE_DOMAINS//' $i
+	done
 
-    for r in $cfg_exim4_router; do
-      echo Processing $r
-
-      tmp1=$(mktemp /tmp/$pattern)
-
-      cp -a $r $tmp1
-
-      perl -e '
-$routerfname = shift ;
-open ROUTERS, $routerfname || die $!;
-my @gf_block = <ROUTERS>;
-close ROUTERS;
-
-while (<>) {
-  print;
-  last if /^\s*begin\s*routers\s*$/;
-};
-my $in_gf_block = 0;
-my $gf_block_done = 0;
-my @line_buf = ();
-while (<>) {
-  last if /^\s*begin\s*$/;
-  if (/^# BEGIN GFORGE BLOCK -- DO NOT EDIT #/) {
-    $in_gf_block = 1;
-    push @line_buf, @gf_block unless $gf_block_done;
-    $gf_block_done = 1;
-  };
-  push @line_buf, $_ unless $in_gf_block;
-  $in_gf_block = 0 if /^# END GFORGE BLOCK #/;
-};
-push @line_buf, $_;
-print @gf_block unless $gf_block_done;
-print @line_buf;
-while (<>) { print; };
-' $cfg_exim4_split_router < $tmp1 > $r
-
-      rm $tmp1
-    done
-
-  ;;
-
-  remove)
-    tmp1=$(mktemp /tmp/$pattern)
-
-    grep -v "^gforge:" /etc/aliases > $tmp1
-    # Redirect "noreply" mail to the bit bucket (if need be)
-    noreply_to_bitbucket=$(forge_get_config noreply_to_bitbucket)
-    if [ "$noreply_to_bitbucket" != "no" ] ; then
-      grep -v "^noreply:" $tmp1 > /etc/aliases
-    else
-      cat $tmp1 > /etc/aliases
-    fi
-
-    rm -f $tmp1
-
-    for m in $cfg_exim4_main; do
-      cfg_gforge_main=$m.gforge-new
-      tmp1=$(mktemp /tmp/$pattern)
-
-      cp -a $m $tmp1
-
-      # First, replace the list of local domains
-      perl -e '
-while (<>) {
-  last if /^\s*domainlist\s*local_domains/;
-  print unless /\s*GFORGE_DOMAINS=/;
-};
-chomp;
-/^(\s*domainlist\s*local_domains\s*=\s*)(\S+)/;
-my $l = $1 . join (":", grep(!/GFORGE_DOMAINS/, (split ":", $2)));
-print "$l\n" ;
-while (<>) { print; };
-' < $tmp1 > $cfg_gforge_main
-
-      rm $tmp1
-    done
-
-    rm -f $cfg_exim4_split_router ]
-
-    for r in $cfg_exim4_router; do
-      cfg_gforge_router=$r.gforge-new
-      tmp1=$(mktemp /tmp/$pattern)
-
-      cp -a $cfg_gforge_router $tmp1
-
-      # Second, kill our forwarding rules
-      perl -e '
-while (<>) {
-  print;
-  last if /^\s*begin\s*routers\s*$/;
-};
-my $in_gf_block = 0;
-while (<>) {
-  last if /^\s*begin\s*$/;
-  $in_gf_block = 1 if /^# BEGIN GFORGE BLOCK -- DO NOT EDIT #/;
-  print unless $in_gf_block;
-  $in_gf_block = 0 if /^# END GFORGE BLOCK #/;
-};
-print;
-while (<>) { print; };
-' < $tmp1 > $cfg_gforge_router
-
-      rm $tmp1
-    done
-  ;;
-
-  *)
-    echo "Usage: $0 {configure|remove}"
-    exit 1
-  ;;
-
+	# routers
+	for i in $cfgs_exim4_router; do
+	    sed -i -e '/^### BEGIN FUSIONFORGE BLOCK/,/^### END FUSIONFORGE BLOCK/d' $i
+	done
+	rm -f /etc/exim4/conf.d/router/01_fusionforge_forwards
+	;;
+    
+    *)
+	echo "Usage: $0 {configure|remove}"
+	exit 1
+	;;
 esac
-
