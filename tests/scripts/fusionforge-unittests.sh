a34a38f648f3c6dd59087fde5b69844aafed0c0a
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-09-03 18:53:16 +0200

    testsuite: document more test dependencies

--- tests/scripts/fusionforge-unittests.sh
+++ tests/scripts/fusionforge-unittests.sh
@@ -6,6 +6,6 @@ set -e
 get_config
 prepare_workspace
 
-# apt-get install pcregrep
+# apt-get install pcregrep moreutils xalan
 cd tests; phpunit --log-junit $WORKSPACE/reports/phpunit.xml --coverage-clover $WORKSPACE/reports/coverage/clover.xml --coverage-html $WORKSPACE/reports/coverage/ AllTests.php
 cp $WORKSPACE/reports/phpunit.xml $WORKSPACE/reports/phpunit.xml.org; xalan -in $WORKSPACE/reports/phpunit.xml.org -xsl fix_phpunit.xslt -out $WORKSPACE/reports/phpunit.xml
