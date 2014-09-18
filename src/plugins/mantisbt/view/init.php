6efca935bc3c7f8d8cb1c695846750395e3c7af2
Franck Villaume <franck.villaume@trivialdev.com>
2014-09-18 18:04:54 +0200

    plugin mantisbt: revamp init project

--- src/plugins/mantisbt/view/init.php
+++ src/plugins/mantisbt/view/init.php
@@ -3,7 +3,7 @@
  * MantisBT plugin
  *
  * Copyright 2011, Franck Villaume - Capgemini
- * Copyright 2011-2012, Franck Villaume - TrivialDev
+ * Copyright 2011-2012,2014 Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -27,8 +27,7 @@
 global $type;
 global $group_id;
 global $mantisbt;
-global $use_tooltips;
-
+global $HTML;
 ?>
 
 <script type="text/javascript">
@@ -49,36 +48,33 @@ jQuery(document).ready(function() {
 
 <?php
 
-echo '<form method="POST" Action="?type='.$type.'&group_id='.$group_id.'&pluginname='.$mantisbt->name.'&action=init" >';
-echo '<table>';
-echo '<tr><td><label id="mantisbtinit-global" ';
-if ($use_tooltips)
-	echo 'title="'._('Use the global configuration defined at forge level').'"';
-echo ' >'._('Use global configuration').'</label></td><td><input id="mantisbtglobalconf" type="checkbox" name="global_conf" value="1" /></td></tr>';
-echo '<tr><td><label id="mantisbtinit-url" ';
-if ($use_tooltips)
-	echo 'title="'._('Specify the Full URL of the MantisBT Web Server.').'"';
-echo ' >URL</label></td><td><input id="mantisbturl" type="text" size="50" maxlength="255" name="url" /></td></tr>';
-echo '<tr><td><label id="mantisbtinit-user" ';
-if ($use_tooltips)
-	echo 'title="'._('Specify the user with admin right to be used thru SOAP API.').'"';
-echo ' >SOAP User</label></td><td><input id="mantisbtuser" type="text" size="50" maxlength="255" name="soap_user" /></td></tr>';
-echo '<tr><td><label id="mantisbtinit-password" ';
-if ($use_tooltips)
-	echo 'title="'._('Specify the password of this user.').'"';
-echo ' >SOAP Password</label></td><td><input id="mantisbtpassword" type="text" size="50" maxlength="255" name="soap_password" /></td></tr>';
-echo '<tr><td><label id="mantisbtinit-create" ';
-if ($use_tooltips)
-	echo 'title="'._('If this project does NOT exist in MantisBT, do you want to create it ?').'"';
-echo ' >Create the project in MantisBT</label></td><td><input id="mantisbtcreate" type="checkbox" name="mantisbtcreate" value="1" /></td></tr>';
-echo '<tr><td><label id="mantisbtinit-name" ';
-if ($use_tooltips)
-	echo 'title="'._('Specify the name of the project in MantisBT if already created in MantisBT').'"';
-echo ' >Name of the project in MantisBT</label></td><td><input id="mantisbtname" type="text" size="50" maxlength="255" name="mantisbtname" /></td></tr>';
-echo '<tr><td><label id="mantisbtinit-syncroles" ';
-if ($use_tooltips)
-	echo 'title="'._('Do you want to sync FusionForge -> MantisBT roles ?').'"';
-echo ' >Sync Roles</label></td><td><input disabled="disabled" type="checkbox" name="sync_roles" /></td></tr>';
-echo '</table>';
-echo '<input type="submit" value="'._('Initialize').'" />';
-echo '</form>';
+echo $HTML->openForm(array('method' => 'post', 'action' => util_make_uri('/plugins/'.$mantisbt->name.'/?type='.$type.'&group_id='.$group_id.'&action=init')));
+echo $HTML->listTableTop();
+$cells = array();
+$cells[] = array(_('Use global configuration')._(':'), 'class' => 'align-right');
+$cells[][] = html_e('input', array('type' => 'checkbox', 'title' => _('Use the global configuration defined at forge level'), 'id' => 'mantisbtglobalconf', 'name' => 'global_conf', 'value' => 1));
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('URL')._(':'), 'class' => 'align-right');
+$cells[][] = html_e('input', array('id' => 'mantisbturl', 'title' => _('Specify the Full URL of the MantisBT Web Server.'), 'type' => 'url', 'size' => 50, 'maxlength' => 255, 'name' => 'url'));
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('SOAP User')._(':'), 'class' => 'align-right');
+$cells[][] = html_e('input', array('id' => 'mantisbtuser', 'title' => _('Specify the user with admin right to be used thru SOAP API.'), 'type' => 'text', 'size' => 50, 'maxlength' => 255, 'name' => 'soap_user'));
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('SOAP Password')._(':'), 'class' => 'align-right');
+$cells[][] = html_e('input', array('id' => 'mantisbtpassword', 'title' => _('Specify the password of this SOAP User.'), 'type' => 'password', 'size' => 50, 'maxlength' => 255, 'name' => 'soap_password'));
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('Create the project in MantisBT')._(':'), 'class' => 'align-right');
+$cells[][] = html_e('input', array('title' => _('If this project does NOT exist in MantisBT, do you want to create it ? The current project name will be used.'), 'id' => 'mantisbtcreate', 'type' => 'checkbox', 'name' => 'mantisbtcreate', 'value' => 1));
+echo $HTML->multiTableRow(array(), $cells);
+$cells = array();
+$cells[] = array(_('Link with a existing project in MantisBT')._(':'), 'class' => 'align-right');
+$cells[][] = html_e('input', array('title' => _('Specify the name of the project already created in MantisBT'), 'id' => 'mantisbtname', 'type' => 'text', 'size' => 50, 'maxlength' => 255, 'name' => 'mantisbtname'));
+echo $HTML->multiTableRow(array(), $cells);
+// echo '<tr><td><label id="mantisbtinit-syncroles" title="'._('Do you want to sync FusionForge -> MantisBT roles ?').'" >Sync Roles</label></td><td><input disabled="disabled" type="checkbox" name="sync_roles" /></td></tr>';
+echo $HTML->listTableBottom();
+echo html_e('input', array('type' => 'submit', 'value' => _('Initialize')));
+echo $HTML->closeForm();
