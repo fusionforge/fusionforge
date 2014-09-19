ae09a30472bc5d0ca2023766695a23b816cc91b5
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-09-19 16:38:01 +0200

    Prepare 5.3.2 release

--- src/www/index_std.php
+++ src/www/index_std.php
@@ -45,8 +45,82 @@ require_once $gfcommon.'include/FusionForge.class.php';
 <li><?php print _('Wiki (using MediaWiki or phpWiki).'); ?></li>
 <li><?php print _('A powerful plugin system to add new features.'); ?></li>
 </ul>
+
+<h3><?php print _("What's new in FusionForge 5.3.2"); ?></h3>
+<p><?php print _('Checkout the roadmap for closed issues (bugs, patches, features requests) ') ?><a href="https://fusionforge.org/tracker/roadmap.php?group_id=6&roadmap_id=1&release=5.3.2"><?php echo _('here') ?></a></p>
+<p><?php print _('Fixes:') ?></p>
+<ul>
+  <li><?php print _('Software map: fix "value too long for type character varying(255)" error in cron db_trove_maint.php (Inria)'); ?></li>
+  <li><?php print _("Projects: fix Project name with html [#687] (TrivialDev)"); ?></li>
+  <li><?php print _("Projects: don't display admins if their account is suspended (Inria)"); ?></li>
+  <li><?php print _("Projects: member lists should check permission [#711] (TrivialDev)"); ?></li>
+  <li><?php print _("Admin: fix edit table themes, fix frs_processor sequence [#691] (TrivialDev)"); ?></li>
+  <li><?php print _("User SSH keys (ssh_create.php): fix harmless warning when user removes all her keys (Inria)"); ?></li>
+  <li><?php print _("News: don't send requests for frontpage display for private projects (Inria)"); ?></li>
+  <li><?php print _("Docman: fix download count [#702] (TrivialDev)"); ?></li>
+  <li><?php print _("Tracker: fix translation support [#688] (TrivialDev)"); ?></li>
+  <li><?php print _("Tracker: fix custom status extrafield not updateable using mass update [#712] (TrivialDev)"); ?></li>
+  <li><?php print _("Mailing lists: handle quotes and accents in description (Inria)"); ?></li>
+  <li><?php print _("SCM Reporting: fix legend block size exceed graph canvas [#718] (TrivialDev)"); ?></li>
+  <li><?php print _("Plugin mediawiki: fix paths in import/export scripts (Inria)"); ?></li>
+  <li><?php print _("Plugin fckeditor: dropped in favor of ckeditor"); ?></li>
+  <li><?php print _("Plugin SCM Git: suppress 'warning: You appear to have cloned an empty repository.' in create_scm_repos.php (Inria)"); ?></li>
+  <li><?php print _("Plugin SCM SVN: fix sql error in activity tab on init log [#715] (TrivialDev)"); ?></li>
+  <li><?php print _("Plugin SCM SVN: fix activity tab on empty commit log [#714] (Inria)"); ?></li>
+  <li><?php print _("Plugin SCM HG (Mercurial): fix user stats [#722] (TrivialDev)"); ?></li>
+  <li><?php print _("Plugin SCM HG (Mercurial): fix iframe size [#721] (TrivialDev)"); ?></li>
+  <li><?php print _("Plugin SCM HG (Mercurial): fix ssl setting [#723] (TrivialDev)"); ?></li>
+  <li><?php print _("Stats: handle bad encoding when gathering Git stats, remove spurious warning when SVN repository isn't created yet (Inria)"); ?></li>
+  <li><?php print _("Stats: fix commits count [#717] (TrivialDev, Roland Mas)"); ?><br />
+      <?php print _("Run 'forge_run_job gather_scm_stats.php --all' to regenerate your stats."); ?><br />
+      <?php print _("Optionally, if some of your repositories have history dating from before the project was created on the forge, use '--allepoch' instead"); ?></li>
+</ul>
+
+<h3><?php print _("What's new in FusionForge 5.3.1"); ?></h3>
+<p><?php print _('Checkout the roadmap for closed issues (bugs, patches, features requests) ') ?><a href="https://fusionforge.org/tracker/roadmap.php?group_id=6&roadmap_id=1&release=5.3.1"><?php echo _('here') ?></a></p>
+<p><?php print _('Fixes:') ?></p>
+<ul>
+  <li><?php print _("Docman: Basic Webdav write mkcol support (TrivialDev)"); ?></li>
+  <li><?php print _("Docman: fix Webdav access in some PHP configurations (Inria)"); ?></li>
+  <li><?php print _("Docman: fix empty home document tree when there was documents pending for approval (Inria)"); ?></li>
+  <li><?php print _("Docman: fix massaction with IE8 [#642] (TrivialDev)"); ?></li>
+  <li><?php print _("Docman: fix empty root folder in zipfile [#640] (TrivialDev)"); ?></li>
+  <li><?php print _("FRS: maintain compatibility with v<=5.1 download URLs (Inria)"); ?></li>
+  <li><?php print _("Tracker: fix email link in notification [#668] (TrivialDev)"); ?></li>
+  <li><?php print _("Tracker: fix 404 in roadmap ajax call [#675] (TrivialDev)"); ?></li>
+  <li><?php print _("Tracker: user-friendly error message when tracker is not enabled in a project (Inria)"); ?></li>
+  <li><?php print _("Tracker: fix file size for text attachments (Inria)"); ?></li>
+  <li><?php print _("Forum: fix database error in rare conditions when saving position (Inria)"); ?></li>
+  <li><?php print _("Forum: fix incorrect access check to post moderation (Inria)"); ?></li>
+  <li><?php print _("Web UI: Fix issue with old cookie that prevented user login after upgrade (Inria)"); ?></li>
+  <li><?php print _("Plugin activation/desactivation (including switching VCS): optimize role normalization (Inria)"); ?></li>
+  <li><?php print _("Home directories creation: optimize memory usage (Inria)"); ?></li>
+  <li><?php print _("User SSH keys (ssh_create.php): deploy keys as-needed, mark deployed keys in the web interface, purge deleted keys (Inria)"); ?></li>
+  <li><?php print _("SCM repositories creation: optimize memory usage (Inria)"); ?></li>
+  <li><?php print _("SCM SVN: 'svnroot-access' update: abort in case of DB error rather than writing an incomplete file [#678] (Inria)"); ?></li>
+  <li><?php print _("SCM Git: fix personal repositories creation which failed in some configurations (Inria)"); ?></li>
+  <li><?php print _("SCM Git: optimize sub-repositories detection time (Inria)"); ?></li>
+  <li><?php print _("Mailing lists: site-wide list configuration override in core:config_path/custom/mailman-config_list.conf (Inria)"); ?></li>
+  <li><?php print _("Mailing lists: reconfigure lists as-needed (Inria)"); ?></li>
+  <li><?php print _("Mailing lists: remove non-error output to prevent cron from sending e-mails (Inria)"); ?></li>
+  <li><?php print _("Homepages: new httpd_log_demux.php utility to split Apache logs per-project (Inria)"); ?></li>
+  <li><?php print _("Cron jobs: add locking to avoid parallel runs of the same cron job (Inria)"); ?></li>
+  <li><?php print _("get_news_notapproved.pl: upgrade for 5.3 (Inria)"); ?></li>
+  <li><?php print _('Migration scripts: handle orphan data when adding relational constraints; more clean-ups for "veteran" databases (Inria)'); ?></li>
+  <li><?php print _("Migration scripts: fix permissions for new directories in core:data_path after migrating attachments from database to filesystem (Inria)"); ?></li>
+  <li><?php print _("Migration scripts: migrate all user SSH keys to new interface (Inria)"); ?></li>
+  <li><?php print _("Security: harden account name validation to detect trailing newlines (Inria)"); ?></li>
+  <li><?php print _("Plugin scmhook: fix configuration reset when switching back&forth between SCM (Inria)"); ?></li>
+  <li><?php print _("Plugin message: handle empty site message (Inria)"); ?></li>
+  <li><?php print _("Plugin mediawiki: fix icon in project page link (Inria)"); ?></li>
+  <li><?php print _("Plugin hudson: fix roles access in widgets [#683] (TrivialDev)"); ?></li>
+  <li><?php print _("Admin User: fix unix_status [#666] (TrivialDev)"); ?></li>
+  <li><?php print _("Search: fix roles based access to documents using the search engine [#682] (TrivialDev)"); ?></li>
+  <li><?php print _("Forum: disable monitoring when not logged [#686] (TrivialDev)"); ?></li>
+</ul>
+
 <h3><?php print _("What's new in FusionForge 5.3"); ?></h3>
-<p><?php print _('Checkout the roadmap for closed issues (bugs, patches, features requests) ') ?><a href="/tracker/roadmap.php?group_id=6&roadmap_id=1&release=5.3"><?php echo _('here') ?></a></p>
+<p><?php print _('Checkout the roadmap for closed issues (bugs, patches, features requests) ') ?><a href="https://fusionforge.org/tracker/roadmap.php?group_id=6&roadmap_id=1&release=5.3"><?php echo _('here') ?></a></p>
 <p><?php print _('Standards features:') ?></p>
 <ul>
 <li><?php print _('Docman:'); ?>
@@ -104,6 +178,7 @@ require_once $gfcommon.'include/FusionForge.class.php';
 <li><?php print _('webanalytics: new plugin to add support for piwik or google analytics tool (TrivialDev)'); ?></li>
 <li><?php print _('new admssw plugin to provide ADMS.SW compatible RDF descriptions of projects'); ?></li>
 </ul>
+
 <h3><?php print _("What's new in FusionForge 5.2"); ?></h3>
 <p><?php print _('Standards features:') ?></p>
 <ul>
@@ -153,37 +228,6 @@ require_once $gfcommon.'include/FusionForge.class.php';
 <li><?php print _('RPM packages for Red Hat (and derived) distributions.'); ?></li>
 </ul>
 
-<h3><?php print _("What's new in FusionForge 5.0"); ?></h3>
-<ul>
-<li><?php print _('Many improvements to the trackers: configurable display, workflow management, links between artifacts, better searches, and more'); ?></li>
-<li><?php print _('Rewritten SCM subsystem, with new plugins for Bazaar, Darcs and Git'); ?></li>
-<li><?php print _('New version of Mediawiki plugin, providing independent wikis for each project'); ?></li>
-<li><?php print _('Various new plugins: projectlabels, globalsearch, extratabs'); ?></li>
-<li><?php print _('An in-depth rewrite of the theme for better accessibility and XHTML conformance'); ?></li>
-</ul>
-
-<h3><?php print _("What's new in FusionForge 4.8"); ?></h3>
-<ul>
-<li><?php print _('New project classification by tags (tag cloud).'); ?></li>
-<li><?php print _('New reporting item for the File Release System: downloads per package.'); ?></li>
-<li><?php print _('List of all projects added in Project List'); ?></li>
-<li><?php print _('New version of phpWiki plugin, using lastest svn code'); ?></li>
-</ul>
-
-<h3><?php print _("What's new in FusionForge 4.7"); ?></h3>
-<ul>
-<li><?php print _('A new name to avoid confusion with proprietary versions of GForge.'); ?></li>
-<li><?php print _('Support for PHP5.'); ?></li>
-<li><?php print _('Support for PostgreSQL 8.x.'); ?></li>
-<li><?php print _('Translations are now managed by gettext.'); ?></li>
-<li><?php print _('Support for several configurations running on the same code.'); ?></li>
-<li><?php print _('Improved security, no need for PHP register_globals.'); ?></li>
-<li><?php print _('Available as full install CD.'); ?></li>
-<li><?php print _('New wiki plugins (using MediaWiki or phpWiki).'); ?></li>
-<li><?php print _('New online_help plugin.'); ?></li>
-<li><?php print _('New phpwebcalendar plugin.'); ?></li>
-<li><?php print _('New project hierarchy plugin.'); ?></li>
-</ul>
 <?php
 if(forge_get_config('use_news')) {
 	echo $HTML->boxTop(_('Latest News'), 'Latest_News');
