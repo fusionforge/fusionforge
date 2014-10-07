9480140504c29609a39f28023a856636ebeddf8d
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-10-07 17:13:34 +0200

    testsuite: ssh: run 'id' to test basic connectivity, because triggers the nss loop without nscd/unscd

--- autoinstall/install-src.sh
+++ autoinstall/install-src.sh
@@ -30,14 +30,14 @@ if [ -e /etc/debian_version ]; then
     backports_deb
     apt-get update
     apt-get install -y make gettext php5-cli php5-pgsql php-htmlpurifier \
-	apache2 locales-all postgresql libnss-pgsql2 \
+	apache2 locales-all postgresql libnss-pgsql2 unscd \
 	subversion augeas-tools viewvc git \
-	mediawiki unscd \
+	mediawiki \
 	python-moinmoin libapache2-mod-wsgi python-psycopg2
 else
     backports_rpm
     yum install -y make gettext php-cli php-pgsql \
-	httpd mod_ssl postgresql-server \
+	httpd mod_ssl postgresql-server nscd \
 	subversion augeas viewvc git \
 	mediawiki119 \
 	moin mod_wsgi python-psycopg2
