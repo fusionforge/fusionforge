cb0bc97197c261ffbc5414b8f0d002bb7d223c44
Franck Villaume <franck.villaume@trivialdev.com>
2014-11-09 18:54:11 +0100

    widgets: add direct link for browsing tab

--- src/common/widget/Widget_ProjectLatestCommits.class.php
+++ src/common/widget/Widget_ProjectLatestCommits.class.php
@@ -91,6 +91,7 @@ class Widget_ProjectLatestCommits extends Widget {
 		} else {
 			$html .= $HTML->information(_('No commit found'));
 		}
+		$html .= html_e('div', array('class' => 'underline-link'), util_make_link('/scm/?group_id='.$project->getID(), _('Browse Source Content Management')));
 		return $html;
 	}
 
