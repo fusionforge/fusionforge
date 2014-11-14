0452075fea3a382bcf8f96e6cf5e7299bda7c5b6
Roland Mas <lolando@debian.org>
2014-11-14 14:12:37 +0100

    Disable code coverage tests for now, in order to test the upgraded buildbot (Jessie)

--- tests/buildbot/fusionforge-unittests.sh
+++ tests/buildbot/fusionforge-unittests.sh
@@ -9,9 +9,11 @@ prepare_workspace
 
 # apt-get install pcregrep moreutils xalan
 cd tests
-phpunit --log-junit $WORKSPACE/reports/phpunit.xml \
+# phpunit --log-junit $WORKSPACE/reports/phpunit.xml \
 #	--coverage-clover $WORKSPACE/reports/coverage/clover.xml \
 #	--coverage-html $WORKSPACE/reports/coverage/ \
+#	code_and_unit_tests.php
+phpunit --log-junit $WORKSPACE/reports/phpunit.xml \
 	code_and_unit_tests.php
 cp $WORKSPACE/reports/phpunit.xml $WORKSPACE/reports/phpunit.xml.org
 xalan -in $WORKSPACE/reports/phpunit.xml.org -xsl unit/fix_phpunit.xslt \
