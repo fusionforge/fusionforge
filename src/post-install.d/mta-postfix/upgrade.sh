fba37cfb59726b3945a1c21290d71d0f96c4d1b4
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-11-18 16:06:21 +0100

    install: upgrade system for configuration files

---
+++ src/post-install.d/mta-postfix/upgrade.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+# Upgrade Postfix configuration
+#
+# Copyright (C) 2014  Inria (Sylvain Beucler)
+#
+# This file is part of FusionForge. FusionForge is free software;
+# you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the Licence, or (at your option)
+# any later version.
+#
+# FusionForge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with FusionForge; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+set -e
+
+PREVVER=${1:-0.0}
+
+# 5.3 -> 6.0
+if [ $(php -r "print version_compare('$PREVVER', '5.3.50');") -eq -1 ]; then
+    rm -f $(forge_get_config data_path)/etc/postfix-transport*
+    sed -i /etc/postfix/main.cf \
+	-e 's/\(^### \(BEGIN\|END\)\) GFORGE BLOCK/\1 FUSIONFORGE BLOCK/' \
+	-e '/^### GFORGE ADDITION.*/d' \
+	-e 's/proxy:pgsql:pgsql_gforge_users/proxy:pgsql:pgsql_fusionforge_users/' \
+	-e "s,hash:$(forge_get_config data_path)/etc/postfix-transport,hash:/etc/postfix/fusionforge-lists-transport,"
+fi
