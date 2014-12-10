0be7f1f48ee9f498c9f929ceca2f514a1bcd4f57
Sylvain Beucler <sylvain.beucler@inria.fr>
2014-12-10 12:06:58 +0100

    project homepage: default to http:// rather than https:// for external homepage links [#752]

--- src/common/include/Group.class.php
+++ src/common/include/Group.class.php
@@ -1552,7 +1552,7 @@ class Group extends Error {
 	function getHomePage() {
 		if (!preg_match("/^[a-zA-Z][a-zA-Z0-9+.-]*:/",
 			$this->data_array['homepage'])) {
-			$this->data_array['homepage'] = util_url_prefix() .
+			$this->data_array['homepage'] = 'http://' .
 				$this->data_array['homepage'];
 		}
 		return $this->data_array['homepage'];
