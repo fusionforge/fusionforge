ce8690b470ead3945f28330f632ebe5e78627a4f
Thorsten Glaser <t.glaser@tarent.de>
2015-01-09 14:53:47 +0100

    exit_error() must append the trailing newline by itself

--- src/common/include/squal_exit.php
+++ src/common/include/squal_exit.php
@@ -37,7 +37,7 @@ $exit_errorlevel = 0;
  * @param		string	Error toptab
  */
 function exit_error($title,$toptab='') {
-	print 'ERROR - '.$title;
+	print 'ERROR - ' . $title . "\n";
 	exit($exit_errorlevel);
 }
 
