1b67cf0da9500a162b809336f63499ccb8fb6818
Sylvain Beucler <sylvain.beucler@inria.fr>
2015-01-16 15:47:30 +0100

    reactivity: user-friendly view of the system tasks queue

--- src/www/my/systasks.php
+++ src/www/my/systasks.php
@@ -42,22 +42,62 @@ if ($_SERVER['QUERY_STRING'] == 'create') {
 }
 
 
-site_user_header(array('title' => _('System actions queue')));
+site_user_header(array('title' => _('System actions queue (past day)')));
 
 $u = session_get_user();
 $groups = $u->getGroups();
 $gids = array();
 foreach($groups as $g)
-		$gids[] = $g->getID();
+	$gids[] = $g->getID();
 $gids = implode(',', $gids);
 
-$res = pg_query_params("SELECT * FROM systasks WHERE user_id=$1 or group_id IN ($gids)"
-					   . " AND requested > NOW() - interval '1 day'",
-					   array($u->getID()));
-while($row = db_fetch_array($res)) {
-		print_r($row);
-		print "<br />";
+$title_arr = array(
+	_('Task ID'),
+	_('Plugin'),
+	_('SysTask Type ID'),
+	_('Group ID'),
+	_('Status'),
+	_('Requested'),
+	_('Started'),
+	_('Ended'),
+);
+
+echo $HTML->listTableTop ($title_arr);
+$query = "
+SELECT systask_id, unix_group_name,
+plugin_name, systasks.status,
+    EXTRACT(epoch FROM requested) AS requested,
+    EXTRACT(epoch FROM started) AS started,
+    EXTRACT(epoch FROM stopped) AS stopped,
+    EXTRACT(epoch FROM started-requested) AS queued,
+    EXTRACT(epoch FROM stopped-started) AS run
+  FROM systasks LEFT JOIN groups ON (systasks.group_id = groups.group_id)
+    LEFT JOIN plugins ON (systasks.plugin_id = plugins.plugin_id)
+  WHERE user_id=$1 or systasks.group_id IN ($gids)
+  AND requested > NOW() - interval '1 day'
+  ORDER BY systask_id";
+$res = pg_query_params($query, array($u->getID()));
+for ($i=0; $i<db_numrows($res); $i++) {
+	$cells = array();
+	$cells[][] = db_result($res,$i,'systask_id');
+	$plugin_name = db_result($res,$i,'plugin_name');
+	if ($plugin_name == null)
+		$cells[][] = 'core';
+	else
+		$cells[][] = $plugin_name;
+	$cells[][] = db_result($res,$i,'systask_type_id');
+	$cells[][] = db_result($res,$i,'unix_group_name');
+	$cells[][] = db_result($res,$i,'status');
+	$cells[][] = date("H:i:s", db_result($res, $i,'requested'));
+	$cells[][] = date("H:i:s", db_result($res, $i,'started'))
+		. ' (+' . round(db_result($res, $i,'queued'), 1) . 's)';
+	$cells[][] = date("H:i:s", db_result($res, $i,'stopped'))
+		. ' (+' . round(db_result($res, $i,'run'), 1) . 's)';
+	echo $HTML->multiTableRow(array('class' => $HTML->boxGetAltRowStyle($i+1, true)), $cells);
 }
+
+
+echo $HTML->listTableBottom();
 
 site_user_footer();
 
