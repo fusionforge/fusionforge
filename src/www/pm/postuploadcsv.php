32e8119ba1c0724af70cb2dc26a73398e8595854
PKeidel <philipp.keidel@edag.de>
2015-03-23 13:06:50 +0100

    Bugfix for Bug [#761]: Choose if csv task import should replace or append the tasks
    Signed-off-by: Franck Villaume <franck.villaume@trivialdev.com>

--- src/www/pm/postuploadcsv.php
+++ src/www/pm/postuploadcsv.php
@@ -107,8 +107,8 @@ if (isset($input_file) && isset($input_file['tmp_name']) &&
 					'dependenton'=>$dependentOn,
 					'notes'=>$cols[11]);
 		}
-		$res=&pm_import_tasks($group_project_id, $tasks);
 	}
+    $res=&pm_import_tasks($group_project_id, $tasks, getIntFromRequest('replace') == 1);
 } else {
 	$res['errormessage'] = _('Parameter error');
 }
