0f37dc682a5f83bc6dc953a7032d048846060364
Sylvain Beucler <sylvain.beucler@inria.fr>
2015-04-07 20:44:12 +0200

    scm: xinetd and rsync anonymous access

--- src/plugins/scmgit/bin/install.sh
+++ src/plugins/scmgit/bin/install.sh
@@ -11,6 +11,41 @@ gitwebdir=$(ls -d /var/www/git /var/www/git/static /var/www/gitweb-caching /usr/
 
 case "$1" in
     configure)
+	scmgit_repos_path=$(forge_get_config repos_path scmgit)
+
+	echo "Modifying (x)inetd for Subversion server"
+	if [ -d /etc/xinetd.d/ ]; then
+	    if [ ! -e /etc/xinetd.d/fusionforge-plugin-scmgit ]; then
+		cat > /etc/xinetd.d/fusionforge-plugin-scmgit <<-EOF
+		service git
+		{
+		    port            = 9418
+		    socket_type     = stream
+		    wait            = no
+		    user            = nobody
+		    server          = /usr/bin/git
+		    server_args     = daemon --inetd --export-all --base-path=$scmgit_repos_path
+		}
+		EOF
+	    fi
+	    service xinetd restart
+	fi
+
+	# rsync access
+	if ! grep -q '^use chroot' /etc/rsyncd.conf 2>/dev/null; then
+	    touch /etc/rsyncd.conf
+	    echo 'use chroot=no' | sed -i -e '1ecat' /etc/rsyncd.conf
+	fi
+	sed -i -e 's/^use chroot.*/use chroot=no/' /etc/rsyncd.conf
+	if ! grep -q '\[git\]' /etc/rsyncd.conf; then
+	    cat <<-EOF >> /etc/rsyncd.conf
+		[git]
+		comment=Git source repositories
+		path=$scmgit_repos_path
+		EOF
+	fi
+
+	# Gitweb
 	if [ -z "$gitwebcgi" -o -z "$gitwebdir" ]; then echo "Cannot find gitweb"; exit 1; fi
 	mkdir -p -m 755 $plugindir/cgi-bin/
 	ln -nfs $gitwebcgi                 $plugindir/cgi-bin/
@@ -19,7 +54,9 @@ case "$1" in
 	ln -nfs $gitwebdir/gitweb.css      $plugindir/www/
 	ln -nfs $gitwebdir/gitweb.js       $plugindir/www/
 	;;
+
     remove)
+	rm -f /etc/xinetd.d/fusionforge-plugin-scmgit
 	rm -rf $plugindir/cgi-bin/
 	find $plugindir/www/ -type l -print0 | xargs -r0 rm
 	;;
