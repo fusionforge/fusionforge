2d1dc667d860ace8e422c8fcc013e86c5572c3ec
Sylvain Beucler <sylvain.beucler@inria.fr>
2015-04-10 17:23:24 +0200

    scmsvn: we're using mpm_itk/prefork now, not worker, remember?

--- src/plugins/scmsvn/bin/install.sh
+++ src/plugins/scmsvn/bin/install.sh
@@ -48,18 +48,6 @@ case "$1" in
 	    a2enmod dav_svn
 	fi
 	# else: Apache modules already enabled in CentOS
-
-	# Work-around memory leak in mod_dav_svn
-	for conf in /etc/apache2/apache2.conf /etc/httpd/conf/httpd.conf \
-	    /etc/apache2/server-tuning.conf; do
-	    if [ -e $conf ] && type augtool >/dev/null 2>&1; then
-		val=$(augtool "print /files$conf/IfModule[arg='mpm_worker_module' or arg='worker.c']/directive[.='MaxRequestsPerChild']/arg" | sed 's/^.*= "\(.*\)"/\1/')
-		if [ "$val" = "0" ]; then
-		    augtool --autosave "set /files$conf/IfModule[arg='mpm_worker_module' or arg='worker.c']/directive[.='MaxRequestsPerChild']/arg 5000" \
-			|| true  # v0.10 always returns 1, v1.0 always returns 0..
-		fi
-	    fi
-	done
 	;;
 
     remove)
