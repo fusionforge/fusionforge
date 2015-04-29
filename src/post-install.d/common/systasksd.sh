375f9601a0666de06c5375ab3900d3d4754a9341
Sylvain Beucler <sylvain.beucler@inria.fr>
2015-04-29 11:27:55 +0200

    systasksd: remove update-rc.d work-around which was fixed in Debian

--- src/post-install.d/common/systasksd.sh
+++ src/post-install.d/common/systasksd.sh
@@ -24,10 +24,6 @@ case "$1" in
 	    chkconfig fusionforge-systasksd on
 	else
 	    update-rc.d fusionforge-systasksd defaults
-	    # Work-around http://bugs.debian.org/774799
-	    if [ -x /bin/systemctl ]; then
-		systemctl daemon-reload
-	    fi
 	fi
 	# not 'start' as systemd will no-op if systasksd started and exited
 	service fusionforge-systasksd restart
