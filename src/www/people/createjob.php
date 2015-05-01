ce2cbc8384d558fa443a24b054fe2157ae5108a3
Franck Villaume <franck.villaume@trivialdev.com>
2015-05-01 17:59:35 +0200

    fix html syntax => move to HTML helpers

--- src/www/people/createjob.php
+++ src/www/people/createjob.php
@@ -4,7 +4,7 @@
  *
  * Copyright 1999-2001 (c) VA Linux Systems
  * Copyright 2002-2004 (c) GForge Team
- * Copyright 2014, Franck Villaume - TrivialDev
+ * Copyright 2014-2015, Franck Villaume - TrivialDev
  * http://fusionforge.org/
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -41,23 +41,17 @@ if ($group_id && (forge_check_perm('project_admin', $group_id))) {
 	/*
 		Fill in the info to create a job
 	*/
-	echo '
-		<p>'._('Start by filling in the fields below. When you click continue, you will be shown a list of skills and experience levels that this job requires.').'</p>
-		<p>
-		<form action="'.util_make_url ('/people/editjob.php').'" method="post">
-		<input type="hidden" name="group_id" value="'.$group_id.'" />
-		<input type="hidden" name="form_key" value="' . form_generate_key() . '">
-		<strong>'._('Category').'</strong>'.utils_requiredField().'</p>
-		'. people_job_category_box('category_id') .'
-		<p>
-		<strong>'._('Short Description').utils_requiredField()._(':').'</strong><br />
-		<input type="text" required="required" name="title" value="" size="40" maxlength="60" /></p>
-		<p>
-		<strong>'._('Long Description').utils_requiredField()._(':').'</strong><br />
-		<textarea required="required" name="description" rows="10" cols="60"></textarea></p>
-		<p>
-		<input type="submit" name="add_job" value="'._('Continue >>').'" />
-		</form></p>';
+	echo html_e('p', array(), _('Start by filling in the fields below. When you click continue, you will be shown a list of skills and experience levels that this job requires.'));
+	echo $HTML->openForm(array('action' => '/people/editjob.php', 'method' => 'post'));
+	echo html_e('input', array('type' => 'hidden', 'name' => 'group_id', 'value' => $group_id));
+	echo html_e('input', array('type' => 'hidden', 'name' => 'form_key', 'value' => form_generate_key()));
+	echo html_e('strong', array(), _('Category')).utils_requiredField().html_e('br').people_job_category_box('category_id');
+	echo html_e('p', array(), html_e('strong', array(), _('Short Description').utils_requiredField()._(':')).html_e('br').
+				html_e('input', array('type' => 'text', 'required' => 'required', 'name' => 'title', 'value' => '', 'size' => 40, 'maxlength' => 60)));
+	echo html_e('p', array(), html_e('strong', array(), _('Long Description').utils_requiredField()._(':')).html_e('br').
+				html_e('textarea', array('required' => 'required', 'name' => 'description', 'rows' => 10, 'cols' =>60), '', false));
+	echo html_e('input', array('type' => 'submit', 'name' => 'add_job', 'value' => _('Continue').'>>'));
+	echo $HTML->closeForm();
 
 	people_footer();
 
