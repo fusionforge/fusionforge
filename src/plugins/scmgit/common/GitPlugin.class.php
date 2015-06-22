5d95bf4fcbb2969642d66ce6829e31b16a7e5ee0
Sylvain Beucler <sylvain.beucler@inria.fr>
2015-06-22 15:16:44 +0200

    activity: first stab at remote git log

--- src/plugins/scmgit/common/GitPlugin.class.php
+++ src/plugins/scmgit/common/GitPlugin.class.php
@@ -962,31 +962,61 @@ control over it to the project's administrator.");
 			return false;
 		}
 		if (in_array('scmgit', $params['show']) || (count($params['show']) < 1)) {
-			$repo = forge_get_config('repos_path', 'scmgit') . '/' . $project->getUnixName() . '/' . $project->getUnixName() . '.git';
-			if (is_dir($repo) && is_dir($repo.'/refs')) {
-				$start_time = $params['begin'];
-				$end_time = $params['end'];
-				$pipe = popen("GIT_DIR=\"$repo\" git log --date=raw --since=@$start_time --until=@$end_time --all --pretty='format:%ad||%ae||%s||%h' --name-status", 'r' );
-				while (!feof($pipe) && $data = fgets($pipe)) {
-					$line = trim($data);
-					$splitedLine = explode('||', $line);
-					if (sizeof($splitedLine) == 4) {
-						$result = array();
-						$result['section'] = 'scm';
-						$result['group_id'] = $group_id;
-						$result['ref_id'] = 'browser.php?group_id='.$group_id.'&commit='.$splitedLine[3];
-						$result['description'] = htmlspecialchars($splitedLine[2]).' (commit '.$splitedLine[3].')';
-						$userObject = user_get_object_by_email($splitedLine[1]);
-						if (is_a($userObject, 'GFUser')) {
-							$result['realname'] = util_display_user($userObject->getUnixName(), $userObject->getID(), $userObject->getRealName());
-						} else {
-							$result['realname'] = '';
-						}
-						$splitedDate = explode(' ', $splitedLine[0]);
-						$result['activity_date'] = $splitedDate[0];
-						$result['subref_id'] = '';
-						$params['results'][] = $result;
+			$start_time = $params['begin'];
+			$end_time = $params['end'];
+
+			// Grab commit log
+			$protocol = forge_get_config('use_ssl', 'scmgit') ? 'https://' : 'http://';
+			$u = session_get_user();
+			if ($project->enableAnonSCM())
+				$server_script = '/anonscm/gitlog';
+			else
+				$server_script = '/authscm/'.$u->getUnixName().'/gitlog';
+			$script_url = $protocol . forge_get_config('scm_host')
+				. $server_script
+				.'?unix_group_name='.$project->getUnixName()
+				.'&mode=date_range'
+				.'&begin='.$params['begin']
+				.'&end='.$params['end'];
+			$filename = tempnam('/tmp', 'gitlog');
+			$f = fopen($filename, 'w');
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $script_url);
+			curl_setopt($ch, CURLOPT_FILE, $f);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_COOKIE, $_SERVER['HTTP_COOKIE']);  // for session validation
+			curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);  // for session validation
+			curl_setopt($ch, CURLOPT_HTTPHEADER,
+						array('X-Forwarded-For: '.$_SERVER['REMOTE_ADDR']));  // for session validation
+			$body = curl_exec($ch);
+			if ($body === false) {
+				$this->setError(curl_error($ch));
+			}
+			curl_close($ch);
+			fclose($f); // flush buffer
+			$f = fopen($filename, 'r');
+			unlink($filename);
+
+			while (!feof($f) && $data = fgets($f)) {
+				$line = trim($data);
+				$splitedLine = explode('||', $line);
+				if (sizeof($splitedLine) == 4) {
+					$result = array();
+					$result['section'] = 'scm';
+					$result['group_id'] = $group_id;
+					$result['ref_id'] = 'browser.php?group_id='.$group_id.'&commit='.$splitedLine[3];
+					$result['description'] = htmlspecialchars($splitedLine[2]).' (commit '.$splitedLine[3].')';
+					$userObject = user_get_object_by_email($splitedLine[1]);
+					if (is_a($userObject, 'GFUser')) {
+						$result['realname'] = util_display_user($userObject->getUnixName(), $userObject->getID(), $userObject->getRealName());
+					} else {
+						$result['realname'] = '';
 					}
+					$splitedDate = explode(' ', $splitedLine[0]);
+					$result['activity_date'] = $splitedDate[0];
+					$result['subref_id'] = '';
+					$params['results'][] = $result;
 				}
 			}
 		}
---
+++ src/plugins/scmgit/libexec/gitlog.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Returns commit log for inclusion in web frontend
+ *
+ * Copyright 2015  Inria (Sylvain Beucler)
+ *
+ * This file is part of FusionForge.
+ *
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+// Don't try to connect to the DB, just redirecting SVN URL
+putenv('FUSIONFORGE_NO_DB=true');
+
+require_once '../../../www/env.inc.php';
+require_once $gfcommon.'include/pre.php';
+
+header('Content-type: text/plain');
+
+# Authentify request
+if (!preg_match(',^/anonscm/,', $_SERVER['REQUEST_URI'])) {
+	$web_host = forge_get_config('web_host');
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://' . $web_host . '/account/check_forwarded_session.php');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_COOKIE, $_SERVER['HTTP_COOKIE']);
+	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Forwarded-For: '.$_SERVER['HTTP_X_FORWARDED_FOR']));
+	//$info = curl_getinfo($ch);
+	$body = curl_exec($ch);
+	curl_close($ch);
+	if ($body != 'OK')  {
+		die($body);
+	}
+}
+
+
+$unix_group_name = $_GET['unix_group_name'];
+$mode = $_GET['mode'];
+if (!preg_match('/^(date_range|latest|latest_user)$/', $mode))
+	die('Invalid mode');
+if (!preg_match('/^[a-z0-9][-a-z0-9_\.]+\z/', $unix_group_name))
+	die('Invalid group name');
+
+if ($mode == 'date_range') {
+	$start_time = $_GET['begin'];
+	$end_time = $_GET['end'];
+	if (!ctype_digit($start_time))
+		die('Invalid start time');
+	if (!ctype_digit($end_time))
+		die('Invalid end time');
+	$options = "--since=@$start_time --until=@$end_time";
+} else if ($mode == 'latest' or $mode == 'latest_user') {
+	$limit = $_GET['limit'];
+	if (!ctype_digit($limit))
+		die('Invalid limit');
+	$options = "--max-count=$nb_commits";
+	
+	if ($mode == 'latest_user') {
+		$user_name = $_GET['user_name'];
+		if (!preg_match('/^[a-z0-9][-a-z0-9_\.]+\z/', $user_name))
+			die('Invalid user name');
+		$options .= " --author=\"$email\" --author=\"$fullname\"  --author=\"$userunixname\"";
+	}
+}
+
+$repo = forge_get_config('repos_path', 'scmgit') . "/$unix_group_name/$unix_group_name.git";
+if (is_dir($repo)) {
+	passthru("GIT_DIR=\"$repo\" git log --date=raw --all --pretty='format:%ad||%ae||%s||%h' --name-status $options");
+}
+
