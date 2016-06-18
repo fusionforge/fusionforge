0fb03b3b89c6bb7bdaa62d47c370e5a6df484111
Franck Villaume <franck.villaume@trivialdev.com>
2016-06-18 18:19:01 +0200

    fix rename Plugin function to __construct. Add missing licence

--- src/plugins/externalsearch/common/ExternalSearchPlugin.class.php
+++ src/plugins/externalsearch/common/ExternalSearchPlugin.class.php
@@ -1,12 +1,24 @@
 <?php
-
 /**
  * External search plugin
  *
  * Copyright 2004 (c) Guillaume Smet
+ * http://fusionforge.org
  *
- * http://gforge.org
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
  *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 global $gfwww;
@@ -19,8 +31,8 @@ forge_define_config_item('engines_file','externalsearch',
 	) ;
 
 class ExternalSearchPlugin extends Plugin {
-	function ExternalSearchPlugin() {
-		$this->Plugin();
+	function __construct() {
+		parent::__construct();
 		$this->name = 'externalsearch';
 		$this->text = _('External Search');
 		$this->pkg_desc =
