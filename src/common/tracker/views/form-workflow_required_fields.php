ee9491db26ed3cd01c50f51da92df5520945866d
Marc-Etienne Vargenau <Marc-Etienne.Vargenau@alcatel-lucent.com>
2016-07-01 14:49:26 +0200

    Indent

--- src/common/tracker/views/form-workflow_required_fields.php
+++ src/common/tracker/views/form-workflow_required_fields.php
@@ -30,65 +30,65 @@ require_once 'common/tracker/ArtifactWorkflow.class.php';
 
 global $HTML;
 
-		$from = getIntFromRequest('from');
-		$next = getIntFromRequest('next');
+$from = getIntFromRequest('from');
+$next = getIntFromRequest('next');
 
 //	FORM TO UPDATE ARTIFACT TYPES
 
-		/*
-			List of possible user built Selection Boxes for an ArtifactType
-		*/
-		$efarr = $ath->getExtraFields(array(ARTIFACT_EXTRAFIELDTYPE_STATUS));
-		if (count($efarr) === 0) {
-			// TODO: Normal status is not implemented right now.
-			return false;
-		} elseif (count($efarr) !== 1) {
-			// Internal error.
-			return false;
-		}
+/*
+	List of possible user built Selection Boxes for an ArtifactType
+*/
+$efarr = $ath->getExtraFields(array(ARTIFACT_EXTRAFIELDTYPE_STATUS));
+if (count($efarr) === 0) {
+	// TODO: Normal status is not implemented right now.
+	return false;
+} elseif (count($efarr) !== 1) {
+	// Internal error.
+	return false;
+}
 
-		$keys=array_keys($efarr);
-		$field_id = $keys[0];
+$keys=array_keys($efarr);
+$field_id = $keys[0];
 
-		$atw = new ArtifactWorkflow($ath, $field_id);
-		$requiredFields = $atw->getRequiredFields($from, $next);
+$atw = new ArtifactWorkflow($ath, $field_id);
+$requiredFields = $atw->getRequiredFields($from, $next);
 
-		$elearray = $ath->getExtraFieldElements($field_id);
-		foreach ($elearray as $e) {
-			$name[ $e['element_id'] ] = $e['element_name'];
-		}
+$elearray = $ath->getExtraFieldElements($field_id);
+foreach ($elearray as $e) {
+	$name[ $e['element_id'] ] = $e['element_name'];
+}
 
-		$title = sprintf(_('Configuring required files for the transitions from %1$s to %2$s'), $name[$from], $name[$next]);
-		$ath->adminHeader(array('title'=>$title,
-			'pagename'=>'tracker_admin_customize_liste',
-			'titlevals'=>array($ath->getName())));
+$title = sprintf(_('Configuring required files for the transitions from %1$s to %2$s'), $name[$from], $name[$next]);
+$ath->adminHeader(array('title'=>$title,
+	'pagename'=>'tracker_admin_customize_liste',
+	'titlevals'=>array($ath->getName())));
 
 echo $HTML->openForm(array('action' => '/tracker/admin/?group_id='.$group_id.'&atid='.$ath->getID(), 'method' => 'post'));
 ?>
-		<input type="hidden" name="field_id" value="<?php echo $field_id ?>" />
-		<input type="hidden" name="workflow_required_fields" value="1" />
-		<input type="hidden" name="from" value="<?php echo $from ?>" />
-		<input type="hidden" name="next" value="<?php echo $next ?>" />
+<input type="hidden" name="field_id" value="<?php echo $field_id ?>" />
+<input type="hidden" name="workflow_required_fields" value="1" />
+<input type="hidden" name="from" value="<?php echo $from ?>" />
+<input type="hidden" name="next" value="<?php echo $next ?>" />
 
 <?php
-		$extra_fields = $ath->getExtraFields() ;
-		//sortRoleList ($group_roles) ;
-		foreach ($extra_fields as $key => $row) {
-			$extra_fields_names[$key] = $row['field_name'];
-		}
-		array_multisort($extra_fields_names, SORT_ASC, SORT_LOCALE_STRING | SORT_FLAG_CASE, $extra_fields);
+$extra_fields = $ath->getExtraFields() ;
+//sortRoleList ($group_roles) ;
+foreach ($extra_fields as $key => $row) {
+	$extra_fields_names[$key] = $row['field_name'];
+}
+array_multisort($extra_fields_names, SORT_ASC, SORT_LOCALE_STRING | SORT_FLAG_CASE, $extra_fields);
 
-		foreach ($extra_fields as $field) {
-			if ($field['field_type'] != ARTIFACT_EXTRAFIELDTYPE_STATUS) {
-				$value = in_array($field['extra_field_id'], $requiredFields)? ' checked="checked"' : '';
-				$str = '<input type="checkbox" name="extrafield['.$field['extra_field_id'].']"'.$value.' />';
-				$str .= ' '.$field['field_name'];
-				echo $str."<br />\n";
-			}
-		}
+foreach ($extra_fields as $field) {
+	if ($field['field_type'] != ARTIFACT_EXTRAFIELDTYPE_STATUS) {
+		$value = in_array($field['extra_field_id'], $requiredFields)? ' checked="checked"' : '';
+		$str = '<input type="checkbox" name="extrafield['.$field['extra_field_id'].']"'.$value.' />';
+		$str .= ' '.$field['field_name'];
+		echo $str."<br />\n";
+	}
+}
 ?>
-		<p>
-		<input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" /></p>
-		<?php
+<p>
+<input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" /></p>
+<?php
 echo $HTML->closeForm();
-		$ath->footer();
+$ath->footer();
