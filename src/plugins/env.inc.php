fd27ef4568bebead87946b140936868dce416bfe
Marc-Etienne Vargenau <Marc-Etienne.Vargenau@alcatel-lucent.com>
2016-07-06 10:42:12 +0200

    Whitespace

--- src/plugins/env.inc.php
+++ src/plugins/env.inc.php
@@ -1,5 +1,5 @@
 <?php
-
-/* Compatibility file installed after move of real env.inc.php to common/include */
+/* Compatibility file installed after move of real env.inc.php to common/include
+ */
 
 require_once (dirname(dirname( __FILE__ )).'/common/include/env.inc.php') ;
