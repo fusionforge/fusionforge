6406e883e0acec9137d284cb8b9cf550759b4493
Franck Villaume <franck.villaume@trivialdev.com>
2016-11-05 11:04:57 +0100

    coding style

--- src/common/tracker/ArtifactQuery.class.php
+++ src/common/tracker/ArtifactQuery.class.php
@@ -24,7 +24,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
+/**
  * Standard Alcatel-Lucent disclaimer for contributing to open source
  *
  * "The Artifact ("Contribution") has not been tested and/or
