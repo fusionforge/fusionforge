bdadb7cb3dfe6ffcdd3c27e16fead717cb3babfe
Roland Mas <lolando@debian.org>
2016-12-02 14:08:57 +0100

    Added testsuite for session improvements

--- tests/func/10_Site/loginTest.php
+++ tests/func/10_Site/loginTest.php
@@ -54,6 +54,7 @@ class LoginProcess extends FForge_SeleniumTestCase
 			$this->logout();
 		}
 		$this->clickAndWait("link=Log In");
+		$this->assertTrue($this->isTextPresent("Log In"));
 
 		// Check that current URL's base is the same as ROOT
 		// If the forge redirects to other URL than the one
@@ -67,34 +68,35 @@ class LoginProcess extends FForge_SeleniumTestCase
 
 		$this->type("form_loginname", FORGE_ADMIN_USERNAME);
 		$this->type("form_pw", FORGE_ADMIN_PASSWORD);
-		$this->click("login");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("login");
+		$this->assertTrue($this->isTextPresent("Forge Admin"));
+		$this->assertTrue($this->isTextPresent("Log Out"));
+		$this->open( ROOT );
 		$this->assertTrue($this->isTextPresent("Forge Admin"));
 		$this->assertTrue($this->isTextPresent("Log Out"));
 		$this->logout();
-		// Verify that logout is succesfull
+		$this->open( ROOT );
+		// Verify that logout is succesful
 		$this->assertTrue($this->isTextPresent("Log In"));
 
 		// Test with an empty password.
 		$this->open( ROOT );
-		$this->click("link=Log In");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("link=Log In");
+		$this->assertTrue($this->isTextPresent("Log In"));
 		$this->type("form_loginname", FORGE_ADMIN_USERNAME);
 		$this->type("form_pw", "");
 		$this->click("login");
-		sleep (1);
+		sleep(1);
 		$this->assertTrue($this->isTextPresent("Login"));
 		$this->assertFalse($this->isTextPresent("Forge Admin"));
 		$this->assertTrue($this->isTextPresent("Log In"));
 
 		// Test with a wrong password.
 		$this->open( ROOT );
-		$this->click("link=Log In");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("link=Log In");
 		$this->type("form_loginname", FORGE_ADMIN_USERNAME);
 		$this->type("form_pw", "awrongpassword");
-		$this->click("login");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("login");
 		$this->assertTrue($this->isTextPresent("Invalid Password Or User Name"));
 		$this->assertFalse($this->isTextPresent("Forge Admin"));
 		$this->assertTrue($this->isTextPresent("Log In"));
@@ -106,20 +108,17 @@ class LoginProcess extends FForge_SeleniumTestCase
 
 		$this->clickAndWait("link=Site Admin");
 		$this->clickAndWait("link=Display Full User List/Edit Users");
-		$this->click("//table/tbody/tr/td/a[contains(@href,'useredit.php') and contains(.,'(admin)')]/../..//a[contains(@href, 'passedit.php?user_id=')]");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("//table/tbody/tr/td/a[contains(@href,'useredit.php') and contains(.,'(admin)')]/../..//a[contains(@href, 'passedit.php?user_id=')]");
 		$this->type("passwd", FORGE_OTHER_PASSWORD);
 		$this->type("passwd2", FORGE_OTHER_PASSWORD);
 		$this->clickAndWait("submit");
 
 		$this->logout();
 		$this->open( ROOT );
-		$this->click("link=Log In");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("link=Log In");
 		$this->type("form_loginname", FORGE_ADMIN_USERNAME);
 		$this->type("form_pw", FORGE_OTHER_PASSWORD);
-		$this->click("login");
-		$this->waitForPageToLoad("30000");
+		$this->clickAndWait("login");
 		$this->assertTrue($this->isTextPresent("Forge Admin"));
 		$this->assertTrue($this->isTextPresent("Log Out"));
 
@@ -141,14 +140,44 @@ class LoginProcess extends FForge_SeleniumTestCase
 
 		$this->logout();
 		$this->open( ROOT );
-		$this->click("link=Log In");
-		$this->waitForPageToLoad();
+		$this->clickAndWait("link=Log In");
 		$this->type("form_loginname", FORGE_ADMIN_USERNAME);
 		$this->type("form_pw", FORGE_ADMIN_PASSWORD);
-		$this->click("login");
-		$this->waitForPageToLoad();
+		$this->clickAndWait("login");
 		$this->assertTrue($this->isTextPresent("Forge Admin"));
 		$this->assertTrue($this->isTextPresent("Log Out"));
+
+		// Test session expiration
+		$this->open( ROOT );
+		if (!$this->isTextPresent("Log In")) {
+			$this->logout();
+		}
+		$this->clickAndWait("link=Log In");
+		$this->type("form_loginname", FORGE_ADMIN_USERNAME);
+		$this->type("form_pw", FORGE_ADMIN_PASSWORD);
+		$this->clickAndWait("login");
+		$this->assertTrue($this->isTextPresent("Forge Admin"));
+		$this->assertTrue($this->isTextPresent("Log Out"));
+		$this->open( ROOT );
+		$this->assertTrue($this->isTextPresent("Forge Admin"));
+		$this->assertTrue($this->isTextPresent("Log Out"));
+		// Shorten session validity delay
+		$this->changeConfig("[core]\nsession_expire = 10\n");
+		// Ensure session cookie is refreshed
+		sleep(5);
+		$this->open( ROOT );
+		sleep(5);
+		$this->open( ROOT );
+		sleep(5);
+		$this->open( ROOT );
+		sleep(5);
+		$this->assertTrue($this->isTextPresent("Forge Admin"));
+		$this->assertTrue($this->isTextPresent("Log Out"));
+		// Now ensure that the session expires
+		sleep(15);
+		$this->open( ROOT );
+		$this->assertFalse($this->isTextPresent("Forge Admin"));
+		$this->assertFalse($this->isTextPresent("Log Out"));
 	}
 }
 
