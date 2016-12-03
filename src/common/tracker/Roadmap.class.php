2559ece7d7ad9a0908020ae93aaf3e207b4a7f4f
Franck Villaume <franck.villaume@trivialdev.com>
2016-12-03 19:32:14 +0100

    coding style

--- src/common/tracker/Roadmap.class.php
+++ src/common/tracker/Roadmap.class.php
@@ -21,7 +21,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
+/**
  * Standard Alcatel-Lucent disclaimer for contributing to open source
  *
  * "The Roadmap ("Contribution") has not been tested and/or
--- src/common/tracker/RoadmapFactory.class.php
+++ src/common/tracker/RoadmapFactory.class.php
@@ -20,7 +20,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/*
+/**
  * Standard Alcatel-Lucent disclaimer for contributing to open source
  *
  * "The Roadmap ("Contribution") has not been tested and/or
@@ -142,5 +142,4 @@ class RoadmapFactory extends FFError {
 
 		return false;
 	}
-
 }
