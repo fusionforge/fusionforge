d645927bd333fe623fd73511006d21baeac32e15
Franck Villaume <franck.villaume@trivialdev.com>
2016-12-15 18:42:28 +0100

    remove empty line

--- src/www/widgets/updatelayout.php
+++ src/www/widgets/updatelayout.php
@@ -87,7 +87,6 @@ if ($owner) {
 				$name = array_pop(array_keys($param));
 			}
 			$instance_id = (int)$param[$name];
-
 			switch($request->get('action')) {
 				case 'widget':
 					if ($name && $request->exist('layout_id')) {
