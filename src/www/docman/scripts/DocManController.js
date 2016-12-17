7474b0f2ff7e5c2c5ce5aa75c2bb5b901ccf3928
Franck Villaume <franck.villaume@trivialdev.com>
2016-12-17 15:44:08 +0100

    fix edit docgroup when access from a child project

--- src/www/docman/scripts/DocManController.js
+++ src/www/docman/scripts/DocManController.js
@@ -269,7 +269,7 @@ DocManListFileController.prototype =
 	 */
 	toggleEditDirectoryView: function() {
 		if (!this.listfileparams.divEditDirectory.is(":visible")) {
-			jQuery.getJSON(this.listfileparams.docManURL + '/?group_id='+this.listfileparams.groupId+'&action=lock&json=1&type=dir&itemid='+this.listfileparams.docgroupId, jQuery.proxy(function(data){
+			jQuery.getJSON(this.listfileparams.docManURL + '/?group_id='+this.listfileparams.groupId+'&action=lock&json=1&type=dir&itemid='+this.listfileparams.docgroupId+'&childgroup_id='+this.listfileparams.childGroupId, jQuery.proxy(function(data){
 				if (typeof data.html != 'undefined') {
 					jQuery('#maindiv > .feedback').remove();
 					jQuery('#maindiv > .error').remove();
@@ -652,7 +652,6 @@ DocManListFileController.prototype =
 
 	deleteVersion: function(params) {
 		this.delversion = params;
-		console.log(this);
 		jQuery.getJSON(this.listfileparams.docManURL + '/?group_id=' + this.delversion.groupId + '&action=deleteversion&docid='+this.delversion.docid+'&version='+this.delversion.version , jQuery.proxy(function(data){
 				if (typeof data.html != 'undefined') {
 					jQuery('#editFile > .feedback').remove();
