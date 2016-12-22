c26ba900c358a93c04b5f831b41c69b4af1199b6
Franck Villaume <franck.villaume@trivialdev.com>
2016-12-22 20:12:06 +0100

    fix require_once

--- src/common/docman/DocumentReviewCommentAttachment.class.php
+++ src/common/docman/DocumentReviewCommentAttachment.class.php
@@ -21,7 +21,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-require_once $gfcommon.'/docman/DocumentReviewStorage.class.php';
+require_once $gfcommon.'docman/DocumentReviewStorage.class.php';
 
 class DocumentReviewCommentAttachment extends FFError {
 
