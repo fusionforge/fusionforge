a83e114e5298577e6185e76a6edd47f8c01c8a24
Roland Mas <lolando@debian.org>
2016-12-26 11:18:52 +0100

    Fixes to repository reconf script

--- src/plugins/softwareheritage/bin/configure-existing-git-repos.sh
+++ src/plugins/softwareheritage/bin/configure-existing-git-repos.sh
@@ -1,18 +1,11 @@
-#!/bin/bash -e
+#! /bin/sh
 
+set -e
 
-case "$1" in
-    configure)
-	repos_path=$(forge_get_config repos_path scmgit)
-	find $repos_path -path \*.git/config | while read i ; do
-	    p="${i%/config}"
-	    r=$(realpath "$p")
-	    GIT_DIR="$r" git config core.logAllRefUpdates true
-	done
-        ;;
-    remove)
-        ;;
-    *)
-        echo "Usage: $0 {configure|remove}"
-        exit 1
-esac
+repos_path=$(forge_get_config repos_path scmgit)
+cd "$repos_path"
+find -path \*.git/config | while read i ; do
+    p="${i%/config}"
+    r=$(realpath "$p")
+    GIT_DIR="$r" git config core.logAllRefUpdates true
+done
