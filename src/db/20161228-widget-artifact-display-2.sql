b81eaa6dde948a4bf0684fee5206c0ccd14b41c9
Franck Villaume <franck.villaume@trivialdev.com>
2016-12-28 15:32:38 +0100

    fix SQL syntax

--- src/db/20161228-widget-artifact-display-2.sql
+++ src/db/20161228-widget-artifact-display-2.sql
@@ -1,4 +1,4 @@
-ALTER TABLE artifact_display_widget DROP COLUMNS cols;
+ALTER TABLE artifact_display_widget DROP COLUMN cols;
 ALTER TABLE artifact_display_widget_field ADD COLUMN width integer;
 ALTER TABLE artifact_display_widget_field ADD COLUMN section text;
 
