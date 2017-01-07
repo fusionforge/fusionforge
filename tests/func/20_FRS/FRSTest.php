7ae563eba2d0996e16e6d8bc2accea3eccb05242
Franck Villaume <franck.villaume@trivialdev.com>
2017-01-07 19:05:42 +0100

    fix test

--- tests/func/20_FRS/FRSTest.php
+++ tests/func/20_FRS/FRSTest.php
@@ -52,7 +52,8 @@ class CreateFRS extends FForge_SeleniumTestCase
 		$this->assertTextPresent("Added Package");
 		$this->clickAndWait("link=Administration");
 		$this->type('//td[2]/input', 'My Named Packaged');
-		$this->clickAndWait('//td[5]/input');
-		$this->assertTextPresent('Package successfully updated');
+		$this->click('//td[5]/input');
+		$this->clickAndWait("link=View File Releases");
+		$this->assertTextPresent('My Named Package');
 	}
 }
