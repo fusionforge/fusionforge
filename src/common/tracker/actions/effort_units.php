998be2cf5c3f353e8f7e716ce5bf3b608f410ce6
Franck Villaume <franck.villaume@trivialdev.com>
2017-01-16 19:55:11 +0100

    fix space in licence header + typo

--- src/common/tracker/actions/effort_units.php
+++ src/common/tracker/actions/effort_units.php
@@ -1,25 +1,25 @@
 <?php
 /**
  * Project Admin page to edit units
-*
-* Copyright 2017, Stéphane-Eymeric Bredthauer - TrivialDev
-* http://fusionforge.org/
-*
-* This file is part of FusionForge. FusionForge is free software;
-* you can redistribute it and/or modify it under the terms of the
-* GNU General Public License as published by the Free Software
-* Foundation; either version 2 of the Licence, or (at your option)
-* any later version.
-*
-* FusionForge is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with FusionForge; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ *
+ * Copyright 2017, Stéphane-Eymeric Bredthauer - TrivialDev
+ * http://fusionforge.org/
+ *
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 require_once $gfcommon.'include/pre.php';
 require_once $gfcommon.'tracker/EffortUnit.class.php';
@@ -446,7 +446,7 @@ function postdelete_unit(&$effortUnitSet) {
 	}
 	$sure = getIntFromRequest('confirm_delete',0);
 	if (!$sure) {
-		echo $HTML->error_msg(_('You haven\' confirm the delete of the Unit'));
+		echo $HTML->error_msg(_('You haven\'t confirm the delete of the Unit'));
 		return false;
 	}
 	$name = $unit->getName();
