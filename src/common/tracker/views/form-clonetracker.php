fd4ba075aa63181b2f4765cee24caf810a556d0d
Franck Villaume <franck.villaume@trivialdev.com>
2017-01-19 23:53:26 +0100

    remove limitation about templating trackers. all template trackers can be used. not only the default "template project"

--- src/common/tracker/views/form-clonetracker.php
+++ src/common/tracker/views/form-clonetracker.php
@@ -3,7 +3,7 @@
  * FusionForge Tracker Cloning Form
  *
  * Copyright 2010, FusionForge Team
- * Copyright 2014-2015	, Franck Villaume - TrivialDev
+ * Copyright 2014-2015,2017, Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -23,50 +23,47 @@
  */
 
 global $HTML;
-
-$g = group_get_object(forge_get_config('template_group'));
-if (!$g || !is_object($g)) {
+global $ath;
+$arr_g = group_get_template_projects();
+if (!$arr_g || !is_array($arr_g)) {
 	exit_no_group();
-} elseif ($g->isError()) {
-	exit_error($g->getErrorMessage(),'tracker');
-} else {
-	$atf = new ArtifactTypeFactory($g);
-	if (!$atf || !is_object($atf)) {
-		exit_error(_('Unable to Create Template Group Object'),'tracker');
-	} elseif ($atf->isError()) {
-		exit_error($atf->getErrorMessage(),'tracker');
-	} else {
-		$ata = $atf->getArtifactTypes();
-		$ids = array();
-		$titles = array();
-		for ($i=0; $i<count($ata); $i++) {
-			if (!$ata[$i] || $ata[$i]->isError()) {
-//skip it
-			} else {
-				$ids[]=$ata[$i]->getID();
-				$titles[]=$g->getPublicName().'::'.$ata[$i]->getName();
+}
+$ids = array();
+$titles = array();
+foreach ($arr_g as $g) {
+	if (!$g->isError()) {
+		$atf = new ArtifactTypeFactory($g);
+		if ($atf && is_object($atf) && !$atf->isError()) {
+			$ata = $atf->getArtifactTypes();
+			for ($i=0; $i<count($ata); $i++) {
+				if (!$ata[$i] || $ata[$i]->isError()) {
+	//skip it
+				} else {
+					$ids[]=$ata[$i]->getID();
+					$titles[]=$g->getPublicName().'::'.$ata[$i]->getName();
+				}
 			}
 		}
-
-		$ath->adminHeader(array ('title'=>_('Clone Tracker'), 'modal'=>1));
-
-		if (empty($ata)) {
-			echo $HTML->warning_msg(_('The site administrator must first set up template trackers in the template project with default values and set permissions properly so you can access them.'));
-		} else {
-			?>
-			<p><?php echo _('Choose the template tracker to clone.') ?></p>
-			<?php
-			echo $HTML->openForm(array('/tracker/admin/?group_id='.$group_id.'&atid='.$ath->getID(), 'method' => 'post'));
-			?>
-			<input type="hidden" name="clone_tracker" value="y" />
-			<?php
-			echo $HTML->warning_msg(_('WARNING!!! Cloning this tracker will duplicate all the fields and all the elements from those fields into this tracker. There is nothing to prevent you from cloning multiple times or making a huge mess. If you have preexisting extrafields with same name, they will be dropped. You have been warned!'));
-			?>
-			<p><?php echo html_build_select_box_from_arrays($ids,$titles,'clone_id','',false); ?></p>
-			<input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" />
-			<?php
-			echo $HTML->closeForm();
-		}
-		$ath->footer();
 	}
 }
+
+$ath->adminHeader(array ('title'=>_('Clone Tracker'), 'modal'=>1));
+
+if (count($ids) < 1) {
+	echo $HTML->warning_msg(_('The site administrator must first set up template trackers in the template project with default values and set permissions properly so you can access them.'));
+} else {
+	?>
+	<p><?php echo _('Choose the template tracker to clone.') ?></p>
+	<?php
+	echo $HTML->openForm(array('/tracker/admin/?group_id='.$group_id.'&atid='.$ath->getID(), 'method' => 'post'));
+	?>
+	<input type="hidden" name="clone_tracker" value="y" />
+	<?php
+	echo $HTML->warning_msg(_('WARNING!!! Cloning this tracker will duplicate all the fields and all the elements from those fields into this tracker. There is nothing to prevent you from cloning multiple times or making a huge mess. If you have preexisting extrafields with same name, they will be dropped. You have been warned!'));
+	?>
+	<p><?php echo html_build_select_box_from_arrays($ids,$titles,'clone_id','',false); ?></p>
+	<input type="submit" name="post_changes" value="<?php echo _('Submit') ?>" />
+	<?php
+	echo $HTML->closeForm();
+}
+$ath->footer();
