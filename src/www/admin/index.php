0ff89bd82f74b5cc8b47e183aaf59f18b7b37c47
Franck Villaume <franck.villaume@trivialdev.com>
2017-05-25 12:24:10 +0200

    remove useless p tag

--- src/www/admin/index.php
+++ src/www/admin/index.php
@@ -84,17 +84,17 @@ echo html_ac(html_ap() - 1);
 	<ul>
 	<li><?php
 
-		echo $HTML->openForm(array('action' => '/admin/globalroleedit.php', 'method' => 'post')).'<p>';
+		echo $HTML->openForm(array('action' => '/admin/globalroleedit.php', 'method' => 'post'));
 		echo global_role_box('role_id');
-		echo '<input type="submit" name="edit" value="'._("Edit Role").'" /></p>'.$HTML->closeForm();
+		echo '<input type="submit" name="edit" value="'._("Edit Role").'" />'.$HTML->closeForm();
 ?>
 </li>
 <li>
 <?php
 
-		echo $HTML->openForm(array('action' => '/admin/globalroleedit.php', 'method' => 'post')).'<p>';
+		echo $HTML->openForm(array('action' => '/admin/globalroleedit.php', 'method' => 'post'));
 		echo '<input type="text" name="role_name" size="10" value="" required="required" />';
-		echo '<input type="submit" name="add" value="'._("Create Role").'" /></p>'.$HTML->closeForm();
+		echo '<input type="submit" name="add" value="'._("Create Role").'" />'.$HTML->closeForm();
 	?></li>
 </ul>
 </div>
