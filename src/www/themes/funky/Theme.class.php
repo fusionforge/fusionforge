81e5c339151c90ceed1dabd3002779bedb227329
Franck Villaume <franck.villaume@trivialdev.com>
2017-06-11 12:43:03 +0200

    html id attr cannot be empty

--- src/www/themes/funky/Theme.class.php
+++ src/www/themes/funky/Theme.class.php
@@ -5,7 +5,7 @@
  * Copyright 2010, Antoine Mercadal - Capgemini
  * Copyright 2010, Marc-Etienne Vargenau, Alcatel-Lucent
  * Copyright 2011, Franck Villaume - Capgemini
- * Copyright 2011-2016, Franck Villaume - TrivialDev
+ * Copyright 2011-2017, Franck Villaume - TrivialDev
  * Copyright (C) 2011 Alain Peyrat - Alcatel-Lucent
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -214,7 +214,7 @@ class Theme_Funky extends Layout {
 			$id = $this->toSlug($id);
 			$idtitle = $id.'-title"';
 		} else {
-			$idtitle = "";
+			$idtitle = rand();
 		}
 
 		return html_e('div', array('id' => $idtitle, 'class' => 'box-middle'), $title, false);
@@ -232,7 +232,7 @@ class Theme_Funky extends Layout {
 			$id = $this->toSlug($id);
 			$idcont = $id.'-content"';
 		} else {
-			$idcont = "";
+			$idcont = rand();
 		}
 
 		return html_e('div', array('id' => $idcont, 'class' => 'box-content'), $content, false);
