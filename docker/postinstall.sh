ef6f96e71f25f7074fd3f147b54c07f7c4c83f4e
Franck Villaume <franck.villaume@trivialdev.com>
2017-06-16 09:01:44 +0000

    enable more plugins in docker

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/local/share/fusionforge/post-install.d/db/db.sh configure
 cd /opt/sources/fusionforge/src
-for pluginname in blocks mediawiki message moinmoin repositoryapi scmgit scmsvn taskboard; do
+for pluginname in blocks compactpreview gravatar headermenu mediawiki message moinmoin repositoryapi scmgit scmhook scmsvn taskboard webanalytics; do
     make post-install-plugin-${pluginname}
 done
