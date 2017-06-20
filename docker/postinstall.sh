9fa15efd7ba1cee6ea88042899b0e9d75838668f
Franck Villaume <franck.villaume@trivialdev.com>
2017-06-20 08:17:17 +0000

    docker: fix database init in swarm mode

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+while [ -z "`netstat -tln | grep 5432`" ]; do
+  echo 'Waiting for PostgreSQL to start ...'
+  sleep 1
+done
+echo 'PostgreSQL started.'
 /usr/local/share/fusionforge/post-install.d/db/db.sh configure
 cd /opt/sources/fusionforge/src
 for pluginname in blocks compactpreview gravatar headermenu mediawiki message moinmoin repositoryapi scmgit scmhook scmsvn taskboard webanalytics; do
