1a5796ffb6da5326f7379dddf66219fd7b0e2977
Franck Villaume <franck.villaume@trivialdev.com>
2017-08-08 19:25:03 +0200

    plugin scmhg: make install.sh debian aware

--- src/plugins/scmhg/bin/install.sh
+++ src/plugins/scmhg/bin/install.sh
@@ -4,14 +4,13 @@
 source $(forge_get_config source_path)/post-install.d/common/service.inc
 
 plugindir=$(forge_get_config plugins_path)/scmhg
-hgwebcgi=$(ls /usr/share/doc/mercurial-*/hgweb.cgi 2>/dev/null | tail -1)
-hgwebdir=$(ls -d /usr/share/doc/mercurial-* 2>/dev/null | tail -1)
-hgtemplatesdir=$(ls -d /usr/lib*/python*/site-packages/mercurial/templates 2>/dev/null | tail -1)
+hgwebcgi=$(ls -1 /usr/share/doc/mercurial-*/hgweb.cgi /usr/share/doc/mercurial/examples/hgweb.cgi 2>/dev/null | tail -1)
+hgtemplatesdir=$(ls -1d /usr/lib*/python*/site-packages/mercurial/templates /usr/share/mercurial/templates 2>/dev/null | tail -1)
 
 case "$1" in
     configure)
 	# hgweb
-	if [ -z "$hgwebcgi" -o -z "$hgwebdir" ]; then echo "Cannot find hgweb"; exit 1; fi
+	if [ -z "$hgwebcgi" -o -z "$hgtemplatesdir" ]; then echo "Cannot find required directories"; exit 1; fi
 	mkdir -p -m 755 $plugindir/cgi-bin/
 	mkdir -p -m 755 $plugindir/www/cgi-bin/
 	ln -nfs $hgwebcgi                 $plugindir/cgi-bin/
