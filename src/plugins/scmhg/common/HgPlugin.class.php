f91547857d5ad2b1e44a7502ba2dd9d43a2eaeb8
Franck Villaume <franck.villaume@trivialdev.com>
2017-08-11 13:58:16 +0000

    fix permission on cgi script

--- src/plugins/scmhg/common/HgPlugin.class.php
+++ src/plugins/scmhg/common/HgPlugin.class.php
@@ -320,7 +320,7 @@ Offer DAV or SSH access.");
 			$apache_user = forge_get_config('apache_user');
 			$apache_group = forge_get_config('apache_group');
 			system("chown $apache_user:$apache_group $project_hgweb");
-			system("chmod u+x $project_hgweb");
+			system("chmod 755 $project_hgweb");
 		}
 		if (!is_dir("$root/.hg")) {
 			system("hg init $root");
