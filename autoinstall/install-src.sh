3857a3e51705554f2583650843336c9224ef2e14
Roland Mas <lolando@debian.org>
2017-10-28 15:31:35 +0200

    Adapt installation from sources to Debian 9 (Stretch)

--- autoinstall/install-src.sh
+++ autoinstall/install-src.sh
@@ -43,9 +43,9 @@ if [ -e /etc/debian_version ]; then
 		    fonts-dejavu-core
 	    apt-get -y install mediawiki -t jessie-backports
 	else
-	    apt-get install -y make gettext php7-cli php7-pgsql php-htmlpurifier php-http php-text-captcha \
+	    apt-get install -y make gettext php-cli php-pgsql php-htmlpurifier php-http php-text-captcha \
 		    libapache2-mpm-itk libapache2-mod-svn \
-		    libapache2-mod-php7 \
+		    libapache2-mod-php \
 		    apache2 postgresql postgresql-contrib libnss-pgsql2 unscd \
 		    cvs subversion viewvc python-pycurl git mercurial xinetd \
 		    python-moinmoin libapache2-mod-wsgi python-psycopg2 \
