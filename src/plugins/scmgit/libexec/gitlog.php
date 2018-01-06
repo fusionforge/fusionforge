d4a1ada33fb2a3edb23aeaee0441bcf7b6666b8d
Franck Villaume <franck.villaume@trivialdev.com>
2018-01-06 15:27:13 +0100

    use util_make_url to get check_forwarded_session

--- src/plugins/scmgit/libexec/gitlog.php
+++ src/plugins/scmgit/libexec/gitlog.php
@@ -31,9 +31,8 @@ header('Content-type: text/plain');
 
 # Authentify request
 if (!preg_match(',^/anonscm/,', $_SERVER['REQUEST_URI'])) {
-	$web_host = forge_get_config('web_host');
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://' . $web_host . '/account/check_forwarded_session.php');
+	curl_setopt($ch, CURLOPT_URL, util_make_url().'/account/check_forwarded_session.php');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
