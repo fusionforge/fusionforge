bf8b642288858e455995b9507f6229309dd5181e
Franck Villaume <franck.villaume@trivialdev.com>
2018-01-20 14:02:37 +0100

    fix PHP syntax

--- src/plugins/scmhg/common/HgPlugin.class.php
+++ src/plugins/scmhg/common/HgPlugin.class.php
@@ -122,7 +122,7 @@ Offer DAV or SSH access.");
 					// Warning : the ssh uri MUST be this form : ssh://username@scmbox//path/reponame
 					//           HAVE YOU SEEN THE // starting the path ? Keep in mind the double /
 					if (forge_get_config('use_shell_limited')) {
-						$htmlRepo .= html_e('kbd', array(), 'hg clone ssh://'.$d.'@'.$this->getBoxForProject($project).$ssh_port'/hg/'.$project->getUnixName()).html_e('br');
+						$htmlRepo .= html_e('kbd', array(), 'hg clone ssh://'.$d.'@'.$this->getBoxForProject($project).$ssh_port.'/hg/'.$project->getUnixName()).html_e('br');
 
 					} else {
 						$htmlRepo .= html_e('kbd', array(), 'hg clone ssh://'.$d.'@'.$this->getBoxForProject($project).$ssh_port.'/'.forge_get_config('repos_path', 'scmhg').'/'.$project->getUnixName()).html_e('br');
