ee86e04654b90c40fc76754f776d07c8627b6355
Franck Villaume <franck.villaume@trivialdev.com>
2019-01-13 17:54:23 +0100

    add missing licence & tabs

--- tests/func_tests.php
+++ tests/func_tests.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Copyright FusionForge Team
+ *
+ * This file is part of FusionForge.
+ *
+ * FusionForge is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 if (!defined('PHPUnit_MAIN_METHOD')) {
 	define('PHPUnit_MAIN_METHOD', 'AllTests::main');
 }
@@ -15,22 +35,20 @@ if (@include_once '/usr/local/share/php/vendor/autoload.php') {
 	}
 }
 
-class AllTests
-{
-	public static function main()
-	{
-        PHPUnit_TextUI_TestRunner::run(self::suite());
+class AllTests {
+	public static function main() {
+		PHPUnit_TextUI_TestRunner::run(self::suite());
 	}
 
-	public static function suite()
-	{
+	public static function suite() {
 		$suite = new PHPUnit_Framework_TestSuite('PHPUnit');
 
 		// Selenium tests
-		if (getenv('TESTGLOB') != FALSE)
+		if (getenv('TESTGLOB') != FALSE) {
 			$files = glob(dirname(__FILE__).'/'.getenv('TESTGLOB'));
-		else
+		} else {
 			$files = glob(dirname(__FILE__).'/func/*/*Test.php');
+		}
 		natsort($files);
 		$suite->addTestFiles($files);
 
