d0982af2967039de3d984718f3c92cfbc6464a7a
Roland Mas <roland.mas@gnurandal.com>
2019-03-15 12:22:48 +0100

    Disable coverage testing in unittests

--- tests/buildbot/fusionforge-unit_tests.sh
+++ tests/buildbot/fusionforge-unit_tests.sh
@@ -11,9 +11,9 @@ prepare_workspace
 cd tests/
 phpunit --configuration buildbot-configuration-phpunit.xml \
 	--log-junit $WORKSPACE/reports/phpunit.xml \
+	code_and_unit_tests.php
 #	--coverage-clover $WORKSPACE/reports/coverage/clover.xml \
 #	--coverage-html $WORKSPACE/reports/coverage/ \
-	code_and_unit_tests.php
-#cp $WORKSPACE/reports/phpunit.xml $WORKSPACE/reports/phpunit.xml.org
+	#cp $WORKSPACE/reports/phpunit.xml $WORKSPACE/reports/phpunit.xml.org
 #xalan -in $WORKSPACE/reports/phpunit.xml.org -xsl unit/fix_phpunit.xslt \
 #      -out $WORKSPACE/reports/phpunit.xml
