56dc6380faea79ed637ef60dd1216b6073543331
Franck Villaume <franck.villaume@trivialdev.com>
2019-04-26 17:49:09 +0200

    enforce apache2 dependency & specific apache2 mods configuration

--- src/post-install.d/web/web.sh
+++ src/post-install.d/web/web.sh
@@ -116,6 +116,10 @@ case "$1" in
 				a2enmod mpm_itk
 				a2enmod mod_access_compat
 			else
+				if grep -q ^9 /etc/debian_version; then
+					a2dismod mpm_event
+					a2enmod mpm_itk
+				fi
 				a2enmod php7.0 || a2enmod php5
 			fi
 			a2enmod ssl
