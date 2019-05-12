d581dc189d46681a2e0f4f7a158211c78a1aa80a
Franck Villaume <franck.villaume@trivialdev.com>
2019-05-12 18:55:26 +0200

    backport from master

--- tests/buildbot/fusionforge-unit_tests.sh
+++ tests/buildbot/fusionforge-unit_tests.sh
@@ -11,9 +11,4 @@ prepare_workspace
 cd tests/
 phpunit --configuration buildbot-configuration-phpunit.xml \
 	--log-junit $WORKSPACE/reports/phpunit.xml \
-	--coverage-clover $WORKSPACE/reports/coverage/clover.xml \
-	--coverage-html $WORKSPACE/reports/coverage/ \
 	code_and_unit_tests.php
-#cp $WORKSPACE/reports/phpunit.xml $WORKSPACE/reports/phpunit.xml.org
-#xalan -in $WORKSPACE/reports/phpunit.xml.org -xsl unit/fix_phpunit.xslt \
-#      -out $WORKSPACE/reports/phpunit.xml
