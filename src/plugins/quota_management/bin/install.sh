3a9747935eabc4940274c2b74b547023aff38123
Franck Villaume <franck.villaume@trivialdev.com>
2019-07-07 16:04:15 +0200

    quota_management: enabling is different per filesystem type...

--- src/plugins/quota_management/bin/install.sh
+++ src/plugins/quota_management/bin/install.sh
@@ -3,23 +3,36 @@
 
 source $(forge_get_config source_path)/post-install.d/common/service.inc
 
-
 case "$1" in
 	configure)
-		
 		mountpointtocheck="$(forge_get_config homedir_prefix) $(forge_get_config groupdir_prefix) $(forge_get_config chroot) $(forge_get_config data_path)"
 		for each in $mountpointtocheck; do
-			echo "Checking mount option: usrquota & grpquota on $each"
 			if [ -d $each ]; then
 				mountpoint=$(df -P $each | tail -1 | awk '{print $6}')
-				mountoptions=$(mount | grep "$mountpoint " | grep "usrquota" | grep "grpquota")
-				if [ -z $mountoptions ];then
-					echo "Enabling usrquota & grpquota on $mountpoint"
-					#add option in fstab
-					mount -o remount,usrquota,grpquota $mountpoint
-				else
-					echo "usrquota & grpquota already enabled on $mountpoint"
-				fi
+				mountfilesystem=$(mount | grep "$mountpoint " | awk '{print $5}')
+				case $mountfilesystem in
+					'xfs')
+						mountoptions=$(mount | grep "$mountpoint " | grep "uquota" | grep "gquota")
+						if [ -z $mountoptions ]; then
+							echo "Enabling uquota & gquota on $mountpoint"
+							#requires boot changes!!!
+						else
+							echo "uquota & pquota already enabled on $mountpoint"
+						fi
+						;;
+					'ext4')
+					'ext3')
+						mountoptions=$(mount | grep "$mountpoint " | grep "usrquota" | grep "grpquota")
+						if [ -z $mountoptions ]; then
+							echo "Enabling usrquota & grpquota on $mountpoint"
+							#add option in fstab
+							mount -o remount,usrquota,grpquota $mountpoint
+						else
+							echo "usrquota & grpquota already enabled on $mountpoint"
+						fi
+						;;
+				esac
+
 			fi
 			#quotaon -guvp -a
 		done
