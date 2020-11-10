36c22b9744e1daec65e516176bb9223d3bac4dfd
Roland Mas <lolando@debian.org>
2020-11-10 18:09:29 +0100

    Try fixing testsuite on Debian 10

--- tests/code_and_unit_tests.php
+++ tests/code_and_unit_tests.php
@@ -22,17 +22,18 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 	define('PHPUnit_MAIN_METHOD', 'AllTests::main');
 }
 
-if (@include_once '/usr/local/share/php/vendor/autoload.php') {
-	$phpunitversion = 6;
-	class PHPUnit_Framework_TestSuite extends PHPUnit\Framework\TestSuite {}
-	class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase {}
-} else {
-	$phpunitversion = 4;
-	if (!@include_once 'PHPUnit/Autoload.php') {
-		include_once 'PHPUnit/Framework.php';
-		require_once 'PHPUnit/TextUI/TestRunner.php';
-	}
-}
+include_once 'PHPUnit/Autoload.php');
+// if (@include_once '/usr/local/share/php/vendor/autoload.php') {
+// 	$phpunitversion = 6;
+// 	class PHPUnit_Framework_TestSuite extends PHPUnit\Framework\TestSuite {}
+// 	class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase {}
+// } else {
+// 	$phpunitversion = 4;
+// 	if (!@include_once 'PHPUnit/Autoload.php') {
+// 		include_once 'PHPUnit/Framework.php';
+// 		require_once 'PHPUnit/TextUI/TestRunner.php';
+// 	}
+// }
 
 @include_once 'PHP/Token/Stream/Autoload.php';
 @include_once 'Text/Template/Autoload.php';
