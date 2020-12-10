39a3dfd6bfcaca6a0627d9fd7c8689f86648612e
Roland Mas <lolando@debian.org>
2020-12-10 16:18:54 +0100

    Try fixing testsuite

--- tests/func/60_PluginsMediawiki/mediawikiTest.php
+++ tests/func/60_PluginsMediawiki/mediawikiTest.php
@@ -49,7 +49,7 @@ class PluginMediawiki extends FForge_SeleniumTestCase
 		$this->assertFalse($this->isTextPresent("not created yet"));
 		$this->assertFalse($this->isTextPresent("not found"));
 
-		$this->clickAndWait("//li[@id='ca-edit']/a");
+		$this->clickAndWait("edit");
 		$this->assertTrue($this->isTextPresent("You have followed a link to a page that does not exist yet."));
 		$this->type("//textarea[@id='wpTextbox1']", "= Bleh =
 == Blahblah ==
