b8cd741dc090f4249a0a16971e2c73ca2db67846
Franck Villaume <franck.villaume@trivialdev.com>
2021-04-17 10:30:55 +0200

    fix crossref with Tasks

--- src/common/include/utils_crossref.php
+++ src/common/include/utils_crossref.php
@@ -4,7 +4,7 @@
  *
  * Copyright 1999-2001 (c) Alcatel-Lucent
  * Copyright 2009, Roland Mas
- * Copyright 2014-2016, Franck Villaume - TrivialDev
+ * Copyright 2014-2016,2021, Franck Villaume - TrivialDev
  *
  * This file is part of FusionForge. FusionForge is free software;
  * you can redistribute it and/or modify it under the terms of the
@@ -42,7 +42,7 @@ function util_gen_cross_ref($text, $group_id) {
 	$text = preg_replace_callback('/\[\#(\d+)\]/', create_function('$matches', 'return _artifactid2url($matches[1]);'), $text);
 
 	// Handle FusionForge [Tnnn] Syntax => links to task.
-	$text = preg_replace_callback('/\[\T(\d+)\]/', create_function('$matches', 'return _taskid2url($matches[1],'.$group_id.');'), $text);
+	$text = preg_replace_callback('/\[T(\d+)\]/', create_function('$matches', 'return _taskid2url($matches[1],'.$group_id.');'), $text);
 
 	// Handle [wiki:<pagename>] syntax
 	$text = preg_replace_callback('/\[wiki:(.*?)\]/', create_function('$matches', 'return _page2url('.$group_id.',$matches[1]);'), $text);
