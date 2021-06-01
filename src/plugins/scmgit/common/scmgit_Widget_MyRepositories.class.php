895de0ee41d77d6e6a22a8317e364691f59ee3bf
Franck Villaume <franck.villaume@trivialdev.com>
2021-06-01 11:21:56 +0000

    fix URL browser for git personal repo

--- src/plugins/scmgit/common/scmgit_Widget_MyRepositories.class.php
+++ src/plugins/scmgit/common/scmgit_Widget_MyRepositories.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright 2011, Franck Villaume - Capgemini
- * Copyright 2012-2014, Franck Villaume - TrivialDev
+ * Copyright 2012-2014,2021, Franck Villaume - TrivialDev
  * http://fusionforge.org
  *
  * This file is part of FusionForge. FusionForge is free software;
@@ -54,7 +54,7 @@ class scmgit_Widget_MyRepositories extends Widget {
 				$project = group_get_object($GitRepository);
 				$cells = array();
 				$cells[][] = '<kbd>git clone git+ssh://'.$user->getUnixName().'@'.$this->getBoxForProject($project).$ssh_port.forge_get_config('repos_path', 'scmgit').'/'.$project->getUnixName() .'/users/'. $user->getUnixName() .'.git</kbd>';
-				$cells[][] = util_make_link('/scm/browser.php?group_id='.$project->getID().'&user_id='.$user->getID(), _('Browse Git Repository'));
+				$cells[][] = util_make_link('/scm/browser.php?group_id='.$project->getID().'&user_id='.$user->getID().'&scm_plugin=scmgit', _('Browse Git Repository'));
 				$returnhtml .= $HTML->multiTableRow(array(), $cells);
 			}
 			$returnhtml .= $HTML->listTableBottom();
