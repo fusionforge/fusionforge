17c92eca920aa53752e0949ba77bc783c5184677
Franck Villaume <franck.villaume@trivialdev.com>
2022-02-01 10:54:12 +0100

    add missing licence.

--- src/www/trove/TroveCategoryAdmin.class.php
+++ src/www/trove/TroveCategoryAdmin.class.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * http://fusionforge.org/
+ *
+ * This file is part of FusionForge. FusionForge is free software;
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or (at your option)
+ * any later version.
+ *
+ * FusionForge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with FusionForge; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 class TroveCategoryAdmin extends TroveCategory {
 
@@ -11,7 +29,7 @@ function __construct() {
 function create() {
 }
 
-function update() {
+function update($shortName, $fullName, $description) {
 }
 
 function move() {
