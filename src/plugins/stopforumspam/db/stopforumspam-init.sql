63b289a6f92ac3551d051a25d0e699b98f02aeba
Roland Mas <roland.mas@gnurandal.com>
2023-01-31 17:41:53 +0100

    Add missing constraints

--- src/plugins/stopforumspam/db/stopforumspam-init.sql
+++ src/plugins/stopforumspam/db/stopforumspam-init.sql
@@ -1,10 +1,12 @@
 CREATE TABLE plugin_stopforumspam_known_entries (
 	datatype text NOT NULL,
 	entry text NOT NULL,
-	last_seen INTEGER NOT NULL
+	last_seen INTEGER NOT NULL,
+        CONSTRAINT stopforumspam_entries_unique UNIQUE (datatype, entry)
 ) ;
 CREATE TABLE plugin_stopforumspam_last_fetch (
 	datatype text NOT NULL,
 	period text NOT NULL,
-	last_fetch INTEGER NOT NULL
+	last_fetch INTEGER NOT NULL,
+        CONSTRAINT stopforumspam_lastfetch_unique UNIQUE (datatype, period)
 ) ;
